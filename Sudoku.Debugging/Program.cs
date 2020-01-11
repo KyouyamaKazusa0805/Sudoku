74af5479d9d6bee2a6d61929b6698314d925c457
Sunnie-Shine <iamxiaoxiang@qq.com>
2020-01-11 10:18:41 +0800

    Add other solving options.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -6,7 +6,9 @@
 		{
 			var solver = new Sudoku.Solving.Manual.ManualSolver
 			{
-				OptimizedApplyingOrder = true
+				OptimizedApplyingOrder = true,
+				EnableFullHouse = true,
+				EnableLastDigit = true
 			};
 			var grid = Sudoku.Data.Meta.Grid.Parse("073004000950067000004000000028900100069473280007002940000000300000320091000600420");
 			var analysisResult = solver.Solve(grid);
