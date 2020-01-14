bd7def4e7edc6a8553c84bc56aee5ae35fdc362a
Sunnie-Shine <iamxiaoxiang@qq.com>
2020-01-14 20:46:19 +0800

    Add IO handling.

---
+++ Sudoku.IO/GridReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Sudoku.Data.Meta;
+
+namespace Sudoku.IO
+{
+	public sealed class GridReader : IDisposable
+	{
+		private readonly TextReader _reader;
+
+
+		public GridReader(string path) => _reader = new StreamReader(path);
+
+
+		public void Dispose() => _reader.Dispose();
+
+		public Grid? ReadLine()
+		{
+			string? result = _reader.ReadLine();
+			return result is null ? null : Grid.Parse(result);
+		}
+
+		public Grid? ReadToEnd()
+		{
+			string? result = _reader.ReadToEnd();
+			return result is null ? null : Grid.Parse(result);
+		}
+
+		public async Task<Grid?> ReadLineAsync(bool continueOnCapturedContext = false) =>
+			await Task.Run(() => ReadLine()).ConfigureAwait(continueOnCapturedContext);
+
+		public async Task<Grid?> ReadToEndAsync(bool continueOnCapturedContext = false) =>
+			await Task.Run(() => ReadLine()).ConfigureAwait(continueOnCapturedContext);
+	}
+}
---
+++ Sudoku.IO/GridWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Sudoku.Data.Meta;
+
+namespace Sudoku.IO
+{
+	public sealed class GridWriter : IDisposable
+	{
+		private readonly TextWriter _writer;
+
+
+		public GridWriter(string path) => _writer = new StreamWriter(path);
+
+
+		public void Dispose() => _writer.Dispose();
+
+		public void Write(Grid grid) => Write(grid, ".");
+
+		public void Write(Grid grid, string format) =>
+			_writer.Write(grid.ToString(format));
+
+		public async Task WriteAsync(Grid grid, bool continueOnCapturedContext = false)
+			=> await WriteAsync(grid, ".", continueOnCapturedContext);
+
+		public async Task WriteAsync(
+			Grid grid, string format, bool continueOnCapturedContext = false) =>
+			await Task.Run(() => Write(grid, format)).ConfigureAwait(continueOnCapturedContext);
+	}
+}
