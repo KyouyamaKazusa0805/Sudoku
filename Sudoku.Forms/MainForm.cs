784103b140689b3ae0e839478cdfecc96dd2248e
Sunnie-Shine <iamxiaoxiang@qq.com>
2020-01-24 11:48:24 +0800

    Add main form.

---
+++ Sudoku.Forms/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Sudoku.Forms
+{
+	/// <summary>
+	/// Provides a form with the main view.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// Initializes with the <see cref="MainForm"/> instance.
+		/// </summary>
+		public MainForm() => InitializeComponent();
+
+	}
+}
