019ad6b318cb3e8dd8ef55311e2ca780c7c2ece7
Sunnie-Shine <HowdySunnie@outlook.com>
2020-02-08 16:25:09 +0800

    Add technique 'BUG' (type 2).

--- Sudoku.Solving/Manual/Uniqueness/Bugs/BivalueUniversalGraveTechniqueSearcher.cs
+++ Sudoku.Solving/Manual/Uniqueness/Bugs/BivalueUniversalGraveTechniqueSearcher.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Sudoku.Data.Meta;
 using Sudoku.Drawing;
+using Sudoku.Runtime;
 using Sudoku.Solving.Checking;
+using Sudoku.Solving.Utils;
+using BugType1 = Sudoku.Solving.Manual.Uniqueness.Bugs.BivalueUniversalGraveTechniqueInfo;
+using BugType2 = Sudoku.Solving.Manual.Uniqueness.Bugs.BivalueUniversalGraveType2TechniqueInfo;
 
 namespace Sudoku.Solving.Manual.Uniqueness.Bugs
 {
@@ -12,38 +17,147 @@ namespace Sudoku.Solving.Manual.Uniqueness.Bugs
 	public sealed class BivalueUniversalGraveTechniqueSearcher : UniquenessTechniqueSearcher
 	{
 		/// <inheritdoc/>
+		/// <exception cref="WrongHandlingException">
+		/// Throws when the number of true candidates is naught.
+		/// </exception>
 		public override IReadOnlyList<TechniqueInfo> TakeAll(Grid grid)
 		{
 			var checker = new BugChecker(grid);
-			var list = checker.TrueCandidates;
-			if (list.Count == 0)
+			var trueCandidates = checker.TrueCandidates;
+			if (trueCandidates.Count == 0)
 			{
-				return Array.Empty<BivalueUniversalGraveTechniqueInfo>();
+				return Array.Empty<UniquenessTechniqueInfo>();
 			}
 
-			var result = new List<BivalueUniversalGraveTechniqueInfo>();
-
-			if (list.Count == 1)
+			var result = new List<UniquenessTechniqueInfo>();
+			int trueCandidatesCount = trueCandidates.Count;
+			switch (trueCandidatesCount)
 			{
-				// BUG + 1 found.
-				result.Add(
-					new BivalueUniversalGraveTechniqueInfo(
-						conclusions: new[] { new Conclusion(ConclusionType.Assignment, list[0]) },
-						views: new[]
-						{
-							new View(
-								cellOffsets: null,
-								candidateOffsets: new[] { (0, list[0]) },
-								regionOffsets: null,
-								linkMasks: null)
-						}));
-			}
-			else
-			{
+				case 0:
+				{
+					throw new WrongHandlingException(grid);
+				}
+				case 1:
+				{
+					// BUG + 1 found.
+					result.Add(
+						new BugType1(
+							conclusions: new[] { new Conclusion(ConclusionType.Assignment, trueCandidates[0]) },
+							views: new[]
+							{
+								new View(
+									cellOffsets: null,
+									candidateOffsets: new[] { (0, trueCandidates[0]) },
+									regionOffsets: null,
+									linkMasks: null)
+							}));
+					break;
+				}
+				default:
+				{
+					if (CheckSingleDigit(trueCandidates))
+					{
+						CheckType2(grid, trueCandidates, result);
+					}
+					else
+					{
 
+					}
+
+					break;
+				}
 			}
 
 			return result;
 		}
+
+		#region BUG utils
+		/// <summary>
+		/// Check type 2.
+		/// </summary>
+		/// <param name="grid">The grid.</param>
+		/// <param name="trueCandidates">All true candidates.</param>
+		/// <param name="result">The result list.</param>
+		private static void CheckType2(
+			Grid grid, IReadOnlyList<int> trueCandidates, IList<UniquenessTechniqueInfo> result)
+		{
+			var map = default(GridMap);
+			int i = 0;
+			foreach (int cand in trueCandidates)
+			{
+				if (i++ == 0)
+				{
+					map = new GridMap(cand / 9);
+				}
+				else
+				{
+					map &= new GridMap(cand / 9);
+				}
+			}
+
+			if (map.Count != 0)
+			{
+				// BUG type 2 found.
+				// Check eliminations.
+				var conclusions = new List<Conclusion>();
+				int digit = trueCandidates[0] % 9;
+				foreach (int cell in map.Offsets)
+				{
+					if (grid.CandidateExists(cell, digit))
+					{
+						conclusions.Add(
+							new Conclusion(
+								ConclusionType.Elimination, cell * 9 + digit));
+					}
+				}
+
+				if (conclusions.Count != 0)
+				{
+					// BUG type 2.
+					result.Add(
+						new BugType2(
+							conclusions,
+							views: new[]
+							{
+								new View(
+									cellOffsets: null,
+									candidateOffsets:
+										new List<(int, int)>(
+											from cand in trueCandidates select (0, cand)),
+									regionOffsets: null,
+									linkMasks: null)
+							},
+							digit,
+							cells: trueCandidates));
+				}
+			}
+		}
+
+		/// <summary>
+		/// Check whether all candidates in the list has same digit value.
+		/// </summary>
+		/// <param name="list">The list of all true candidates.</param>
+		/// <returns>A <see cref="bool"/> indicating that.</returns>
+		private static bool CheckSingleDigit(IReadOnlyList<int> list)
+		{
+			int i = 0;
+			int comparer = default;
+			foreach (int cand in list)
+			{
+				if (i++ == 0)
+				{
+					comparer = cand % 9;
+					continue;
+				}
+
+				if (comparer != cand % 9)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+		#endregion
 	}
 }
---
+++ Sudoku.Solving/Manual/Uniqueness/Bugs/BivalueUniversalGraveType2TechniqueInfo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Sudoku.Drawing;
+using Sudoku.Solving.Utils;
+
+namespace Sudoku.Solving.Manual.Uniqueness.Bugs
+{
+	/// <summary>
+	/// Provides a usage of bivalue universal grave (BUG) type 2 technique.
+	/// </summary>
+	public sealed class BivalueUniversalGraveType2TechniqueInfo : UniquenessTechniqueInfo
+	{
+		/// <summary>
+		/// The table of extra difficulty values.
+		/// </summary>
+		private static readonly decimal[] DifficultyExtra = new[]
+		{
+			.1m, .2m, .2m, .3m, .3m, .3m, .4m, .4m, .4m, .4m,
+			.5m, .5m, .5m, .5m, .5m, .6m, .6m, .6m, .6m, .6m
+		};
+
+
+		/// <summary>
+		/// Initializes an instance with the specified information.
+		/// </summary>
+		/// <param name="conclusions">All conclusions.</param>
+		/// <param name="views">All views.</param>
+		/// <param name="digit">The digit.</param>
+		/// <param name="cells">All cell offsets.</param>
+		public BivalueUniversalGraveType2TechniqueInfo(
+			IReadOnlyList<Conclusion> conclusions, IReadOnlyList<View> views,
+			int digit, IReadOnlyList<int> cells)
+			: base(conclusions, views) => (Digit, Cells) = (digit, cells);
+
+
+		/// <summary>
+		/// Indicates the digit.
+		/// </summary>
+		public int Digit { get; }
+
+		/// <summary>
+		/// Indicates the cell offsets.
+		/// </summary>
+		public IReadOnlyList<int> Cells { get; }
+
+		/// <inheritdoc/>
+		public override string Name => "Bivalue Universal Grave (Type 2)";
+
+		/// <inheritdoc/>
+		public override decimal Difficulty => 5.6m + DifficultyExtra[Cells.Count - 1];
+
+		/// <inheritdoc/>
+		public override DifficultyLevels DifficultyLevel => DifficultyLevels.Hard;
+
+
+		/// <inheritdoc/>
+		public override string ToString()
+		{
+			int digit = Digit + 1;
+			string cellsStr = CellCollection.ToString(Cells);
+			string elimStr = ConclusionCollection.ToString(Conclusions);
+			return $"{Name}: {digit} with cells {cellsStr} => {elimStr}";
+		}
+	}
+}
