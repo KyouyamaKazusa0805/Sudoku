f71e365848e19dbe923f19b079bfc92a02d57b00
Sunnie-Shine <HowdySunnie@outlook.com>
2020-02-14 20:22:02 +0800

    Update doc comment.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -22,7 +22,6 @@ namespace Sudoku.Debugging
 				OptimizedApplyingOrder = true,
 				EnableBruteForce = false,
 				EnablePatternOverlayMethod = false,
-				BowmanBingoMaximumLength = 20,
 			};
 			var grid = Grid.Parse(".4...93......3..........8...9...5.1.8....47...5..7......23.749.7..4....858.......");
 			var analysisResult = solver.Solve(grid);
