f37399e6c2f3b9ce9de9507a3e7f974c6ff1cec5
Sunnie-Shine <HowdySunnie@outlook.com>
2020-02-15 20:21:11 +0800

    Add file 'How to use CodeCounter'.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -25,12 +25,6 @@ namespace Sudoku.Debugging
 			var grid = Grid.Parse(".1.....49..9315.68......5......96......4...2..3........6...78......4...67.5.3...4");
 			var analysisResult = solver.Solve(grid);
 			Console.WriteLine($"{analysisResult}");
-
-			//Line counter.
-			//string solutionFolder = Solution.PathRoot;
-			//var codeCounter = new CodeCounter(solutionFolder, @".*\.cs$");
-			//int linesCount = codeCounter.CountCodeLines(out int filesCount);
-			//Console.WriteLine($"Found {filesCount} files, {linesCount} lines.");
 		}
 	}
 }
