85c7e402ef9406fea2990c0b30538f862faa67d7
Sunnie-Shine <HowdySunnie@outlook.com>
2020-03-12 14:41:46 +0800

    Remove 'SolveAsync'.

--- Sudoku.Solving/Solver.cs
+++ Sudoku.Solving/Solver.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Sudoku.Data;
+﻿using Sudoku.Data;
 
 namespace Sudoku.Solving
 {
@@ -22,18 +21,5 @@ namespace Sudoku.Solving
 		/// An <see cref="AnalysisResult"/> displaying all information of solving.
 		/// </returns>
 		public abstract AnalysisResult Solve(IReadOnlyGrid grid);
-
-		/// <summary>
-		/// Solves the specified puzzle asynchronizedly.
-		/// </summary>
-		/// <param name="grid">The puzzle.</param>
-		/// <param name="continueOnCapturedContext">
-		/// <see langword="true"/> to attempt to marshal the continuation back to
-		/// the original context captured; otherwise, <see langword="false"/>.
-		/// </param>
-		/// <returns>The solving task.</returns>
-		public virtual async Task<AnalysisResult> SolveAsync(
-			IReadOnlyGrid grid, bool continueOnCapturedContext) =>
-			await Task.Run(() => Solve(grid)).ConfigureAwait(continueOnCapturedContext);
 	}
 }
