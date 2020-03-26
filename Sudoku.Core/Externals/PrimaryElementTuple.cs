38c88eb9c934688ec14466555c1723a427645209
Sunnie-Shine <HowdySunnie@outlook.com>
2020-03-26 13:31:22 +0800

    Add doc comments.

--- Sudoku.Core/Externals/PrimaryElementTuple.cs
+++ Sudoku.Core/Externals/PrimaryElementTuple.cs
@@ -4,12 +4,32 @@ using Sudoku.Data.Extensions;
 
 namespace System
 {
+	/// <summary>
+	/// Provides a tuple with a primary element.
+	/// </summary>
+	/// <typeparam name="T1">The type of value 1.</typeparam>
+	/// <typeparam name="T2">The type of value 2.</typeparam>
+	/// <typeparam name="T3">The type of value 3.</typeparam>
 	public readonly struct PrimaryElementTuple<T1, T2, T3> : ITuple
 	{
+		/// <summary>
+		/// Initializes an instance with three values, and the first one
+		/// is the primary key.
+		/// </summary>
+		/// <param name="v1">The value 1.</param>
+		/// <param name="v2">The value 2.</param>
+		/// <param name="v3">The value 3.</param>
 		public PrimaryElementTuple(T1 v1, T2 v2, T3 v3) : this(v1, v2, v3, 1)
 		{
 		}
 
+		/// <summary>
+		/// Initializes an instance with three values and a primary key.
+		/// </summary>
+		/// <param name="v1">The value 1.</param>
+		/// <param name="v2">The value 2.</param>
+		/// <param name="v3">The value 3.</param>
+		/// <param name="primaryElementKey">The primary key.</param>
 		public PrimaryElementTuple(T1 v1, T2 v2, T3 v3, int primaryElementKey)
 		{
 			(Value1, Value2, Value3, PrimaryElementKey) = (
@@ -20,17 +40,31 @@ namespace System
 		}
 
 
+		/// <summary>
+		/// Indicates the value 1.
+		/// </summary>
 		public T1 Value1 { get; }
 
+		/// <summary>
+		/// Indicates the value 2.
+		/// </summary>
 		public T2 Value2 { get; }
 
+		/// <summary>
+		/// Indicates the value 3.
+		/// </summary>
 		public T3 Value3 { get; }
 
+		/// <summary>
+		/// Indicates the index of the primary value.
+		/// </summary>
 		public int PrimaryElementKey { get; }
 
+		/// <inheritdoc/>
 		int ITuple.Length => 3;
 
 
+		/// <inheritdoc/>
 		object? ITuple.this[int index]
 		{
 			get
@@ -46,9 +80,14 @@ namespace System
 		}
 
 
+		/// <include file='../../GlobalDocComments.xml' path='comments/method[@name="Deconstruct"]'/>
+		/// <param name="v1">(<see langword="out"/> parameter) The value 1.</param>
+		/// <param name="v2">(<see langword="out"/> parameter) The value 2.</param>
+		/// <param name="v3">(<see langword="out"/> parameter) The value 3.</param>
 		public void Deconstruct(out T1 v1, out T2 v2, out T3 v3) =>
 			(v1, v2, v3) = (Value1, Value2, Value3);
 
+		/// <include file='../../GlobalDocComments.xml' path='comments/method[@name="ToString" and @paramType="__noparam"]'/>
 		public override string ToString() => ((ITuple)this)[PrimaryElementKey].NullableToString();
 	}
 }
