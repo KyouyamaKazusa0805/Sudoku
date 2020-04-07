83c16078b7286194d247c278986406f3dc8f0d56
Sunnie-Shine <HowdySunnie@outlook.com>
2020-04-07 18:53:34 +0800

    Remove unused namespace.

--- Sudoku.Solving/Manual/Alses/DeathBlossomTechniqueSearcher.cs
+++ Sudoku.Solving/Manual/Alses/DeathBlossomTechniqueSearcher.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Sudoku.Data;
 using Sudoku.Data.Extensions;
