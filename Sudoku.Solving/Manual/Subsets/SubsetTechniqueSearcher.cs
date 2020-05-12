d948ed482530c40b9c90c13d858b042f3be8ca8e
Sunnie-Shine <HowdySunnie@outlook.com>
2020-05-12 13:50:45 +0800

    Optimize the naked subsets.

--- Sudoku.Solving/Manual/Subsets/SubsetTechniqueSearcher.cs
+++ Sudoku.Solving/Manual/Subsets/SubsetTechniqueSearcher.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Sudoku.Data;
 using Sudoku.Data.Extensions;
 using Sudoku.Drawing;
 using Sudoku.Extensions;
+using static System.Algorithms;
 using static Sudoku.Constants.Processings;
-using static Sudoku.Data.CellStatus;
-using Action = System.Action<System.Collections.Generic.IBag<Sudoku.Solving.TechniqueInfo>, Sudoku.Data.IReadOnlyGrid, int>;
+using static Sudoku.Data.ConclusionType;
+using static Sudoku.Data.GridMap.InitializeOption;
 
 namespace Sudoku.Solving.Manual.Subsets
 {
@@ -34,276 +34,103 @@ namespace Sudoku.Solving.Manual.Subsets
 		{
 			for (int size = 2; size <= 4; size++)
 			{
-				foreach (var act in new Action[] { GetNakedSubsetsBySize, GetHiddenSubsetsBySize })
-				{
-					act(accumulator, grid, size);
-				}
+				GetNakedSubsetsBySize(accumulator, grid, size);
+				GetHiddenSubsetsBySize(accumulator, grid, size);
 			}
 		}
 
 		/// <summary>
 		/// Get all naked subsets technique information, for searching the specified size.
 		/// </summary>
-		/// <param name="result">The result accumulator.</param>
+		/// <param name="accumulator">The result accumulator.</param>
 		/// <param name="grid">The grid.</param>
 		/// <param name="size">The size.</param>
 		/// <returns>All technique information searched.</returns>
-		private static void GetNakedSubsetsBySize(IBag<TechniqueInfo> result, IReadOnlyGrid grid, int size)
+		private static void GetNakedSubsetsBySize(IBag<TechniqueInfo> accumulator, IReadOnlyGrid grid, int size)
 		{
-			// Iterates on each region.
+			var candMaps = grid.GetCandidatesMap();
+			var emptyMap = grid.GetEmptyCellsMap();
 			for (int region = 0; region < 27; region++)
 			{
-				// Level 1 (Get first cell mask).
-				for (int i1 = 0; i1 < 10 - size; i1++)
+				var currentEmptyMap = RegionMaps[region] & emptyMap;
+				if (currentEmptyMap.Count < 2)
 				{
-					int pos1 = RegionCells[region][i1];
-					if (grid.GetStatus(pos1) != Empty)
+					continue;
+				}
+
+				foreach (int[] cells in GetCombinationsOfArray(currentEmptyMap.ToArray(), size))
+				{
+					short mask = 0;
+					foreach (int cell in cells)
+					{
+						mask |= grid.GetCandidatesReversal(cell);
+					}
+					if (mask.CountSet() != size)
 					{
 						continue;
 					}
 
-					// Level 2.
-					short mask1 = grid.GetMask(pos1);
-					for (int i2 = i1 + 1; i2 < 11 - size; i2++)
+					// Naked subset found. Now check eliminations.
+					short flagMask = 0;
+					var conclusions = new List<Conclusion>();
+					foreach (int digit in mask.GetAllSets())
 					{
-						int pos2 = RegionCells[region][i2];
-						if (grid.GetStatus(pos2) != Empty)
+						var map =
+							new GridMap(new GridMap(cells) & candMaps[digit], ProcessPeersWithoutItself)
+							& candMaps[digit];
+						flagMask |= map.AllSetsAreInOneRegion(out _) ? (short)0 : (short)(1 << digit);
+
+						foreach (int cell in map.Offsets)
 						{
-							continue;
-						}
-
-						short mask2 = (short)(grid.GetMask(pos2) & mask1);
-						if (size == 2)
-						{
-							// Check if is naked pair or not.
-							int mask = mask2 & 511;
-							if (mask.CountSet() != 7)
-							{
-								continue;
-							}
-
-							// Naked pair found.
-							var digits = new List<int>((511 & ~mask2).GetAllSets());
-							var offsets = new[] { pos1, pos2 };
-							var conclusions = GetNakedSubsetConclusions(grid, offsets, digits, out bool? isLocked);
-
-							if (conclusions.Count == 0)
-							{
-								continue;
-							}
-
-							// Gather this conclusion.
-							GatherConclusion(grid, result, region, digits, offsets, conclusions, isLocked);
-						}
-						else // size > 2
-						{
-							// Level 3.
-							for (int i3 = i2 + 1; i3 < 12 - size; i3++)
-							{
-								int pos3 = RegionCells[region][i3];
-								if (grid.GetStatus(pos3) != Empty)
-								{
-									continue;
-								}
-
-								short mask3 = (short)(grid.GetMask(pos3) & mask2);
-								if (size == 3)
-								{
-									// Check if is naked triple or not.
-									int mask = mask3 & 511;
-									if (mask.CountSet() != 6)
-									{
-										continue;
-									}
-
-									// Naked triple found.
-									var digits = new List<int>((511 & ~mask3).GetAllSets());
-									var offsets = new[] { pos1, pos2, pos3 };
-									var conclusions =
-										GetNakedSubsetConclusions(grid, offsets, digits, out bool? isLocked);
-
-									if (conclusions.Count == 0)
-									{
-										continue;
-									}
-
-									GatherConclusion(grid, result, region, digits, offsets, conclusions, isLocked);
-								}
-								else // size == 4
-								{
-									// Level 4. Final level.
-									for (int i4 = i3 + 1; i4 < 9; i4++)
-									{
-										int pos4 = RegionCells[region][i4];
-										if (grid.GetStatus(pos4) != Empty)
-										{
-											continue;
-										}
-
-										short mask4 = (short)(grid.GetMask(pos4) & mask3);
-
-										// Check if is naked quadruple or not.
-										int mask = mask4 & 511;
-										if (mask.CountSet() != 5)
-										{
-											continue;
-										}
-
-										// Naked triple found.
-										var digits = new List<int>((511 & ~mask4).GetAllSets());
-										var offsets = new[] { pos1, pos2, pos3, pos4 };
-										var conclusions =
-											GetNakedSubsetConclusions(grid, offsets, digits, out bool? isLocked);
-
-										if (conclusions.Count == 0)
-										{
-											continue;
-										}
-
-										GatherConclusion(grid, result, region, digits, offsets, conclusions, isLocked);
-									}
-								}
-							}
+							conclusions.Add(new Conclusion(Elimination, cell, digit));
 						}
 					}
+					if (conclusions.Count == 0)
+					{
+						continue;
+					}
+
+					var candidateOffsets = new List<(int, int)>();
+					foreach (int cell in cells)
+					{
+						foreach (int digit in grid.GetCandidatesReversal(cell).GetAllSets())
+						{
+							candidateOffsets.Add((0, cell * 9 + digit));
+						}
+					}
+
+					accumulator.Add(
+						new NakedSubsetTechniqueInfo(
+							conclusions,
+							views: new[]
+							{
+								new View(
+									cellOffsets: null,
+									candidateOffsets,
+									regionOffsets: new[] { (0, region) },
+									links: null)
+							},
+							regionOffset: region,
+							cellOffsets: cells,
+							digits: mask.GetAllSets().ToArray(),
+							isLocked: true switch
+							{
+								_ when flagMask == mask => true,
+								_ when flagMask != 0 => false,
+								_ => null
+							}));
 				}
 			}
-		}
-
-		/// <summary>
-		/// To gather a conclusion with essential information.
-		/// </summary>
-		/// <param name="grid">The grid.</param>
-		/// <param name="result">The information instances searched.</param>
-		/// <param name="region">The region offset.</param>
-		/// <param name="digits">All digits.</param>
-		/// <param name="offsets">The cell offsets.</param>
-		/// <param name="conclusions">All conclusions.</param>
-		/// <param name="isLocked">Indicates whether the subset is locked.</param>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private static void GatherConclusion(
-			IReadOnlyGrid grid, IBag<TechniqueInfo> result, int region,
-			IReadOnlyList<int> digits, IReadOnlyList<int> offsets,
-			IReadOnlyList<Conclusion> conclusions, bool? isLocked)
-		{
-			result.Add(
-				new NakedSubsetTechniqueInfo(
-					conclusions,
-					views: new[]
-					{
-						new View(
-							cellOffsets: null,
-							candidateOffsets: GetNakedSubsetsHighlightedCandidateOffsets(grid, offsets, digits),
-							regionOffsets: new[] { (0, region) },
-							links: null)
-					},
-					regionOffset: region,
-					cellOffsets: offsets,
-					digits,
-					isLocked));
-		}
-
-		/// <summary>
-		/// Get conclusions after searched a subset technique.
-		/// </summary>
-		/// <param name="grid">The grid.</param>
-		/// <param name="offsets">All cell offsets.</param>
-		/// <param name="digits">All digits.</param>
-		/// <param name="isLocked">
-		/// (<see langword="out"/> parameter) Indicates whether the subset is locked.
-		/// </param>
-		/// <returns>All conclusions.</returns>
-		private static IReadOnlyList<Conclusion> GetNakedSubsetConclusions(
-			IReadOnlyGrid grid, IReadOnlyList<int> offsets, IReadOnlyList<int> digits, out bool? isLocked)
-		{
-			var result = new List<Conclusion>();
-			int count = digits.Count;
-			bool[] series = new bool[count];
-			for (int i = 0; i < count; i++)
-			{
-				// Get first offset whose value has been set.
-				int digit = digits[i];
-				int firstOffset = offsets[0], firstOffsetIndex = 0;
-				for (int j = 0; j < offsets.Count; j++)
-				{
-					int offset = offsets[j];
-					if (!grid[offset, digit])
-					{
-						firstOffset = offset;
-						firstOffsetIndex = j;
-						break;
-					}
-				}
-
-				// Get intersection by each digit.
-				var tempMap = new GridMap(firstOffset, false);
-				for (int j = firstOffsetIndex + 1; j < offsets.Count; j++)
-				{
-					int offset = offsets[j];
-					if (!grid[offset, digit])
-					{
-						tempMap &= new GridMap(offset, false);
-					}
-				}
-				series[i] = tempMap.Count > 9;
-
-				// Set bit 'false' on themselves (do not regard them as eliminations).
-				foreach (int offset in offsets)
-				{
-					tempMap.Remove(offset);
-				}
-
-				// Add eliminations by each digit.
-				foreach (int offset in tempMap.Offsets)
-				{
-					if (grid.Exists(offset, digit) is true)
-					{
-						result.Add(new Conclusion(ConclusionType.Elimination, offset, digit));
-					}
-				}
-			}
-			bool? isLockedTemp = series.Any(v => v) ? series.All(v => v) : (bool?)null;
-			isLocked = digits.Count == 4
-				? isLockedTemp != true ? isLockedTemp : false
-				: isLockedTemp;
-			return result;
-		}
-
-		/// <summary>
-		/// Get all candidate offsets highlight in a specified naked subset technique.
-		/// </summary>
-		/// <param name="grid">The grid.</param>
-		/// <param name="offsets">All cell offsets.</param>
-		/// <param name="digits">All digits.</param>
-		/// <returns>All candidate offsets and its ID.</returns>
-		private static IReadOnlyList<(int, int)> GetNakedSubsetsHighlightedCandidateOffsets(
-			IReadOnlyGrid grid, IReadOnlyList<int> offsets, IReadOnlyList<int> digits)
-		{
-			var result = new List<(int, int)>();
-			foreach (int offset in offsets)
-			{
-				foreach (int digit in digits)
-				{
-					if (!grid[offset, digit])
-					{
-						// The candidate does exist in the current grid,
-						// which should be highlight.
-						result.Add((0, offset * 9 + digit));
-					}
-				}
-			}
-
-			return result;
 		}
 
 		/// <summary>
 		/// Get all hidden subsets technique information, for searching the specified size.
 		/// </summary>
-		/// <param name="result">The result accumulator.</param>
+		/// <param name="accumulator">The result accumulator.</param>
 		/// <param name="grid">The grid.</param>
 		/// <param name="size">The size.</param>
 		/// <returns>All technique information searched.</returns>
-		private static void GetHiddenSubsetsBySize(IBag<TechniqueInfo> result, IReadOnlyGrid grid, int size)
+		private static void GetHiddenSubsetsBySize(IBag<TechniqueInfo> accumulator, IReadOnlyGrid grid, int size)
 		{
 			for (int region = 0; region < 27; region++)
 			{
@@ -342,7 +169,7 @@ namespace Sudoku.Solving.Manual.Subsets
 								continue;
 							}
 
-							result.Add(
+							accumulator.Add(
 								new HiddenSubsetTechniqueInfo(
 									conclusions,
 									views: new[]
@@ -350,10 +177,7 @@ namespace Sudoku.Solving.Manual.Subsets
 										new View(
 											cellOffsets: null,
 											candidateOffsets: highlightedCandidates,
-											regionOffsets: new[]
-											{
-												(0, region)
-											},
+											regionOffsets: new[] { (0, region) },
 											links: null)
 									},
 									regionOffset: region,
@@ -389,7 +213,7 @@ namespace Sudoku.Solving.Manual.Subsets
 										continue;
 									}
 
-									result.Add(
+									accumulator.Add(
 										new HiddenSubsetTechniqueInfo(
 											conclusions,
 											views: new[]
@@ -397,10 +221,7 @@ namespace Sudoku.Solving.Manual.Subsets
 												new View(
 													cellOffsets: null,
 													candidateOffsets: highlightedCandidates,
-													regionOffsets: new[]
-													{
-														(0, region)
-													},
+													regionOffsets: new[] { (0, region) },
 													links: null)
 											},
 											regionOffset: region,
@@ -436,7 +257,7 @@ namespace Sudoku.Solving.Manual.Subsets
 											continue;
 										}
 
-										result.Add(
+										accumulator.Add(
 											new HiddenSubsetTechniqueInfo(
 												conclusions,
 												views: new[]
@@ -444,10 +265,7 @@ namespace Sudoku.Solving.Manual.Subsets
 													new View(
 														cellOffsets: null,
 														candidateOffsets: highlightedCandidates,
-														regionOffsets: new[]
-														{
-															(0, region)
-														},
+														regionOffsets: new[] { (0, region) },
 														links: null)
 												},
 												regionOffset: region,
@@ -505,7 +323,7 @@ namespace Sudoku.Solving.Manual.Subsets
 							{
 								// Does not contain in the list.
 								// These candidates are eliminations.
-								result.Add(new Conclusion(ConclusionType.Elimination, offset, digit));
+								result.Add(new Conclusion(Elimination, offset, digit));
 							}
 						}
 					}
