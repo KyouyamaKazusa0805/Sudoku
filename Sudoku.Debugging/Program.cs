002c5aa1a8ee4519a5c694cd955f54c51c142dde
Sunnie-Shine <HowdySunnie@outlook.com>
2020-05-17 10:52:26 +0800

    [Debugging] Count code lines.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -14,6 +14,21 @@ namespace Sudoku.Debugging
 		/// </summary>
 		private static void Main()
 		{
+			var w = new System.Diagnostics.Stopwatch();
+
+			var z = new Sudoku.Diagnostics.CodeCounter(Sudoku.Diagnostics.Solution.PathRoot, @".+\.cs$");
+
+			w.Start();
+			int codeLines = z.CountCodeLines(out int count);
+			w.Stop();
+
+			foreach (var fileName in z.FileList)
+			{
+				System.Console.WriteLine(fileName);
+			}
+
+			System.Console.WriteLine(
+				$"Code lines: {codeLines}, found files: {count}, time elapsed: {w.Elapsed:hh':'mm'.'ss'.'fff}");
 		}
 	}
 }
