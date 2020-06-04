f1a838c5ab206dbdbbb9a735872f689947e85dca
Sunnie-Shine <HowdySunnie@outlook.com>
2020-06-04 21:48:55 +0800

    Update guardian handling.

--- Sudoku.Solving/Manual/Sdps/GuardianTechniqueSearcher.cs
+++ Sudoku.Solving/Manual/Sdps/GuardianTechniqueSearcher.cs
@@ -2,6 +2,7 @@
 using Sudoku.Constants;
 using Sudoku.Data;
 using Sudoku.Drawing;
+using Sudoku.Extensions;
 using Sudoku.Solving.Annotations;
 using Sudoku.Solving.Manual.LastResorts;
 using static Sudoku.Constants.Processings;
@@ -16,6 +17,12 @@ namespace Sudoku.Solving.Manual.Sdps
 	[TechniqueDisplay("Guardian")]
 	public sealed class GuardianTechniqueSearcher : SdpTechniqueSearcher
 	{
+		/// <summary>
+		/// Indicates the invalid label (used in the start of the recursion).
+		/// </summary>
+		private const RegionLabel InvalidLabel = (RegionLabel)(-1);
+
+
 		/// <summary>
 		/// Indicates the priority of this technique.
 		/// </summary>
@@ -39,6 +46,8 @@ namespace Sudoku.Solving.Manual.Sdps
 				elimKeys[info.Digit] = true;
 			}
 
+			var tempList = new List<int>();
+			var tempGuardians = new List<GridMap>();
 			for (int digit = 0; digit < 9; digit++)
 			{
 				var candMap = CandMaps[digit];
@@ -49,6 +58,119 @@ namespace Sudoku.Solving.Manual.Sdps
 
 				foreach (int cell in candMap)
 				{
+					tempList.Clear();
+					tempGuardians.Clear();
+					var loop = GridMap.Empty;
+
+					f(cell, InvalidLabel);
+
+					void f(int cell, RegionLabel lastLabel)
+					{
+						tempList.Add(cell);
+						loop.Add(cell);
+
+						bool flag = false;
+						foreach (int tempRegion in loop.Regions)
+						{
+							if ((RegionMaps[tempRegion] & loop).Count >= 3)
+							{
+								flag = true;
+								break;
+							}
+						}
+						if (flag)
+						{
+							return;
+						}
+
+						for (var label = Block; label <= Column; label++)
+						{
+							if (label == lastLabel)
+							{
+								continue;
+							}
+
+							int region = GetRegion(cell, label);
+							foreach (int nextCell in RegionMaps[region] & candMap)
+							{
+								if (nextCell == cell)
+								{
+									continue;
+								}
+
+								if (tempList[0] == nextCell && loop.Count >= 5 && (loop.Count & 1) == 1)
+								{
+									// Check eliminations.
+									tempGuardians.Add((RegionMaps[region] & candMap) - loop);
+
+									var guardians = GridMap.Empty;
+									foreach (var guardian in tempGuardians)
+									{
+										guardians |= guardian;
+									}
+									if (guardians.Count > 20)
+									{
+										continue;
+									}
+
+									var peerMap = guardians.PeerIntersection;
+									if (peerMap.IsEmpty)
+									{
+										continue;
+									}
+
+									var elimMap = peerMap & candMap;
+									if (elimMap.IsEmpty)
+									{
+										continue;
+									}
+
+									var conclusions = new List<Conclusion>();
+									foreach (int elimCell in elimMap)
+									{
+										conclusions.Add(new Conclusion(Elimination, elimCell, digit));
+									}
+
+									var candidateOffsets = new List<(int, int)>();
+									foreach (int loopCell in loop)
+									{
+										candidateOffsets.Add((0, loopCell * 9 + digit));
+									}
+									foreach (int guardianCell in guardians)
+									{
+										candidateOffsets.Add((1, guardianCell * 9 + digit));
+									}
+
+									accumulator.Add(
+										new GuardianTechniqueInfo(
+											conclusions,
+											views: new[]
+											{
+												new View(
+													cellOffsets: null,
+													candidateOffsets,
+													regionOffsets: null,
+													links: null)
+											},
+											digit,
+											loop,
+											guardians));
+
+									return;
+								}
+								else if (!loop[nextCell])
+								{
+									tempGuardians.Add((RegionMaps[region] & candMap) - loop);
+
+									f(nextCell, label);
+
+									loop.Remove(nextCell);
+									tempList.RemoveLastElement();
+									tempGuardians.RemoveLastElement();
+								}
+							}
+						}
+					}
 				}
 			}
 		}
