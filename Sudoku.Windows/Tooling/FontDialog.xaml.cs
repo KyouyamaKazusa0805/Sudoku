8491d407a3144db4201cfb283a5deab517d0f838
Sunnie-Shine <HowdySunnie@outlook.com>
2020-06-16 20:05:42 +0800

    Clean code for FontDialog.

--- Sudoku.Windows/Tooling/FontDialog.xaml.cs
+++ Sudoku.Windows/Tooling/FontDialog.xaml.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Text;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Sudoku.Constants;
 using Sudoku.Drawing.Extensions;
-using d = System.Drawing;
+using static System.Windows.Input.Key;
+using static System.Drawing.StringAlignment;
+using static System.Drawing.Text.TextRenderingHint;
+using DFontStyle = System.Drawing.FontStyle;
 
 namespace Sudoku.Windows.Tooling
 {
 	/// <summary>
 	/// Interaction logic for FontDialog.xaml
 	/// </summary>
-	public partial class FontDialog : Window
+	public partial class FontDialog : Window, IDisposable
 	{
+		/// <summary>
+		/// Indicates the sample text.
+		/// </summary>
+		private const string SampleText = "0123456789";
+
+
 		/// <summary>
 		/// The internal brush.
 		/// </summary>
@@ -31,20 +40,10 @@ namespace Sudoku.Windows.Tooling
 		{
 			base.OnInitialized(e);
 
-			foreach (var font in new InstalledFontCollection().Families)
-			{
-				_listBoxFonts.Items.Add(font.Name);
-			}
+			_listBoxFonts.ItemsSource = from font in new InstalledFontCollection().Families select font.Name;
 			_listBoxFonts.SelectedIndex = 0;
 
-			SelectedFont = new Font(
-				_listBoxFonts.Items[0].ToString(), (float)FontSize, (d::FontStyle)0);
-
-			_listBoxStyle.Items.Add("Normal");
-			_listBoxStyle.Items.Add("Italic");
-			_listBoxStyle.Items.Add("Bold");
-			_listBoxStyle.Items.Add("Bold Italic");
-			_listBoxStyle.SelectedIndex = 0;
+			SelectedFont = new Font(_listBoxFonts.Items[0].ToString(), (float)FontSize, DFontStyle.Regular);
 
 			_textBoxSize.Text = FontSize.ToString();
 
@@ -55,14 +54,10 @@ namespace Sudoku.Windows.Tooling
 		{
 			var bitmap = new Bitmap((int)_imagePreview.Width, (int)_imagePreview.Height);
 			using var g = Graphics.FromImage(bitmap);
-			g.TextRenderingHint = TextRenderingHint.AntiAlias;
+			g.TextRenderingHint = AntiAlias;
 			g.DrawString(
-				"0123456789", SelectedFont, _brush, bitmap.Width / 2, bitmap.Height / 2,
-				new StringFormat
-				{
-					Alignment = StringAlignment.Center,
-					LineAlignment = StringAlignment.Center
-				});
+				SampleText, SelectedFont, _brush, bitmap.Width >> 1, bitmap.Height >> 1,
+				new StringFormat { Alignment = Center, LineAlignment = Center });
 
 			_imagePreview.Source = bitmap.ToImageSource();
 		}
@@ -86,24 +81,10 @@ namespace Sudoku.Windows.Tooling
 
 		private void ListBoxStyle_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			if (sender is ListBox listBox)
+			// While initializing, 'SelectedFont' is null.
+			if (sender is ListBox listBox && !(SelectedFont is null))
 			{
-				if (SelectedFont is null)
-				{
-					// While initializing, 'SelectedFont' is null.
-					e.Handled = true;
-					return;
-				}
-
-				SelectedFont = new Font(
-					SelectedFont, listBox.SelectedIndex switch
-					{
-						0 => d::FontStyle.Regular,
-						1 => d::FontStyle.Bold,
-						2 => d::FontStyle.Italic,
-						3 => d::FontStyle.Bold | d::FontStyle.Italic,
-						_ => throw Throwings.ImpossibleCase
-					});
+				SelectedFont = new Font(SelectedFont, (DFontStyle)listBox.SelectedIndex);
 
 				DrawString();
 			}
@@ -111,17 +92,10 @@ namespace Sudoku.Windows.Tooling
 
 		private void ListBoxFonts_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			if (sender is ListBox listBox)
+			// While initializing, 'SelectedFont' is null.
+			if (sender is ListBox listBox && !(SelectedFont is null))
 			{
-				if (SelectedFont is null)
-				{
-					// While initializing, 'SelectedFont' is null.
-					e.Handled = true;
-					return;
-				}
-
-				SelectedFont = new Font(
-					listBox.SelectedItem.ToString(), SelectedFont.Size, SelectedFont.Style);
+				SelectedFont = new Font(listBox.SelectedItem.ToString(), SelectedFont.Size, SelectedFont.Style);
 
 				DrawString();
 			}
@@ -149,33 +123,25 @@ namespace Sudoku.Windows.Tooling
 		{
 			if (sender is TextBox textBox)
 			{
+				bool c() =>
+					(e.Key > D0 && e.Key <= D9 || e.Key > NumPad0 && e.Key <= NumPad9)
+					&& Keyboard.Modifiers == ModifierKeys.None;
+
 				if (textBox.Text == "0")
 				{
-					if (!(e.Key > Key.D0 && e.Key <= Key.D9 || e.Key > Key.NumPad0 && e.Key <= Key.NumPad9)
-						|| Keyboard.Modifiers != ModifierKeys.None)
+					if (c())
 					{
-						e.Handled = true;
-						return;
+						textBox.Text = e.Key > D0 && e.Key <= D9 ? (e.Key - D0).ToString() : (e.Key - NumPad0).ToString();
 					}
-
-					textBox.Text = e.Key > Key.D0 && e.Key <= Key.D9
-						? (e.Key - Key.D0).ToString()
-						: (e.Key - Key.NumPad0).ToString();
 				}
-				else
+				else if (c())
 				{
-					if (!(e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
-						|| Keyboard.Modifiers != ModifierKeys.None)
-					{
-						e.Handled = true;
-						return;
-					}
-
-					textBox.Text = e.Key >= Key.D0 && e.Key <= Key.D9
-						? (e.Key - Key.D0).ToString()
-						: (e.Key - Key.NumPad0).ToString();
+					textBox.Text = e.Key >= D0 && e.Key <= D9 ? (e.Key - D0).ToString() : (e.Key - NumPad0).ToString();
 				}
 			}
 		}
+
+		/// <inheritdoc/>
+		public void Dispose() => _brush.Dispose();
 	}
 }
