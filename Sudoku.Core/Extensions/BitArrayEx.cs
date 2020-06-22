7746a744364adaa12d9148adaa00686c7a532000
Sunnie-Shine <HowdySunnie@outlook.com>
2020-06-22 10:30:02 +0800

    Update doc comments.

--- Sudoku.Core/Extensions/BitArrayEx.cs
+++ Sudoku.Core/Extensions/BitArrayEx.cs
@@ -14,7 +14,7 @@ namespace Sudoku.Extensions
 		/// Get the cardinality of the specified <see cref="BitArray"/>.
 		/// </summary>
 		/// <param name="this">(<see langword="this"/> parameter) The array.</param>
-		/// <returns>The total number of bits set true.</returns>
+		/// <returns>The total number of bits set <see langword="true"/>.</returns>
 		public static int GetCardinality(this BitArray @this)
 		{
 			int[] integers = new int[((@this.Length >> 5) + 1)];
