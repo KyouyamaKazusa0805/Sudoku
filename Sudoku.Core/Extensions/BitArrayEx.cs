8f0801cab22e94bcdc4294b9b08fd10cc97a0572
Sunnie-Shine <HowdySunnie@outlook.com>
2020-06-22 10:29:07 +0800

    Add method 'BitArray.GetCardinality'.

---
+++ Sudoku.Core/Extensions/BitArrayEx.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Sudoku.Extensions
+{
+	/// <summary>
+	/// Provides extension methods on <see cref="BitArray"/>.
+	/// </summary>
+	/// <seealso cref="BitArray"/>
+	[DebuggerStepThrough]
+	public static class BitArrayEx
+	{
+		/// <summary>
+		/// Get the cardinality of the specified <see cref="BitArray"/>.
+		/// </summary>
+		/// <param name="this">(<see langword="this"/> parameter) The array.</param>
+		/// <returns>The total number of bits set true.</returns>
+		public static int GetCardinality(this BitArray @this)
+		{
+			int[] integers = new int[((@this.Length >> 5) + 1)];
+			@this.CopyTo(integers, 0);
+
+			int result = 0;
+			foreach (int integer in integers)
+			{
+				result += integer.CountSet();
+			}
+
+			return result;
+		}
+	}
+}
