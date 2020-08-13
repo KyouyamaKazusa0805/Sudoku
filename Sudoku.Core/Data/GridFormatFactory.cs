ddd3ce7c83fbee8a0d17a15355a23c98de7da08d
Sunnie-Shine <HowdySunnie@outlook.com>
2020-08-13 21:32:27 +0800

    Use switch expression on grid format factory.

--- Sudoku.Core/Data/GridFormatFactory.cs
+++ Sudoku.Core/Data/GridFormatFactory.cs
@@ -61,251 +61,285 @@ namespace Sudoku.Data
 		/// <exception cref="FormatException">
 		/// Throws when the format string is invalid.
 		/// </exception>
-		public static GridFormatter Create(string? format)
-		{
-			switch (format)
+		public static GridFormatter Create(string? format) =>
+			format switch
 			{
-				case null:
-				case ".":
-				{
-					return new GridFormatter(false);
-				}
-				case "+":
-				case ".+":
-				case "+.":
-				{
-					return new GridFormatter(false) { WithModifiables = true };
-				}
-				case "0":
-				{
-					return new GridFormatter(false) { Placeholder = '0' };
-				}
-				case ":":
-				{
-					return new GridFormatter(false) { WithCandidates = true };
-				}
-				case "!":
-				case ".!":
-				case "!.":
-				{
-					return new GridFormatter(false) { WithModifiables = true };
-				}
-				case "0!":
-				case "!0":
-				{
-					return new GridFormatter(false)
-					{
-						Placeholder = '0',
-						WithModifiables = true
-					};
-				}
-				case ".:":
-				{
-					return new GridFormatter(false) { WithCandidates = true };
-				}
-				case "0:":
-				{
-					return new GridFormatter(false)
-					{
-						Placeholder = '0',
-						WithCandidates = true
-					};
-				}
-				case "0+":
-				case "+0":
-				{
-					return new GridFormatter(false)
-					{
-						Placeholder = '0',
-						WithModifiables = true
-					};
-				}
-				case "+:":
-				case "+.:":
-				case ".+:":
-				{
-					return new GridFormatter(false)
-					{
-						WithModifiables = true,
-						WithCandidates = true
-					};
-				}
-				case "0+:":
-				case "+0:":
-				{
-					return new GridFormatter(false)
-					{
-						Placeholder = '0',
-						WithModifiables = true,
-						WithCandidates = true
-					};
-				}
-				case ".!:":
-				case "!.:":
-				{
-					return new GridFormatter(false) { WithModifiables = true };
-				}
-				case "0!:":
-				case "!0:":
-				{
-					return new GridFormatter(false)
-					{
-						Placeholder = '0',
-						WithModifiables = true
-					};
-				}
-				case "#":
-				case "#.":
-				{
-					// Formats representing 'intelligence processor' is equal to
-					// format '.+:' and '0+:'.
-					goto case ".+:";
-				}
-				case "#0":
-				{
-					goto case "0+:";
-				}
-				case "@":
-				case "@.":
-				{
-					return new GridFormatter(true)
-					{
-						SubtleGridLines = true
-					};
-				}
-				case "@0":
-				{
-					return new GridFormatter(true)
-					{
-						Placeholder = '0',
-						SubtleGridLines = true
-					};
-				}
-				case "@!":
-				case "@.!":
-				case "@!.":
-				{
-					return new GridFormatter(true)
-					{
-						TreatValueAsGiven = true,
-						SubtleGridLines = true
-					};
-				}
-				case "@0!":
-				case "@!0":
-				{
-					return new GridFormatter(true)
-					{
-						Placeholder = '0',
-						TreatValueAsGiven = true,
-						SubtleGridLines = true
-					};
-				}
-				case "@*":
-				case "@.*":
-				case "@*.":
-				{
-					return new GridFormatter(true);
-				}
-				case "@0*":
-				case "@*0":
-				{
-					return new GridFormatter(true) { Placeholder = '0' };
-				}
-				case "@!*":
-				case "@*!":
-				{
-					return new GridFormatter(true) { TreatValueAsGiven = true };
-				}
-				case "@:":
-				{
-					return new GridFormatter(true)
-					{
-						WithCandidates = true,
-						SubtleGridLines = true
-					};
-				}
-				case "@:!":
-				case "@!:":
-				{
-					return new GridFormatter(true)
-					{
-						WithCandidates = true,
-						TreatValueAsGiven = true,
-						SubtleGridLines = true
-					};
-				}
-				case "@*:":
-				case "@:*":
-				{
-					return new GridFormatter(true) { WithCandidates = true };
-				}
-				case "@!*:":
-				case "@*!:":
-				case "@!:*":
-				case "@*:!":
-				case "@:!*":
-				case "@:*!":
-				{
-					return new GridFormatter(true)
-					{
-						WithCandidates = true,
-						TreatValueAsGiven = true
-					};
-				}
-				case "~":
-				case "~0":
-				{
-					return new GridFormatter(false)
-					{
-						Sukaku = true,
-						Placeholder = '0'
-					};
-				}
-				case "~.":
-				{
-					return new GridFormatter(false)
-					{
-						Sukaku = true,
-						Placeholder = '.'
-					};
-				}
-				case "@~":
-				case "~@":
-				{
-					return new GridFormatter(true) { Sukaku = true };
-				}
-				case "@~0":
-				case "@0~":
-				case "~@0":
-				case "~0@":
-				{
-					return new GridFormatter(true)
-					{
-						Sukaku = true,
-						Placeholder = '0'
-					};
-				}
-				case "@~.":
-				case "@.~":
-				case "~@.":
-				case "~.@":
-				{
-					return new GridFormatter(true)
-					{
-						Sukaku = true,
-						Placeholder = '.'
-					};
-				}
-				case "%":
-				{
-					return new GridFormatter(true) { Excel = true };
-				}
-				default:
-				{
-					throw Throwings.FormatError;
-				}
-			}
-		}
+				null or "." => new GridFormatter(false),
+				"+" or ".+" or "+." => new GridFormatter(false) { WithModifiables = true },
+				"0" => new GridFormatter(false) { Placeholder = '0' },
+				":" => new GridFormatter(false) { WithCandidates = true },
+				"!" or ".!" or "!." => new GridFormatter(false) { WithModifiables = true, TreatValueAsGiven = true },
+				"0!" or "!0" => new GridFormatter(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
+				".:" => new GridFormatter(false) { WithCandidates = true },
+				"0:" => new GridFormatter(false) { Placeholder = '0', WithCandidates = true },
+				"0+" or "+0" => new GridFormatter(false) { Placeholder = '0', WithModifiables = true },
+				"+:" or "+.:" or ".+:" or "#" or "#." => new GridFormatter(false) { WithModifiables = true, WithCandidates = true },
+				"0+:" or "+0:" or "#0" => new GridFormatter(false) { Placeholder = '0', WithModifiables = true, WithCandidates = true },
+				".!:" or "!.:" => new GridFormatter(false) { WithModifiables = true, TreatValueAsGiven = true },
+				"0!:" or "!0:" => new GridFormatter(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
+				"@" or "@." => new GridFormatter(true) { SubtleGridLines = true },
+				"@0" => new GridFormatter(true) { Placeholder = '0', SubtleGridLines = true },
+				"@!" or "@.!" or "@!." => new GridFormatter(true) { TreatValueAsGiven = true, SubtleGridLines = true },
+				"@0!" or "@!0" => new GridFormatter(true) { Placeholder = '0', TreatValueAsGiven = true, SubtleGridLines = true },
+				"@*" or "@.*" or "@*." => new GridFormatter(true),
+				"@0*" or "@*0" => new GridFormatter(true) { Placeholder = '0' },
+				"@!*" or "@*!" => new GridFormatter(true) { TreatValueAsGiven = true },
+				"@:" => new GridFormatter(true) { WithCandidates = true, SubtleGridLines = true },
+				"@:!" or "@!:" => new GridFormatter(true) { WithCandidates = true, TreatValueAsGiven = true, SubtleGridLines = true },
+				"@*:" or "@:*" => new GridFormatter(true) { WithCandidates = true },
+				"@!*:" or "@*!:" or "@!:*" or "@*:!" or "@:!*" or "@:*!" => new GridFormatter(true) { WithCandidates = true, TreatValueAsGiven = true },
+				"~" or "~0" => new GridFormatter(false) { Sukaku = true, Placeholder = '0' },
+				"~." => new GridFormatter(false) { Sukaku = true, Placeholder = '.' },
+				"@~" or "~@" => new GridFormatter(true) { Sukaku = true },
+				"@~0" or "@0~" or "~@0" or "~0@" => new GridFormatter(true) { Sukaku = true, Placeholder = '0' },
+				"@~." or "@.~" or "~@." or "~.@" => new GridFormatter(true) { Sukaku = true, Placeholder = '.' },
+				"%" => new GridFormatter(true) { Excel = true },
+				_ => throw Throwings.FormatError
+			};
+		#region Obsolete code
+		//switch (format)
+		//{
+		//	case null:
+		//	case ".":
+		//	{
+		//		return new GridFormatter(false);
+		//	}
+		//	case "+":
+		//	case ".+":
+		//	case "+.":
+		//	{
+		//		return new GridFormatter(false) { WithModifiables = true };
+		//	}
+		//	case "0":
+		//	{
+		//		return new GridFormatter(false) { Placeholder = '0' };
+		//	}
+		//	case ":":
+		//	{
+		//		return new GridFormatter(false) { WithCandidates = true };
+		//	}
+		//	case "!":
+		//	case ".!":
+		//	case "!.":
+		//	{
+		//		return new GridFormatter(false) { WithModifiables = true };
+		//	}
+		//	case "0!":
+		//	case "!0":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Placeholder = '0',
+		//			WithModifiables = true
+		//		};
+		//	}
+		//	case ".:":
+		//	{
+		//		return new GridFormatter(false) { WithCandidates = true };
+		//	}
+		//	case "0:":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Placeholder = '0',
+		//			WithCandidates = true
+		//		};
+		//	}
+		//	case "0+":
+		//	case "+0":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Placeholder = '0',
+		//			WithModifiables = true
+		//		};
+		//	}
+		//	case "+:":
+		//	case "+.:":
+		//	case ".+:":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			WithModifiables = true,
+		//			WithCandidates = true
+		//		};
+		//	}
+		//	case "0+:":
+		//	case "+0:":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Placeholder = '0',
+		//			WithModifiables = true,
+		//			WithCandidates = true
+		//		};
+		//	}
+		//	case ".!:":
+		//	case "!.:":
+		//	{
+		//		return new GridFormatter(false) { WithModifiables = true };
+		//	}
+		//	case "0!:":
+		//	case "!0:":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Placeholder = '0',
+		//			WithModifiables = true
+		//		};
+		//	}
+		//	case "#":
+		//	case "#.":
+		//	{
+		//		// Formats representing 'intelligence processor' is equal to
+		//		// format '.+:' and '0+:'.
+		//		goto case ".+:";
+		//	}
+		//	case "#0":
+		//	{
+		//		goto case "0+:";
+		//	}
+		//	case "@":
+		//	case "@.":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@0":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			Placeholder = '0',
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@!":
+		//	case "@.!":
+		//	case "@!.":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			TreatValueAsGiven = true,
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@0!":
+		//	case "@!0":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			Placeholder = '0',
+		//			TreatValueAsGiven = true,
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@*":
+		//	case "@.*":
+		//	case "@*.":
+		//	{
+		//		return new GridFormatter(true);
+		//	}
+		//	case "@0*":
+		//	case "@*0":
+		//	{
+		//		return new GridFormatter(true) { Placeholder = '0' };
+		//	}
+		//	case "@!*":
+		//	case "@*!":
+		//	{
+		//		return new GridFormatter(true) { TreatValueAsGiven = true };
+		//	}
+		//	case "@:":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			WithCandidates = true,
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@:!":
+		//	case "@!:":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			WithCandidates = true,
+		//			TreatValueAsGiven = true,
+		//			SubtleGridLines = true
+		//		};
+		//	}
+		//	case "@*:":
+		//	case "@:*":
+		//	{
+		//		return new GridFormatter(true) { WithCandidates = true };
+		//	}
+		//	case "@!*:":
+		//	case "@*!:":
+		//	case "@!:*":
+		//	case "@*:!":
+		//	case "@:!*":
+		//	case "@:*!":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			WithCandidates = true,
+		//			TreatValueAsGiven = true
+		//		};
+		//	}
+		//	case "~":
+		//	case "~0":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Sukaku = true,
+		//			Placeholder = '0'
+		//		};
+		//	}
+		//	case "~.":
+		//	{
+		//		return new GridFormatter(false)
+		//		{
+		//			Sukaku = true,
+		//			Placeholder = '.'
+		//		};
+		//	}
+		//	case "@~":
+		//	case "~@":
+		//	{
+		//		return new GridFormatter(true) { Sukaku = true };
+		//	}
+		//	case "@~0":
+		//	case "@0~":
+		//	case "~@0":
+		//	case "~0@":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			Sukaku = true,
+		//			Placeholder = '0'
+		//		};
+		//	}
+		//	case "@~.":
+		//	case "@.~":
+		//	case "~@.":
+		//	case "~.@":
+		//	{
+		//		return new GridFormatter(true)
+		//		{
+		//			Sukaku = true,
+		//			Placeholder = '.'
+		//		};
+		//	}
+		//	case "%":
+		//	{
+		//		return new GridFormatter(true) { Excel = true };
+		//	}
+		//	default:
+		//	{
+		//		throw Throwings.FormatError;
+		//	}
+		//}
+		#endregion
 	}
 }
