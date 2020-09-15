03a41694df48873c6eeeaa6ca0f128846c772eb9
Sunnie-Shine <HowdySunnie@outlook.com>
2020-09-15 15:19:43 +0800

    Fix typo.

--- Sudoku.Solving/Manual/Wings/WingTechniqueInfo.cs
+++ Sudoku.Solving/Manual/Wings/WingTechniqueInfo.cs
@@ -7,7 +7,7 @@ namespace Sudoku.Solving.Manual.Wings
 	/// <summary>
 	/// Provides a usage of <b>wing</b> technique.
 	/// </summary>
-	/// <param name="Conclusios">All conclusions.</param>
+	/// <param name="Conclusions">All conclusions.</param>
 	/// <param name="Views">All views.</param>
 	public abstract record WingTechniqueInfo(IReadOnlyList<Conclusion> Conclusions, IReadOnlyList<View> Views)
 		: TechniqueInfo(Conclusions, Views);
