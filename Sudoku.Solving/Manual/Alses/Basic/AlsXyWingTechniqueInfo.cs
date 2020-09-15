87209f992bb0cf728bb18466a1255cd0d2bf0c12
Sunnie-Shine <HowdySunnie@outlook.com>
2020-09-15 15:16:12 +0800

    Fix typo.

--- Sudoku.Solving/Manual/Alses/Basic/AlsXyWingTechniqueInfo.cs
+++ Sudoku.Solving/Manual/Alses/Basic/AlsXyWingTechniqueInfo.cs
@@ -39,7 +39,7 @@ namespace Sudoku.Solving.Manual.Alses.Basic
 			string yStr = new DigitCollection(YDigitsMask.GetAllSets()).ToString();
 			string zStr = new DigitCollection(ZDigitsMask.GetAllSets()).ToString();
 			string elimStr = new ConclusionCollection(Conclusions).ToString();
-			return $"{Name}: {Als1} -> {BridgeAls} -> {Als2}, x = {xStr}, y = {yStr}, z = {zStr} => {elimStr}";
+			return $"{Name}: {Als1} -> {Bridge} -> {Als2}, x = {xStr}, y = {yStr}, z = {zStr} => {elimStr}";
 		}
 	}
 }
