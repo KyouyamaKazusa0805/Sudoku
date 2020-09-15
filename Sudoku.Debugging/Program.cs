39d4f9ac61a60a41075cd17a3224fa107a99ba39
Sunnie-Shine <HowdySunnie@outlook.com>
2020-09-15 15:57:04 +0800

    Change the behavior of Brute Force.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -17,10 +17,32 @@ using Sudoku.Solving.Annotations;
 using Sudoku.Solving.BruteForces.Bitwise;
 using Sudoku.Solving.Manual;
 using Sudoku.Solving.Manual.Chaining;
+using Sudoku.Solving.Manual.LastResorts;
 using Sudoku.Windows;
 using static System.Console;
 
-#if TEST_FOR_VALUE_GRID || true
+#if GENERATE_BRUTE_FORCE_TABLE || false
+WriteLine("private static readonly int[] TryAndErrorOrder =");
+WriteLine("{");
+for (int i = 0; i < 9; i++)
+{
+	Write("    ");
+	for (int j = 0; j < 9; j++)
+	{
+		int index = i * 9 + j, k = Array.IndexOf(BruteForceTechniqueSearcher.TryAndErrorOrder, index);
+		Write($"{k,2}");
+		if (index != 80)
+		{
+			Write(", ");
+		}
+	}
+	WriteLine();
+}
+
+WriteLine("};");
+#endif
+
+#if TEST_FOR_VALUE_GRID || false
 var vGrid = ValueGrid.Empty;
 var iGrid = Grid.Empty.Clone();
 vGrid[0] = 4;
@@ -33,7 +55,7 @@ WriteLine($"{vGrid:.+:}");
 WriteLine($"{iGrid:.+:}");
 #endif
 
-#if FILE_COUNTER || false
+#if FILE_COUNTER || true
 string root = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
 
 WriteLine(new FileCounter(root, "cs", withBinOrObjDirectory: false).CountUp());
