33ea0a7067d3c1bae43aa92797539a8b4e6b5eeb
Sunnie-Shine <HowdySunnie@outlook.com>
2020-10-19 20:07:55 +0800

    Use lambda style method returning.

--- Sudoku.Core/Data/SudokuGrid.GridFormatter.cs
+++ Sudoku.Core/Data/SudokuGrid.GridFormatter.cs
@@ -550,45 +550,33 @@ namespace Sudoku.Data
 			/// <param name="format">The format.</param>
 			/// <returns>The grid formatter.</returns>
 			/// <exception cref="FormatException">Throws when the format string is invalid.</exception>
-			public static GridFormatter Create(string? format)
-			{
-				GridFormatter result = format switch
+			public static GridFormatter Create(string? format) =>
+				format switch
 				{
 					null or "." => new(false),
 					"+" or ".+" or "+." => new(false) { WithModifiables = true },
 					"0" => new(false) { Placeholder = '0' },
 					":" => new(false) { WithCandidates = true },
-					"!" or ".!" or "!." =>
-						new(false) { WithModifiables = true, TreatValueAsGiven = true },
-					"0!" or "!0" =>
-						new(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
+					"!" or ".!" or "!." => new(false) { WithModifiables = true, TreatValueAsGiven = true },
+					"0!" or "!0" => new(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
 					".:" => new(false) { WithCandidates = true },
 					"0:" => new(false) { Placeholder = '0', WithCandidates = true },
 					"0+" or "+0" => new(false) { Placeholder = '0', WithModifiables = true },
-					"+:" or "+.:" or ".+:" or "#" or "#." =>
-						new(false) { WithModifiables = true, WithCandidates = true },
-					"0+:" or "+0:" or "#0" =>
-						new(false) { Placeholder = '0', WithModifiables = true, WithCandidates = true },
-					".!:" or "!.:" =>
-						new(false) { WithModifiables = true, TreatValueAsGiven = true },
-					"0!:" or "!0:" =>
-						new(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
+					"+:" or "+.:" or ".+:" or "#" or "#." => new(false) { WithModifiables = true, WithCandidates = true },
+					"0+:" or "+0:" or "#0" => new(false) { Placeholder = '0', WithModifiables = true, WithCandidates = true },
+					".!:" or "!.:" => new(false) { WithModifiables = true, TreatValueAsGiven = true },
+					"0!:" or "!0:" => new(false) { Placeholder = '0', WithModifiables = true, TreatValueAsGiven = true },
 					"@" or "@." => new(true) { SubtleGridLines = true },
 					"@0" => new(true) { Placeholder = '0', SubtleGridLines = true },
-					"@!" or "@.!" or "@!." =>
-						new(true) { TreatValueAsGiven = true, SubtleGridLines = true },
-					"@0!" or "@!0" =>
-						new(true) { Placeholder = '0', TreatValueAsGiven = true, SubtleGridLines = true },
+					"@!" or "@.!" or "@!." => new(true) { TreatValueAsGiven = true, SubtleGridLines = true },
+					"@0!" or "@!0" => new(true) { Placeholder = '0', TreatValueAsGiven = true, SubtleGridLines = true },
 					"@*" or "@.*" or "@*." => new(true),
 					"@0*" or "@*0" => new(true) { Placeholder = '0' },
 					"@!*" or "@*!" => new(true) { TreatValueAsGiven = true },
 					"@:" => new(true) { WithCandidates = true, SubtleGridLines = true },
-					"@:!" or "@!:" =>
-						new(true) { WithCandidates = true, TreatValueAsGiven = true, SubtleGridLines = true },
-					"@*:" or "@:*" =>
-						new(true) { WithCandidates = true },
-					"@!*:" or "@*!:" or "@!:*" or "@*:!" or "@:!*" or "@:*!" =>
-						new(true) { WithCandidates = true, TreatValueAsGiven = true },
+					"@:!" or "@!:" => new(true) { WithCandidates = true, TreatValueAsGiven = true, SubtleGridLines = true },
+					"@*:" or "@:*" => new(true) { WithCandidates = true },
+					"@!*:" or "@*!:" or "@!:*" or "@*:!" or "@:!*" or "@:*!" => new(true) { WithCandidates = true, TreatValueAsGiven = true },
 					"~" or "~0" => new(false) { Sukaku = true, Placeholder = '0' },
 					"~." => new(false) { Sukaku = true },
 					"@~" or "~@" => new(true) { Sukaku = true },
@@ -597,9 +585,6 @@ namespace Sudoku.Data
 					"%" => new(true) { Excel = true },
 					_ => throw new FormatException("The specified format is invalid.")
 				};
-
-				return result;
-			}
 
 			/// <summary>
 			/// Get the first <see langword="false"/> candidate.
