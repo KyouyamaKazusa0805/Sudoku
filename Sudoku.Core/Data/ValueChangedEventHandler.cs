c3181d24311c22195ab8d75f67898b5814e9edd3
Sunnie-Shine <HowdySunnie@outlook.com>
2020-10-23 21:21:58 +0800

    Remove other unused files.

--- Sudoku.Core/Data/ValueChangedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Data
-{
-	/// <summary>
-	/// The event handler used when the value changed.
-	/// </summary>
-	/// <param name="sender">The object triggering the event.</param>
-	/// <param name="e">The data while triggering.</param>
-	public delegate void ValueChangedEventHandler(object sender, ValueChangedEventArgs e);
-}
