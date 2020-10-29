75d3289fad52cbbb56d3aa9ebed2b2dfae9cd361
Sunnie-Shine <HowdySunnie@outlook.com>
2020-10-29 11:17:03 +0800

    Extract 'Enumerator' to reduce reference types using.

---
+++ Sudoku.Core/Data/BitCombinationGenerator.Enumerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sudoku.Data
+{
+	public readonly ref partial struct BitCombinationGenerator
+	{
+		/// <summary>
+		/// Indicates the enumerator of the current instance.
+		/// </summary>
+		public struct Enumerator : IEnumerator<long>
+		{
+			/// <summary>
+			/// The mask.
+			/// </summary>
+			private readonly long _mask;
+
+
+			/// <summary>
+			/// Indicates whether that the value is the last one.
+			/// </summary>
+			private bool _isLast;
+
+
+			/// <summary>
+			/// Initializes an instance with the specified number of bits
+			/// and <see langword="true"/> bits.
+			/// </summary>
+			/// <param name="bitCount">The number of bits.</param>
+			/// <param name="oneCount">The number of <see langword="true"/> bits.</param>
+			public Enumerator(int bitCount, int oneCount) =>
+				(Current, _mask, _isLast) = ((1 << oneCount) - 1, (1 << bitCount - oneCount) - 1, bitCount == 0);
+
+
+			/// <inheritdoc/>
+			public long Current { readonly get; private set; }
+
+			/// <inheritdoc/>
+			readonly object IEnumerator.Current => Current;
+
+
+			/// <inheritdoc/>
+			/// <remarks>Here will do nothing.</remarks>
+			public readonly void Dispose()
+			{
+			}
+
+			private bool HasNext()
+			{
+				bool result = !_isLast;
+				_isLast = (Current & -Current & _mask) == 0;
+				return result;
+			}
+
+			/// <inheritdoc/>
+			public bool MoveNext()
+			{
+				if (HasNext() is var result && result && !_isLast)
+				{
+					long smallest = Current & -Current;
+					long ripple = Current + smallest;
+					long ones = Current ^ ripple;
+					ones = (ones >> 2) / smallest;
+					Current = ripple | ones;
+				}
+
+				return result;
+			}
+
+			/// <inheritdoc/>
+			/// <remarks>Here will do nothing.</remarks>
+			public readonly void Reset()
+			{
+			}
+		}
+	}
+}
--- Sudoku.Core/Data/BitCombinationGenerator.cs
+++ Sudoku.Core/Data/BitCombinationGenerator.cs
@@ -18,22 +18,12 @@ namespace Sudoku.Data
 	/// }
 	/// </code>
 	/// </remarks>
-	public ref struct BitCombinationGenerator
+	public readonly ref partial struct BitCombinationGenerator
 	{
 		/// <summary>
-		/// The mask.
+		/// The inner enumerator.
 		/// </summary>
-		private readonly long _mask;
-
-		/// <summary>
-		/// The value.
-		/// </summary>
-		private long _value;
-
-		/// <summary>
-		/// Indicates whether that the value is the last one.
-		/// </summary>
-		private bool _isLast;
+		private readonly Enumerator _enumerator;
 
 
 		/// <summary>
@@ -42,74 +32,41 @@ namespace Sudoku.Data
 		/// </summary>
 		/// <param name="bitCount">The number of bits.</param>
 		/// <param name="oneCount">The number of <see langword="true"/> bits.</param>
-		public BitCombinationGenerator(int bitCount, int oneCount)
-		{
-			(BitCount, OneCount) = (bitCount, oneCount);
-			(_value, _mask, _isLast) = ((1 << oneCount) - 1, (1 << bitCount - oneCount) - 1, bitCount == 0);
-		}
+		public BitCombinationGenerator(int bitCount, int oneCount) =>
+			_enumerator = new(BitCount = bitCount, OneCount = oneCount);
 
 
 		/// <summary>
 		/// Indicates how many bits should be generated.
 		/// </summary>
-		public readonly int BitCount { get; }
+		public int BitCount { get; }
 
 		/// <summary>
 		/// Indicates how many <see langword="true"/> bits (1) are in
 		/// the number.
 		/// </summary>
-		public readonly int OneCount { get; }
+		public int OneCount { get; }
 
 
 		/// <inheritdoc/>
 		/// <exception cref="NotSupportedException">Always throws.</exception>
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		[DoesNotReturn]
-		public override readonly bool Equals(object? obj) => throw Throwings.RefStructNotSupported;
+		public override bool Equals(object? obj) => throw Throwings.RefStructNotSupported;
 
 		/// <inheritdoc cref="object.GetHashCode"/>
 		/// <exception cref="NotSupportedException">Always throws.</exception>
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		[DoesNotReturn]
-		public override readonly int GetHashCode() => throw Throwings.RefStructNotSupported;
+		public override int GetHashCode() => throw Throwings.RefStructNotSupported;
 
 		/// <inheritdoc cref="object.ToString"/>
 		/// <exception cref="NotSupportedException">Always throws.</exception>
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		[DoesNotReturn]
-		public override readonly string ToString() => throw Throwings.RefStructNotSupported;
-
-		/// <summary>
-		/// Indicates whether the generator has the next combination number to iterate.
-		/// </summary>
-		/// <returns>A <see cref="bool"/> result indicating that.</returns>
-		public bool HasNext()
-		{
-			bool result = !_isLast;
-			_isLast = (_value & -_value & _mask) == 0;
-			return result;
-		}
+		public override string ToString() => throw Throwings.RefStructNotSupported;
 
 		/// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
-		public IEnumerator<long> GetEnumerator()
-		{
-			var list = new List<long>();
-			while (HasNext())
-			{
-				long result = _value;
-				if (!_isLast)
-				{
-					long smallest = _value & -_value;
-					long ripple = _value + smallest;
-					long ones = _value ^ ripple;
-					ones = (ones >> 2) / smallest;
-					_value = ripple | ones;
-				}
-
-				list.Add(result);
-			}
-
-			return list.GetEnumerator();
-		}
+		public Enumerator GetEnumerator() => _enumerator;
 	}
 }
