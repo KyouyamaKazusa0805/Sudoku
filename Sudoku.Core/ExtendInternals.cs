acf32dfef9eef820abfdc7b0408fddff3ba0cba2
Sunnie-Shine <HowdySunnie@outlook.com>
2020-11-18 19:26:49 +0800

    Remove comment.

--- Sudoku.Core/ExtendInternals.cs
+++ Sudoku.Core/ExtendInternals.cs
@@ -1,8 +1,4 @@
-﻿// The attribute 'InternalVisibleToAttribute' is used for enhancing the access ability
-// of the keyword 'internal'.
-// If using this attribute, 'internal' members in the current assembly can be seen in other assemblies
-// specified as parameters in the attribute statements.
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Sudoku.Bot")]
 [assembly: InternalsVisibleTo("Sudoku.CodeAnalysis")]
