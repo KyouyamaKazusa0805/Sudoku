aed7ae027bc0fe7ab186ebfa4565f7f7758289da
Sunnie-Shine <HowdySunnie@outlook.com>
2020-11-19 13:28:27 +0800

    Update code.

--- Sudoku.Bot.Console/Program.cs
+++ Sudoku.Bot.Console/Program.cs
@@ -13,6 +13,7 @@ using Sudoku.Bot.Constants;
 const string pluginConfigPath = @"P:\Bot\插件设置.json";
 const long myNumber = (long)(222 * 1E7M) + (1 << 21);
 
+// Enable the bot.
 var sessionSettings = new SessionSettings("127.0.0.1", 8080, "1234567890");
 await using var session = new Session(sessionSettings, myNumber);
 await session.ConnectAsync();
@@ -24,10 +25,12 @@ await handler.ListenAsync();
 
 var plugin = new SudokuPlugin(currentUserEventSource);
 
+// Output the information.
 Console.WriteLine("启动成功，可以愉快地使用啦！");
 Console.WriteLine("（输入换行符退出程序）");
 Console.ReadLine();
 
+// Dispose resources.
 string dir = Path.GetDirectoryName(pluginConfigPath)!;
 DirectoryEx.CreateIfDoesNotExist(dir);
 
