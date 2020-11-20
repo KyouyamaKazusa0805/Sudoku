be06008ee62d29e4b7b19605811a19b5210134cd
Sunnie-Shine <HowdySunnie@outlook.com>
2020-11-20 18:23:09 +0800

    Remove Parse.

--- Sudoku.Bot/SudokuPlugin.Processings.cs
+++ Sudoku.Bot/SudokuPlugin.Processings.cs
@@ -2,10 +2,8 @@
 
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using HuajiTech.Mirai;
 using HuajiTech.Mirai.Events;
@@ -14,6 +12,7 @@ using Sudoku.Constants;
 using Sudoku.Data;
 using Sudoku.Data.Extensions;
 using Sudoku.Drawing;
+using Sudoku.Drawing.Extensions;
 using Sudoku.Globalization;
 using Sudoku.Solving;
 using Sudoku.Solving.Checking;
@@ -147,7 +146,7 @@ namespace Sudoku.Bot
 				return;
 			}
 
-			if (!Parser.TryParseColorId(s[2], false, out long colorId))
+			if (!ParseColorId(s[2], false, out long colorId))
 			{
 				return;
 			}
@@ -580,7 +579,7 @@ namespace Sudoku.Bot
 			}
 
 			long colorId = default;
-			if (!remove && !Parser.TryParseColorId(s[3], true, out colorId))
+			if (!remove && !ParseColorId(s[3], true, out colorId))
 			{
 				return;
 			}
@@ -629,7 +628,7 @@ namespace Sudoku.Bot
 			}
 
 			long colorId = default;
-			if (!remove && !Parser.TryParseColorId(s[4], false, out colorId))
+			if (!remove && !ParseColorId(s[4], false, out colorId))
 			{
 				return;
 			}
@@ -669,7 +668,7 @@ namespace Sudoku.Bot
 			}
 
 			long colorId = default;
-			if (!remove && !Parser.TryParseColorId(s[3], true, out colorId))
+			if (!remove && !ParseColorId(s[3], true, out colorId))
 			{
 				return;
 			}
@@ -693,7 +692,7 @@ namespace Sudoku.Bot
 			}
 
 			long colorId = default;
-			if (!remove && !Parser.TryParseColorId(s[3], true, out colorId))
+			if (!remove && !ParseColorId(s[3], true, out colorId))
 			{
 				return;
 			}
@@ -717,7 +716,7 @@ namespace Sudoku.Bot
 			}
 
 			long colorId = default;
-			if (!remove && !Parser.TryParseColorId(s[3], true, out colorId))
+			if (!remove && !ParseColorId(s[3], true, out colorId))
 			{
 				return;
 			}
@@ -883,6 +882,35 @@ namespace Sudoku.Bot
 			_painter = null;
 
 			await e.Reply("清除画板资源成功。");
+		}
+
+		/// <summary>
+		/// 尝试解析字符串，并转换为一个表示颜色的 ID。这个颜色 ID 一般用来封装和保存到 <see cref="DrawingInfo"/>
+		/// 对象里。
+		/// </summary>
+		/// <param name="str">字符串。</param>
+		/// <param name="withTransparency">表示当前解析后的颜色是否保留透明度。</param>
+		/// <param name="colorId">(<see langword="out"/> 参数) 转换后的颜色 ID。</param>
+		/// <returns>表示是否转换成功。</returns>
+		private static bool ParseColorId(string str, bool withTransparency, out long colorId)
+		{
+			switch (str)
+			{
+				case "红色" or "红": colorId = ColorId.ToCustomColorId(withTransparency ? 128 : 255, 235, 0, 0); return true;
+				case "浅红色" or "浅红": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 247, 165, 167); return true;
+				case "橙色" or "橙": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 255, 192, 89); return true;
+				case "浅橙色" or "浅橙": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 247, 222, 143); return true;
+				case "黄色" or "黄": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 255, 255, 150); return true;
+				case "绿色" or "绿": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 134, 242, 128); return true;
+				case "浅绿色" or "浅绿": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 215, 255, 215); return true;
+				case "青色" or "青": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 134, 232, 208); return true;
+				case "浅青色" or "浅青": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 206, 251, 237); return true;
+				case "蓝色" or "蓝": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 0, 0, 255); return true;
+				case "浅蓝色" or "浅蓝": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 127, 187, 255); return true;
+				case "紫色" or "紫": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 177, 165, 243); return true;
+				case "浅紫色" or "浅紫": colorId = ColorId.ToCustomColorId(withTransparency ? 64 : 255, 220, 212, 252); return true;
+				default: return ColorIdParser.TryParse(str, out colorId, withTransparency ? 64 : null, new[] { ',', '，' });
+			}
 		}
 	}
 }
