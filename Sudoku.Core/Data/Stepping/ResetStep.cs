4d07849945b592f06ef1b9701053d848d41b4e92
Sunnie-Shine <HowdySunnie@outlook.com>
2020-12-09 12:59:09 +0800

    Use managed type (IntPtr) instead of unmanaged one (short*) because now C# doesn't support records with properties of pointer types.

--- Sudoku.Core/Data/Stepping/ResetStep.cs
+++ Sudoku.Core/Data/Stepping/ResetStep.cs
@@ -7,6 +7,11 @@ namespace Sudoku.Data.Stepping
 	/// </summary>
 	/// <param name="OldMasks">Indicates the table of new grid masks.</param>
 	/// <param name="NewMasks">Indicates the table of old grid masks.</param>
+	/// <remarks>
+	/// Note that <see langword="record"/>s can't support pointers at present. Therefore,
+	/// I use <see cref="IntPtr"/> instead.
+	/// </remarks>
+	/// <seealso cref="IntPtr"/>
 	public sealed record ResetStep(IntPtr OldMasks, IntPtr NewMasks) : IStep
 	{
 		/// <inheritdoc/>
