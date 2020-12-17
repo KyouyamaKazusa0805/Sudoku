031afe40b9a4649687ba697819b200602fad8827
Sunnie-Shine <HowdySunnie@outlook.com>
2020-12-17 18:04:28 +0800

    Update code.

--- Sudoku.Core/Data/Extensions/RegionLabelEx.cs
+++ Sudoku.Core/Data/Extensions/RegionLabelEx.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
-using Sudoku.Constants;
+using static Sudoku.Data.RegionLabel;
 
 namespace Sudoku.Data.Extensions
 {
@@ -10,6 +10,55 @@ namespace Sudoku.Data.Extensions
 	public static class RegionLabelEx
 	{
 		/// <summary>
+		/// The block table.
+		/// </summary>
+		private static readonly int[] BlockTable =
+		{
+			0, 0, 0, 1, 1, 1, 2, 2, 2,
+			0, 0, 0, 1, 1, 1, 2, 2, 2,
+			0, 0, 0, 1, 1, 1, 2, 2, 2,
+			3, 3, 3, 4, 4, 4, 5, 5, 5,
+			3, 3, 3, 4, 4, 4, 5, 5, 5,
+			3, 3, 3, 4, 4, 4, 5, 5, 5,
+			6, 6, 6, 7, 7, 7, 8, 8, 8,
+			6, 6, 6, 7, 7, 7, 8, 8, 8,
+			6, 6, 6, 7, 7, 7, 8, 8, 8
+		};
+
+		/// <summary>
+		/// The row table.
+		/// </summary>
+		private static readonly int[] RowTable =
+		{
+			9, 9, 9, 9, 9, 9, 9, 9, 9,
+			10, 10, 10, 10, 10, 10, 10, 10, 10,
+			11, 11, 11, 11, 11, 11, 11, 11, 11,
+			12, 12, 12, 12, 12, 12, 12, 12, 12,
+			13, 13, 13, 13, 13, 13, 13, 13, 13,
+			14, 14, 14, 14, 14, 14, 14, 14, 14,
+			15, 15, 15, 15, 15, 15, 15, 15, 15,
+			16, 16, 16, 16, 16, 16, 16, 16, 16,
+			17, 17, 17, 17, 17, 17, 17, 17, 17
+		};
+
+		/// <summary>
+		/// The column table.
+		/// </summary>
+		private static readonly int[] ColumnTable =
+		{
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26,
+			18, 19, 20, 21, 22, 23, 24, 25, 26
+		};
+
+
+		/// <summary>
 		/// Get the region index for the specified cell and the region type.
 		/// </summary>
 		/// <param name="cell">The cell.</param>
@@ -17,14 +66,7 @@ namespace Sudoku.Data.Extensions
 		/// <returns>The region index (<c>0..27</c>).</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int ToRegion(this RegionLabel @this, int cell) =>
-		(
-			@this switch
-			{
-				RegionLabel.Row => Processings.RowTable,
-				RegionLabel.Column => Processings.ColumnTable,
-				RegionLabel.Block => Processings.BlockTable
-			}
-		)[cell];
+			(@this switch { Row => RowTable, Column => ColumnTable, Block => BlockTable })[cell];
 
 #if DOUBLE_LAYERED_ASSUMPTION
 		/// <summary>
@@ -36,9 +78,9 @@ namespace Sudoku.Data.Extensions
 		public static Cause GetRegionCause(this RegionLabel @this) =>
 			@this switch
 			{
-				RegionLabel.Row => Cause.RowHiddenSingle,
-				RegionLabel.Column => Cause.ColumnHiddenSingle,
-				RegionLabel.Block => Cause.BlockHiddenSingle
+				Row => Cause.RowHiddenSingle,
+				Column => Cause.ColumnHiddenSingle,
+				Block => Cause.BlockHiddenSingle
 			};
 #endif
 	}
