2bb6489ee508552353ab92161a0ad3a250f299af
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-02 19:54:50 +0800

    Update code.

--- Sudoku.Recognition/Extensions/RotateRectEx.cs
+++ Sudoku.Recognition/Extensions/RotateRectEx.cs
@@ -14,7 +14,10 @@ namespace Sudoku.Recognition.Extensions
 		/// <param name="this">(<see langword="this"/> parameter) The rectangle.</param>
 		/// <param name="center">(<see langword="out"/> parameter) The center.</param>
 		/// <param name="size">(<see langword="out"/> parameter) The size.</param>
-		public static void Deconstruct(this RotatedRect @this, out PointF center, out SizeF size) =>
-			(center, size) = (@this.Center, @this.Size);
+		public static void Deconstruct(this RotatedRect @this, out PointF center, out SizeF size)
+		{
+			center = @this.Center;
+			size = @this.Size;
+		}
 	}
 }
