cd68359c3ddba5a92b55bc689d59bb881317889a
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-03 16:45:32 +0800

    Remove unused namespace.

--- Sudoku.Solving/Manual/Sdps/GuardianStepSearcher.cs
+++ Sudoku.Solving/Manual/Sdps/GuardianStepSearcher.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Extensions;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Sudoku.Data;
 using Sudoku.Data.Extensions;
 using Sudoku.DocComments;
