8dd49274733f9cfefbdd92b4fe02b994684b1950
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-08 22:25:31 +0800

    Add the handling framework on command lines in WPF project.

---
+++ Sudoku.Windows/App.InternalMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace Sudoku.Windows
+{
+	partial class App
+	{
+		/// <summary>
+		/// Do nothing. This method is only used for a placeholder.
+		/// </summary>
+		/// <returns>Always return <see langword="true"/>.</returns>
+		private static bool DoNothing(string[] _) => true;
+	}
+}
--- Sudoku.Windows/App.xaml.cs
+++ Sudoku.Windows/App.xaml.cs
@@ -2,9 +2,29 @@
 
 namespace Sudoku.Windows
 {
-	/// <summary>Interaction logic for <c>App.xaml</c>.</summary>
-	/// <remarks>Nothing is here.</remarks>
+	/// <summary>
+	/// Interaction logic for <c>App.xaml</c>.
+	/// </summary>
 	public partial class App : Application
 	{
+		/// <inheritdoc/>
+		protected override unsafe void OnStartup(StartupEventArgs e)
+		{
+			if (e is { Args: { Length: not 0 } args })
+			{
+				delegate* managed<string[], bool> method = args[0] switch
+				{
+					_ => &DoNothing
+				};
+
+				method(args[1..]);
+
+				Shutdown();
+			}
+			else
+			{
+				base.OnStartup(e);
+			}
+		}
 	}
 }
