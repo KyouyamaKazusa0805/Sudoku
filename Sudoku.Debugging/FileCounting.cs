33169b3e66eca9d600e6ea1d5827820927664d6d
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-23 17:13:51 +0800

    Move the default operation to a new static class.

---
+++ Sudoku.Debugging/FileCounting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Sudoku.Diagnostics;
+
+namespace Sudoku.Debugging
+{
+	/// <summary>
+	/// Provides the method to create a file counter, and count all files up.
+	/// </summary>
+	internal static class FileCounting
+	{
+		/// <summary>
+		/// To count all files, and output the result using the <see cref="Console"/>.
+		/// </summary>
+		internal static void CountUp() =>
+			Console.WriteLine
+			(
+				value: new FileCounter
+				(
+					root: Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName,
+					extension: "cs", 
+					withBinOrObjDirectory: false
+				).CountUp()
+			);
+	}
+}
