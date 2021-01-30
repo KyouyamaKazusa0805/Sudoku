10e4f525ae3b68470cc4e65bfa3f4bd29d8c3ed7
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-30 14:19:31 +0800

    Update doc comment.

--- Sudoku.UI/Extensions/MessageDialogEx.cs
+++ Sudoku.UI/Extensions/MessageDialogEx.cs
@@ -25,6 +25,11 @@ namespace Sudoku.UI.Extensions
 		/// <param name="this">(<see langword="this"/> parameter) The current dialog instance.</param>
 		/// <param name="command">The command to assign.</param>
 		/// <returns>The instance of <see langword="this"/>.</returns>
+		/// <remarks>
+		/// If you are stuck in the second parameter <paramref name="command"/>, please visit
+		/// <see cref="WithCommand(MessageDialog, string, UICommandInvokedHandler)"/> for more information.
+		/// </remarks>
+		/// <seealso cref="WithCommand(MessageDialog, string, UICommandInvokedHandler)"/>
 		public static MessageDialog WithCommand(this MessageDialog @this, IUICommand command)
 		{
 			@this.Commands.Add(command);
@@ -36,7 +41,19 @@ namespace Sudoku.UI.Extensions
 		/// </summary>
 		/// <param name="this">(<see langword="this"/> parameter) The current dialog instance.</param>
 		/// <param name="label">The label of the new command (displayed as a button-like control).</param>
-		/// <param name="handler">A method that handles the command.</param>
+		/// <param name="handler">
+		/// <para>A method that handles the command.</para>
+		/// <para>
+		/// The example value using lambda:
+		/// <code>
+		/// uiCommand => textBlock.Text = $"You just pressed: {uiCommand.Label}"
+		/// </code>
+		/// or using anonymous function:
+		/// <code>
+		/// deleagte(IUICommand uiCommand) { textBlock.Text = $"You just pressed: {uiCommand.Label}"; }
+		/// </code>
+		/// </para>
+		/// </param>
 		/// <returns>The instance of <see langword="this"/>.</returns>
 		public static MessageDialog WithCommand(
 			this MessageDialog @this, string label, UICommandInvokedHandler handler) =>
