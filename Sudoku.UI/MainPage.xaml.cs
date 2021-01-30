f7ad996e57b316d0941618aea91f36b577260e9e
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-30 11:48:14 +0800

    Add a new project for UWP Application.

---
+++ Sudoku.UI/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace Sudoku.UI
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		/// <summary>
+		/// Initializes a <see cref="MainPage"/> instance using the default behavior.
+		/// </summary>
+		public MainPage() => InitializeComponent();
+
+
+		/// <summary>
+		/// Triggers when a item control of <see cref="NavigationViewMain"/> is invoked.
+		/// </summary>
+		/// <param name="sender">The object to trigger this event.</param>
+		/// <param name="args">The event arguments provided.</param>
+		private void NavigationViewMain_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+		{
+			if (args.IsSettingsInvoked)
+			{
+				// Settings page.
+				//ContentFrame.Navigate(typeof(SettingsPage));
+			}
+			else
+			{
+				// Other pages.
+				var navViews = sender.MenuItems.OfType<NavigationViewItem>();
+				var item = navViews.First(findFirstItem);
+
+				// Then navigate to the specified page.
+				switch (item.Tag)
+				{
+					//case nameof(NaviagtionViewItemSudokuGrid):
+					//{
+					//	ContentFrame.Navigate(typeof(SudokuGridPage));
+					//	break;
+					//}
+					//case nameof(NavigationViewItemInfo):
+					//{
+					//	ContentFrame.Navigate(typeof(InfoPage));
+					//	break;
+					//}
+					case nameof(NavigationViewItemQuit):
+					{
+						break;
+					}
+				}
+			}
+
+			bool findFirstItem(NavigationViewItem x) => (string)x.Content == (string)args.InvokedItem;
+		}
+	}
+}
---
+++ Sudoku.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sunnie's Sudoku Solution")]
+[assembly: AssemblyDescription("This is the main sudoku program on UWP.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sunnie")]
+[assembly: AssemblyProduct("Sudoku.UI")]
+[assembly: AssemblyCopyright("Copyright © Sunnie 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: ComVisible(false)]
