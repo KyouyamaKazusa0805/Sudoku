fd42d3d98b904eda97f5ea5f627d9348ffceb05d
Sunnie-Shine <HowdySunnie@outlook.com>
2021-01-31 09:31:11 +0800

    Create a new project called Sudoku.UI instead of WPF project.

---
+++ Sudoku.UI/Sudoku.UI/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.UI.Xaml;
+using Windows.ApplicationModel;
+
+namespace Sudoku.UI
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public partial class App : Application
+	{
+		/// <summary>
+		/// Indicates the base window.
+		/// </summary>
+		private Window? _window;
+
+
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			InitializeComponent();
+			Suspending += OnSuspending;
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="args">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs args)
+		{
+			_window = new MainWindow();
+			_window.Activate();
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			// Save application state and stop any background activity
+		}
+	}
+}
---
+++ Sudoku.UI/Sudoku.UI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Sudoku.UI
+{
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Initializes a <see cref="MainWindow"/> instance with the default instantiation behavior.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			InitializeTitle();
+		}
+
+
+		/// <summary>
+		/// To initialize the property <see cref="Window.Title"/>.
+		/// </summary>
+		/// <remarks>
+		/// When I filled with this property into the XAML page, it'll give me a complier error
+		/// <c>WMC061: Property not found</c>, so I moved that statement to here.
+		/// </remarks>
+		private void InitializeTitle() => Title = "Sunnie's Sudoku Solution";
+	}
+}
