0eee2db9b4e345e7be0c5fd7b2915d05fb45e65b
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-10 12:04:54 +0800

    Add project Sudoku.UI.

---
+++ Sudoku.UI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Sudoku.UI
+{
+	/// <summary>
+	/// Interaction logic for <see cref="MainWindow"/>.
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Initializes an instance with the normal instantiation behavior.
+		/// </summary>
+		public MainWindow() => InitializeComponent();
+	}
+}
---
+++ Sudoku.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Sudoku.UI.ViewModels
+{
+	/// <summary>
+	/// The view model that is bound by <see cref="MainWindow"/>.
+	/// </summary>
+	public sealed class MainWindowViewModel : INotifyPropertyChanged
+	{
+		/// <inheritdoc/>
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
