747925346d5661bdd1b377e5ddc21c5e414215d5
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-10 18:46:28 +0800

    Add tool tips.

--- Sudoku.UI/MainWindow.xaml.cs
+++ Sudoku.UI/MainWindow.xaml.cs
@@ -11,5 +11,13 @@ namespace Sudoku.UI
 		/// Initializes an instance with the normal instantiation behavior.
 		/// </summary>
 		public MainWindow() => InitializeComponent();
+
+
+		/// <summary>
+		/// Triggers when the quit command is triggered.
+		/// </summary>
+		/// <param name="sender">The object to trigger the event.</param>
+		/// <param name="e">The event arguments provided.</param>
+		private void ButtonQuit_Click(object sender, RoutedEventArgs e) => Close();
 	}
 }
