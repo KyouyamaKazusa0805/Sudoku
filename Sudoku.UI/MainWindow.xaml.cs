8e239e6d39e36dd683cbdcecdef4160c1112e619
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-12 10:36:10 +0800

    Add Sudoku.UI.

---
+++ Sudoku.UI/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Sudoku.UI
+{
+	/// <summary>
+	/// Interaction logic for <see cref="MainWindow"/>.
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Initializes a <see cref="MainWindow"/> instance with the default instantiation behavior.
+		/// </summary>
+		public MainWindow() => InitializeComponent();
+
+
+
+	}
+}
---
+++ Sudoku.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Sudoku.UI.ViewModels
+{
+	/// <summary>
+	/// The view model of the window <see cref="MainWindow"/>.
+	/// </summary>
+	/// <seealso cref="MainWindow"/>
+	public sealed class MainWindowViewModel : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		/// <inheritdoc/>
+		public event PropertyChangingEventHandler? PropertyChanging;
+
+		/// <inheritdoc/>
+		public event PropertyChangedEventHandler? PropertyChanged;
+	}
+}
