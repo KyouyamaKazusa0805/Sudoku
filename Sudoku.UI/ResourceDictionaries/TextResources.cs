f28826eea43d76104809329517c5c5288adceecf
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-13 16:06:36 +0800

    Update code.

---
+++ Sudoku.UI/ResourceDictionaries/TextResources.cs
@@ -0,0 +1,63 @@
+﻿using System.Dynamic;
+using System.Windows;
+using Sudoku.DocComments;
+
+namespace Sudoku.UI.ResourceDictionaries
+{
+	/// <summary>
+	/// Provides a way to get the local text resources, and save the resources to the local path.
+	/// </summary>
+	public sealed class TextResources : DynamicObject
+	{
+		/// <summary>
+		/// Indicates the singleton to get items and method invokes.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// This is a <see langword="dynamic"/> instance, which means you can get anything you want
+		/// using the following code style to get the items in this class:
+		/// <list type="bullet">
+		/// <item><c>Current.PropertyName</c> (Property invokes)</item>
+		/// <item><c>Current[PropertyIndex]</c> (Indexer invokes)</item>
+		/// </list>
+		/// </para>
+		/// <para>
+		/// For example,
+		/// if you want to get the <see cref="string"/> value from the key <c>"Bug"</c>, now you may
+		/// write <c>Current.Bug</c> or <c>Current["Bug"]</c> to get that value.
+		/// </para>
+		/// </remarks>
+		public static readonly dynamic CurrentApp = new TextResources();
+
+
+		/// <see cref="DefaultConstructor"/>
+		private TextResources()
+		{
+		}
+
+
+		/// <inheritdoc/>
+		public override bool TryGetMember(GetMemberBinder binder, out object? result) =>
+			(result = TryGetValue(binder.Name)) is not null;
+
+		/// <inheritdoc/>
+		public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object? result)
+		{
+			if (indexes is null || indexes.Length == 0 || indexes[0] is not string index)
+			{
+				result = null;
+				return false;
+			}
+
+			return (result = TryGetValue(index)) is not null;
+		}
+
+
+		/// <summary>
+		/// Try to get the inner value.
+		/// </summary>
+		/// <param name="index">The index that you want to get. The index is a <see cref="string"/> key.</param>
+		/// <returns>The result.</returns>
+		private static object? TryGetValue(string index) => Application.Current.Resources[index];
+	}
+}
---
+++ Sudoku.UI/ViewModels/SudokuPanelViewModel.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Sudoku.Data;
+using Sudoku.Drawing;
+using Sudoku.UI.Controls;
+using Sudoku.UI.ResourceDictionaries;
+
+namespace Sudoku.UI.ViewModels
+{
+	/// <summary>
+	/// Indicates the view model bound by <see cref="SudokuPanel"/>.
+	/// </summary>
+	/// <seealso cref="SudokuPanel"/>
+	public sealed class SudokuPanelViewModel : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Indicates the inner generator.
+		/// </summary>
+		private GridImageGenerator _generator = new(
+			new(
+				(float)(double)TextResources.CurrentApp.DefaultSudokuGridRenderingSize,
+				(float)(double)TextResources.CurrentApp.DefaultSudokuGridRenderingSize
+			),
+			new()
+		);
+
+
+		/// <inheritdoc/>
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+
+		/// <summary>
+		/// Gets or sets the focused cells.
+		/// </summary>
+		/// <value>The focused cells.</value>
+		public Cells FocusedCells
+		{
+			get => _generator.FocusedCells;
+
+			set
+			{
+				_generator.FocusedCells = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(FocusedCells)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the grid.
+		/// </summary>
+		/// <value>The grid.</value>
+		public SudokuGrid Grid
+		{
+			get => _generator.Grid;
+
+			set
+			{
+				_generator.Grid = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(Grid)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the converter.
+		/// </summary>
+		/// <value>The converter.</value>
+		public DrawingPointConverter Converter
+		{
+			get => _generator.Converter;
+
+			set
+			{
+				_generator = new(value, _generator.Preferences);
+
+				PropertyChanged?.Invoke(this, new(nameof(Converter)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the preferences.
+		/// </summary>
+		/// <value>The preferences.</value>
+		public Settings Preferences
+		{
+			get => _generator.Preferences;
+
+			set
+			{
+				_generator = new(_generator.Converter, value);
+
+				PropertyChanged?.Invoke(this, new(nameof(Preferences)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the view.
+		/// </summary>
+		/// <value>The view.</value>
+		public PresentationData? View
+		{
+			get => _generator.View;
+
+			set
+			{
+				_generator.View = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(View)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the custom view.
+		/// </summary>
+		/// <value>The custom view.</value>
+		public PresentationData? CustomView
+		{
+			get => _generator.CustomView;
+
+			set
+			{
+				_generator.CustomView = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(CustomView)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the generator.
+		/// </summary>
+		/// <value>The generator.</value>
+		public GridImageGenerator Generator
+		{
+			get => _generator;
+
+			set
+			{
+				_generator = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(Generator)));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the conclusions.
+		/// </summary>
+		/// <value>The conclusions.</value>
+		public IEnumerable<Conclusion>? Conclusions
+		{
+			get => _generator.Conclusions;
+
+			set
+			{
+				_generator.Conclusions = value;
+
+				PropertyChanged?.Invoke(this, new(nameof(Conclusions)));
+			}
+		}
+	}
+}
