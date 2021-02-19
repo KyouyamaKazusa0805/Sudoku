355097c6359b1d7d72d153f81464ce00887f36ac
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-19 10:43:12 +0800

    Remove unused constant.

--- Sudoku.Core/Resources/Paths.cs
+++ Sudoku.Core/Resources/Paths.cs
@@ -19,21 +19,5 @@
 		/// Indicates the language source dictionary (Chinese).
 		/// </summary>
 		public const string LangSourceZhCn = @"lang\Resources.zh-cn.dic";
-
-		/// <summary>
-		/// The appropriate file path that can store configuration file.
-		/// </summary>
-		/// <remarks>
-		/// <para>
-		/// Please note that the return value is a path, but this path may not exist in the hard drive.
-		/// You can use the code like <c>Directory.Create(path)</c> to create the path.
-		/// </para>
-		/// <para>
-		/// The extension name <c>ssss</c> is the abbreviation of
-		/// <c><b>S</b>unnie's <b>s</b>udoku <b>s</b>olution <b>s</b>ettings</c>.
-		/// </para>
-		/// <para>The program is only running on Windows 10, so the path can be fixed.</para>
-		/// </remarks>
-		public const string ConfigurationFile = @"config.ssss";
 	}
 }
