f94b605d9799331f4233bdf94a7ada6f256b0ecc
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-21 21:17:19 +0800

    Update link.

--- Sudoku.CodeAnalysis/DiagnosticIds.cs
+++ Sudoku.CodeAnalysis/DiagnosticIds.cs
@@ -25,7 +25,7 @@
 		public const string Sudoku004 = "https://gitee.com/SunnieShine/Sudoku/wikis/SUDOKU004%EF%BC%9A%E4%BB%8E%20StepSearcher%20%E6%B4%BE%E7%94%9F%E7%9A%84%E7%B1%BB%E5%BF%85%E9%A1%BB%E6%8B%A5%E6%9C%89%20Properties%20%E5%B1%9E%E6%80%A7?sort_id=3599824";
 		public const string Sudoku005 = "https://gitee.com/SunnieShine/Sudoku/wikis/SUDOKU005%EF%BC%9AProperties%20%E5%B1%9E%E6%80%A7%E4%B8%8D%E5%8F%AF%E4%B8%BA%20null?sort_id=3599826";
 		public const string Sudoku006 = "https://gitee.com/SunnieShine/Sudoku/wikis/SUDOKU006%EF%BC%9AProperties%20%E5%B1%9E%E6%80%A7%E9%9C%80%E8%A6%81%E6%9C%89%E9%BB%98%E8%AE%A4%E5%80%BC?sort_id=3602787";
-		public const string Sudoku007 = "https://gitee.com/SunnieShine/Sudoku/wikis/SUDOKU007%EF%BC%9A%E4%B8%8D%E8%83%BD%E4%B8%BA%20Properties%20%E5%B1%9E%E6%80%A7%E5%8F%AA%E8%83%BD%E4%BB%A5%20new%20%E8%AF%AD%E5%8F%A5%E8%BF%9B%E8%A1%8C%E5%88%9D%E5%A7%8B%E5%8C%96?sort_id=3607697";
+		public const string Sudoku007 = "https://gitee.com/SunnieShine/Sudoku/wikis/SUDOKU007%EF%BC%9AProperties%20%E5%B1%9E%E6%80%A7%E5%8F%AA%E8%83%BD%E4%BB%A5%20new%20%E8%AF%AD%E5%8F%A5%E8%BF%9B%E8%A1%8C%E5%88%9D%E5%A7%8B%E5%8C%96?sort_id=3607697";
 	}
 
 	/// <summary>
