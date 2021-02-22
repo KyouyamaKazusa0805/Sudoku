907120ddc9aaee3f1f202db93786c5a7dd82c1b4
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-22 14:30:16 +0800

    Implement the inner syntax node walker.

---
+++ Sudoku.CodeAnalysis/DynamicallyInvocationOfCurrentAnalyzer.DynamicallyInvocationSearcher.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Sudoku.CodeAnalysis
+{
+	public sealed partial class DynamicallyInvocationOfCurrentAnalyzer
+	{
+		/// <summary>
+		/// Indicates the searcher that stores the syntax nodes of the dynamically invocation of
+		/// that <see langword="dynamic"/> field <c>Current</c>.
+		/// </summary>
+		private sealed class DynamicallyInvocationSearcher : CSharpSyntaxWalker
+		{
+			/// <summary>
+			/// Indicates the semantic model.
+			/// </summary>
+			private readonly SemanticModel _semanticModel;
+
+
+			/// <summary>
+			/// Initializes an instance with the specified semantic model.
+			/// </summary>
+			/// <param name="semanticModel">The semantic model.</param>
+			public DynamicallyInvocationSearcher(SemanticModel semanticModel) => _semanticModel = semanticModel;
+
+
+			/// <summary>
+			/// Indicates the valid collection of the <c>Current</c> dynamically invocation.
+			/// </summary>
+			public IList<(InvocationExpressionSyntax Node, string MethodName, ArgumentListSyntax ArgumentsNode)>? Collection { get; private set; }
+
+
+			/// <inheritdoc/>
+			public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+			{
+				if
+				(
+					_semanticModel.GetOperation(node) is not IDynamicInvocationOperation
+					{
+						Kind: OperationKind.DynamicInvocation
+					} operation
+				)
+				{
+					return;
+				}
+
+				if
+				(
+					node is not
+					{
+						Expression: MemberAccessExpressionSyntax
+						{
+							Expression: MemberAccessExpressionSyntax
+							{
+								Expression: IdentifierNameSyntax
+								{
+									Identifier: { ValueText: TextResourcesClassName }
+								},
+								Name: IdentifierNameSyntax
+								{
+									Identifier: { ValueText: TextResourcesStaticReadOnlyFieldName }
+								}
+							},
+							Name: IdentifierNameSyntax
+							{
+								Identifier: { ValueText: var methodName }
+							}
+						},
+						ArgumentList: var argList
+					}
+				)
+				{
+					return;
+				}
+
+				Collection ??= new List<(InvocationExpressionSyntax, string, ArgumentListSyntax)>();
+
+				Collection.Add((node, methodName, argList));
+			}
+		}
+	}
+}
---
+++ Sudoku.CodeAnalysis/DynamicallyInvocationOfCurrentAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Sudoku.CodeAnalysis
+{
+	/// <summary>
+	/// Indicates the analyzer that checks the dynamically invocation of the <see langword="dynamic"/>
+	/// field <c>TextResources.Current</c>.
+	/// </summary>
+	[Generator]
+	public sealed partial class DynamicallyInvocationOfCurrentAnalyzer : ISourceGenerator
+	{
+		/// <summary>
+		/// Indicates the text resources class name.
+		/// </summary>
+		private const string TextResourcesClassName = "TextResources";
+
+		/// <summary>
+		/// Indicates that field dynamically bound.
+		/// </summary>
+		private const string TextResourcesStaticReadOnlyFieldName = "Current";
+
+
+		/// <inheritdoc/>
+		public void Execute(GeneratorExecutionContext context)
+		{
+			var compilation = context.Compilation;
+			if (compilation.AssemblyName is "Sudoku.UI" or "Sudoku.Windows")
+			{
+				// We don't check on those two WPF projects, because those two projects has already used
+				// their own resource dictionary (MergedDictionary).
+				return;
+			}
+
+			foreach (var syntaxTree in compilation.SyntaxTrees)
+			{
+				// Check whether the syntax contains the root node.
+				if (!syntaxTree.TryGetRoot(out var root))
+				{
+					continue;
+				}
+
+				// Create the semantic model and the property list.
+				var semanticModel = compilation.GetSemanticModel(syntaxTree);
+				var collector = new DynamicallyInvocationSearcher(semanticModel);
+				collector.Visit(root);
+
+				// If the syntax tree doesn't contain any dynamically called clause,
+				// just skip it.
+				if (collector.Collection is null)
+				{
+					continue;
+				}
+
+				// Iterate on each dynamically called location.
+				foreach (var (node, methodName, argNodes) in collector.Collection)
+				{
+
+				}
+			}
+		}
+
+		/// <inheritdoc/>
+		public void Initialize(GeneratorInitializationContext context)
+		{
+		}
+	}
+}
