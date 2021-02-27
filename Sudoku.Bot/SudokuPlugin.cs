62912efa5349ee3776a88dc3e23ddd3c2ff3718a
Sunnie-Shine <HowdySunnie@outlook.com>
2021-02-27 17:28:41 +0800

    Remove the functions which aren't relative to sudoku.

--- Sudoku.Bot/SudokuPlugin.cs
+++ Sudoku.Bot/SudokuPlugin.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -26,13 +25,6 @@ namespace Sudoku.Bot
 		/// </summary>
 		private static readonly string Desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-#if DEBUG && TEST
-		/// <summary>
-		/// Indicates the required directory.
-		/// </summary>
-		private static readonly DirectoryInfo VoicesDirectory = new(@"..\..\..\..\required\bot\Voices");
-#endif
-
 		/// <summary>
 		/// Indicates the command hanlder.
 		/// </summary>
@@ -54,19 +46,7 @@ namespace Sudoku.Bot
 			new CommandRouter(GenerateEmptyAsync, (string)X.CommandGenerateEmpty) +
 			new CommandRouter(CleanAsync, (string)X.CommandClean) +
 			new CommandRouter(AboutAsync, (string)X.CommandAbout) +
-			new CommandRouter(ExtractPuzzleAsync, (string)X.CommandExtractPuzzle) +
-			new CommandRouter(JinxAsync, (string)X.CommandJinx) +
-			new CommandRouter(UnjinxAsync, (string)X.CommandUnjinx) +
-			new CommandRouter(SetTitleAsync, (string)X.CommandSetTitle) +
-			new ComplexCommandRouter(
-				RemoveTitleAsync,
-				(string)X.CommandRemoveTitle1, (string)X.CommandRemoveTitle2,
-				(string)X.CommandRemoveTitle3, (string)X.CommandRemoveTitle4
-			)
-#if DEBUG && TEST
-			+ new CommandRouter(PlayMusicAsync, (string)X.CommandPlayMusic)
-#endif
-			;
+			new CommandRouter(ExtractPuzzleAsync, (string)X.CommandExtractPuzzle);
 
 
 		/// <summary>
@@ -151,86 +131,6 @@ namespace Sudoku.Bot
 			await Handler.HandleAsync(args[0], args, sender, e);
 		}
 
-		/// <summary>
-		/// Try to jinx or unjinx someone.
-		/// </summary>
-		/// <param name="args">The arguments.</param>
-		/// <param name="e">The event arguments.</param>
-		/// <param name="jinx">Indicates whether the mode is to jinx somebody.</param>
-		/// <returns>The task with the result value of type <see cref="bool"/>.</returns>
-		private static async Task<bool> TryJinxOrUnjinxAsync(
-			string[] args, GroupMessageReceivedEventArgs e, bool jinx)
-		{
-			var member = await GetMemberAsync(args[1], e);
-			if (member is null)
-			{
-				return false;
-			}
-
-			if (jinx)
-			{
-				var timeSpan = TimeSpan.Zero;
-				for (int index = 3; index < args.Length - 1; index += 2)
-				{
-					if (!uint.TryParse(args[index], out uint current) || index + 1 >= args.Length)
-					{
-						return false;
-					}
-
-					string unit = args[index + 1];
-
-					TimeSpan currentUnit;
-					if (unit == X.SettingsTextWeek) currentUnit = TimeSpan.FromDays(current);
-					else if (unit == X.SettingsTextDay) currentUnit = TimeSpan.FromDays(7 * current);
-					else if (unit == X.SettingsTextHour) currentUnit = TimeSpan.FromHours(current);
-					else if (unit == X.SettingsTextMinute) currentUnit = TimeSpan.FromMinutes(current);
-					else if (unit == X.SettingsTextSecond) currentUnit = TimeSpan.FromSeconds(current);
-					else return false;
-
-					timeSpan += currentUnit;
-				}
-
-				try
-				{
-					await member.MuteAsync(timeSpan);
-					return true;
-				}
-				catch (ApiException ex) when (ex.ErrorCode == 10)
-				{
-					await e.Source.SendAsync((string)X.CommandValueJinxFailed2);
-					return false;
-				}
-			}
-			else
-			{
-				try
-				{
-					await member.UnmuteAsync();
-					return true;
-				}
-				catch (ApiException ex) when (ex.ErrorCode == 10)
-				{
-					await e.Source.SendAsync((string)X.CommandValueUnjinxFailed2);
-					return false;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Try to get member information via his/her ID (number or nickname).
-		/// </summary>
-		/// <param name="name">The name.</param>
-		/// <param name="e">The event arguments.</param>
-		/// <returns>The member information.</returns>
-		private static async Task<Member?> GetMemberAsync(string name, GroupMessageReceivedEventArgs e) =>
-		(
-			from m in await e.Source.GetMembersAsync()
-			where string.Equals(m.Alias, name, StringComparison.OrdinalIgnoreCase)
-				|| string.Equals(m.DisplayName, name, StringComparison.OrdinalIgnoreCase)
-				|| m.Number.ToString() == name
-			select m
-		).FirstOrDefault();
-
 
 		private static partial Task HelpAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
 		private static partial Task AnalyzeAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
@@ -239,12 +139,5 @@ namespace Sudoku.Bot
 		private static partial Task CleanAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
 		private static partial Task AboutAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
 		private static partial Task ExtractPuzzleAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-		private static partial Task JinxAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-		private static partial Task UnjinxAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-		private static partial Task SetTitleAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-		private static partial Task RemoveTitleAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-#if DEBUG && TEST
-		private static partial Task PlayMusicAsync(string[] args, Session sender, GroupMessageReceivedEventArgs e);
-#endif
 	}
 }
