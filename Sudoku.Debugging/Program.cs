94307e8b4019a4c901cf7554e195f9d8b36769a3
SunnieShine <HowdySunnie@outlook.com>
2021-03-05 15:42:42 +0800

    Remove and update debugging project code.

--- Sudoku.Debugging/Program.cs
+++ Sudoku.Debugging/Program.cs
@@ -1,17 +1,3 @@
-﻿#pragma warning disable 219
+﻿using System;
 
-using Sudoku.Data;
-
-var grid = new SudokuGrid(); // SUDOKU021.
-var cells = new Cells(); // SUDOKU021.
-var cells2 = new Cells { 3, 10, 40 };
-var candidates = new Candidates(); // SUDOKU021.
-var candidates2 = new Candidates { 3, 10, 40 };
-SudokuGrid grid2 = new(); // SUDOKU021.
-Cells cells3 = new(); // SUDOKU021.
-Candidates candidates3 = new(); // SUDOKU021.
-Candidates candidates4 = new() { 1, 10 };
-SudokuGrid grid3 = default; // SUDOKU021.
-var grid4 = default(SudokuGrid); // SUDOKU021.
-var candidates5 = default(Candidates); // SUDOKU021.
-Candidates candidates6 = default; // SUDOKU021.
+Console.WriteLine("Hello");
