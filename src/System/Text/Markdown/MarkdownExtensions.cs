9fe954e5f6d380f3fe8e1f937ec168d973d5f7a5
SunnieShine <HowdySunnie@outlook.com>
2021-04-02 20:39:37 +0800

    Add overload methods on ValueStringBuilder.

--- src/System/Text/Markdown/MarkdownExtensions.cs
+++ src/System/Text/Markdown/MarkdownExtensions.cs
@@ -19,6 +19,211 @@ namespace System.Text.Markdown
 		/// <exception cref="FormatException">
 		/// Throws when the <paramref name="text"/> is <see langword="null"/>, empty or whitespaces.
 		/// </exception>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendText(this ref ValueStringBuilder @this, string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+			{
+				throw new FormatException("The text shouldn't be an empty string, whitespaces or null.");
+			}
+
+			@this.Append(text);
+		}
+
+		/// <summary>
+		/// Append a paragraph with a new line.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="text">The text.</param>
+		/// <returns>The current instance.</returns>
+		/// <exception cref="FormatException">
+		/// Throws when the <paramref name="text"/> is <see langword="null"/>, empty or whitespaces.
+		/// </exception>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendParagraph(this ref ValueStringBuilder @this, string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+			{
+				throw new FormatException("The text shouldn't be an empty string, whitespaces or null.");
+			}
+
+			@this.Append(text);
+			@this.AppendLine();
+			@this.AppendLine();
+		}
+
+		/// <summary>
+		/// Append inline code block.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="text">The inner text.</param>
+		/// <param name="appendPaddingSpaces">
+		/// Indicates whether the block will append padding spaces surrounded
+		/// the block. If <see langword="true"/>,
+		/// the method will append a trailing space and a leading space to the block.
+		/// The default value is <see langword="false"/>.
+		/// </param>
+		/// <returns>The current instance.</returns>
+		/// <exception cref="FormatException">
+		/// Throws when the <paramref name="text"/> contains double tilde mark <c>"``"</c>.
+		/// </exception>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendInlineCodeBlock(
+			this ref ValueStringBuilder @this, string text, bool appendPaddingSpaces)
+		{
+			if (text.Contains(MarkdownSymbols.InlineCodeBlockStartDouble))
+			{
+				throw new FormatException(
+					$"The text can't contain double tilde mark \"{MarkdownSymbols.InlineCodeBlockStartDouble}\"."
+				);
+			}
+
+			switch ((text.Contains(MarkdownSymbols.InlineCodeBlockStartSingle), appendPaddingSpaces))
+			{
+				case (true, true):
+				{
+					@this.Append(' ');
+					@this.Append(MarkdownSymbols.InlineCodeBlockStartDouble);
+					@this.Append(text);
+					@this.Append(MarkdownSymbols.InlineCodeBlockEndDouble);
+					@this.Append(' ');
+
+					break;
+				}
+				case (true, false):
+				{
+					@this.Append(MarkdownSymbols.InlineCodeBlockStartDouble);
+					@this.Append(text);
+					@this.Append(MarkdownSymbols.InlineCodeBlockEndDouble);
+
+					break;
+				}
+				case (false, true):
+				{
+					@this.Append(' ');
+					@this.Append(MarkdownSymbols.InlineCodeBlockStartSingle);
+					@this.Append(text);
+					@this.Append(MarkdownSymbols.InlineCodeBlockEndSingle);
+					@this.Append(' ');
+
+					break;
+				}
+				default:
+				{
+					@this.Append(MarkdownSymbols.InlineCodeBlockStartSingle);
+					@this.Append(text);
+					@this.Append(MarkdownSymbols.InlineCodeBlockEndSingle);
+
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Append the code block.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="code">The inner code.</param>
+		/// <param name="codelang">The code language.</param>
+		/// <returns>The current instance.</returns>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendCodeBlock(this ref ValueStringBuilder @this, string code, string? codelang)
+		{
+			@this.Append(MarkdownSymbols.CodeBlockStart);
+			@this.Append(codelang ?? string.Empty);
+			@this.AppendLine();
+			@this.Append(code);
+			@this.AppendLine();
+			@this.Append(MarkdownSymbols.CodeBlockEnd);
+			@this.AppendLine();
+			@this.AppendLine();
+		}
+
+		/// <summary>
+		/// Append header text.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="text">The inner text.</param>
+		/// <returns>The current instance.</returns>
+		/// <exception cref="FormatException">
+		/// Throws when the <paramref name="text"/> is <see langword="null"/>, empty
+		/// or only contains whitespace characters.
+		/// </exception>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendHeaderText(this ref ValueStringBuilder @this, string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+			{
+				throw new FormatException("The text shouldn't be an empty string, whitespaces or null.");
+			}
+
+			@this.Append(MarkdownSymbols.H1);
+			@this.Append(text);
+			@this.AppendLine();
+			@this.AppendLine();
+		}
+
+		/// <summary>
+		/// Append header text, with the specified header level.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="level">The header level. The value should be between 1 and 6.</param>
+		/// <param name="text">The inner text.</param>
+		/// <returns>The current instance.</returns>
+		/// <exception cref="FormatException">
+		/// Throws when the <paramref name="text"/> is <see langword="null"/>, empty
+		/// or only contains whitespace characters.
+		/// </exception>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendHeaderText(this ref ValueStringBuilder @this, int level, string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+			{
+				throw new FormatException("The text shouldn't be an empty string, whitespaces or null.");
+			}
+
+			@this.Append(level switch
+			{
+				1 => MarkdownSymbols.H1,
+				2 => MarkdownSymbols.H2,
+				3 => MarkdownSymbols.H3,
+				4 => MarkdownSymbols.H4,
+				5 => MarkdownSymbols.H5,
+				6 => MarkdownSymbols.H6
+			});
+			@this.Append(text);
+			@this.AppendLine();
+			@this.AppendLine();
+		}
+
+		/// <summary>
+		/// Append a new line.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <returns>The current instance.</returns>
+		[CLSCompliant(false)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendNewLine(this ref ValueStringBuilder @this)
+		{
+			@this.AppendLine();
+			@this.AppendLine();
+		}
+
+		/// <summary>
+		/// Append plain text.
+		/// </summary>
+		/// <param name="this">The current instance.</param>
+		/// <param name="text">The text.</param>
+		/// <returns>The current instance.</returns>
+		/// <exception cref="FormatException">
+		/// Throws when the <paramref name="text"/> is <see langword="null"/>, empty or whitespaces.
+		/// </exception>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static StringBuilder AppendText(this StringBuilder @this, string text) =>
 			string.IsNullOrWhiteSpace(text)
@@ -56,7 +261,8 @@ namespace System.Text.Markdown
 		/// Throws when the <paramref name="text"/> contains double tilde mark <c>"``"</c>.
 		/// </exception>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static StringBuilder AppendInlineCodeBlock(this StringBuilder @this, string text, bool appendPaddingSpaces) =>
+		public static StringBuilder AppendInlineCodeBlock(
+			this StringBuilder @this, string text, bool appendPaddingSpaces) =>
 			text.Contains(MarkdownSymbols.InlineCodeBlockStartDouble)
 			? throw new FormatException(
 				$"The text can't contain double tilde mark \"{MarkdownSymbols.InlineCodeBlockStartDouble}\"."
