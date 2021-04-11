1fd089fb22d5279465c642a42f1c870d30fdcf10
SunnieShine <HowdySunnie@outlook.com>
2021-04-11 09:28:44 +0800

    Add TypeInfo.

---
+++ src/Sudoku.XmlDocs/MemberInfo/TypeInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Sudoku.XmlDocs.MemberInfo
+{
+	/// <summary>
+	/// Indicates the information to introduce a type.
+	/// </summary>
+	/// <param name="Summary">Indicates the "summary" block in the documentation comments.</param>
+	/// <param name="Remarks">Indicates the "remarks" block in the documentation comments.</param>
+	/// <param name="TypeParameters">Indicates the "typeparam" block in the documentation comments.</param>
+	/// <param name="Example">Indicates the "example" block in the documentation comments.</param>
+	/// <param name="Exceptions">Indicates the "exception" block in the documentation comments.</param>
+	/// <param name="SeeAlsos">Indicates the "seealso" block in the documentation comments.</param>
+	public sealed record TypeInfo(
+		string Summary,
+		string Remarks,
+		(string TypeParameter, string? Description)[]? TypeParameters,
+		string Example,
+		(string ExceptionName, string? Description)[]? Exceptions,
+		string[]? SeeAlsos
+	);
+}
