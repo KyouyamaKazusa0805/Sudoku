023d8daf0bd80162a1e72b8b92a10bead8f24a9a
SunnieShine <HowdySunnie@outlook.com>
2021-04-15 20:57:54 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,20 +1,3 @@
 ﻿using System;
-using Sudoku.CodeGen.Annotations;
 
-// May be unmeaningful.
 Console.WriteLine("Hello, world!");
-
-namespace Sudoku.Test
-{
-	[AutoGeneratePrimaryConstructor]
-	internal sealed partial class TestClass
-	{
-		private readonly int _field1;
-		private readonly long _field2;
-		private readonly string? _field3;
-
-		public static readonly int Field4 = default;
-
-		public const string Field5 = "Hello, world!";
-	}
-}
