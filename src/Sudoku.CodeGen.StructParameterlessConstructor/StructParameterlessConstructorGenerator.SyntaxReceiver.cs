c5b7445094388f0ac0b7bd9ffabf74b682c84008
SunnieShine <HowdySunnie@outlook.com>
2021-04-16 19:43:31 +0800

    Append handling code.

---
+++ src/Sudoku.CodeGen.StructParameterlessConstructor/StructParameterlessConstructorGenerator.SyntaxReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Sudoku.CodeGen.StructParameterlessConstructor
+{
+	partial class StructParameterlessConstructorGenerator
+	{
+		/// <summary>
+		/// Defines the syntax receiver.
+		/// </summary>
+		private sealed class SyntaxReceiver : ISyntaxReceiver
+		{
+			/// <summary>
+			/// Indicates all possible candidate <see langword="class"/>es used.
+			/// </summary>
+			public IList<ClassDeclarationSyntax> CandidateStructs { get; } = new List<ClassDeclarationSyntax>();
+
+
+			/// <inheritdoc/>
+			public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+			{
+				// Any field with at least one attribute is a candidate for property generation.
+				if (syntaxNode is ClassDeclarationSyntax { AttributeLists: { Count: not 0 } } classDeclaration)
+				{
+					CandidateStructs.Add(classDeclaration);
+				}
+			}
+		}
+	}
+}
--- src/Sudoku.CodeGen.StructParameterlessConstructor/StructParameterlessConstructorGenerator.cs
+++ src/Sudoku.CodeGen.StructParameterlessConstructor/StructParameterlessConstructorGenerator.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.CodeAnalysis;
+﻿#pragma warning disable IDE0057
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Sudoku.CodeGen.StructParameterlessConstructor.Annotations;
+using Sudoku.CodeGen.StructParameterlessConstructor.Extensions;
+using GenericsOptions = Microsoft.CodeAnalysis.SymbolDisplayGenericsOptions;
+using GlobalNamespaceStyle = Microsoft.CodeAnalysis.SymbolDisplayGlobalNamespaceStyle;
+using MiscellaneousOptions = Microsoft.CodeAnalysis.SymbolDisplayMiscellaneousOptions;
+using TypeQualificationStyle = Microsoft.CodeAnalysis.SymbolDisplayTypeQualificationStyle;
 
 namespace Sudoku.CodeGen.StructParameterlessConstructor
 {
@@ -13,16 +24,118 @@ namespace Sudoku.CodeGen.StructParameterlessConstructor
 	/// (e.g. <see langword="default"/>(<see langword="int"/>)).
 	/// </remarks>
 	[Generator]
-	public sealed class StructParameterlessConstructorGenerator : ISourceGenerator
+	public sealed partial class StructParameterlessConstructorGenerator : ISourceGenerator
 	{
+		/// <summary>
+		/// Indicates whether the project uses tabs <c>'\t'</c> as indenting characters.
+		/// </summary>
+		private static readonly bool UsingTabsAsIndentingCharacters = true;
+
+		/// <summary>
+		/// Indicates the type format, and the property type format.
+		/// </summary>
+		private static readonly SymbolDisplayFormat
+			TypeFormat = new(
+				globalNamespaceStyle: GlobalNamespaceStyle.OmittedAsContaining,
+				typeQualificationStyle: TypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+				genericsOptions: GenericsOptions.IncludeTypeParameters | GenericsOptions.IncludeTypeConstraints,
+				miscellaneousOptions:
+					MiscellaneousOptions.UseSpecialTypes
+					| MiscellaneousOptions.EscapeKeywordIdentifiers
+					| MiscellaneousOptions.IncludeNullableReferenceTypeModifier
+			),
+			PropertyTypeFormat = new(
+				globalNamespaceStyle: GlobalNamespaceStyle.OmittedAsContaining,
+				typeQualificationStyle: TypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+				genericsOptions: GenericsOptions.IncludeTypeParameters,
+				miscellaneousOptions:
+					MiscellaneousOptions.UseSpecialTypes
+					| MiscellaneousOptions.EscapeKeywordIdentifiers
+					| MiscellaneousOptions.IncludeNullableReferenceTypeModifier
+			);
+
+
 		/// <inheritdoc/>
 		public void Execute(GeneratorExecutionContext context)
 		{
+			if (context.SyntaxReceiver is not SyntaxReceiver receiver)
+			{
+				return;
+			}
+
+			var classNameDic = new Dictionary<string, int>();
+			foreach (var classSymbol in g(context, receiver))
+			{
+				_ = classNameDic.TryGetValue(classSymbol.Name, out int i);
+				var name = i == 0 ? classSymbol.Name : $"{classSymbol.Name}{i + 1}";
+				classNameDic[classSymbol.Name] = i + 1;
+				context.AddSource($"{name}.PrimaryConstructor.g.cs", getParameterlessCtorCode(classSymbol));
+			}
+
+
+			static IEnumerable<INamedTypeSymbol> g(GeneratorExecutionContext context, SyntaxReceiver receiver)
+			{
+				var compilation = context.Compilation;
+
+				return from candidateStruct in receiver.CandidateStructs
+					   let model = compilation.GetSemanticModel(candidateStruct.SyntaxTree)
+					   select model.GetDeclaredSymbol(candidateStruct)! into structSymbol
+					   where structSymbol.Marks<DisallowParameterlessConstructorAttribute>()
+					   select (INamedTypeSymbol)structSymbol;
+			}
+
+			static string getParameterlessCtorCode(INamedTypeSymbol structSymbol)
+			{
+				string namespaceName = structSymbol.ContainingNamespace.ToDisplayString();
+				string fullTypeName = structSymbol.ToDisplayString(TypeFormat);
+				int i = fullTypeName.IndexOf('<');
+				string genericParametersList = i == -1 ? string.Empty : fullTypeName.Substring(i);
+
+				var source = new StringBuilder()
+					.AppendLine(PrintHeader())
+					.AppendLine(PrintUsingDirectives())
+					.AppendLine()
+					.Append(PrintNamespaceKeywordToken())
+					.AppendLine(namespaceName)
+					.AppendLine(PrintOpenBracketToken())
+					.Append(PrintIndenting(1))
+					.Append(PrintPartialKeywordToken())
+					.Append(PrintStructKeywordToken())
+					.Append(structSymbol.Name)
+					.AppendLine(genericParametersList)
+					.AppendLine(PrintOpenBracketToken(1))
+					.AppendLine(PrintCompilerGenerated(2))
+					.Append(PrintIndenting(2))
+					.Append(PrintPrivateKeywordToken())
+					.Append(structSymbol.Name)
+					.Append(PrintOpenBraceToken())
+					.AppendLine(PrintClosedBraceToken())
+					.Append(PrintOpenBracketToken(2))
+					.AppendLine()
+					.AppendLine(PrintClosedBracketToken(2))
+					.AppendLine(PrintClosedBracketToken(1))
+					.AppendLine(PrintClosedBracketToken());
+
+				return source.ToString();
+			}
 		}
 
 		/// <inheritdoc/>
-		public void Initialize(GeneratorInitializationContext context)
-		{
-		}
+		public void Initialize(GeneratorInitializationContext context) =>
+			context.RegisterForSyntaxNotifications(static () => new SyntaxReceiver());
+
+
+		private static partial string PrintHeader();
+		private static partial string PrintOpenBraceToken();
+		private static partial string PrintClosedBraceToken();
+		private static partial string PrintNamespaceKeywordToken();
+		private static partial string PrintPartialKeywordToken();
+		private static partial string PrintStructKeywordToken();
+		private static partial string PrintPrivateKeywordToken();
+		private static partial string PrintOpenBracketToken(int indentingCount = 0);
+		private static partial string PrintClosedBracketToken(int indentingCount = 0);
+		private static partial string PrintUsingDirectives();
+		private static partial string PrintIndenting(int indentingCount = 0);
+		private static partial string PrintCompilerGenerated(int indentingCount = 0);
 	}
 }
