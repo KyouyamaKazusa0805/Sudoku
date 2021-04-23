9fe5cf84db672772f1f06e2e2bd7eba42a873dce
SunnieShine <HowdySunnie@outlook.com>
2021-04-23 10:35:49 +0800

    Add project 'Sudoku.CodeGen.Deconstruction'.

---
+++ src/Sudoku.CodeGen.Deconstruction/Annotations/AutoDeconstructAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.AttributeTargets;
+
+namespace Sudoku.CodeGen.Deconstruction.Annotations
+{
+	/// <summary>
+	/// Allows the type can be deconstructed to multiple elements.
+	/// </summary>
+	[AttributeUsage(Class | Struct | Interface, AllowMultiple = true)]
+	public sealed class AutoDeconstructAttribute : Attribute
+	{
+		/// <summary>
+		/// Initializes an instance with the specified member list.
+		/// </summary>
+		/// <param name="members">The members.</param>
+		public AutoDeconstructAttribute(params string[] members) => FieldOrPropertyList = members;
+
+
+		/// <summary>
+		/// All members to deconstruct.
+		/// </summary>
+		public string[] FieldOrPropertyList { get; }
+	}
+}
---
+++ src/Sudoku.CodeGen.Deconstruction/DeconstructMethodGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Sudoku.CodeGen.Deconstruction
+{
+	/// <summary>
+	/// Provides a generator that generates the deconstruction methods.
+	/// </summary>
+#if false
+	[Generator]
+#endif
+	public sealed class DeconstructMethodGenerator : ISourceGenerator
+	{
+		/// <inheritdoc/>
+		public void Execute(GeneratorExecutionContext context)
+		{
+		}
+
+		/// <inheritdoc/>
+		public void Initialize(GeneratorInitializationContext context)
+		{
+		}
+	}
+}
