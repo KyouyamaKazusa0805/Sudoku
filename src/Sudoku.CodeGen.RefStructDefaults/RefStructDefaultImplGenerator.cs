ae98989759cb0048a3bb7e1b409193ce90a3f711
SunnieShine <HowdySunnie@outlook.com>
2021-04-24 17:38:54 +0800

    Add project 'Sudoku.CodeGen.RefStructDefaults'.

---
+++ src/Sudoku.CodeGen.RefStructDefaults/RefStructDefaultImplGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Sudoku.CodeGen.RefStructDefaults
+{
+	/// <summary>
+	/// Indicates the generator that generates the default overriden methods in a <see langword="ref struct"/>.
+	/// </summary>
+	[Generator]
+	public sealed class RefStructDefaultImplGenerator : ISourceGenerator
+	{
+		/// <inheritdoc/>
+		public void Execute(GeneratorExecutionContext context)
+		{
+		}
+
+		/// <inheritdoc/>
+		public void Initialize(GeneratorInitializationContext context)
+		{
+		}
+	}
+}
