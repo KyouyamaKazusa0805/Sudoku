1e40aefe4a2a579da41f5c925adbe9180e9b8008
SunnieShine <HowdySunnie@outlook.com>
2021-04-24 08:27:42 +0800

    Add project 'Sudoku.CodeGen.HashCode'.

---
+++ src/Sudoku.CodeGen.HashCode/AutoGetHashCodeGenerator.SyntaxReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Sudoku.CodeGen.HashCode
+{
+	public sealed partial class AutoGetHashCodeGenerator
+	{
+		/// <summary>
+		/// Defines the syntax receiver.
+		/// </summary>
+		private sealed class SyntaxReceiver : ISyntaxReceiver
+		{
+			/// <summary>
+			/// Indicates all possible candidate types used.
+			/// </summary>
+			public IList<TypeDeclarationSyntax> Candidates { get; } = new List<TypeDeclarationSyntax>();
+
+
+			/// <inheritdoc/>
+			public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+			{
+				// Any field with at least one attribute is a candidate for property generation.
+				if (syntaxNode is TypeDeclarationSyntax { AttributeLists: { Count: not 0 } } classDeclaration)
+				{
+					Candidates.Add(classDeclaration);
+				}
+			}
+		}
+	}
+}
---
+++ src/Sudoku.CodeGen.HashCode/AutoGetHashCodeGenerator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Sudoku.CodeGen.HashCode
+{
+	/// <summary>
+	/// Indicates the generator that generates the code that overrides <see cref="object.GetHashCode"/>.
+	/// </summary>
+	/// <seealso cref="object.GetHashCode"/>
+#if false
+	[Generator]
+#endif
+	public sealed partial class AutoGetHashCodeGenerator : ISourceGenerator
+	{
+		/// <inheritdoc/>
+		public void Execute(GeneratorExecutionContext context)
+		{
+
+		}
+
+		/// <inheritdoc/>
+		public void Initialize(GeneratorInitializationContext context) =>
+			context.RegisterForSyntaxNotifications(static () => new SyntaxReceiver());
+	}
+}
