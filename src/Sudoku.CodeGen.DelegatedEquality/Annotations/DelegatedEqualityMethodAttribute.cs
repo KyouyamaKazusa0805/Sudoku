97931ffba977eb1f62166fb60b27f7955427347b
SunnieShine <HowdySunnie@outlook.com>
2021-04-30 17:07:57 +0800

    Update doc comment.

--- src/Sudoku.CodeGen.DelegatedEquality/Annotations/DelegatedEqualityMethodAttribute.cs
+++ src/Sudoku.CodeGen.DelegatedEquality/Annotations/DelegatedEqualityMethodAttribute.cs
@@ -7,6 +7,11 @@ namespace Sudoku.CodeGen
 	/// to tell the users and the compiler that this method is an equality
 	/// method to judge whether two instances contain the same value.
 	/// </summary>
+	/// <remarks>
+	/// Because of the limitation of the algorithm and the source generator, the method marked this
+	/// attribute must be <see langword="static"/>. If you marks on a method
+	/// that isn't a <see langword="static"/> method, the source generator will do nothing.
+	/// </remarks>
 	[AttributeUsage(AttributeTargets.Method)]
 	public sealed class DelegatedEqualityMethodAttribute : Attribute
 	{
