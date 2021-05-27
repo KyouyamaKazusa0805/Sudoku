f9929b1fed4bd194a320f3423090130e9b09ae53
SunnieShine <HowdySunnie@outlook.com>
2021-05-27 21:07:46 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -9,12 +9,9 @@ if (s is (a: 1, b: 2, c: _))
 readonly struct S
 {
 	readonly int _a, _b, _c;
-	readonly double _d;
 
 	public S(int a, int b, int c) : this() { _a = a; _b = b; _c = c; }
 
 	public void Deconstruct(out int a, out int b) { a = _a; b = _b; }
-	public void Deconstruct(out int a, out double d) { a = _a; d = _d; }
-	public void Deconstruct(out int a) { a = _a; }
 	public void Deconstruct(out int a, out int b, out int c) { a = _a; b = _b; c = _c; }
 }
