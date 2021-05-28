84f1d3043b3fe1d11832ea3da5e063862decaf33
SunnieShine <HowdySunnie@outlook.com>
2021-05-28 14:42:01 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 
 var s = new S(1, 2, 3);
-if (s is () and (a: 10) and (a: 10, b: 30))
-{
-	Console.WriteLine(nameof(s));
-}
-
 if (s is (a: 1, b: 2, c: _))
 {
 	Console.WriteLine(nameof(s));
@@ -17,8 +12,6 @@ readonly struct S
 
 	public S(int a, int b, int c) : this() { _a = a; _b = b; _c = c; }
 
-	public void Deconstruct() { }
-	public void Deconstruct(out int a) { a = _a; }
 	public void Deconstruct(out int a, out int b) { a = _a; b = _b; }
 	public void Deconstruct(out int a, out int b, out int c) { a = _a; b = _b; c = _c; }
 }
