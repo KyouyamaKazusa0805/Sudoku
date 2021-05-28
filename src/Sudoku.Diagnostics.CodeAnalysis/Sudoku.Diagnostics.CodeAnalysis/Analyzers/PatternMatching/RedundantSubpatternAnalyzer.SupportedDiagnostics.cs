0b3d9779f96c8e1c24919448bfd0ac73fe619c1c
SunnieShine <HowdySunnie@outlook.com>
2021-05-28 19:30:48 +0800

    Add code analyzer for SS0609.

---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/PatternMatching/RedundantSubpatternAnalyzer.SupportedDiagnostics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.Analyzers
+{
+	partial class RedundantSubpatternAnalyzer
+	{
+		/// <summary>
+		/// Indicates the
+		/// <a href="https://gitee.com/SunnieShine/Sudoku/wikis/pages?sort_id=4067681&amp;doc_id=633030">
+		/// SS0609
+		/// </a>
+		/// diagnostic result (The positional subpattern is redundant).
+		/// </summary>
+		private static readonly DiagnosticDescriptor SS0609 = new(
+			DiagnosticIds.SS0609, Titles.SS0609, Messages.SS0609, Categories.Design,
+			DiagnosticSeverity.Warning, true, helpLinkUri: HelpLinks.SS0609
+		);
+
+
+		/// <inheritdoc/>
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			SS0609
+		);
+	}
+}
---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/PatternMatching/RedundantSubpatternAnalyzer.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Diagnostics.Extensions;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.Analyzers
+{
+	/// <summary>
+	/// Indicates an analyzer that analyzes the code for the redundant subpattern.
+	/// </summary>
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed partial class RedundantSubpatternAnalyzer : DiagnosticAnalyzer
+	{
+		/// <inheritdoc/>
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+
+			context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, new[] { SyntaxKind.IsPatternExpression });
+		}
+
+
+		private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			var (semanticModel, _, originalNode) = context;
+
+			if (
+				originalNode is not IsPatternExpressionSyntax
+				{
+					Pattern: var pattern
+				} node
+			)
+			{
+				return;
+			}
+
+			var subpatterns = new List<(SubpatternSyntax Node, string BoundParameterName)>();
+			foreach (var descendantPattern in pattern.DescendantNodes().OfType<PositionalPatternClauseSyntax>())
+			{
+				if (
+					descendantPattern is not
+					{
+						Parent: RecursivePatternSyntax parentNode,
+						Subpatterns: { Count: >= 2 } currentSubpatterns
+					}
+				)
+				{
+					continue;
+				}
+
+				if (
+					semanticModel.GetOperation(parentNode) is not IRecursivePatternOperation
+					{
+						DeconstructSymbol: IMethodSymbol { Parameters: var boundMethodParameters },
+						MatchedType: { } type
+					} operation
+				)
+				{
+					return;
+				}
+
+				for (int i = 0; i < currentSubpatterns.Count; i++)
+				{
+					switch (currentSubpatterns[i])
+					{
+						case { NameColon: null, Pattern: not DiscardPatternSyntax } currentSubpattern:
+						{
+							string boundParameterName = boundMethodParameters[i].Name;
+							subpatterns.Add((currentSubpattern, boundParameterName));
+
+							break;
+						}
+						case
+						{
+							NameColon: { Name: { Identifier: { ValueText: var parameterName } } } nameColonNode,
+							Pattern: not DiscardPatternSyntax
+						} currentSubpattern:
+						{
+							subpatterns.Add((currentSubpattern, parameterName));
+
+							break;
+						}
+					}
+				}
+			}
+
+			for (int i = 0, count = subpatterns.Count; i < count - 1; i++)
+			{
+				var (_, firstName) = subpatterns[i];
+				for (int j = i + 1; j < count; j++)
+				{
+					var (secondSubpattern, secondName) = subpatterns[j];
+					if (firstName == secondName)
+					{
+						// Duplicate judgement.
+						context.ReportDiagnostic(
+							Diagnostic.Create(
+								descriptor: SS0609,
+								location: secondSubpattern.GetLocation(),
+								messageArgs: new[] { secondSubpattern.ToString() }
+							)
+						);
+					}
+				}
+			}
+		}
+	}
+}
