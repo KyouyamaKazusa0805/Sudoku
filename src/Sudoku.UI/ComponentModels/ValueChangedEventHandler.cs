08d4c2c7ddac74a116b5ebd96fb5d9f8f3881ff6
SunnieShine <HowdySunnie@outlook.com>
2021-06-01 13:13:24 +0800

    Remove Sudoku.UI.

--- src/Sudoku.UI/ComponentModels/ValueChangedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sudoku.UI.ComponentModels
-{
-	/// <summary>
-	/// Indicates the event handler that executed when the specified value is changed.
-	/// </summary>
-	public delegate void ValueChangedEventHandler();
-}
