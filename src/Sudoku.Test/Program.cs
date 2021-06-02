38cd70c7f23d57143a9e66f9345142b5807cb9e9
SunnieShine <HowdySunnie@outlook.com>
2021-06-02 21:16:43 +0800

    Remove unused temporary generator.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,12 +1,3 @@
-﻿#if DEBUG && (NET5_0 || NETSTANDARD2_0)
+﻿using System;
 
-using System.IO;
-using Sudoku.Diagnostics.CodeAnalysis.IO;
-using static System.Environment;
-
-DiagnosticResultOutput.Output(
-	"SupportedDiagnosticIds.txt",
-	Path.Combine(GetFolderPath(SpecialFolder.Desktop), "Result.csv")
-);
-
-#endif
+Console.WriteLine("Hello, world!");
