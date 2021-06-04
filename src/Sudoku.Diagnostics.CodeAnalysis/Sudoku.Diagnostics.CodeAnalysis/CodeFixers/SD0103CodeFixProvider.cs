9b1d3987c2f03681ce54a61f53eb0f96378777c6
SunnieShine <HowdySunnie@outlook.com>
2021-06-04 15:40:42 +0800

    Add code fixer for SS0103.

---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/CodeFixers/SD0103CodeFixProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Extensions;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.CodeFixers
+{
+	/// <summary>
+	/// Indicates the code fixer for solving the diagnostic result
+	/// <a href="https://github.com/SunnieShine/Sudoku/wiki/Rule-SD0103">SD0103</a>.
+	/// </summary>
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SD0103CodeFixProvider)), Shared]
+	public sealed class SD0103CodeFixProvider : CodeFixProvider
+	{
+		/// <inheritdoc/>
+		public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(
+			DiagnosticIds.SD0103
+		);
+
+		/// <inheritdoc/>
+		public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+
+		/// <inheritdoc/>
+		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var document = context.Document;
+			var diagnostic = context.Diagnostics.First(static d => d.Id == nameof(DiagnosticIds.SD0103));
+			var root = (await document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false))!;
+			var ((_, span), _) = diagnostic;
+			var propertyDecl = (PropertyDeclarationSyntax)root.FindNode(span, getInnermostNodeForTie: true);
+
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: CodeFixTitles.SD0103,
+					createChangedDocument: c => AppendStaticKeywordAsync(document, propertyDecl, c),
+					equivalenceKey: nameof(CodeFixTitles.SD0103)
+				),
+				diagnostic
+			);
+		}
+
+
+		private static async Task<Document> AppendStaticKeywordAsync(
+			Document document, PropertyDeclarationSyntax propertyDeclaration,
+			CancellationToken cancellationToken)
+		{
+			var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+			editor.SetModifiers(propertyDeclaration, DeclarationModifiers.Static);
+
+			return document.WithSyntaxRoot(editor.GetChangedRoot());
+		}
+	}
+}
