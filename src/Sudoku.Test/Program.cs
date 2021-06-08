84f82c24d46550a0bc6ec3bb62d5c65801185277
SunnieShine <HowdySunnie@outlook.com>
2021-06-08 21:35:38 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,23 +1,3 @@
 ﻿using System;
 
-object? o = 3;
-
-if (o is not null)
-{
-	Console.WriteLine(o);
-}
-
-if (o is object variable1)
-{
-	Console.WriteLine(variable1);
-}
-
-if (o is not null and var variable2)
-{
-	Console.WriteLine(variable2);
-}
-
-if (o is not null && o is var variable3) // This case can be converted to the above one via IDE0078.
-{
-	Console.WriteLine(variable3);
-}
+Console.WriteLine();
