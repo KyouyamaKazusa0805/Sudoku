6edb0f3cbe1b757690043e8535d388849a26ebc3
SunnieShine <HowdySunnie@outlook.com>
2021-06-08 14:14:36 +0800

    Add code fixer for SS0704.

---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/CodeFixers/SS0704CodeFixProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Extensions;
+using Sudoku.CodeGen;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.CodeFixers
+{
+	[CodeFixProvider("SS0704")]
+	public sealed partial class SS0704CodeFixProvider : CodeFixProvider
+	{
+		/// <inheritdoc/>
+		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var document = context.Document;
+			var diagnostic = context.Diagnostics.First(static d => d.Id == nameof(DiagnosticIds.SS0704));
+			var root = (await document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false))!;
+			var (_, exprSpan) = diagnostic.AdditionalLocations[0];
+			if (
+				root.FindNode(exprSpan, getInnermostNodeForTie: true) is not PostfixUnaryExpressionSyntax
+				{
+					Operand: var baseExpr,
+				} expr
+			)
+			{
+				return;
+			}
+
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: CodeFixTitles.SS0704,
+					createChangedDocument: async c =>
+					{
+						var editor = await DocumentEditor.CreateAsync(document, c);
+						editor.ReplaceNode(expr, baseExpr);
+
+						return document.WithSyntaxRoot(editor.GetChangedRoot());
+					},
+					equivalenceKey: nameof(CodeFixTitles.SS0704)
+				),
+				diagnostic
+			);
+		}
+	}
+}
--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,23 +1,5 @@
 ﻿using System;
 
-P p = new();
-string q = p!.ToString();
+int i = 20;
 
-P a = new();
-int r = a!.A;
-
-P b = new();
-int s = b![3];
-
-Console.WriteLine(q);
-Console.WriteLine(r);
-Console.WriteLine(s);
-
-class P
-{
-	public int A => 3;
-
-	public int this[int index] => index + 6;
-
-	public override string ToString() => string.Empty;
-}
+Console.WriteLine(i!);
