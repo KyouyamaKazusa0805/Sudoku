8fbd598ddc4e1cd0727e5d016012eff464797c68
SunnieShine <HowdySunnie@outlook.com>
2021-06-12 14:21:01 +0800

    Add code analyzer for SS0627.

---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/PatternMatching/AvailableLengthPatternAnalyzer.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Diagnostics.Extensions;
+using Sudoku.CodeGen;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.Analyzers
+{
+	[CodeAnalyzer("SS0627")]
+	public sealed partial class AvailableLengthPatternAnalyzer : DiagnosticAnalyzer
+	{
+		/// <inheritdoc/>
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+
+			context.RegisterSyntaxNodeAction(
+				AnalyzeSyntaxNode,
+				new[]
+				{
+					SyntaxKind.RecursivePattern,
+					SyntaxKind.EqualsExpression,
+					SyntaxKind.NotEqualsExpression
+				}
+			);
+		}
+
+
+		private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			var (semanticModel, _, originalNode) = context;
+
+			switch (originalNode)
+			{
+				// obj.Length == length
+				// obj.Length != length
+				case BinaryExpressionSyntax
+				{
+					RawKind: var kind and (
+						(int)SyntaxKind.EqualsExpression or (int)SyntaxKind.NotEqualsExpression
+					),
+					Left: MemberAccessExpressionSyntax
+					{
+						RawKind: (int)SyntaxKind.SimpleMemberAccessExpression,
+						Expression: var expr,
+						Name: { Identifier: { ValueText: "Count" or "Length" } }
+					},
+					Right: var possibleConstantExpr
+				}
+				when semanticModel.GetOperation(possibleConstantExpr) is { ConstantValue: { HasValue: true } }:
+				{
+					context.ReportDiagnostic(
+						Diagnostic.Create(
+							descriptor: SS0627,
+							location: originalNode.GetLocation(),
+							properties: ImmutableDictionary.CreateRange(
+								new KeyValuePair<string, string?>[]
+								{
+									new(
+										"IsEqualExpression",
+										(kind == (int)SyntaxKind.EqualsExpression).ToString()
+									)
+								}
+							),
+							messageArgs: new[]
+							{
+								$@"{expr} is [{(
+									kind != (int)SyntaxKind.EqualsExpression ? "not" : string.Empty
+								)}{possibleConstantExpr}]"
+							}
+						)
+					);
+
+					break;
+				}
+
+				// length == obj.Length
+				// length != obj.Length
+				case BinaryExpressionSyntax
+				{
+					RawKind: var kind and (
+						(int)SyntaxKind.EqualsExpression or (int)SyntaxKind.NotEqualsExpression
+					),
+					Left: var possibleConstantExpr,
+					Right: MemberAccessExpressionSyntax
+					{
+						RawKind: (int)SyntaxKind.SimpleMemberAccessExpression,
+						Expression: var expr,
+						Name: { Identifier: { ValueText: "Count" or "Length" } }
+					}
+				}
+				when semanticModel.GetOperation(possibleConstantExpr) is { ConstantValue: { HasValue: true } }:
+				{
+					context.ReportDiagnostic(
+						Diagnostic.Create(
+							descriptor: SS0627,
+							location: originalNode.GetLocation(),
+							properties: ImmutableDictionary.CreateRange(
+								new KeyValuePair<string, string?>[]
+								{
+									new(
+										"IsEqualExpression",
+										(kind == (int)SyntaxKind.EqualsExpression).ToString()
+									)
+								}
+							),
+							messageArgs: new[]
+							{
+								$@"{expr} is [{(
+									kind != (int)SyntaxKind.EqualsExpression ? "not" : string.Empty
+								)}{possibleConstantExpr}]"
+							}
+						)
+					);
+
+					break;
+				}
+
+				// s is { Length: length }
+				// s is not { Length: length }
+				case RecursivePatternSyntax
+				{
+					Parent: var parent,
+					PropertyPatternClause: { Subpatterns: { Count: not 0 } subpatterns }
+				} recursivePattern:
+				{
+					foreach (var subpattern in subpatterns)
+					{
+						if (
+							subpattern is not
+							{
+								NameColon: { Name: { Identifier: { ValueText: "Length" or "Count" } } },
+								Pattern: var pattern
+							}
+						)
+						{
+							continue;
+						}
+
+						var node = recursivePattern.Ancestors().OfType<IsPatternExpressionSyntax>().First();
+						context.ReportDiagnostic(
+							Diagnostic.Create(
+								descriptor: SS0627,
+								location: originalNode.GetLocation(),
+								properties: ImmutableDictionary.CreateRange(
+									new KeyValuePair<string, string?>[] { new("IsEqualExpression", null) }
+								),
+								messageArgs: new[]
+								{
+									$@"{node.Expression} {(
+										parent is UnaryPatternSyntax ? "is not" : "is"
+									)} [{pattern}]"
+								}
+							)
+						);
+					}
+
+					break;
+				}
+			}
+		}
+	}
+}
--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,3 +1,9 @@
 ﻿using System;
 
-Console.WriteLine();
+int[] p = { 3, 8, 1, 6, 5, 4, 7, 2, 9 };
+if (p.Length == 5) Console.WriteLine(p[0]);
+if (p.Length != 5) Console.WriteLine(p[0]);
+if (p is { Length: 5 }) Console.WriteLine(p[0]);
+if (p is { Length: not 5 }) Console.WriteLine(p[0]);
+if (p is not { Length: 5 }) Console.WriteLine(p[0]);
+if (p is not { Length: not 5 }) Console.WriteLine(p[0]);
