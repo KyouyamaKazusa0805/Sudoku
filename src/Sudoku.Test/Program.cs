7e8af28c74ac24e78d600411c6c692980555a837
SunnieShine <HowdySunnie@outlook.com>
2021-06-12 09:20:07 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,13 +1,3 @@
 ﻿using System;
 
-object? o = 13;
-
-if (o is not not not not not not not not not not not 3)
-{
-	Console.WriteLine(o);
-}
-
-if (o is not not not not not not not not not not not not 3)
-{
-	Console.WriteLine(o);
-}
+Console.WriteLine();
