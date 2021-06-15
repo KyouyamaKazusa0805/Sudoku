1afa54a9064ba1ee43dd96ef4cc8d46a8cab43b0
SunnieShine <HowdySunnie@outlook.com>
2021-06-15 09:06:17 +0800

    Add code analyzer for SD0403.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,3 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using Sudoku.Data;
+using Sudoku.Solving.Manual;
 
 Console.WriteLine();
+
+
+[DirectSearcher]
+record RecordType;
+
+[DirectSearcher]
+class ClassType { }
+
+[DirectSearcher]
+class TestStepSearcher : StepSearcher
+{
+	public static TechniqueProperties Properties { get; } = new(default, string.Empty);
+
+
+	public override void GetAll(IList<StepInfo> accumulator, in SudokuGrid grid) =>
+		throw new NotImplementedException();
+}
