5dd5860d47ffb3112b468b253f29d52b61fb1622
SunnieShine <HowdySunnie@outlook.com>
2021-06-19 10:50:15 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,15 +1,3 @@
 ﻿using System;
 
 Console.WriteLine();
-
-
-[AttributeUsage(AttributeTargets.All)]
-internal class AAttribute : Attribute
-{
-	public int Property { get; set; }
-}
-
-internal sealed class AnotherAttribute : AAttribute
-{
-
-}
