559bd372a013665cd8e60a1306862656caefc3a5
SunnieShine <HowdySunnie@outlook.com>
2021-06-27 16:45:30 +0800

    Add code analyzer for SS0311.

---
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/Linq/ContinuousOrderbyAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Sudoku.CodeGen;
+
+namespace Sudoku.Diagnostics.CodeAnalysis.Analyzers
+{
+	[CodeAnalyzer("SS0311F")]
+	public sealed partial class ContinuousOrderbyAnalyzer : DiagnosticAnalyzer
+	{
+		/// <inheritdoc/>
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+
+			context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, new[] { SyntaxKind.QueryExpression });
+		}
+
+
+		private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			if (context.Node is not QueryExpressionSyntax { Body: { Clauses: { Count: not 0 } clauses } } node)
+			{
+				return;
+			}
+
+			foreach (var clause in clauses)
+			{
+				if (clause is not OrderByClauseSyntax { Orderings: { Count: var count and not 0 } orderings })
+				{
+					continue;
+				}
+
+				for (int i = 0; i < count - 1; i++)
+				{
+					for (int j = i + 1; j < count; j++)
+					{
+						OrderingSyntax former = orderings[i], latter = orderings[j];
+						if (
+							(former, latter) is not (
+#pragma warning disable IDE0055
+								{
+									Expression: IdentifierNameSyntax
+									{
+										Identifier: { ValueText: var formerIdentifier }
+									}
+								},
+								{
+									Expression: IdentifierNameSyntax
+									{
+										Identifier: { ValueText: var latterIdentifier }
+									}
+								}
+#pragma warning restore IDE0055
+							)
+						)
+						{
+							continue;
+						}
+
+						if (formerIdentifier != latterIdentifier)
+						{
+							continue;
+						}
+
+						context.ReportDiagnostic(
+							Diagnostic.Create(
+								descriptor: SS0311,
+								location: latter.GetLocation(),
+								messageArgs: null,
+								additionalLocations: new[] { clause.GetLocation() }
+							)
+						);
+					}
+				}
+			}
+		}
+	}
+}
--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -2,9 +2,5 @@
 
 int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15, 20 };
 
-_ =
-	from x in arr
-	orderby x
-	where x == 3
-	orderby x
-	select x;
+_ = from x in arr orderby x, x select x;
+_ = from x in arr orderby x, x descending select x;
