04b240d68db2e88853ef171586d17bfbcaa0b5d8
SunnieShine <HowdySunnie@outlook.com>
2021-07-02 11:04:08 +0800

    Update code.

--- src/Sudoku.CodeGenerating/AssemblyAttributes.cs
+++ src/Sudoku.CodeGenerating/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
 ﻿#if !NETSTANDARD2_1_OR_GREATER
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "<Pending>")]
+[module: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "<Pending>")]
 #endif
