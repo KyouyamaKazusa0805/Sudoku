4e6135423d817d2011a91c4a0621f3891e403a55
SunnieShine <HowdySunnie@outlook.com>
2021-07-05 07:39:09 +0800

    Code refactoring.

--- src/Sudoku.CodeGenerating/Generators/FormattableMethodsGenerator.cs
+++ src/Sudoku.CodeGenerating/Generators/FormattableMethodsGenerator.cs
@@ -20,35 +20,24 @@ namespace Sudoku.CodeGenerating
 		public void Execute(GeneratorExecutionContext context)
 		{
 			var receiver = (SyntaxReceiver)context.SyntaxReceiver!;
-			var nameDic = new Dictionary<string, int>();
 			var compilation = context.Compilation;
-			foreach (var classSymbol in
+			foreach (var type in
 				from candidate in receiver.Candidates
 				let model = compilation.GetSemanticModel(candidate.SyntaxTree)
-				select (INamedTypeSymbol)model.GetDeclaredSymbol(candidate)! into symbol
-				where symbol.Marks<AutoFormattableAttribute>()
-				select symbol)
+				select (INamedTypeSymbol)model.GetDeclaredSymbol(candidate)! into type
+				where type.Marks<AutoFormattableAttribute>()
+				select type)
 			{
-				_ = nameDic.TryGetValue(classSymbol.Name, out int i);
-				string name = i == 0 ? classSymbol.Name : $"{classSymbol.Name}{(i + 1).ToString()}";
-				nameDic[classSymbol.Name] = i + 1;
-
-				if (getToStringMethodsCode(context, classSymbol) is { } c)
-				{
-					context.AddSource($"{name}.ToString.g.cs", c);
-				}
-			}
-
-
-			static string? getToStringMethodsCode(in GeneratorExecutionContext context, INamedTypeSymbol symbol)
-			{
-				symbol.DeconstructInfo(
+				type.DeconstructInfo(
 					false, out string fullTypeName, out string namespaceName, out string genericParametersList,
 					out string genericParametersListWithoutConstraint, out string typeKind,
 					out string readonlyKeyword, out _
 				);
 
-				return $@"#pragma warning disable 1591
+				context.AddSource(
+					type.ToFileName(),
+					"ToString",
+					$@"#pragma warning disable 1591
 
 using System.Runtime.CompilerServices;
 
@@ -56,11 +45,10 @@ using System.Runtime.CompilerServices;
 
 namespace {namespaceName}
 {{
-	partial {typeKind}{symbol.Name}{genericParametersList}
+	partial {typeKind}{type.Name}{genericParametersList}
 	{{
 		/// <inheritdoc cref=""object.ToString""/>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		[CompilerGenerated]
+		[CompilerGenerated, MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public override {readonlyKeyword}partial string ToString() => ToString(null, null);
 
 		/// <summary>
@@ -70,11 +58,11 @@ namespace {namespaceName}
 		/// The format. If available, the parameter can be <see langword=""null""/>.
 		/// </param>
 		/// <returns>The string result.</returns>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		[CompilerGenerated]
+		[CompilerGenerated, MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public {readonlyKeyword}partial string ToString(string? format) => ToString(format, null);
 	}}
-}}";
+}}"
+				);
 			}
 		}
 
