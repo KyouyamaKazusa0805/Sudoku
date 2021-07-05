2d714c414ecbbb0da2735c00444430a3e648395e
SunnieShine <HowdySunnie@outlook.com>
2021-07-05 08:22:55 +0800

    Code refactoring.

--- src/Sudoku.CodeGenerating/Generators/RefStructDefaultImplGenerator.cs
+++ src/Sudoku.CodeGenerating/Generators/RefStructDefaultImplGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.CodeAnalysis;
 using Sudoku.CodeGenerating.Extensions;
 
@@ -15,27 +14,13 @@ namespace Sudoku.CodeGenerating
 		public void Execute(GeneratorExecutionContext context)
 		{
 			var receiver = (SyntaxReceiver)context.SyntaxReceiver!;
-			var nameDic = new Dictionary<string, int>();
 			var compilation = context.Compilation;
-			foreach (var classSymbol in
+			foreach (var type in
 				from candidateStruct in receiver.CandidateRefStructs
 				let model = compilation.GetSemanticModel(candidateStruct.SyntaxTree)
 				select (INamedTypeSymbol)model.GetDeclaredSymbol(candidateStruct)!)
 			{
-				_ = nameDic.TryGetValue(classSymbol.Name, out int i);
-				string name = i == 0 ? classSymbol.Name : $"{classSymbol.Name}{(i + 1).ToString()}";
-				nameDic[classSymbol.Name] = i + 1;
-
-				if (getCode(classSymbol, context.Compilation) is { } c)
-				{
-					context.AddSource($"{name}.RefStructDefaults.g.cs", c);
-				}
-			}
-
-
-			static string? getCode(INamedTypeSymbol symbol, Compilation compilation)
-			{
-				symbol.DeconstructInfo(
+				type.DeconstructInfo(
 					false, out string fullTypeName, out string namespaceName, out string genericParametersList,
 					out _, out _, out string readonlyKeyword, out _
 				);
@@ -47,7 +32,7 @@ namespace Sudoku.CodeGenerating
 				var objectSymbol = compilation.GetSpecialType(SpecialType.System_Object);
 				var nullableObjectSymbol = objectSymbol.WithNullableAnnotation(NullableAnnotation.Annotated);
 
-				string equalsMethod = symbol.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
+				string equalsMethod = type.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
 					symbol is { Name: "Equals", Parameters: { Length: not 0 } parameters }
 					&& (
 						SymbolEqualityComparer.Default.Equals(parameters[0].Type, objectSymbol)
@@ -56,22 +41,20 @@ namespace Sudoku.CodeGenerating
 					&& SymbolEqualityComparer.Default.Equals(symbol.ReturnType, boolSymbol)
 				) ? string.Empty : $@"/// <inheritdoc cref=""object.Equals(object?)""/>
 		/// <exception cref=""NotSupportedException"">Always throws.</exception>
-		[CompilerGenerated, DoesNotReturn]
-		[EditorBrowsable(EditorBrowsableState.Never)]
+		[CompilerGenerated, DoesNotReturn, EditorBrowsable(EditorBrowsableState.Never)]
 		[Obsolete(""You can't use or call this method."", true, DiagnosticId = ""BAN"")]
 		public override {readonlyKeyword}bool Equals(object? other) => throw new NotSupportedException();";
 
-				string getHashCodeMethod = symbol.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
+				string getHashCodeMethod = type.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
 					symbol is { Name: "GetHashCode", Parameters: { Length: 0 } parameters }
 					&& SymbolEqualityComparer.Default.Equals(symbol.ReturnType, intSymbol)
 				) ? string.Empty : $@"/// <inheritdoc cref=""object.GetHashCode""/>
 		/// <exception cref=""NotSupportedException"">Always throws.</exception>
-		[CompilerGenerated, DoesNotReturn]
-		[EditorBrowsable(EditorBrowsableState.Never)]
+		[CompilerGenerated, DoesNotReturn, EditorBrowsable(EditorBrowsableState.Never)]
 		[Obsolete(""You can't use or call this method."", true, DiagnosticId = ""BAN"")]
 		public override {readonlyKeyword}int GetHashCode() => throw new NotSupportedException();";
 
-				string toStringMethod = symbol.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
+				string toStringMethod = type.GetMembers().OfType<IMethodSymbol>().Any(symbol =>
 					symbol is { Name: "ToString", Parameters: { Length: 0 } parameters }
 					&& (
 						SymbolEqualityComparer.Default.Equals(symbol.ReturnType, stringSymbol)
@@ -79,12 +62,14 @@ namespace Sudoku.CodeGenerating
 					)
 				) ? string.Empty : $@"/// <inheritdoc cref=""object.ToString""/>
 		/// <exception cref=""NotSupportedException"">Always throws.</exception>
-		[CompilerGenerated, DoesNotReturn]
-		[EditorBrowsable(EditorBrowsableState.Never)]
+		[CompilerGenerated, DoesNotReturn, EditorBrowsable(EditorBrowsableState.Never)]
 		[Obsolete(""You can't use or call this method."", true, DiagnosticId = ""BAN"")]
 		public override {readonlyKeyword}string? ToString() => throw new NotSupportedException();";
 
-				return $@"#pragma warning disable 809, IDE0005
+				context.AddSource(
+					type.ToFileName(),
+					"RefStructDefaults",
+					$@"#pragma warning disable 809, IDE0005
 
 using System;
 using System.ComponentModel;
@@ -95,11 +80,14 @@ using System.Runtime.CompilerServices;
 
 namespace {namespaceName}
 {{
-	partial struct {symbol.Name}{genericParametersList}
+	partial struct {type.Name}{genericParametersList}
 	{{
+#line hidden
 {equalsMethod}{getHashCodeMethod}{toStringMethod}
+#line default
 	}}
-}}";
+}}"
+				);
 			}
 		}
 
