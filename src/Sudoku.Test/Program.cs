8ccb2f7a4fbc779a5d4bbf01b543d136d5ad29e6
SunnieShine <HowdySunnie@outlook.com>
2021-07-30 15:57:25 +0800

    Remove test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-int[] arrToSort = { 3, 8, 1, 6, 5, 4, 7, 2, 9 };
-
-int length = 19;
-
-for (int i = 10; i < arrToSort.Length; i++)
-{
-	// Do something.
-}
-
-Console.WriteLine(length);
+Console.WriteLine("Hello!");
