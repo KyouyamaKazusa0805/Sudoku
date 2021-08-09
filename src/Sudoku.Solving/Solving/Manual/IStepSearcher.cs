a6e2c12ea6d58070e686ed1a596597ddce54f510
SunnieShine <HowdySunnie@outlook.com>
2021-08-09 20:33:56 +0800

    Update code.

--- src/Sudoku.Solving/Solving/Manual/IStepSearcher.cs
+++ src/Sudoku.Solving/Solving/Manual/IStepSearcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Sudoku.Data;
 
 namespace Sudoku.Solving.Manual
@@ -15,5 +17,20 @@ namespace Sudoku.Solving.Manual
 		/// <param name="accumulator">The accumulator to store technique information.</param>
 		/// <param name="grid">The grid to search for techniques.</param>
 		void GetAll(IList<StepInfo> accumulator, in SudokuGrid grid);
+
+		/// <summary>
+		/// Take a technique step after searched all solving steps.
+		/// </summary>
+		/// <param name="grid">The grid to search steps.</param>
+		/// <returns>A technique information.</returns>
+		public StepInfo? GetOne(in SudokuGrid grid)
+		{
+			var bag = new NotifyChangedList<StepInfo>();
+			bag.ElementAdded += static (_, _) => throw new InvalidOperationException(nameof(GetOne));
+
+			try { GetAll(bag, grid); } catch (InvalidOperationException ex) when (ex.Message == nameof(GetOne)) { }
+
+			return bag.FirstOrDefault();
+		}
 	}
 }
--- src/Sudoku.Solving/Solving/Manual/StepSearcher.cs
+++ src/Sudoku.Solving/Solving/Manual/StepSearcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Extensions;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Sudoku.Data;
 using Sudoku.Resources;
 
@@ -40,25 +41,8 @@ namespace Sudoku.Solving.Manual
 		/// <inheritdoc/>
 		public abstract void GetAll(IList<StepInfo> accumulator, in SudokuGrid grid);
 
-		/// <summary>
-		/// Take a technique step after searched all solving steps.
-		/// </summary>
-		/// <param name="grid">The grid to search steps.</param>
-		/// <returns>A technique information.</returns>
-		public StepInfo? GetOne(in SudokuGrid grid)
-		{
-			var bag = new NotifyChangedList<StepInfo>();
-			bag.ElementAdded += static (_, _) => throw new InvalidOperationException(nameof(GetOne));
-
-			try
-			{
-				GetAll(bag, grid);
-			}
-			catch (InvalidOperationException ex) when (ex.Message == nameof(GetOne))
-			{
-			}
-
-			return bag.FirstOrDefault();
-		}
+		/// <inheritdoc/>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public StepInfo? GetOne(in SudokuGrid grid) => ((IStepSearcher)this).GetOne(grid);
 	}
 }
