0aac5fbe4a8a21a11a924c83331b1aa5ba0b04e1
SunnieShine <HowdySunnie@outlook.com>
2021-08-13 18:03:09 +0800

    Remove deprecated analyzers.

--- src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/InterpolatedString/InterpolatedStringAnalyzer.cs
+++ src/Sudoku.Diagnostics.CodeAnalysis/Sudoku.Diagnostics.CodeAnalysis/Analyzers/InterpolatedString/InterpolatedStringAnalyzer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sudoku.Diagnostics.CodeAnalysis.Analyzers;
 
-[CodeAnalyzer("SS0101", "SS0102F")]
+[CodeAnalyzer("SS0102F")]
 public sealed partial class InterpolatedStringAnalyzer : DiagnosticAnalyzer
 {
 	/// <inheritdoc/>
@@ -10,58 +10,13 @@ public sealed partial class InterpolatedStringAnalyzer : DiagnosticAnalyzer
 		context.EnableConcurrentExecution();
 
 		context.RegisterSyntaxNodeAction(
-			static context =>
-			{
-				CheckSS0101(context);
-				CheckSS0102(context);
-			},
+			AnalyzeSyntaxNode,
 			new[] { SyntaxKind.Interpolation, SyntaxKind.InterpolatedStringExpression }
 		);
 	}
 
 
-	private static void CheckSS0101(SyntaxNodeAnalysisContext context)
-	{
-		var (semanticModel, _, orginalNode) = context;
-		if (orginalNode is not InterpolatedStringExpressionSyntax node)
-		{
-			return;
-		}
-
-		if (
-			node.DescendantNodes().OfType<InterpolationSyntax>().ToArray() is not
-			{
-				Length: not 0
-			} interpolationParts
-		)
-		{
-			return;
-		}
-
-		foreach (var interpolation in interpolationParts)
-		{
-			if (
-				semanticModel.GetOperation(interpolation) is not IInterpolationOperation
-				{
-					Kind: OperationKind.Interpolation,
-					Expression.Type.IsValueType: true
-				}
-			)
-			{
-				return;
-			}
-
-			context.ReportDiagnostic(
-				Diagnostic.Create(
-					descriptor: SS0101,
-					location: interpolation.Expression.GetLocation(),
-					messageArgs: null
-				)
-			);
-		}
-	}
-
-	private static void CheckSS0102(SyntaxNodeAnalysisContext context)
+	private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
 	{
 		if (context.Node is not InterpolatedStringExpressionSyntax node)
 		{
