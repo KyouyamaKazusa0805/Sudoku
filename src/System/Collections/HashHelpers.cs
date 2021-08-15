08f69a152db2c42de025c856958ca8b83e20535a
SunnieShine <HowdySunnie@outlook.com>
2021-08-15 08:30:26 +0800

    Format code.

--- src/System/Collections/HashHelpers.cs
+++ src/System/Collections/HashHelpers.cs
@@ -51,12 +51,12 @@ internal static class HashHelpers
 	/// </summary>
 	private static readonly int[] Primes =
 	{
-			3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
-			1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
-			17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
-			187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
-			1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369
-		};
+		3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+		1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+		17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+		187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+		1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369
+	};
 
 
 	/// <summary>
@@ -164,7 +164,7 @@ internal static class HashHelpers
 		if (divisor > int.MaxValue)
 		{
 			throw new ArgumentException(
-				"The divisor can't be greater than int.MaxValue (2147483647) even though it's an uint value.",
+				"The divisor can't be greater than 'int.MaxValue' even though it's an uint value.",
 				nameof(divisor)
 			);
 		}
@@ -174,7 +174,7 @@ internal static class HashHelpers
 		uint highbits = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
 		if (highbits != value % divisor)
 		{
-			throw new ArithmeticException("The operation handled wrong.");
+			throw new ArithmeticException("The operation gets something wrong to handle.");
 		}
 
 		return highbits;
