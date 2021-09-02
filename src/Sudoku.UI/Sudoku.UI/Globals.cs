1752bfda586622fb7b10d71f5feea12d1cfa1fb7
SunnieShine <HowdySunnie@outlook.com>
2021-09-03 04:36:46 +0800

    Update code.

--- src/Sudoku.UI/Sudoku.UI/Globals.cs
+++ src/Sudoku.UI/Sudoku.UI/Globals.cs
@@ -1,10 +1,8 @@
 ﻿global using System;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Dynamic;
-global using System.Linq;
 global using System.Runtime.CompilerServices;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
-global using Microsoft.UI.Xaml.Navigation;
 global using Sudoku.UI.Pages.MainWindow;
 global using Sudoku.UI.Resources;
