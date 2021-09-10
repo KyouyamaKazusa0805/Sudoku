01e2b038325cba79a19c1221c0c70b06c1590a21
SunnieShine <HowdySunnie@outlook.com>
2021-09-10 21:55:10 +0800

    Update code.

--- src/Sudoku.UI/Sudoku.UI/Globals.cs
+++ src/Sudoku.UI/Sudoku.UI/Globals.cs
@@ -1,7 +1,9 @@
 ﻿global using System;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Dynamic;
 global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
 global using System.Runtime.Versioning;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
