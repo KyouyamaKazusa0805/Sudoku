63d1200e51259846ed3c978f75970844debf9074
SunnieShine <HowdySunnie@outlook.com>
2021-09-26 15:26:39 +0800

    Update doc comment.

--- src/Sudoku.CodeGenerating/Annotations/ToggleSwitchRouteAttribute.cs
+++ src/Sudoku.CodeGenerating/Annotations/ToggleSwitchRouteAttribute.cs
@@ -5,6 +5,37 @@
 /// that the preference item is bound with a <c>ToggleSwitch</c>, and creates a method
 /// that delegated to the XAML file.
 /// </summary>
+/// <remarks>
+/// <para>
+/// For example, in the type <c>Preference</c> we can find many properties
+/// that can both be settable and gettable. If I mark this attribute onto one property such as:
+/// <code><![CDATA[
+/// [ToggleSwitchRoute("ToggleSwitch_UseSizedFishName", "OptionItem_UseSizedFishName")]
+/// public bool UseSizedFishName { get; set; } = false;
+/// ]]></code>
+/// Then we will get the delegated method:
+/// <code><![CDATA[
+/// private void ToggleSwitch_UseSizedFishName_Toggled(object sender, RoutedEventArgs e)
+/// {
+///     if (
+///         (Sender: sender, PageIsInitialized: _pageIsInitialized) is (
+///             Sender: ToggleSwitch { IsOn: var isOn },
+///             PageIsInitialized: true
+///         )
+///     )
+///     {
+///         _boundSteps.Add(new(OptionItem_UseSizedFishName, () => _preference.UseSizedFishName = isOn));
+///     }
+/// }
+/// ]]></code>
+/// Where the field <c>_preference</c> and <c>_pageIsInitialized</c>
+/// you can find them in the <c>SettingsPage</c>.
+/// </para>
+/// <para>
+/// Please note that the method is a <see langword="private partial"/> method,
+/// which means you must manually write the declaration into the type.
+/// </para>
+/// </remarks>
 public sealed class ToggleSwitchRouteAttribute : PreferenceRouteAttribute
 {
 	/// <summary>
