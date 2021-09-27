fe6de18b271b6be83e5b8ab927e0f4b6467a1ebb
SunnieShine <HowdySunnie@outlook.com>
2021-09-27 21:35:24 +0800

    Add project `Sudoku.UI.CodeGenerating`.

---
+++ src/Sudoku.UI.CodeGenerating/DependencyObjectGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Sudoku.UI.CodeGenerating;
+
+/// <summary>
+/// Defines a source generator that generates the code for the dependency objects.
+/// </summary>
+//[Generator]
+public sealed class DependencyObjectGenerator : ISourceGenerator
+{
+	/// <inheritdoc/>
+	public void Execute(GeneratorExecutionContext context)
+	{
+	}
+
+	/// <inheritdoc/>
+	public void Initialize(GeneratorInitializationContext context)
+	{
+	}
+}
