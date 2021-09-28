b6c480d2b8280622943f6b27b96aff6155a00050
SunnieShine <HowdySunnie@outlook.com>
2021-09-28 14:10:07 +0800

    Update source generator.

--- src/Sudoku.CodeGenerating/Generators/AutoGetHashCodeGenerator.cs
+++ src/Sudoku.CodeGenerating/Generators/AutoGetHashCodeGenerator.cs
@@ -11,47 +11,38 @@ public sealed partial class AutoGetHashCodeGenerator : ISourceGenerator
 	public void Execute(GeneratorExecutionContext context)
 	{
 		var receiver = (SyntaxReceiver)context.SyntaxReceiver!;
-
 		var compilation = context.Compilation;
 		var attributeSymbol = compilation.GetTypeByMetadataName<AutoHashCodeAttribute>();
-		Func<ISymbol?, ISymbol?, bool> f = SymbolEqualityComparer.Default.Equals;
-		foreach (var type in
+
+		foreach (var (typeSymbol, attributeData) in
 			from type in receiver.Candidates
-			let model = compilation.GetSemanticModel(type.SyntaxTree)
-			select model.GetDeclaredSymbol(type)! into symbol
-			where symbol.GetAttributes().Any(a => f(a.AttributeClass, attributeSymbol))
-			select symbol)
+			let semanticModel = compilation.GetSemanticModel(type.SyntaxTree)
+			select semanticModel.GetDeclaredSymbol(type, context.CancellationToken)! into typeSymbol
+			let attributesData = typeSymbol.GetAttributes()
+			let attributeData = attributesData.FirstOrDefault(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, attributeSymbol))
+			where attributeData is { ConstructorArguments.IsDefaultOrEmpty: false }
+			select (typeSymbol, attributeData))
 		{
-			if (type.GetAttributeString(attributeSymbol) is not { } attributeStr)
-			{
-				continue;
-			}
-
-			int tokenStartIndex = attributeStr.IndexOf("({");
-			if (tokenStartIndex == -1)
-			{
-				continue;
-			}
-
-			if (attributeStr.GetMemberValues(tokenStartIndex) is not { Length: not 0 } members)
-			{
-				continue;
-			}
-
-			type.DeconstructInfo(
+			typeSymbol.DeconstructInfo(
 				true, out string fullTypeName, out string namespaceName, out string genericParametersList,
 				out _, out string typeKind, out string readonlyKeyword, out _
 			);
-			string hashCodeStr = string.Join(" ^ ", from member in members select $"{member}.GetHashCode()");
+
+			string hashCodeStr = string.Join(
+				" ^ ",
+				from member in attributeData.ConstructorArguments[0].Values
+				let memberValue = ((string)member.Value!).Trim()
+				select $"{memberValue}.GetHashCode()"
+			);
 
 			context.AddSource(
-				type.ToFileName(),
+				typeSymbol.ToFileName(),
 				"g",
 				$@"#nullable enable
 
 namespace {namespaceName};
 
-partial {typeKind}{type.Name}{genericParametersList}
+partial {typeKind}{typeSymbol.Name}{genericParametersList}
 {{
 	/// <inheritdoc cref=""object.GetHashCode""/>
 	[global::System.CodeDom.Compiler.GeneratedCode(""{GetType().FullName}"", ""{VersionValue}"")]
