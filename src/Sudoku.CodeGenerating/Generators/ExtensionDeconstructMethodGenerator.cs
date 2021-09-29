a598dd09cfb7889db9ee8791ce665b23be9249f1
SunnieShine <HowdySunnie@outlook.com>
2021-09-29 21:48:11 +0800

    Update source generator.

--- src/Sudoku.CodeGenerating/Generators/ExtensionDeconstructMethodGenerator.cs
+++ src/Sudoku.CodeGenerating/Generators/ExtensionDeconstructMethodGenerator.cs
@@ -9,95 +9,93 @@ public sealed partial class ExtensionDeconstructMethodGenerator : ISourceGenerat
 	/// <inheritdoc/>
 	public void Execute(GeneratorExecutionContext context)
 	{
-		var receiver = (SyntaxReceiver)context.SyntaxReceiver!;
 		var compilation = context.Compilation;
-		var nameDic = new Dictionary<string, int>();
+		var attributeSymbol = compilation
+			.GetTypeByMetadataName(typeof(AutoDeconstructExtensionAttribute<>).FullName)!
+			.ConstructUnboundGenericType();
 
-		foreach (var groupResult in
-			from attribute in receiver.Attributes
-			let argList = attribute.ArgumentList
-			where argList is { Arguments.Count: >= 1 }
-			select (Name: attribute.Name as GenericNameSyntax, attribute.ArgumentList) into pair
-			let typeArgList = pair.Name?.TypeArgumentList
-			where typeArgList is { Arguments.Count: 1 }
-			let firstTypeArg = typeArgList.Arguments[0]
-			let semanticModel = compilation.GetSemanticModel(firstTypeArg.SyntaxTree)
-			let type = semanticModel.GetTypeInfo(firstTypeArg, context.CancellationToken).Type
-			where type is not null
-			group (pair.ArgumentList, type) by type.ToDisplayString(FormatOptions.TypeFormat))
+		foreach (var groupedResult in
+			from attributeData in compilation.Assembly.GetAttributes()
+			let a = attributeData.AttributeClass
+			where a.IsGenericType
+			let unboundAttribute = a.ConstructUnboundGenericType()
+			where SymbolEqualityComparer.Default.Equals(unboundAttribute, attributeSymbol)
+			let typeArgs = a.TypeArguments
+			where !typeArgs.IsDefaultOrEmpty
+			let typeArg = typeArgs[0]
+			let typeArgConverted = typeArg as INamedTypeSymbol
+			where typeArgConverted is not null
+			let typeArgStr = typeArgConverted.ToDisplayString(FormatOptions.PropertyTypeFormat)
+			let arguments = attributeData.ConstructorArguments
+			where !arguments.IsDefaultOrEmpty
+			let argStrs = from arg in arguments[0].Values select ((string)arg.Value!).Trim()
+			let n = attributeData.TryGetNamedArgument(nameof(AutoDeconstructExtensionAttribute<object>.Namespace), out var na) ? ((string)na.Value!).Trim() : null
+			group (TypeArgument: typeArgConverted, Members: argStrs, Namespace: n) by typeArgStr)
 		{
-			string key = groupResult.Key;
-			foreach (var (p, typeSymbol) in groupResult)
+			var (typeArg, argStrs, n) = groupedResult.First();
+			context.AddSource(
+				typeArg.ToFileName(),
+				GeneratedFileShortcuts.ExtensionDeconstructionMethod,
+				$@"#pragma warning disable CS0618, CS1591
+
+#nullable enable
+
+namespace {n ?? typeArg.ContainingNamespace.ToDisplayString()};
+
+/// <summary>
+/// Provides the extension methods on this type.
+/// </summary>
+public static class {typeArg.Name}_DeconstructionMethods
+{{
+	{string.Join("\r\n\r\n\t", q())}
+}}
+"
+			);
+
+
+			IEnumerable<string> q()
 			{
-				_ = nameDic.TryGetValue(typeSymbol.Name, out int i);
-				string name = i == 0 ? typeSymbol.Name : $"{typeSymbol.Name}{i + 1}";
-				nameDic[typeSymbol.Name] = i + 1;
-				context.AddSource(
-					$"{name}",
-					GeneratedFileShortcuts.ExtensionDeconstructionMethod,
-					getDeconstructionCode(typeSymbol, p)
-				);
-			}
-		}
-
-
-		string getDeconstructionCode(ITypeSymbol symbol, AttributeArgumentListSyntax argList)
-		{
-			string? tempNamespace = argList.Arguments.FirstOrDefault(
-				static arg => arg.NameEquals?.Name.Identifier.ValueText == "Namespace"
-			)?.Expression.ToString();
-			string namespaceName = tempNamespace?.Substring(1, tempNamespace.Length - 2)
-				?? $"{symbol.ContainingNamespace}";
-			string typeName = symbol.Name;
-			symbol.DeconstructInfo(
-				out string fullTypeName, out _, out string genericParametersList,
-				out string genericParameterListWithoutConstraint, out string fullTypeNameWithoutConstraint,
-				out string constraint, out _
-			);
-			var members =
-				from arg in argList.Arguments
-				let expr = arg.Expression as InvocationExpressionSyntax
-				where expr.IsNameOfExpression() && expr!.ArgumentList.Arguments.Count == 1
-				let p = expr.ArgumentList.Arguments[0].ToString()
-				let lastDotTokenPos = p.LastIndexOf('.')
-				where lastDotTokenPos != -1
-				select p.Substring(lastDotTokenPos + 1);
-			string inModifier = symbol.TypeKind == TypeKind.Struct ? "in " : string.Empty;
-			string parameterList = string.Join(
-				", ",
-				from member in members
-				let memberFound = symbol.GetAllMembers().FirstOrDefault(m => m.Name == member)
-				where memberFound is not null
-				let memberType = memberFound.GetMemberType()
-				where memberType is not null
-				select $@"out {memberType} {member.ToCamelCase()}"
-			);
-			string assignments = string.Join(
-				"\r\n\t\t",
-				from member in members select $"{member.ToCamelCase()} = @this.{member};"
-			);
-			string deconstructMethods = $@"[global::System.CodeDom.Compiler.GeneratedCode(""{GetType().FullName}"", ""{VersionValue}"")]
+				foreach (var (typeArgument, arguments, namedArgumentNamespace) in groupedResult)
+				{
+					typeArgument.DeconstructInfo(
+						false, out _, out _, out _, out string genericParameterListWithoutConstraint,
+						out _, out _, out _
+					);
+					string fullTypeNameWithoutConstraint = typeArgument.ToDisplayString(FormatOptions.TypeFormat);
+					string constraint = fullTypeNameWithoutConstraint.IndexOf("where") is var index and not -1
+						? fullTypeNameWithoutConstraint.Substring(index)
+						: string.Empty;
+					string inModifier = typeArgument.TypeKind == TypeKind.Struct ? "in " : string.Empty;
+					string parameterList = string.Join(
+						", ",
+						from member in arguments
+						let memberFound = typeArgument.GetAllMembers().FirstOrDefault(m => m.Name == member)
+						where memberFound is not null
+						let memberType = memberFound.GetMemberType()
+						where memberType is not null
+						select $@"out {memberType} {member.ToCamelCase()}"
+					);
+					string assignments = string.Join(
+						"\r\n\t\t",
+						from member in arguments select $"{member.ToCamelCase()} = @this.{member};"
+					);
+					yield return $@"/// <summary>
+	/// Deconstruct the instance to multiple elements.
+	/// </summary>
+	[global::System.CodeDom.Compiler.GeneratedCode(""{GetType().FullName}"", ""{VersionValue}"")]
 	[global::System.Runtime.CompilerServices.CompilerGenerated]
 	[global::System.Runtime.CompilerServices.MethodImpl(global::System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
 	public static void Deconstruct{genericParameterListWithoutConstraint}(this {inModifier}{fullTypeNameWithoutConstraint} @this, {parameterList}){constraint}
 	{{
 		{assignments}
 	}}";
-
-			return $@"#pragma warning disable CS0618, CS1591
-
-#nullable enable
-
-namespace {namespaceName};
-
-public static partial class {typeName}_DeconstructionMethods
-{{
-	{deconstructMethods}
-}}
-";
+				}
+			}
 		}
 	}
 
 	/// <inheritdoc/>
-	public void Initialize(GeneratorInitializationContext context) => context.FastRegister<SyntaxReceiver>();
+	public void Initialize(GeneratorInitializationContext context)
+	{
+	}
 }
