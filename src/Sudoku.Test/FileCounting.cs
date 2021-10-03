680626675b0d980d4aee0de6ddd3b542c17799a6
SunnieShine <HowdySunnie@outlook.com>
2021-10-04 04:10:30 +0800

    Format code.

--- src/Sudoku.Test/FileCounting.cs
+++ src/Sudoku.Test/FileCounting.cs
@@ -14,24 +14,26 @@ internal static class FileCounting
 	/// <summary>
 	/// To count all files, and output the result using the <see cref="Console"/>.
 	/// </summary>
-	internal static void CountUp() => Console.WriteLine(
-		new FileCounter(
-			root: Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName,
-			extension: "cs",
-			withBinOrObjDirectory: false
-		).CountUp()
-	);
+	internal static void CountUp() =>
+		Console.WriteLine(
+			new FileCounter(
+				root: Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName,
+				extension: "cs",
+				withBinOrObjDirectory: false
+			).CountUp()
+		);
 
 	/// <summary>
 	/// To count all files, and output the result using the <see cref="Console"/> asynchronously.
 	/// </summary>
 	/// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
 	/// <returns>The task of the operation.</returns>
-	internal static async Task CountUpAsync(CancellationToken cancellationToken = default) => Console.WriteLine(
-		await new FileCounter(
-			root: Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName,
-			extension: "cs",
-			withBinOrObjDirectory: false
-		).CountUpAsync(cancellationToken)
-	);
+	internal static async Task CountUpAsync(CancellationToken cancellationToken = default) =>
+		Console.WriteLine(
+			await new FileCounter(
+				root: Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName,
+				extension: "cs",
+				withBinOrObjDirectory: false
+			).CountUpAsync(cancellationToken)
+		);
 }
