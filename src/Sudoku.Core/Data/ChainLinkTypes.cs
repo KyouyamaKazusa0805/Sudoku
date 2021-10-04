e03526d3ddc573c3eeccfd79d8150aa720dbce56
SunnieShine <HowdySunnie@outlook.com>
2021-10-04 19:27:41 +0800

    Update code.

--- src/Sudoku.Core/Data/ChainLinkTypes.cs
+++ src/Sudoku.Core/Data/ChainLinkTypes.cs
@@ -8,24 +8,20 @@ public static class ChainLinkTypes
 	/// <summary>
 	/// Indicates the default link.
 	/// </summary>
-	[ChainLinkTypeConstant(0)]
 	public static readonly ChainLinkType Default = new(0);
 
 	/// <summary>
 	/// Indicates the weak link.
 	/// </summary>
-	[ChainLinkTypeConstant(1)]
 	public static readonly ChainLinkType Weak = new(1);
 
 	/// <summary>
 	/// Indicates the strong link.
 	/// </summary>
-	[ChainLinkTypeConstant(2)]
 	public static readonly ChainLinkType Strong = new(2);
 
 	/// <summary>
 	/// Indicates the line link.
 	/// </summary>
-	[ChainLinkTypeConstant(3)]
 	public static readonly ChainLinkType Line = new(3);
 }
