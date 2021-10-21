438499580d7144799ff3e7ada3613fff469ccdc2
SunnieShine <HowdySunnie@outlook.com>
2021-10-21 14:10:10 +0800

    Use `IIncrementalGenerator`.

--- src/Sudoku.CodeGenerating/Generators/ExtensionDeconstructMethodGenerator.cs
+++ src/Sudoku.CodeGenerating/Generators/ExtensionDeconstructMethodGenerator.cs
@@ -1,45 +1,119 @@
-﻿namespace Sudoku.CodeGenerating.Generators;
+﻿using Gathered = System.ValueTuple<
+	Microsoft.CodeAnalysis.INamedTypeSymbol, // typeArg
+	System.Collections.Generic.IEnumerable<string>, // argStrs
+	string?, // customNamespace
+	string // typeArgStr
+>;
+using GatheredArray = System.Collections.Immutable.ImmutableArray<
+	System.Linq.IGrouping<
+		string,
+		System.ValueTuple<
+			Microsoft.CodeAnalysis.INamedTypeSymbol, // typeArg
+			System.Collections.Generic.IEnumerable<string>, // argStrs
+			string?, // customNamespace
+			string // typeArgStr
+		>
+	>
+>;
+
+namespace Sudoku.CodeGenerating.Generators;
 
 /// <summary>
 /// Provides a generator that generates the deconstruction methods that are extension methods.
 /// </summary>
 [Generator]
-public sealed partial class ExtensionDeconstructMethodGenerator : ISourceGenerator
+public sealed partial class ExtensionDeconstructMethodGenerator : IIncrementalGenerator, ISyntaxHandling<GatheredArray>
 {
+	/// <summary>
+	/// Indicates whether the current project has any assembly attribute.
+	/// </summary>
+	private volatile bool _hasAssemblyAttribute;
+
+
 	/// <inheritdoc/>
-	public void Execute(GeneratorExecutionContext context)
+	public void Initialize(IncrementalGeneratorInitializationContext context) =>
+		context.RegisterImplementationSourceOutput(
+			context.SyntaxProvider.CreateSyntaxProvider(WithAttributes, Transform),
+			SourceProduce
+		);
+
+	/// <inheritdoc cref="ISyntaxHandling{TStruct}.WithAttributes(SyntaxNode, CancellationToken)"/>
+	private bool WithAttributes(SyntaxNode n, CancellationToken _) =>
+		n is CompilationUnitSyntax { AttributeLists.Count: not 0, Members.Count: 0 }
+		&& !_hasAssemblyAttribute
+		&& (_hasAssemblyAttribute = true);
+
+	/// <inheritdoc cref="ISyntaxHandling{TStruct}.Transform(GeneratorSyntaxContext, CancellationToken)"/>
+	private GatheredArray? Transform(GeneratorSyntaxContext gsc, CancellationToken _)
 	{
-		var compilation = context.Compilation;
+		if (gsc is not { SemanticModel: { Compilation: var compilation } semanticModel })
+		{
+			return null;
+		}
+
+		var listOfPossibleResults = new List<Gathered>();
 		var attributeSymbol = compilation
 			.GetTypeByMetadataName(typeof(AutoDeconstructExtensionAttribute<>).FullName)!
 			.ConstructUnboundGenericType();
-
-		foreach (var groupedResult in
-			from attributeData in compilation.Assembly.GetAttributes()
-			let a = attributeData.AttributeClass
-			where a.IsGenericType
-			let unboundAttribute = a.ConstructUnboundGenericType()
-			where SymbolEqualityComparer.Default.Equals(unboundAttribute, attributeSymbol)
-			let typeArgs = a.TypeArguments
-			where !typeArgs.IsDefaultOrEmpty
-			let typeArg = typeArgs[0]
-			let typeArgConverted = typeArg as INamedTypeSymbol
-			where typeArgConverted is not null
-			let typeArgStr = typeArgConverted.ToDisplayString(TypeFormats.FullName)
-			let arguments = attributeData.ConstructorArguments
-			where !arguments.IsDefaultOrEmpty
-			let argStrs = from arg in arguments[0].Values select ((string)arg.Value!).Trim()
-			let n = attributeData.TryGetNamedArgument(nameof(AutoDeconstructExtensionAttribute<object>.Namespace), out var na) ? ((string)na.Value!).Trim() : null
-			group (TypeArgument: typeArgConverted, Members: argStrs, Namespace: n) by typeArgStr)
+		var attributesData = compilation.Assembly.GetAttributes();
+		foreach (var attributeData in attributesData)
 		{
-			var (typeArg, argStrs, n) = groupedResult.First();
+			if (
+				attributeData is not
+				{
+					AttributeClass:
+					{
+						IsGenericType: true,
+						TypeArguments: { IsDefaultOrEmpty: false } typeArgs
+					} a,
+					ConstructorArguments: { IsDefaultOrEmpty: false } ctorArgs
+				}
+			)
+			{
+				continue;
+			}
+
+			var unboundAttribute = a.ConstructUnboundGenericType();
+			if (!SymbolEqualityComparer.Default.Equals(unboundAttribute, attributeSymbol))
+			{
+				continue;
+			}
+
+			if (typeArgs[0] is not INamedTypeSymbol typeArg)
+			{
+				continue;
+			}
+
+			string typeArgStr = typeArg.ToDisplayString(TypeFormats.FullName);
+			var argStrs = from arg in ctorArgs[0].Values select ((string)arg.Value!).Trim();
+			string? customNamespace = attributeData.TryGetNamedArgument(
+				nameof(AutoDeconstructExtensionAttribute<object>.Namespace),
+				out var na
+			) ? ((string)na.Value!).Trim() : null;
+
+			listOfPossibleResults.Add((typeArg, argStrs, customNamespace, typeArgStr));
+		}
+
+		/*naked-value-tuple-name*/
+		return (from item in listOfPossibleResults group item by item.Item4).ToImmutableArray();
+	}
+
+	/// <inheritdoc cref="ISyntaxHandling{TStruct}.SourceProduce(SourceProductionContext, TStruct?)"/>
+	private void SourceProduce(SourceProductionContext spc, GatheredArray? gatheredArray)
+	{
+		if (!gatheredArray.HasValue)
+		{
+			return;
+		}
+
+		foreach (var groupedResult in gatheredArray)
+		{
+			var (typeArg, argStrs, n, _) = groupedResult.First();
 			string namespaceResult = n ?? typeArg.ContainingNamespace.ToDisplayString();
 			string typeResult = typeArg.Name;
 			string deconstructionMethodsCode = string.Join("\r\n\r\n\t", q());
-			context.AddSource(
-				typeArg.ToFileName(),
-				GeneratedFileShortcuts.ExtensionDeconstructionMethod,
-				$@"#nullable enable
+
+			string sourceCode = $@"{Configuration.GetNullableEnableString(GetType())}
 
 namespace {namespaceResult};
 
@@ -50,13 +124,13 @@ public static class {typeResult}_DeconstructionMethods
 {{
 	{deconstructionMethodsCode}
 }}
-"
-			);
+";
+			spc.AddSource(typeArg.ToFileName(), GeneratedFileShortcuts.ExtensionDeconstructionMethod, sourceCode);
 
 
 			IEnumerable<string> q()
 			{
-				foreach (var (typeArgument, arguments, namedArgumentNamespace) in groupedResult)
+				foreach (var (typeArgument, arguments, namedArgumentNamespace, _) in groupedResult)
 				{
 					typeArgument.DeconstructInfo(
 						false, out _, out _, out _, out string genericParameterListWithoutConstraint,
@@ -96,7 +170,17 @@ public static class {typeResult}_DeconstructionMethods
 	}
 
 	/// <inheritdoc/>
-	public void Initialize(GeneratorInitializationContext context)
-	{
-	}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	bool ISyntaxHandling<GatheredArray>.WithAttributes(SyntaxNode n, CancellationToken c) =>
+		WithAttributes(n, c);
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	GatheredArray? ISyntaxHandling<GatheredArray>.Transform(GeneratorSyntaxContext gsc, CancellationToken c) =>
+		Transform(gsc, c);
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	void ISyntaxHandling<GatheredArray>.SourceProduce(SourceProductionContext spc, GatheredArray? gathered) =>
+		SourceProduce(spc, gathered);
 }
