50b5e23a16bfdb25642ac20ec1384b85abbad6bc
SunnieShine <HowdySunnie@outlook.com>
2021-10-22 14:46:05 +0800

    Update code.

---
+++ src/System/IProxyEqualityComparer.cs
@@ -0,0 +1,16 @@
+﻿namespace System;
+
+/// <summary>
+/// Defines a type that can compare the equality with other instances.
+/// </summary>
+/// <typeparam name="T">The type to compare.</typeparam>
+internal interface IProxyEqualityComparer<in T>
+{
+	/// <summary>
+	/// Determines whether two <typeparamref name="T"/> instances hold the same value.
+	/// </summary>
+	/// <param name="left">The left-side-operator instance to compare.</param>
+	/// <param name="right">The right-side-operator instance to compare.</param>
+	/// <returns>A <see cref="bool"/> result.</returns>
+	static abstract bool Equals([NotNullIfNotNull("right")] T? left, [NotNullIfNotNull("left")] T? right);
+}
