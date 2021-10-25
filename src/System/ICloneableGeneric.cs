4f81ea69ca50f8fc2fc0a374a67ae91e39931b72
SunnieShine <HowdySunnie@outlook.com>
2021-10-25 22:27:04 +0800

    Update doc comment.

--- src/System/ICloneableGeneric.cs
+++ src/System/ICloneableGeneric.cs
@@ -1,6 +1,8 @@
 ﻿namespace System;
 
-/// <inheritdoc cref="ICloneable"/>
+/// <summary>
+/// Supports cloning, which creates a new instance of a class with the same value as an existing instance.
+/// </summary>
 /// <typeparam name="TClass">
 /// The type of this instance. This type should be only a class because the cloning
 /// operation is needed only in reference types, while the value types will be passed
@@ -14,7 +16,10 @@
 /// <seealso cref="ICloneable"/>
 public interface ICloneable<out TClass> : ICloneable where TClass : class
 {
-	/// <inheritdoc cref="ICloneable.Clone"/>
+	/// <summary>
+	/// Creates a new object of type <typeparamref name="TClass"/> that is a copy of the current instance.
+	/// </summary>
+	/// <returns>A new object of type <typeparamref name="TClass"/> that is a copy of this instance.</returns>
 	new TClass Clone();
 
 	/// <inheritdoc/>
