53216573fed89ad4fd6c35bb9ce4129c1d903fbf
SunnieShine <HowdySunnie@outlook.com>
2021-11-24 12:52:37 +0800

    Remove unused namespaces.

--- src/Sudoku.Diagnostics/Globals.cs
+++ src/Sudoku.Diagnostics/Globals.cs
@@ -1,13 +1,8 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.IO;
 global using System.Linq;
-global using System.Runtime.CompilerServices;
-global using System.Text;
-global using System.Text.RegularExpressions;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using System.Xml;
 global using static Sudoku.Constants;
