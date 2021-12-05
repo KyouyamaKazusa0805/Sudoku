80f0c5b3ee8e91ff8c6fec7aec727aa034ac71b7
SunnieShine <HowdySunnie@outlook.com>
2021-12-05 20:33:54 +0800

    Remove files.

--- src/Sudoku.Diagnostics.CodeAnalysis/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sudoku.Diagnostics.CodeAnalysis;
-
-/// <summary>
-/// Provides the constant values used in this project.
-/// </summary>
-internal static partial class Constants
-{
-}
--- src/Sudoku.Diagnostics.CodeAnalysis/Providers/GeneratorSyntaxContext_Dap.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Diagnostics.CodeAnalysis.Providers;
-
-[PrivatizeParameterlessConstructor]
-internal sealed partial class GeneratorSyntaxContext_Dap
-{
-	[DeconstructArgumentProvider]
-	internal static IOperation? Operation(GeneratorSyntaxContext generatorSyntaxContext) =>
-		generatorSyntaxContext.SemanticModel.GetOperation(generatorSyntaxContext.Node);
-}
