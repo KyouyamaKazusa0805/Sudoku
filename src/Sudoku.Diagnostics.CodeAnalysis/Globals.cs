64838d428e37a4387be0e6cd70477d120d59a403
SunnieShine <HowdySunnie@outlook.com>
2021-12-11 17:16:31 +0800

    Remove test code.

--- src/Sudoku.Diagnostics.CodeAnalysis/Globals.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Runtime.CompilerServices;
-global using Microsoft.CodeAnalysis;
-global using Microsoft.CodeAnalysis.CSharp;
-global using Microsoft.CodeAnalysis.CSharp.Syntax;
