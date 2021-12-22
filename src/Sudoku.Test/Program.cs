d846691394556e89b39df745b1eeb0e33e45e0aa
SunnieShine <HowdySunnie@outlook.com>
2021-12-22 17:11:11 +0800

    Update the test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,11 +1,8 @@
 ﻿using Sudoku.Data;
-using Sudoku.Diagnostics.CodeAnalysis;
 
 var grid = Grid.Parse("....6..3....8..2..2..5........29.5...76............1.......7.645...............7.");
-_ = grid.ToString(A);
+_ = grid.ToString();
 
 static partial class Program
 {
-	[IsRegex]
-	private static readonly string A = @"";
 }
