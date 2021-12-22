459b44cecc03f3068c19a9abeaacf6d7ac7fd83f
SunnieShine <HowdySunnie@outlook.com>
2021-12-22 10:48:03 +0800

    Update test code.

--- src/Sudoku.Test/Program.cs
+++ src/Sudoku.Test/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Sudoku.Data;
 using Sudoku.Diagnostics.CodeAnalysis;
 
-var grid = Grid.Parse("Test");
+var grid = Grid.Parse("....6..3....8..2..2..5........29.5...76............1.......7.645...............7.");
 _ = grid.ToString(A);
 
 static partial class Program
