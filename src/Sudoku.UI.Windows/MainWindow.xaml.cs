2dd708c4c32169af02d2f9c61b9fbf0154d18451
SunnieShine <HowdySunnie@outlook.com>
2022-01-05 17:01:59 +0800

    Add `Sudoku.UI.Windows` project.

---
+++ src/Sudoku.UI.Windows/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Sudoku.UI.Windows;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+	/// <summary>
+	/// Initializes a <see cref="MainWindow"/> instance.
+	/// </summary>
+	public MainWindow() => InitializeComponent();
+
+
+	private void myButton_Click(object sender, RoutedEventArgs e)
+	{
+		myButton.Content = "Clicked";
+	}
+}
