49100ff007d3b1d57569854bf8f393df6f00d716
SunnieShine <HowdySunnie@outlook.com>
2022-01-10 15:00:10 +0800

    Move enumerator files into `System.Collections.Generic`.

--- src/System/Collections/Generic/OneDimensionalArrayEnumerable.Enumerator.cs
+++ src/System/Collections/Generic/OneDimensionalArrayEnumerable.Enumerator.cs
@@ -1,4 +1,4 @@
-﻿namespace System;
+﻿namespace System.Collections.Generic;
 
 partial struct OneDimensionalArrayEnumerable<TStruct>
 {
--- src/System/Collections/Generic/OneDimensionalArrayEnumerable.cs
+++ src/System/Collections/Generic/OneDimensionalArrayEnumerable.cs
@@ -1,23 +1,25 @@
-﻿namespace System;
+﻿namespace System.Collections.Generic;
 
 /// <summary>
 /// Defines an enumerable collection that is only used for itertion on a one-dimensional array.
+/// Different with the default iterator, this iterator don't allow reference type in order to prevent
+/// any modifications on each element.
 /// </summary>
-/// <typeparam name="T">The type of the array elements.</typeparam>
-public readonly ref partial struct OneDimensionalArrayRefEnumerable<T>
+/// <typeparam name="TStruct">The type of the array elements.</typeparam>
+public partial struct OneDimensionalArrayEnumerable<TStruct> where TStruct : struct
 {
 	/// <summary>
 	/// Indicates the array to iterate.
 	/// </summary>
-	private readonly T[] _innerArray;
+	private readonly TStruct[] _innerArray;
 
 
 	/// <summary>
-	/// Initializes a <see cref="OneDimensionalArrayRefEnumerable{T}"/> instance with the specified array.
+	/// Initializes a <see cref="OneDimensionalArrayEnumerable{TStruct}"/> instance with the specified array.
 	/// </summary>
 	/// <param name="innerArray">The array to iterate.</param>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public OneDimensionalArrayRefEnumerable(T[] innerArray) => _innerArray = innerArray;
+	public OneDimensionalArrayEnumerable(TStruct[] innerArray) => _innerArray = innerArray;
 
 	/// <summary>
 	/// Gets the enumerator to iterate on each elements that is with the <see langword="ref"/> keyword.
