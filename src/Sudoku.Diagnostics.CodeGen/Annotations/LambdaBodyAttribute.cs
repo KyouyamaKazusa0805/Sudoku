9807798dc89d31668926bab91cfb9ffead2a37df
SunnieShine <HowdySunnie@outlook.com>
2022-01-10 10:47:00 +0800

    Remove unused files.

--- src/Sudoku.Diagnostics.CodeGen/Annotations/LambdaBodyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Diagnostics.CodeGen;
-
-/// <summary>
-/// Indicates the method is provided as a lambda conversion that is used by source generators.
-/// </summary>
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-public sealed class LambdaBodyAttribute : Attribute
-{
-}
