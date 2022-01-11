67d9343101cddd5a2ee3117234ab8a83f19f6b06
SunnieShine <HowdySunnie@outlook.com>
2022-01-11 10:34:32 +0800

    Remove unused global using.

--- src/Sudoku.Diagnostics.CodeGen/Globals.cs
+++ src/Sudoku.Diagnostics.CodeGen/Globals.cs
@@ -1,6 +1,5 @@
 ﻿global using System;
 global using System.Collections.Generic;
-global using System.Collections.Immutable;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Runtime.CompilerServices;
