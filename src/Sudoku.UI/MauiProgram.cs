a6235f5e5685497c95dafa669f9810fc375cc085
SunnieShine <HowdySunnie@outlook.com>
2022-01-12 21:33:45 +0800

    Update code.

--- src/Sudoku.UI/MauiProgram.cs
+++ src/Sudoku.UI/MauiProgram.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Maui.Controls.Hosting;
-using Microsoft.Maui.Hosting;
+﻿#pragma warning disable IDE0005
 
-#if WINDOWS
-using Microsoft.Maui;
-using Microsoft.Maui.LifecycleEvents;
-#endif
+using Microsoft.Maui; // Windows
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents; // Windows
+using Microsoft.UI.Xaml; // Windows
+using Sudoku.Diagnostics.CodeAnalysis; // Windows
 
 namespace Sudoku.UI;
 
@@ -17,25 +18,24 @@ public static class MauiProgram
 	/// Creates the MAUI application.
 	/// </summary>
 	/// <returns>The MAUI application instance.</returns>
-	public static MauiApp CreateMauiApp() =>
-		MauiApp
+	public static MauiApp CreateMauiApp()
+	{
+		return MauiApp
 			.CreateBuilder()
 			.UseMauiApp<App>()
-			.ConfigureFonts(static fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"))
+			.ConfigureFonts(static f => f.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"))
 #if WINDOWS
-			.ConfigureLifecycleEvents(
-				static lifecycle =>
-					lifecycle.AddWindows(
-						static windows =>
-							windows.OnLaunched(
-								static (_, _) =>
-								{
-									var app = (MauiWinUIWindow)MauiWinUIApplication.Current.Application.Windows[0].Handler!.NativeView!;
-									app.SetIcon("Resources/Icon/appicon.ico");
-								}
-							)
-					)
-			)
+			.ConfigureLifecycleEvents(static l => l.AddWindows(static w => w.OnLaunched(setIcon)))
 #endif
 			.Build();
+
+
+#if WINDOWS
+		static void setIcon([IsDiscard] Application _, [IsDiscard] LaunchActivatedEventArgs __)
+		{
+			var app = (MauiWinUIWindow)MauiWinUIApplication.Current.Application.Windows[0].Handler!.NativeView!;
+			app.SetIcon("Resources/Icon/appicon.ico");
+		}
+#endif
+	}
 }
