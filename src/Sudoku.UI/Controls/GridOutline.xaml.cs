26dd46ca596a97094dadc148887049bcbf132122
SunnieShine <HowdySunnie@outlook.com>
2022-01-12 21:53:08 +0800

    Extract the control.

---
+++ src/Sudoku.UI/Controls/GridOutline.xaml.cs
@@ -0,0 +1,12 @@
+namespace Sudoku.UI.Controls;
+
+/// <summary>
+/// Indicates the grid outline.
+/// </summary>
+public partial class GridOutline
+{
+	/// <summary>
+	/// Initializes a <see cref="GridOutline"/> instance.
+	/// </summary>
+	public GridOutline() => InitializeComponent();
+}
