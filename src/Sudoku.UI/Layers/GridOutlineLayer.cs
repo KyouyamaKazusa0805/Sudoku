2f722c36b180158fe7c347b25e6c20c5ee1d4591
SunnieShine <HowdySunnie@outlook.com>
2022-01-12 16:35:32 +0800

    Add default graphics view.

---
+++ src/Sudoku.UI/Layers/GridOutlineLayer.cs
@@ -0,0 +1,66 @@
+﻿namespace Sudoku.UI.Layers;
+
+/// <summary>
+/// Defines the grid outline layer that only handles and draws the outlines of the grid,
+/// i.e. the block outlines and the cell outlines.
+/// </summary>
+public sealed class GridOutlineLayer : Layer
+{
+	/// <inheritdoc/>
+	public GridOutlineLayer() : base() { }
+
+
+	/// <inheritdoc/>
+	public override void Draw(ICanvas canvas, RectangleF dirtyRect)
+	{
+		const float epsilon = 1E-3F;
+
+		_ = dirtyRect is { Width: var width, Height: var height };
+		if (width.NearlyEquals(0, epsilon) || height.NearlyEquals(0, epsilon))
+		{
+			throw new ArgumentException("The width or height value is too small.");
+		}
+
+		if (!width.NearlyEquals(height))
+		{
+			throw new ArgumentException("The drawing must require the width equals to the height.");
+		}
+
+		canvas.StrokeLineJoin = LineJoin.Miter;
+		canvas.StrokeColor = Colors.Black;
+
+		float gridSize = width - (Offset << 1);
+		float blockSize = gridSize / 3;
+		float cellSize = gridSize / 9;
+		//float candidateSize = gridSize / 27;
+
+		// Draw block outlines.
+		canvas.StrokeSize = 3F;
+		for (int i = 0; i < 4; i++) // Row index.
+		{
+			for (int j = 0; j < 4; j++) // Column index.
+			{
+				canvas.DrawLine(Offset, Offset + blockSize * j, width - Offset, Offset + blockSize * j);
+				canvas.DrawLine(Offset + blockSize * i, Offset, Offset + blockSize * i, width - Offset);
+			}
+		}
+
+		// Draw cell outlines.
+		canvas.StrokeSize = 1F;
+		for (int i = 0; i < 10; i++) // Row index.
+		{
+			for (int j = 0; j < 10; j++) // Column index.
+			{
+				// We can omit the cascaded cell outlines
+				// that has been already drawn as the block outlines.
+				if (i % 3 == 0 || j % 3 == 0)
+				{
+					continue;
+				}
+
+				canvas.DrawLine(Offset, Offset + cellSize * j, width - Offset, Offset + cellSize * j);
+				canvas.DrawLine(Offset + cellSize * i, Offset, Offset + cellSize * i, width - Offset);
+			}
+		}
+	}
+}
--- src/Sudoku.UI/MauiProgram.cs
+++ src/Sudoku.UI/MauiProgram.cs
@@ -14,5 +14,22 @@ public static class MauiProgram
 			.CreateBuilder()
 			.UseMauiApp<App>()
 			.ConfigureFonts(static fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"))
+#if WINDOWS && false
+			.ConfigureLifecycleEvents(
+				static lifecycle =>
+				{
+					lifecycle.AddWindows(
+						static windows =>
+							windows.OnLaunched(
+								static (app, args) =>
+								{
+									var winuiApp = (Microsoft.UI.Xaml.Window)MauiWinUIApplication.Current.Application.Windows[0].Handler!.NativeView!;
+									winuiApp.SetIcon("Platforms/Windows/trayicon.ico");
+								}
+							)
+					);
+				}
+			)
+#endif
 			.Build();
 }
