64a828dd672964e95a712d66e19f4be41462cd51
SunnieShine <HowdySunnie@outlook.com>
2022-01-13 19:17:50 +0800

    Rename type to `GridPane`.

--- src/Sudoku.UI/Controls/GridPane.xaml.cs
+++ src/Sudoku.UI/Controls/GridPane.xaml.cs
@@ -1,9 +1,11 @@
+using Microsoft.Maui.Controls;
+
 namespace Sudoku.UI.Controls;
 
 /// <summary>
 /// Indicates the grid pane.
 /// </summary>
-public partial class GridPane
+public partial class GridPane : GridLayout
 {
 	/// <summary>
 	/// Initializes a <see cref="GridPane"/> instance.
