696280ecc53d162cd479a7b217ac8111e106d8fa
SunnieShine <HowdySunnie@outlook.com>
2022-01-24 22:47:51 +0800

    Remove MAUI projects.

--- src/Sudoku.Graphics/PictureSizeChangedEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sudoku.Graphics;
-
-/// <summary>
-/// Indicates the event handler that handles the event being triggered when the picture size is changed.
-/// </summary>
-/// <param name="sender">The object that triggers the event.</param>
-/// <param name="e">The event data provided.</param>
-public delegate void PictureSizeChangedEventHandler(GridCanvas sender, PictureSizeChangedEventArgs e);
