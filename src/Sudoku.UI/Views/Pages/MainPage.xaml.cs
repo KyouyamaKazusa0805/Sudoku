49d15eb0326ac077e42d6962274018edb8e93b3d
SunnieShine <HowdySunnie@outlook.com>
2022-01-28 16:03:22 +0800

    Sets the `MainPage` as the inner control of `MainWindow`.

--- src/Sudoku.UI/Views/Pages/MainPage.xaml.cs
+++ src/Sudoku.UI/Views/Pages/MainPage.xaml.cs
@@ -7,7 +7,98 @@
 public sealed partial class MainPage : Page
 {
 	/// <summary>
+	/// Indicates the navigation pairs that controls to route pages.
+	/// </summary>
+	private readonly (string ViewItemTag, Type PageType)[] _navigationPairs =
+	{
+		(nameof(SettingsPage), typeof(SettingsPage)),
+		(nameof(AboutPage), typeof(AboutPage))
+	};
+
+
+	/// <summary>
 	/// Initializes a <see cref="MainPage"/> instance.
 	/// </summary>
 	public MainPage() => InitializeComponent();
+
+
+	/// <summary>
+	/// Try to navigate the pages.
+	/// </summary>
+	/// <param name="tag">The specified tag of the navigate page item.</param>
+	/// <param name="transitionInfo">The transition information.</param>
+	private void OnNavigate(string tag, NavigationTransitionInfo transitionInfo)
+	{
+		var (_, pageType) = _navigationPairs.FirstOrDefault(p => p.ViewItemTag == tag);
+
+		// Get the page type before navigation so you can prevent duplicate entries in the backstack.
+		// Only navigate if the selected page isn't currently loaded.
+		var preNavPageType = _viewRouterFrame.CurrentSourcePageType;
+		if (pageType is not null && preNavPageType != pageType)
+		{
+			_viewRouterFrame.Navigate(pageType, null, transitionInfo);
+		}
+	}
+
+
+	private void ViewRouterFrame_NavigationFailed([IsDiscard] object sender, NavigationFailedEventArgs e) =>
+		throw new InvalidOperationException($"Cannot find the page '{e.SourcePageType.FullName}'.");
+
+	private void ViewRouterFrame_Navigated(object sender, NavigationEventArgs e)
+	{
+		if (
+			(Sender: sender, EventArg: e, Router: _viewRouter) is not (
+				Sender: Frame { SourcePageType: not null },
+				EventArg: { SourcePageType: var sourcePageType },
+				Router: { MenuItems: var menuItems, FooterMenuItems: var footerMenuItems }
+			)
+		)
+		{
+			return;
+		}
+
+		var (tag, _) = _navigationPairs.FirstOrDefault(tagSelector);
+		var item = menuItems.Concat(footerMenuItems).OfType<NavigationViewItem>().First(itemSelector);
+		_viewRouter.SelectedItem = item;
+		_viewRouter.Header = item.Content?.ToString();
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		bool tagSelector((string, Type PageType) p) => p.PageType == sourcePageType;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		bool itemSelector(NavigationViewItem n) => n.Tag as string == tag;
+	}
+
+	private void ViewRouter_ItemInvoked([IsDiscard] NavigationView sender, NavigationViewItemInvokedEventArgs args)
+	{
+		if (
+			args is not
+			{
+				InvokedItemContainer.Tag: string itemTag,
+				RecommendedNavigationTransitionInfo: var info
+			}
+		)
+		{
+			return;
+		}
+
+		OnNavigate(itemTag, info);
+	}
+
+	private void ViewRouter_SelectionChanged([IsDiscard] NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+	{
+		if (
+			args is not
+			{
+				SelectedItemContainer.Tag: string itemTag,
+				RecommendedNavigationTransitionInfo: var info
+			}
+		)
+		{
+			return;
+		}
+
+		OnNavigate(itemTag, info);
+	}
 }
