ef558dad26f201755ef7a5f28fbda58e9fbeba1b
SunnieShine <HowdySunnie@outlook.com>
2022-02-18 13:20:20 +0800

    Remove unused files.

--- src/Sudoku.UI/Models/GridChangedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.UI.Models;
-
-/// <summary>
-/// Defines a delegate type that holds a set of methods that is invoked when the corresponding
-/// event is triggered.
-/// </summary>
-/// <param name="sender">The object that triggers the corresponding event.</param>
-/// <param name="e">The data provided.</param>
-public delegate void GridChangedEventHandler(object sender, GridChangedEventArgs e);
