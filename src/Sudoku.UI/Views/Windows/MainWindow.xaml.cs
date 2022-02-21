587405a07442d5e6e26147931e9c43c99694ab2f
SunnieShine <HowdySunnie@outlook.com>
2022-02-21 16:42:36 +0800

    Customized title bar (Fixes 223).

--- src/Sudoku.UI/Views/Windows/MainWindow.xaml.cs
+++ src/Sudoku.UI/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace Sudoku.UI.Views.Windows;
+﻿#if TITLE_BAR_CUSTOMIZATION
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+#endif
+
+namespace Sudoku.UI.Views.Windows;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a <see cref="Frame"/>.
@@ -6,8 +12,59 @@
 /// <seealso cref="Frame"/>
 public sealed partial class MainWindow : Window
 {
+#if TITLE_BAR_CUSTOMIZATION
+	/// <summary>
+	/// The app window instance.
+	/// </summary>
+	private readonly AppWindow _appWindow;
+#endif
+
+
 	/// <summary>
 	/// Initializes a <see cref="MainWindow"/> instance.
 	/// </summary>
-	public MainWindow() => InitializeComponent();
+	public MainWindow()
+	{
+		// Intializes the controls.
+		InitializeComponent();
+
+#if TITLE_BAR_CUSTOMIZATION
+		// Title bar customization.
+		_appWindow = getAppWindowForCurrentWindow();
+
+		// Check to see if customization is supported.
+		// Currently only supported on Windows 11.
+		if (AppWindowTitleBar.IsCustomizationSupported() && _appWindow.TitleBar is var titleBar)
+		{
+			// Hide default title bar.
+			titleBar.ExtendsContentIntoTitleBar = true;
+
+			// Sets the background color on "those" three buttons to transparent.
+			titleBar.ButtonBackgroundColor = Colors.Transparent;
+			titleBar.ButtonForegroundColor = Colors.Black;
+			titleBar.ButtonHoverBackgroundColor = Colors.Transparent;
+			titleBar.ButtonHoverForegroundColor = Colors.Black;
+			titleBar.ButtonPressedBackgroundColor = Colors.Transparent;
+			titleBar.ButtonPressedForegroundColor = Colors.Black;
+			titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+			titleBar.ButtonInactiveForegroundColor = Colors.Gray;
+		}
+		else
+		{
+			// Title bar customization using these APIs is currently
+			// supported only on Windows 11. In other cases, hide
+			// the custom title bar element.
+			_cAppTitleBar.Visibility = Visibility.Collapsed;
+		}
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		AppWindow getAppWindowForCurrentWindow()
+		{
+			var hWnd = WindowNative.GetWindowHandle(this);
+			var wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+			return AppWindow.GetFromWindowId(wndId);
+		}
+#endif
+	}
 }
