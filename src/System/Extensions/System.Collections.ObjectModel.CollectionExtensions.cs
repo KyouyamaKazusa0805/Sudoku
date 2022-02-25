f724b701749c14eeae0576f308502998dde5e8ee
SunnieShine <HowdySunnie@outlook.com>
2022-02-25 16:49:25 +0800

    Update the method signature.

--- src/System/Extensions/System.Collections.ObjectModel.CollectionExtensions.cs
+++ src/System/Extensions/System.Collections.ObjectModel.CollectionExtensions.cs
@@ -10,8 +10,12 @@ public static class CollectionExtensions
 	/// Prepends the specified element into the collection.
 	/// </summary>
 	/// <typeparam name="T">The type of the element.</typeparam>
+	/// <typeparam name="TDerived">
+	/// The type of the element to be prepended. The type must be derived from <typeparamref name="T"/>.
+	/// </typeparam>
 	/// <param name="this">The collection.</param>
 	/// <param name="element">The element to be prepended.</param>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static void Prepend<T>(this Collection<T> @this, T element) => @this.Insert(0, element);
+	public static void Prepend<T, TDerived>(this Collection<T> @this, TDerived element) where TDerived : T =>
+		@this.Insert(0, element);
 }
