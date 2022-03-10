fb4ce6ef2d49c8c9edd73d5dded0ff25bd285a57
SunnieShine <HowdySunnie@outlook.com>
2022-03-11 00:04:51 +0800

    Add comments.

--- src/Sudoku.Test/Searcher.cs
+++ src/Sudoku.Test/Searcher.cs
@@ -8,34 +8,36 @@ namespace Sudoku.Test;
 internal sealed class Searcher
 {
 	private const string Separator = ", ";
-
 	private readonly Dictionary<int, HashSet<int>?> _strongInferences = new();
 	private readonly Dictionary<int, HashSet<int>?> _weakInferences = new();
 	private readonly Dictionary<int, Node> _id2NodeLookup = new();
 	private readonly Dictionary<Node, int> _node2IdLookup = new();
 	private readonly ITestOutputHelper _output;
-	private int _globalId = 0;
 
 	public Searcher(ITestOutputHelper output) => _output = output;
 
 	public void GetAll(in Grid grid)
 	{
+		// Clear all possible lists.
 		_strongInferences.Clear();
 		_weakInferences.Clear();
 		_id2NodeLookup.Clear();
 		_node2IdLookup.Clear();
 
+		// Gather the basic information on basic nodes.
+		int id = 0;
 		foreach (int candidate in grid)
 		{
 			var node = new SoleCandidateNode((byte)(candidate / 9), (byte)(candidate % 9));
-			_id2NodeLookup.Add(_globalId, node);
-			_node2IdLookup.Add(node, _globalId);
-			_strongInferences.Add(_globalId, null);
-			_weakInferences.Add(_globalId, null);
+			_id2NodeLookup.Add(id, node);
+			_node2IdLookup.Add(node, id);
+			_strongInferences.Add(id, null);
+			_weakInferences.Add(id, null);
 
-			_globalId++;
+			id++;
 		}
 
+		// Then checks and searches for the strong and weak inferences by each candidate.
 		foreach (int candidate in grid)
 		{
 			var node = new SoleCandidateNode((byte)(candidate / 9), (byte)(candidate % 9));
@@ -43,9 +45,11 @@ internal sealed class Searcher
 			GetWeak(node, grid, candidate);
 		}
 
-		// Tests.
+		// Test the result and output the test result.
 		PrintStringInferences(_strongInferences);
 		PrintStringInferences(_weakInferences);
+
+		// TODO: Construct chains.
 	}
 
 	private void GetStrong(Node currentNode, in Grid grid, int candidate)
