51383b4daccd5a7f87cd02e067029918e51a266d
SunnieShine <HowdySunnie@outlook.com>
2022-03-20 20:47:56 +0800

    Update code.

--- src/Sudoku.Test/NodeType.cs
+++ src/Sudoku.Test/NodeType.cs
@@ -9,17 +9,23 @@ public enum NodeType
 	/// Indicates the node type is a sole candidate.
 	/// </summary>
 	[NodeTypeName<NodeType>("Sole candidate")]
+	[MemoryUsageEvaluation<NodeType>(20)]
+	[TimeElapsedEvaluation<NodeType>(80)]
 	Sole,
 
 	/// <summary>
 	/// Indicates the node type is a locked candidates.
 	/// </summary>
 	[NodeTypeName<NodeType>("Locked candidates node")]
+	[MemoryUsageEvaluation<NodeType>(60)]
+	[TimeElapsedEvaluation<NodeType>(200)]
 	LockedCandidates,
 
 	/// <summary>
 	/// Indicates the node type is an almost locked sets.
 	/// </summary>
 	[NodeTypeName<NodeType>("Almost locked sets node")]
+	[MemoryUsageEvaluation<NodeType>(60)]
+	[TimeElapsedEvaluation<NodeType>(700)]
 	AlmostLockedSets,
 }
