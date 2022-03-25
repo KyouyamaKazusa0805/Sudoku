bff8aaff873e759fcdfef4f91ffb4ce67dad02a8
SunnieShine <HowdySunnie@outlook.com>
2022-03-25 21:37:50 +0800

    Move files from `Sudoku.Test` to `Sudoku.Solving`.

---
+++ src/Sudoku.Solving/Solving/Collections/NodeType.cs
@@ -0,0 +1,25 @@
+﻿namespace Sudoku.Solving.Collections;
+
+/// <summary>
+/// Defines a node type.
+/// </summary>
+public enum NodeType
+{
+	/// <summary>
+	/// Indicates the node type is a sole candidate.
+	/// </summary>
+	[NodeTypeName("Sole candidate")]
+	Sole,
+
+	/// <summary>
+	/// Indicates the node type is a locked candidates.
+	/// </summary>
+	[NodeTypeName("Locked candidates")]
+	LockedCandidates,
+
+	/// <summary>
+	/// Indicates the node type is an almost locked sets.
+	/// </summary>
+	[NodeTypeName("Almost locked set")]
+	AlmostLockedSets,
+}
