75263c6035f9723c46eaa9ff12044ef60105e783
SunnieShine <HowdySunnie@outlook.com>
2022-03-26 16:46:08 +0800

    Add types.

---
+++ src/Sudoku.Core/Presentation/View.cs
@@ -0,0 +1,96 @@
+﻿namespace Sudoku.Presentation;
+
+/// <summary>
+/// Provides with a data structure that displays a view for basic information.
+/// </summary>
+public sealed class View : ICloneable, IEnumerable<ViewNode>
+{
+	/// <summary>
+	/// Indicates the inner dictionary.
+	/// </summary>
+	private readonly List<ViewNode> _nodes = new();
+
+
+	/// <summary>
+	/// Creates an empty <see cref="View"/> instance.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public View()
+	{
+	}
+
+	/// <summary>
+	/// Initializes a <see cref="View"/> instance via the specified list as the raw value.
+	/// </summary>
+	/// <param name="nodes">The list as the raw value.</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private View(List<ViewNode> nodes) => _nodes = nodes;
+
+
+	/// <summary>
+	/// Indicates the number of elements stored in the current collection.
+	/// </summary>
+	public int Count => _nodes.Count;
+
+
+	/// <summary>
+	/// Gets the <see cref="ViewNode"/> at the specified position.
+	/// </summary>
+	/// <param name="index">The index.</param>
+	/// <returns>The <see cref="ViewNode"/> instance.</returns>
+	public ViewNode this[int index] => _nodes[index];
+
+
+	/// <summary>
+	/// Adds the specified <see cref="ViewNode"/> into the collection.
+	/// </summary>
+	/// <param name="node">The <see cref="ViewNode"/> instance.</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Add(ViewNode node)
+	{
+		if (!_nodes.Contains(node))
+		{
+			_nodes.Add(node);
+		}
+	}
+
+	/// <summary>
+	/// Adds a serial of elements into the collection.
+	/// </summary>
+	/// <param name="nodes">The <see cref="ViewNode"/> instances to be added.</param>
+	public void AddRange(IEnumerable<ViewNode> nodes)
+	{
+		foreach (var node in nodes)
+		{
+			Add(node);
+		}
+	}
+
+	/// <summary>
+	/// Determines whether an element is in the current collection.
+	/// </summary>
+	/// <param name="node">The node.</param>
+	/// <returns>A <see cref="bool"/> result indicating that.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool Contains(ViewNode node) => _nodes.Contains(node);
+
+	/// <inheritdoc cref="ICloneable.Clone"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public View Clone() => Count == 0 ? new() : new(new(from node in _nodes select node.Clone()));
+
+	/// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public List<ViewNode>.Enumerator GetEnumerator() => _nodes.GetEnumerator();
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	object ICloneable.Clone() => Clone();
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	IEnumerator<ViewNode> IEnumerable<ViewNode>.GetEnumerator() => GetEnumerator();
+}
---
+++ src/Sudoku.Core/Presentation/ViewNode.cs
@@ -0,0 +1,71 @@
+﻿namespace Sudoku.Presentation;
+
+/// <summary>
+/// Defines a view node.
+/// </summary>
+public abstract class ViewNode :
+	ICloneable,
+	IEquatable<ViewNode>
+#if FEATURE_GENERIC_MATH
+	,
+	IEqualityOperators<ViewNode, ViewNode>
+#endif
+{
+	/// <summary>
+	/// Indicates the identifier used.
+	/// </summary>
+	protected readonly Identifier _identifier;
+
+
+	/// <summary>
+	/// Assigns the <see cref="Identifier"/> instance as the basic information.
+	/// </summary>
+	/// <param name="identifier">The <see cref="Identifier"/> instance.</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	protected ViewNode(Identifier identifier) => _identifier = identifier;
+
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public sealed override bool Equals([NotNullWhen(true)] object? obj) =>
+		obj is ViewNode comparer && Equals(comparer);
+
+	/// <inheritdoc/>
+	public abstract bool Equals([NotNullWhen(true)] ViewNode? other);
+
+	/// <inheritdoc/>
+	public abstract override int GetHashCode();
+
+	/// <inheritdoc/>
+	public abstract override string ToString();
+
+	/// <summary>
+	/// Creates a new instance whose inner data is totally same as the current instance.
+	/// </summary>
+	/// <returns>The result <see cref="ViewNode"/> as the copy.</returns>
+	public abstract ViewNode Clone();
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	object ICloneable.Clone() => Clone();
+
+
+	/// <summary>
+	/// Determines whether two <see cref="ViewNode"/>s are same type, and hold a same value.
+	/// </summary>
+	/// <param name="left">The first instance to be compared.</param>
+	/// <param name="right">The second instance to be compared.</param>
+	/// <returns>A <see cref="bool"/> result indicating that.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool operator ==(ViewNode? left, ViewNode? right) =>
+		(left, right) switch { (null, null) => true, (not null, not null) => left.Equals(right), _ => false };
+
+	/// <summary>
+	/// Determines whether two <see cref="ViewNode"/>s are not same type, or not totally hold a same value.
+	/// </summary>
+	/// <param name="left">The first instance to be compared.</param>
+	/// <param name="right">The second instance to be compared.</param>
+	/// <returns>A <see cref="bool"/> result indicating that.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool operator !=(ViewNode? left, ViewNode? right) => !(left == right);
+}
