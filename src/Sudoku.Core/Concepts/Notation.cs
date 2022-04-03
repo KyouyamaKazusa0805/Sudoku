d307b4327587b4dc13923a89f65fd86dc802d5a9
SunnieShine <HowdySunnie@outlook.com>
2022-04-03 16:51:51 +0800

    Add type.

---
+++ src/Sudoku.Core/Concepts/Notation.cs
@@ -0,0 +1,22 @@
+﻿namespace Sudoku.Concepts;
+
+/// <summary>
+/// Represents a notation.
+/// </summary>
+public enum Notation : byte
+{
+	/// <summary>
+	/// Indicates the RxCy notation.
+	/// </summary>
+	RxCy,
+
+	/// <summary>
+	/// Indicates the K9 notation.
+	/// </summary>
+	K9,
+
+	/// <summary>
+	/// Indicates the notation that is used by the program called Hodoku, as the eliminations.
+	/// </summary>
+	HodokuElimination
+}
--- src/Sudoku.Core/Concepts/Notations/INotation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sudoku.Concepts.Notations;
-
-/// <summary>
-/// Defines an notation that obeys the rule of the sudoku basic elements.
-/// </summary>
-public interface INotation
-{
-}
