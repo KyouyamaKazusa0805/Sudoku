67ecf3f797e837ecad345a9af27291d90aacb81b
SunnieShine <HowdySunnie@outlook.com>
2022-04-17 14:33:56 +0800

    Update code.

--- src/Sudoku.Core/Concepts/Solving/ConclusionTypeExtensions.cs
+++ src/Sudoku.Core/Concepts/Solving/ConclusionTypeExtensions.cs
@@ -11,16 +11,7 @@ public static class ConclusionTypeExtensions
 	/// </summary>
 	/// <param name="this">The conclusion type.</param>
 	/// <returns>The notation of the conclusion type.</returns>
-	/// <exception cref="ArgumentOutOfRangeException">
-	/// Throws when the argument is not <see cref="ConclusionType.Assignment"/>
-	/// or <see cref="ConclusionType.Elimination"/>.
-	/// </exception>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static string GetNotation(this ConclusionType @this) =>
-		@this switch
-		{
-			ConclusionType.Assignment => " = ",
-			ConclusionType.Elimination => " <> ",
-			_ => throw new ArgumentOutOfRangeException(nameof(@this))
-		};
+		typeof(ConclusionType).GetField(@this.ToString())!.GetCustomAttribute<EnumFieldNameAttribute>()!.Name;
 }
