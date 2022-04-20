0bfc638b81ddb886b6b27e3a67dfb536083945eb
SunnieShine <HowdySunnie@outlook.com>
2022-04-20 19:53:33 +0800

    Add type `CommandLineParserException`.

---
+++ src/System/CommandLine/CommandLineParserException.cs
@@ -0,0 +1,20 @@
+﻿namespace System.CommandLine;
+
+/// <summary>
+/// Defines the command line parser exception.
+/// </summary>
+public sealed class CommandLineParserException : Exception
+{
+	/// <summary>
+	/// Initializes a <see cref="CommandLineParserException"/> instance via the specified error case.
+	/// </summary>
+	/// <param name="errorCode">The error code.</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public CommandLineParserException(ParserError errorCode) : base() => ErrorCode = errorCode;
+
+
+	/// <summary>
+	/// Indicates the error code.
+	/// </summary>
+	public ParserError ErrorCode { get; }
+}
---
+++ src/System/CommandLine/ParserError.cs
@@ -0,0 +1,8 @@
+﻿namespace System.CommandLine;
+
+/// <summary>
+/// Defines a parser error.
+/// </summary>
+public enum ParserError
+{
+}
