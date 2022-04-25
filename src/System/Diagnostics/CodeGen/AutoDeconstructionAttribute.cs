da4302837b7778fed42e0a29c8bba21b4ff5169b
SunnieShine <HowdySunnie@outlook.com>
2022-04-25 16:13:19 +0800

    Update code.

--- src/System/Diagnostics/CodeGen/AutoDeconstructionAttribute.cs
+++ src/System/Diagnostics/CodeGen/AutoDeconstructionAttribute.cs
@@ -10,18 +10,44 @@ public sealed class AutoDeconstructionAttribute : Attribute
 {
 	/// <summary>
 	/// Initializes an <see cref="AutoDeconstructionAttribute"/> instance
-	/// via the specified property names you want to deconstruct.
+	/// via the specified member expressions you want to deconstruct.
 	/// </summary>
-	/// <param name="propertyNames">The name of the properties you want to deconstruct.</param>
+	/// <param name="memberExpression">The name of the members you want to deconstruct.</param>
 	/// <exception cref="ArgumentException">Throws when the argument is empty.</exception>
-	public AutoDeconstructionAttribute(params string[] propertyNames)
-		=> PropertyNames = propertyNames is []
+	public AutoDeconstructionAttribute(params string[] memberExpression)
+		=> MemberExpression = memberExpression is []
 			? throw new ArgumentException("You must set at least one instance to be deconstructed.")
-			: propertyNames;
+			: memberExpression;
 
 
 	/// <summary>
-	/// Indicates the property names whose corresponding properties will be able to be deconstructed.
+	/// <para>
+	/// Indicates whether the source generator will generate the current deconstruction method as extension ones.
+	/// </para>
+	/// <para>The default value is <see langword="false"/>.</para>
 	/// </summary>
-	public string[] PropertyNames { get; }
+	public bool GenerateAsExtension { get; init; }
+
+	/// <summary>
+	/// Indicates the suffix of the generated file. The property is available when the property
+	/// <see cref="GenerateAsExtension"/> is <see langword="true"/>.
+	/// </summary>
+	public string GeneratedFileNameSuffix
+	{
+		get
+		{
+			var hashCode = new HashCode();
+			foreach (string propertyName in MemberExpression)
+			{
+				hashCode.Add(propertyName);
+			}
+
+			return $"Extensions_{hashCode.ToHashCode():X}";
+		}
+	}
+
+	/// <summary>
+	/// Indicates the member names whose corresponding members will be able to be deconstructed.
+	/// </summary>
+	public string[] MemberExpression { get; }
 }
