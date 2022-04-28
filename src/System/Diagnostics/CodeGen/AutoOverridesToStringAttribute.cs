e016d1a7ef1f1b0632f1e1f0af811fa8ee6c056e
SunnieShine <HowdySunnie@outlook.com>
2022-04-28 14:18:45 +0800

    Add type `AutoOverridesToStringAttribute`.

---
+++ src/System/Diagnostics/CodeGen/AutoOverridesToStringAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Diagnostics.CodeGen;
+
+/// <summary>
+/// Defines an attributes that allows user using it applying to a type
+/// (especially for <see langword="class"/> or <see langword="struct"/>), indicating the overriden metadata
+/// for method <see cref="object.ToString"/>, and make source generator generates their own <c>ToString</c>
+/// method automatically.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+public sealed class AutoOverridesToStringAttribute : Attribute
+{
+}
