302e1a53cf198c6a427a9340ac4cb584ae91293e
SunnieShine <HowdySunnie@outlook.com>
2022-05-12 15:48:51 +0800

    Code style: Promote local methods to static ones.

--- src/Sudoku.Bot/MainMethod.cs
+++ src/Sudoku.Bot/MainMethod.cs
@@ -4,7 +4,7 @@ Log.LogLevel = LogLevel.Info;
 // Initializes an identity instance.
 // Please note that the token and secret code corresponds to the info for the bot.
 // For more information please visit the link https://q.qq.com/bot/#/home
-var identity = loadBotConfiguration(StringResource.Get("__LocalBotConfigPath")!, out string testChannelId);
+var identity = LoadBotConfiguration(StringResource.Get("__LocalBotConfigPath")!, out string testChannelId);
 
 // Initializes a bot client.
 var bot = new BotClient(identity, sandBoxApi: true, reportApiError: true)
@@ -15,13 +15,13 @@ var bot = new BotClient(identity, sandBoxApi: true, reportApiError: true)
 };
 
 // Sets the elementary event handlers.
-initializeEvents(bot);
+InitializeEvents(bot);
 
 // Add commands.
-initializeCommands(bot);
+InitializeCommands(bot);
 
 // Starts the bot with the specified number of trial times and callback functions.
-bot.StartAsync(3, onSuccessfullyConnected, onFailedConnected);
+bot.StartAsync(3, OnSuccessfullyConnected, OnFailedConnected);
 
 // Here we should use an infinite loop to make the console don't exit fast.
 // Issue: https://github.com/Antecer/QQChannelBot/issues/1
@@ -31,143 +31,122 @@ while (true)
 }
 
 
-static void onSuccessfullyConnected() => Console.WriteLine(StringResource.Get("BotConnectedCallbackOutput_Success")!);
-
-static void onFailedConnected() => Console.WriteLine(StringResource.Get("BotConnectedCallbackOutput_Fail")!);
-
-static void fastExitOnBot(BotClient bot)
+/// <summary>
+/// Indicates the entry point of the program.
+/// </summary>
+internal static partial class Program
 {
-	bot.Close();
-	Environment.Exit(0);
-}
-
-static void initializeEvents(BotClient bot)
-{
-	Console.CancelKeyPress += (_, _) => fastExitOnBot(bot);
-
-	bot.AuthorizationPassed += onAuthorizationPassed;
-	bot.MessageCreated += onMessageCreated;
-}
-
-static void onAuthorizationPassed(BotClient? _, AuthoizationPassedEventArgs e)
-{
-	if (e is { User: { UserName: var userName, Id: var id } user }
-		&& typeof(BotClient).Assembly.GetName() is { Name: var sdkName, Version: var sdkVersion })
+	private static async void RepeatAsync(Sender sender, string message)
 	{
-		string suffix = StringResource.Get("PrivateDomainBotSuffix")!;
-		string sdkVersionName = StringResource.Get("SdkVersionName")!;
-		string colon = StringResource.Get("Colon")!;
-
-		Console.Title = $"{userName}{suffix} <{id}> - {sdkVersionName}{colon}{sdkName}_{sdkVersion}";
+		if (!string.IsNullOrWhiteSpace(message) && sender.AtMe)
+		{
+			await sender.ReplyAsync($"{sender.Author.Tag} {message}");
+		}
 	}
-}
 
-static async void onMessageCreated(BotClient? _, MessageCreatedEventArgs e)
-{
-	if (e.Sender is { AtMe: true, Content: var content, Bot.Info.Tag: var botInfoTag, Author.Tag: var authorTag } sender)
+	private static async void PrintAboutInfoAsync(Sender sender, string message)
 	{
-		await sender.ReplyAsync(content.Replace(botInfoTag, authorTag));
-	}
-}
-
-static void initializeCommands(BotClient bot)
-{
-	// Clock in.
-	// e.g. User sends the message:
-	//     @bot /clockIn
-	bot.AddCommand(new(StringResource.Get("Command_ClockIn")!, clockInAsync));
-
-	// Rank the experience point.
-	// e.g. User sends the message:
-	//     @bot /rank
-	bot.AddCommand(new(StringResource.Get("Command_Rank")!, printRankResultAsync));
-
-	// Prints the information about the author of the bot, and the bot itself.
-	// e.g. User sends the message:
-	//     @bot /about
-	bot.AddCommand(new(StringResource.Get("Command_About")!, printAboutInfoAsync));
-
-	// Repeats the message.
-	// e.g. User sends the message:
-	//     @bot /repeat 123
-	// Then the bot will reply:
-	//     @user 123
-	// Empty content won't be repeated by bot.
-	bot.AddCommand(new(StringResource.Get("Command_Repeat")!, repeatAsync));
-}
-
-static async void repeatAsync(Sender sender, string message)
-{
-	if (!string.IsNullOrWhiteSpace(message) && sender.AtMe)
-	{
-		await sender.ReplyAsync($"{sender.Author.Tag} {message}");
-	}
-}
-
-static async void printAboutInfoAsync(Sender sender, string message)
-{
-	if (sender.AtMe)
-	{
-		await sender.ReplyAsync(
-			$"""
+		if (sender.AtMe)
+		{
+			await sender.ReplyAsync(
+				$"""
 			{StringResource.Get("AboutInfo_Segment1")!}{AssemblyVersion}
 			---
 			{StringResource.Get("AboutInfo_Segment2")!}
 			"""
-		);
-	}
-}
-
-static async void clockInAsync(Sender sender, string message)
-{
-	// Checks the user data.
-	// If the user is the new, we should create a file to store it;
-	// otherwise, check the user data file, and get the latest time that he/she clocked in.
-	// If the 'yyyyMMdd' is same, we should disallow the user re-clocking in.
-	if (sender is not { Author.Id: var userId, AtMe: true })
-	{
-		return;
+			);
+		}
 	}
 
-	if (StringResource.Get("__LocalPlayerConfigPath") is not { } path)
+	private static async void ClockInAsync(Sender sender, string message)
 	{
-		// The configuration path is not found.
-		await sender.ReplyAsync(StringResource.Get("ClockInError_ResourceNotFound")!);
-		return;
-	}
-
-	if (!Directory.Exists(path))
-	{
-		// If the local path doesn't exist, create it.
-		Directory.CreateDirectory(path);
-	}
-
-	string userPath = $"""{path}\{userId}.json""";
-	var generalSerializerOptions = new JsonSerializerOptions { WriteIndented = true }.AddConverter<DictionaryConverter>();
-	if (File.Exists(userPath))
-	{
-		string originalJson = await File.ReadAllTextAsync(userPath);
-		var rootElement = JsonSerializer.Deserialize<JsonElement>(originalJson);
-		var foundPairs = rootElement.TryGetPropertyValues(ConfigurationPaths.LastTimeCheckedIn, ConfigurationPaths.ExperiencePoint);
-
-		static bool checkInPredicate((string Name, string?) e) => e.Name == ConfigurationPaths.LastTimeCheckedIn;
-		static bool expPredicate((string Name, string?) e) => e.Name == ConfigurationPaths.ExperiencePoint;
-		if (foundPairs is not null)
+		// Checks the user data.
+		// If the user is the new, we should create a file to store it;
+		// otherwise, check the user data file, and get the latest time that he/she clocked in.
+		// If the 'yyyyMMdd' is same, we should disallow the user re-clocking in.
+		if (sender is not { Author.Id: var userId, AtMe: true })
 		{
-			if (Array.FindIndex(foundPairs, checkInPredicate) is var checkInIndex and not -1)
-			{
-				try
-				{
-					var date = DateTime.Parse(foundPairs[checkInIndex].CorrespondingValue!).Date;
-					if (date == DateTime.Today)
-					{
-						// A same day. Notice the user that he/she cannot clock in again.
-						await sender.ReplyAsync(StringResource.Get("ClockInWarning_CannotClockInInSameDay")!);
-						return;
-					}
+			return;
+		}
 
-					// Valid. Now clock in and update the value.
+		if (StringResource.Get("__LocalPlayerConfigPath") is not { } path)
+		{
+			// The configuration path is not found.
+			await sender.ReplyAsync(StringResource.Get("ClockInError_ResourceNotFound")!);
+			return;
+		}
+
+		if (!Directory.Exists(path))
+		{
+			// If the local path doesn't exist, create it.
+			Directory.CreateDirectory(path);
+		}
+
+		string userPath = $"""{path}\{userId}.json""";
+		var generalSerializerOptions = new JsonSerializerOptions { WriteIndented = true }.AddConverter<DictionaryConverter>();
+		if (File.Exists(userPath))
+		{
+			string originalJson = await File.ReadAllTextAsync(userPath);
+			var rootElement = JsonSerializer.Deserialize<JsonElement>(originalJson);
+			var foundPairs = rootElement.TryGetPropertyValues(ConfigurationPaths.LastTimeCheckedIn, ConfigurationPaths.ExperiencePoint);
+
+			static bool checkInPredicate((string Name, string?) e) => e.Name == ConfigurationPaths.LastTimeCheckedIn;
+			static bool expPredicate((string Name, string?) e) => e.Name == ConfigurationPaths.ExperiencePoint;
+			if (foundPairs is not null)
+			{
+				if (Array.FindIndex(foundPairs, checkInPredicate) is var checkInIndex and not -1)
+				{
+					try
+					{
+						var date = DateTime.Parse(foundPairs[checkInIndex].CorrespondingValue!).Date;
+						if (date == DateTime.Today)
+						{
+							// A same day. Notice the user that he/she cannot clock in again.
+							await sender.ReplyAsync(StringResource.Get("ClockInWarning_CannotClockInInSameDay")!);
+							return;
+						}
+
+						// Valid. Now clock in and update the value.
+						int extraExp = Random.Shared.Next() % 10 + 1;
+						var tempDic = rootElement
+							.ToDictionaryOfStringElements()
+							.AppendOrCover(ConfigurationPaths.LastTimeCheckedIn, DateTime.Today.ToString());
+						var (realFinalValue, dictionary) = expPropertyExists(extraExp, out int finalValue) switch
+						{
+							true => (
+								finalValue,
+								tempDic.AppendOrCover(ConfigurationPaths.ExperiencePoint, finalValue.ToString())
+							),
+							_ => (
+								extraExp,
+								tempDic.AppendOrCover(ConfigurationPaths.ExperiencePoint, extraExp.ToString())
+							)
+						};
+						string updatedJson = JsonSerializer.Serialize(dictionary, generalSerializerOptions);
+
+						await File.WriteAllTextAsync(userPath, updatedJson);
+
+						string a = StringResource.Get("ClockInSuccess_ValueUpdatedSegment1")!;
+						string b = StringResource.Get("ClockInSuccess_ValueUpdatedSegment2")!;
+						string c = StringResource.Get("ClockInSuccess_ValueUpdatedSegment3")!;
+						await sender.ReplyAsync($"{a} {extraExp} {b} {realFinalValue} {c}");
+					}
+					catch (ArgumentNullException)
+					{
+						await sender.ReplyAsync(StringResource.Get("ClockInError_DateStringIsNull")!);
+					}
+					catch (Exception ex) when (ex is FormatException or InvalidOperationException)
+					{
+						// Invalid date value parsed.
+						await sender.ReplyAsync(StringResource.Get("ClockInError_InvalidDateValue")!, true);
+					}
+				}
+				else
+				{
+					// The file exists but the current property doesn't.
+					// We should create the property with the current value and update the file.
 					int extraExp = Random.Shared.Next() % 10 + 1;
+
 					var tempDic = rootElement
 						.ToDictionaryOfStringElements()
 						.AppendOrCover(ConfigurationPaths.LastTimeCheckedIn, DateTime.Today.ToString());
@@ -186,185 +165,121 @@ static async void clockInAsync(Sender sender, string message)
 
 					await File.WriteAllTextAsync(userPath, updatedJson);
 
-					string a = StringResource.Get("ClockInSuccess_ValueUpdatedSegment1")!;
-					string b = StringResource.Get("ClockInSuccess_ValueUpdatedSegment2")!;
-					string c = StringResource.Get("ClockInSuccess_ValueUpdatedSegment3")!;
-					await sender.ReplyAsync($"{a} {extraExp} {b} {realFinalValue} {c}");
-				}
-				catch (ArgumentNullException)
-				{
-					await sender.ReplyAsync(StringResource.Get("ClockInError_DateStringIsNull")!);
-				}
-				catch (Exception ex) when (ex is FormatException or InvalidOperationException)
-				{
-					// Invalid date value parsed.
-					await sender.ReplyAsync(StringResource.Get("ClockInError_InvalidDateValue")!, true);
+					string a = StringResource.Get("ClockInSuccess_ValueCreatedSegment1")!;
+					string b = StringResource.Get("ClockInSuccess_ValueCreatedSegment2")!;
+					string c = StringResource.Get("ClockInSuccess_ValueCreatedSegment3")!;
+					await sender.ReplyAsync($"{a} {extraExp} {b} {realFinalValue} {c}", true);
 				}
 			}
-			else
+
+
+			bool expPropertyExists(int extraExp, out int finalValue)
 			{
-				// The file exists but the current property doesn't.
-				// We should create the property with the current value and update the file.
-				int extraExp = Random.Shared.Next() % 10 + 1;
+				Unsafe.SkipInit(out finalValue);
 
-				var tempDic = rootElement
-					.ToDictionaryOfStringElements()
-					.AppendOrCover(ConfigurationPaths.LastTimeCheckedIn, DateTime.Today.ToString());
-				var (realFinalValue, dictionary) = expPropertyExists(extraExp, out int finalValue) switch
+				if (Array.FindIndex(foundPairs, expPredicate) is var expIndex and not -1
+					&& int.Parse(foundPairs[expIndex].CorrespondingValue!) + extraExp is var f)
 				{
-					true => (
-						finalValue,
-						tempDic.AppendOrCover(ConfigurationPaths.ExperiencePoint, finalValue.ToString())
-					),
-					_ => (
-						extraExp,
-						tempDic.AppendOrCover(ConfigurationPaths.ExperiencePoint, extraExp.ToString())
-					)
-				};
-				string updatedJson = JsonSerializer.Serialize(dictionary, generalSerializerOptions);
-
-				await File.WriteAllTextAsync(userPath, updatedJson);
-
-				string a = StringResource.Get("ClockInSuccess_ValueCreatedSegment1")!;
-				string b = StringResource.Get("ClockInSuccess_ValueCreatedSegment2")!;
-				string c = StringResource.Get("ClockInSuccess_ValueCreatedSegment3")!;
-				await sender.ReplyAsync($"{a} {extraExp} {b} {realFinalValue} {c}", true);
+					finalValue = f;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
 			}
 		}
-
-
-		bool expPropertyExists(int extraExp, out int finalValue)
+		else
 		{
-			Unsafe.SkipInit(out finalValue);
+			// The file doesn't exist. Just create a file that contains the data.
+			int extraExp = Random.Shared.Next() % 10 + 1;
 
-			if (Array.FindIndex(foundPairs, expPredicate) is var expIndex and not -1
-				&& int.Parse(foundPairs[expIndex].CorrespondingValue!) + extraExp is var f)
-			{
-				finalValue = f;
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
-	else
-	{
-		// The file doesn't exist. Just create a file that contains the data.
-		int extraExp = Random.Shared.Next() % 10 + 1;
-
-		string indenting = new(' ', 2);
-		await File.WriteAllTextAsync(
-			userPath,
-			$$"""
+			string indenting = new(' ', 2);
+			await File.WriteAllTextAsync(
+				userPath,
+				$$"""
 			{
 			{{indenting}}"{{ConfigurationPaths.LastTimeCheckedIn}}": "{{DateTime.Today}}",
 			{{indenting}}"{{ConfigurationPaths.ExperiencePoint}}": "{{extraExp}}"
 			}
 			"""
-		);
+			);
 
-		string a = StringResource.Get("ClockInSuccess_FileCreatedSegment1")!;
-		string b = StringResource.Get("ClockInSuccess_FileCreatedSegment2")!;
-		await sender.ReplyAsync($"{a} {extraExp} {b}", true);
+			string a = StringResource.Get("ClockInSuccess_FileCreatedSegment1")!;
+			string b = StringResource.Get("ClockInSuccess_FileCreatedSegment2")!;
+			await sender.ReplyAsync($"{a} {extraExp} {b}", true);
+		}
 	}
-}
 
-static async void printRankResultAsync(Sender sender, string message)
-{
-	if (sender is not { Author.Id: var userId, AtMe: true })
+	private static async void PrintRankResultAsync(Sender sender, string message)
 	{
-		return;
-	}
+		if (sender is not { Author.Id: var userId, AtMe: true })
+		{
+			return;
+		}
 
-	if (StringResource.Get("__LocalPlayerConfigPath") is not { } path)
-	{
-		// The configuration path is not found.
-		await sender.ReplyAsync(StringResource.Get("ClockInError_ResourceNotFound")!);
-		return;
-	}
+		if (StringResource.Get("__LocalPlayerConfigPath") is not { } path)
+		{
+			// The configuration path is not found.
+			await sender.ReplyAsync(StringResource.Get("ClockInError_ResourceNotFound")!);
+			return;
+		}
 
-	string[] filePaths = (
-		from fileInfo in new DirectoryInfo(path).GetFiles()
-		where fileInfo.Extension == ".json"
-		select fileInfo.FullName
-	).ToArray();
-	if (filePaths.Length == 0)
-	{
-		// No configuration files found.
-		await sender.ReplyAsync(StringResource.Get("RankExpFailed_NoConfigFileFound")!);
-	}
+		string[] filePaths = (
+			from fileInfo in new DirectoryInfo(path).GetFiles()
+			where fileInfo.Extension == ".json"
+			select fileInfo.FullName
+		).ToArray();
+		if (filePaths.Length == 0)
+		{
+			// No configuration files found.
+			await sender.ReplyAsync(StringResource.Get("RankExpFailed_NoConfigFileFound")!);
+		}
 
-	var rankingPairs = (
-		from filePath in filePaths
-		let jsonString = File.ReadAllText(filePath)
-		let rootElement = JsonSerializer.Deserialize<JsonElement>(jsonString)
-		let exp = (
-			from jsonElement in rootElement.EnumerateObject()
-			where jsonElement.Name == ConfigurationPaths.ExperiencePoint
-			let expStr = jsonElement.Value.ToString()
-			where int.TryParse(expStr, out _)
-			select int.Parse(expStr)
-		).FirstOrDefault()
-		let id = Path.GetFileNameWithoutExtension(filePath)
-		orderby exp descending, id
-		select (Id: id, ExperiencePoint: exp)
-	).ToArray();
+		var rankingPairs = (
+			from filePath in filePaths
+			let jsonString = File.ReadAllText(filePath)
+			let rootElement = JsonSerializer.Deserialize<JsonElement>(jsonString)
+			let exp = (
+				from jsonElement in rootElement.EnumerateObject()
+				where jsonElement.Name == ConfigurationPaths.ExperiencePoint
+				let expStr = jsonElement.Value.ToString()
+				where int.TryParse(expStr, out _)
+				select int.Parse(expStr)
+			).FirstOrDefault()
+			let id = Path.GetFileNameWithoutExtension(filePath)
+			orderby exp descending, id
+			select (Id: id, ExperiencePoint: exp)
+		).ToArray();
 
-	(string Id, int ExperiencePoint)? pair = null;
-	var sb = new StringBuilder();
-	for (int rankingOrder = 0, i = 0, rankingPairsLength = rankingPairs.Length; i < rankingPairsLength; i++)
-	{
-		var (id, exp) = rankingPairs[i];
+		(string Id, int ExperiencePoint)? pair = null;
+		var sb = new StringBuilder();
+		for (int rankingOrder = 0, i = 0, rankingPairsLength = rankingPairs.Length; i < rankingPairsLength; i++)
+		{
+			var (id, exp) = rankingPairs[i];
 
-		var member = await sender.GetMemberAsync(new() { Id = id });
-		string nickname = member?.Nickname ?? $"<{id}>";
-		string expText = StringResource.Get("ExperiencePointText")!;
-		string comma = StringResource.Get("Comma")!;
-		string colon = StringResource.Get("Colon")!;
-		string di = StringResource.Get("Di")!;
-		string ming = StringResource.Get("Ming")!;
+			var member = await sender.GetMemberAsync(new() { Id = id });
+			string nickname = member?.Nickname ?? $"<{id}>";
+			string expText = StringResource.Get("ExperiencePointText")!;
+			string comma = StringResource.Get("Comma")!;
+			string colon = StringResource.Get("Colon")!;
+			string di = StringResource.Get("Di")!;
+			string ming = StringResource.Get("Ming")!;
 
-		// Special case: if the current user has a same experience point value with the former one,
-		// we should treat him/her as same ranking value as the former one.
-		int finalRankingOrder = pair is (_, var formerExp) && formerExp == exp ? rankingOrder : ++rankingOrder;
-		sb.AppendLine($"{di} {finalRankingOrder} {ming}{colon}{nickname}{comma}{exp} {expText}");
+			// Special case: if the current user has a same experience point value with the former one,
+			// we should treat him/her as same ranking value as the former one.
+			int finalRankingOrder = pair is (_, var formerExp) && formerExp == exp ? rankingOrder : ++rankingOrder;
+			sb.AppendLine($"{di} {finalRankingOrder} {ming}{colon}{nickname}{comma}{exp} {expText}");
 
-		pair = (id, exp);
-	}
+			pair = (id, exp);
+		}
 
-	await sender.ReplyAsync(
-		$"""
+		await sender.ReplyAsync(
+			$"""
 		{StringResource.Get("RankExpSuccessful_Segment1")!}
 		---
 		{sb}
 		"""
-	);
-}
-
-static Identity loadBotConfiguration(string localFilePath, out string testChannelId)
-{
-	Unsafe.SkipInit(out testChannelId);
-
-	string text = File.ReadAllText(localFilePath);
-	var options = new JsonSerializerOptions { AllowTrailingCommas = true };
-	var jsonElement = JsonSerializer.Deserialize<JsonElement>(text, options);
-	Unsafe.SkipInit(out string appid);
-	Unsafe.SkipInit(out string token);
-	Unsafe.SkipInit(out string secret);
-	foreach (var element in jsonElement.EnumerateObject())
-	{
-		string realValue = element.Value.ToString();
-		_ = element.Name switch
-		{
-			"appid" => appid = realValue,
-			"token" => token = realValue,
-			"secret" => secret = realValue,
-			"testChannelId" => testChannelId = realValue,
-			_ => default
-		};
+		);
 	}
-
-	return new() { BotAppId = appid, BotToken = token, BotSecret = secret };
 }
--- src/Sudoku.Bot/Program.cs
+++ src/Sudoku.Bot/Program.cs
@@ -1,10 +1,131 @@
-﻿/// <summary>
-/// Indicates the entry point of the program.
-/// </summary>
-internal static partial class Program
+﻿partial class Program
 {
 	/// <summary>
 	/// Indicates the assembly version.
 	/// </summary>
 	private static Version? AssemblyVersion => typeof(Program).Assembly.GetName().Version;
+
+
+	/// <summary>
+	/// Indicates callback method that will be invoked when the bot has successfully connected to the server,
+	/// and successfully passed the authorization.
+	/// </summary>
+	private static void OnSuccessfullyConnected()
+		=> Console.WriteLine(StringResource.Get("BotConnectedCallbackOutput_Success")!);
+
+	/// <summary>
+	/// Indicates callback method that will be invoked when the bot has failed connected to the server,
+	/// or failed passed the authorization.
+	/// </summary>
+	private static void OnFailedConnected()
+		=> Console.WriteLine(StringResource.Get("BotConnectedCallbackOutput_Fail")!);
+
+	/// <summary>
+	/// Exits the program fast.
+	/// </summary>
+	/// <param name="bot">The bot client which should be closed.</param>
+	private static void FastExitOnBot(BotClient bot)
+	{
+		bot.Close();
+		Environment.Exit(0);
+	}
+
+	/// <summary>
+	/// Initializes the events.
+	/// </summary>
+	/// <param name="bot">The bot client.</param>
+	private static void InitializeEvents(BotClient bot)
+	{
+		Console.CancelKeyPress += (_, _) => FastExitOnBot(bot);
+
+		bot.AuthorizationPassed += static (_, e) =>
+		{
+			if (e is { User: { UserName: var userName, Id: var id } user }
+				&& typeof(BotClient).Assembly.GetName() is { Name: var sdkName, Version: var sdkVersion })
+			{
+				string suffix = StringResource.Get("PrivateDomainBotSuffix")!;
+				string sdkVersionName = StringResource.Get("SdkVersionName")!;
+				string colon = StringResource.Get("Colon")!;
+
+				Console.Title = $"{userName}{suffix} <{id}> - {sdkVersionName}{colon}{sdkName}_{sdkVersion}";
+			}
+		};
+		bot.MessageCreated += static async (_, e) =>
+		{
+			if (
+				e.Sender is
+				{
+					AtMe: true,
+					Content: var content,
+					Bot.Info.Tag: var botInfoTag,
+					Author.Tag: var authorTag
+				} sender
+			)
+			{
+				await sender.ReplyAsync(content.Replace(botInfoTag, authorTag));
+			}
+		};
+	}
+
+	/// <summary>
+	/// Initializes the commands.
+	/// </summary>
+	/// <param name="bot">The bot instance.</param>
+	private static void InitializeCommands(BotClient bot)
+	{
+		// Clock in.
+		// e.g. User sends the message:
+		//     @bot /clockIn
+		bot.AddCommand(new(StringResource.Get("Command_ClockIn")!, ClockInAsync));
+
+		// Rank the experience point.
+		// e.g. User sends the message:
+		//     @bot /rank
+		bot.AddCommand(new(StringResource.Get("Command_Rank")!, PrintRankResultAsync));
+
+		// Prints the information about the author of the bot, and the bot itself.
+		// e.g. User sends the message:
+		//     @bot /about
+		bot.AddCommand(new(StringResource.Get("Command_About")!, PrintAboutInfoAsync));
+
+		// Repeats the message.
+		// e.g. User sends the message:
+		//     @bot /repeat 123
+		// Then the bot will reply:
+		//     @user 123
+		// Empty content won't be repeated by bot.
+		bot.AddCommand(new(StringResource.Get("Command_Repeat")!, RepeatAsync));
+	}
+
+	/// <summary>
+	/// Loads the configuration file from local path.
+	/// </summary>
+	/// <param name="localFilePath">The local path storing the configuration of the bot.</param>
+	/// <param name="testChannelId">Indicates the test channel ID used.</param>
+	/// <returns>The <see cref="Identity"/> instance.</returns>
+	private static Identity LoadBotConfiguration(string localFilePath, out string testChannelId)
+	{
+		Unsafe.SkipInit(out testChannelId);
+
+		string text = File.ReadAllText(localFilePath);
+		var options = new JsonSerializerOptions { AllowTrailingCommas = true };
+		var jsonElement = JsonSerializer.Deserialize<JsonElement>(text, options);
+		Unsafe.SkipInit(out string appid);
+		Unsafe.SkipInit(out string token);
+		Unsafe.SkipInit(out string secret);
+		foreach (var element in jsonElement.EnumerateObject())
+		{
+			string realValue = element.Value.ToString();
+			_ = element.Name switch
+			{
+				"appid" => appid = realValue,
+				"token" => token = realValue,
+				"secret" => secret = realValue,
+				"testChannelId" => testChannelId = realValue,
+				_ => default
+			};
+		}
+
+		return new() { BotAppId = appid, BotToken = token, BotSecret = secret };
+	}
 }
