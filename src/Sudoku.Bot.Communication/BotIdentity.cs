365154c1c70c6496969d31b5ed71683445ca4318
SunnieShine <HowdySunnie@outlook.com>
2022-05-12 16:03:08 +0800

    Change `BotIdentity` to a `readonly record struct`.

--- src/Sudoku.Bot.Communication/BotIdentity.cs
+++ src/Sudoku.Bot.Communication/BotIdentity.cs
@@ -6,4 +6,4 @@
 /// <param name="AppId">The ID of the bot. The value corresponds to the AppID.</param>
 /// <param name="Token">The token of the bot.</param>
 /// <param name="Secret">The secret key of the bot.</param>
-public record struct BotIdentity(string AppId, string Token, string Secret);
+public readonly record struct BotIdentity(string AppId, string Token, string Secret);
