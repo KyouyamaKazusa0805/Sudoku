4847f7ed3ac68025e3b36e7fd567c60095e77dda
SunnieShine <HowdySunnie@outlook.com>
2022-05-12 17:26:39 +0800

    Update doc comments.

--- src/Sudoku.Bot.Communication/BotClient.Channel.cs
+++ src/Sudoku.Bot.Communication/BotClient.Channel.cs
@@ -3,104 +3,74 @@
 partial class BotClient
 {
 	/// <summary>
-	/// 获取子频道详情
+	/// Gets the details for a channel.
 	/// </summary>
-	/// <param name="channel_id">子频道Id</param>
-	/// <param name="sender"></param>
-	/// <returns>Channel?</returns>
-	public async Task<Channel?> GetChannelAsync(string channel_id, Sender? sender = null)
-	{
-		_ = BotApis.GetChannelDetail is { Path: var path, Method: var method };
-		var response = await HttpSendAsync(path.Replace("{channel_id}", channel_id), method, null, sender);
-		return response is null ? null : await response.Content.ReadFromJsonAsync<Channel?>();
-	}
+	/// <param name="channel_id">The channel ID. <b>The argument cannot be renamed.</b></param>
+	/// <param name="sender">The sender who sends the message.</param>
+	/// <returns>A task encapsulates the channel instance as the result value.</returns>
+	public async Task<Channel?> GetChannelAsync(string channel_id, Sender? sender)
+		=> BotApis.GetChannelDetail is { Path: var path, Method: var method }
+		&& path.ReplaceArgument(channel_id) is var replacedPath
+		&& await HttpSendAsync(replacedPath, method, null, sender) is { Content: var responseContent }
+			? await responseContent.ReadFromJsonAsync<Channel?>()
+			: null;
 
 	/// <summary>
-	/// 获取频道下的子频道列表
+	/// Gets a list of channels in the specified GUILD.
 	/// </summary>
-	/// <param name="guild_id">频道Id</param>
-	/// <param name="channelType">筛选子频道类型</param>
-	/// <param name="channelSubType">筛选子频道子类型</param>
-	/// <param name="sender"></param>
-	/// <returns></returns>
-	public async Task<List<Channel>?> GetChannelsAsync(
-		string guild_id, ChannelType? channelType = null, ChannelSubtype? channelSubType = null, Sender? sender = null)
-	{
-		_ = BotApis.GetChannelsInGuild is { Path: var path, Method: var method };
-		var response = await HttpSendAsync(path.Replace("{guild_id}", guild_id), method, null, sender);
-		var channels = response is null ? null : await response.Content.ReadFromJsonAsync<List<Channel>?>();
-		if (channels is not null)
-		{
-			if (channelType is not null)
-			{
-				channels = (
-					from channel in channels
-					where channel.Type == channelType
-					select channel
-				).ToList();
-			}
-
-			if (channelSubType is not null)
-			{
-				channels = (
-					from channel in channels
-					where channel.Subtype == channelSubType
-					select channel
-				).ToList();
-			}
-		}
-
-		return channels;
-	}
+	/// <param name="guild_id">The GUILD ID. <b>The argument cannot be renamed.</b></param>
+	/// <param name="type">The type of the channel.</param>
+	/// <param name="subtype">The sub type of the channel.</param>
+	/// <param name="sender">The sender who sends the message.</param>
+	/// <returns>A task instance encapsulates the channel list as the result value.</returns>
+	public async Task<List<Channel>?> GetChannelsAsync(string guild_id, ChannelType? type, ChannelSubtype? subtype, Sender? sender)
+		=> (
+			BotApis.GetChannelsInGuild is { Path: var path, Method: var method }
+				&& path.ReplaceArgument(guild_id) is var replacedPath
+				&& await HttpSendAsync(replacedPath, method, null, sender) is { Content: var responseContent }
+				? await responseContent.ReadFromJsonAsync<List<Channel>?>()
+				: null
+		)?.Where(e => e.Type == type)?.Where(e => e.Subtype == subtype)?.ToList();
 
 	/// <summary>
-	/// 创建子频道（仅私域可用）
+	/// Creates a channel. <b>The method can only be used for the private domain.</b>
 	/// </summary>
-	/// <param name="channel">用于创建子频道的对象（需提前填充必要字段）</param>
-	/// <param name="sender"></param>
-	/// <returns></returns>
-	public async Task<Channel?> CreateChannelAsync(Channel channel, Sender? sender = null)
-	{
-		_ = BotApis.CreateChannel is { Path: var path, Method: var method };
-		var response = await HttpSendAsync(
-			path.Replace("{guild_id}", channel.GuildId),
-			method,
-			JsonContent.Create(channel),
-			sender
-		);
-
-		return response is null ? null : await response.Content.ReadFromJsonAsync<Channel?>();
-	}
+	/// <param name="channel">Indicates the channel instance that is filled with the data you want to create.</param>
+	/// <param name="sender">The sender who sends the message.</param>
+	/// <returns>A task instance encapsulates the channel instance as the result.</returns>
+	public async Task<Channel?> CreateChannelAsync(Channel channel, Sender? sender)
+		=> BotApis.CreateChannel is { Path: var path, Method: var method }
+		&& path.Replace("{guild_id}", channel.GuildId) is var replacedPath
+		&& JsonContent.Create(channel) is var jsonContent
+		&& await HttpSendAsync(replacedPath, method, jsonContent, sender) is { Content: var responseContent }
+			? await responseContent.ReadFromJsonAsync<Channel?>()
+			: null;
 
 	/// <summary>
-	/// 修改子频道（仅私域可用）
+	/// Edits a channel. <b>The method can only be used for the private domain.</b>
 	/// </summary>
-	/// <param name="channel">修改属性后的子频道对象</param>
-	/// <param name="sender"></param>
-	/// <returns></returns>
-	public async Task<Channel?> EditChannelAsync(Channel channel, Sender? sender = null)
-	{
-		_ = BotApis.ModifyChannel is { Path: var path, Method: var method };
-		var response = await HttpSendAsync(
-			path.Replace("{channel_id}", channel.Id),
-			method,
-			JsonContent.Create(channel),
-			sender
-		);
-
-		return response is null ? null : await response.Content.ReadFromJsonAsync<Channel?>();
-	}
+	/// <param name="channel">The channel you want to edit.</param>
+	/// <param name="sender">The sender who sends the message.</param>
+	/// <returns>A task instance encapsulates the channel instance as the result.</returns>
+	public async Task<Channel?> EditChannelAsync(Channel channel, Sender? sender)
+		=> BotApis.ModifyChannel is { Path: var path, Method: var method }
+		&& path.Replace("{channel_id}", channel.Id) is var replacedPath
+		&& JsonContent.Create(channel) is var jsonContent
+		&& await HttpSendAsync(replacedPath, method, jsonContent, sender) is { Content: var responseContent }
+			? await responseContent.ReadFromJsonAsync<Channel?>()
+			: null;
 
 	/// <summary>
-	/// 删除指定子频道（仅私域可用）
+	/// Deletes a channel. <b>The method can only be used for the private domain.</b>
 	/// </summary>
-	/// <param name="channel_id">要删除的子频道Id</param>
-	/// <param name="sender"></param>
-	/// <returns></returns>
-	public async Task<bool> DeleteChannelAsync(string channel_id, Sender? sender = null)
-	{
-		_ = BotApis.DeleteChannel is { Path: var path, Method: var method };
-		var response = await HttpSendAsync(path.Replace("{channel_id}", channel_id), method, null, sender);
-		return response?.IsSuccessStatusCode ?? false;
-	}
+	/// <param name="channel_id">The channel you want to delete. <b>The argument cannot be renamed.</b></param>
+	/// <param name="sender">The sender who sends the message.</param>
+	/// <returns>
+	/// A task instance encapsulates the <see cref="bool"/> value indicating
+	/// whether the deletion operation is successful.
+	/// </returns>
+	public async Task<bool> DeleteChannelAsync(string channel_id, Sender? sender)
+		=> BotApis.DeleteChannel is { Path: var path, Method: var method }
+		&& path.ReplaceArgument(channel_id) is var replacedPath
+		&& ((await HttpSendAsync(replacedPath, method, null, sender))?.IsSuccessStatusCode ?? false);
 }
