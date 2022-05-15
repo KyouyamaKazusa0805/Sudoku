ffdca93c26785583da2c58c2539b8bdc7cf4e769
SunnieShine <HowdySunnie@outlook.com>
2022-05-15 17:11:34 +0800

    Remove unused usings.

--- src/Sudoku.Core/GlobalUsings.cs
+++ src/Sudoku.Core/GlobalUsings.cs
@@ -1,5 +1,4 @@
 ﻿global using System;
-global using System.Collections;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.CommonComparers.Equality;
