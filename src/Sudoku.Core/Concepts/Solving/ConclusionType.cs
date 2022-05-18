49e4daa4fc8e1ed292bd57c27083036a88f7f5aa
SunnieShine <HowdySunnie@outlook.com>
2022-05-18 16:44:30 +0800

    Use attribute to automatically generate source code.

--- src/Sudoku.Core/Concepts/Solving/ConclusionType.cs
+++ src/Sudoku.Core/Concepts/Solving/ConclusionType.cs
@@ -3,17 +3,18 @@
 /// <summary>
 /// Provides a conclusion type.
 /// </summary>
+[EnumSwitchExpressionRoot("GetNotation", MethodDescription = "Gets the notation of the conclusion type.", ThisParameterDescription = "The conclusion type.", ReturnValueDescription = "The notation of the conclusion type.")]
 public enum ConclusionType : byte
 {
 	/// <summary>
 	/// Indicates the conclusion is a value filling into a cell.
 	/// </summary>
-	[EnumFieldName(" = ")]
+	[EnumSwitchExpressionArm("GetNotation", " = ")]
 	Assignment,
 
 	/// <summary>
 	/// Indicates the conclusion is a candidate being remove from a cell.
 	/// </summary>
-	[EnumFieldName(" <> ")]
+	[EnumSwitchExpressionArm("GetNotation", " <> ")]
 	Elimination
 }
