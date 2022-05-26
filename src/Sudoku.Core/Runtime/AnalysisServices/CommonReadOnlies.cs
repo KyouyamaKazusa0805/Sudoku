045a1ecbe17a832a3fea08ab2c5bb2a400e13aea
SunnieShine <HowdySunnie@outlook.com>
2022-05-26 08:50:48 +0800

    Move arrays to `CommonReadOnlies`.

--- src/Sudoku.Core/Runtime/AnalysisServices/CommonReadOnlies.cs
+++ src/Sudoku.Core/Runtime/AnalysisServices/CommonReadOnlies.cs
@@ -352,6 +352,45 @@ public static partial class CommonReadOnlies
 	/// </summary>
 	public static readonly IReadOnlyDictionary<(byte Line, byte Block), (Cells LineMap, Cells BlockMap, Cells IntersectionMap, byte[] OtherBlocks)> IntersectionMaps;
 
+	internal static readonly int[] BlockTable =
+	{
+		0,  0,  0,  1,  1,  1,  2,  2,  2,
+		0,  0,  0,  1,  1,  1,  2,  2,  2,
+		0,  0,  0,  1,  1,  1,  2,  2,  2,
+		3,  3,  3,  4,  4,  4,  5,  5,  5,
+		3,  3,  3,  4,  4,  4,  5,  5,  5,
+		3,  3,  3,  4,  4,  4,  5,  5,  5,
+		6,  6,  6,  7,  7,  7,  8,  8,  8,
+		6,  6,  6,  7,  7,  7,  8,  8,  8,
+		6,  6,  6,  7,  7,  7,  8,  8,  8
+	};
+
+	internal static readonly int[] RowTable =
+	{
+		9,  9,  9,  9,  9,  9,  9,  9,  9,
+		10, 10, 10, 10, 10, 10, 10, 10, 10,
+		11, 11, 11, 11, 11, 11, 11, 11, 11,
+		12, 12, 12, 12, 12, 12, 12, 12, 12,
+		13, 13, 13, 13, 13, 13, 13, 13, 13,
+		14, 14, 14, 14, 14, 14, 14, 14, 14,
+		15, 15, 15, 15, 15, 15, 15, 15, 15,
+		16, 16, 16, 16, 16, 16, 16, 16, 16,
+		17, 17, 17, 17, 17, 17, 17, 17, 17
+	};
+
+	internal static readonly int[] ColumnTable =
+	{
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26,
+		18, 19, 20, 21, 22, 23, 24, 25, 26
+	};
+
 	/// <summary>
 	/// <para>The table of all blocks to iterate for each blocks.</para>
 	/// <para>
