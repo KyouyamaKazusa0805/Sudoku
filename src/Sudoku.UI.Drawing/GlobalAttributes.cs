99c079df259d38e745c7bed3368dab02a12921ff
SunnieShine <HowdySunnie@outlook.com>
2022-05-26 21:30:10 +0800

    Add project `Sudoku.UI.Drawing`.

---
+++ src/Sudoku.UI.Drawing/GlobalAttributes.cs
@@ -0,0 +1,2 @@
+﻿[assembly: AutoExtensionDeconstruction(typeof(Line), nameof(Line.X1), nameof(Line.X2), nameof(Line.Y1), nameof(Line.Y2))]
+[assembly: AutoExtensionDeconstruction(typeof(Point), nameof(Point.X), nameof(Point.Y))]
---
+++ src/Sudoku.UI.Drawing/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using System.Diagnostics.CodeGen;
+global using System.Runtime.CompilerServices;
+global using Microsoft.UI.Xaml.Shapes;
+global using Windows.Foundation;
