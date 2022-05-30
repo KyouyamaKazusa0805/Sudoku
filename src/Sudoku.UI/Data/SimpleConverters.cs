1f310e49da60d64b7af789763c9a07c32badb267
SunnieShine <HowdySunnie@outlook.com>
2022-05-30 16:20:27 +0800

    Update code to support hints.

--- src/Sudoku.UI/Data/SimpleConverters.cs
+++ src/Sudoku.UI/Data/SimpleConverters.cs
@@ -2,9 +2,12 @@
 
 internal static class SimpleConverters
 {
+	public static string SliderPossibleValueString(double min, double max)
+		=> $"{Get("SliderPossibleValue")}{min:0.0} - {max:0.0}";
+
 	public static Visibility StringToVisibility(string? s)
 		=> string.IsNullOrWhiteSpace(s) ? Visibility.Collapsed : Visibility.Visible;
 
 	public static Thickness SettingItemToMargin(string? s)
-		=> StringToVisibility(s) == Visibility.Collapsed ? new(20, 32, 0, 0) : new(20, 60, 0, 0);
+		=> StringToVisibility(s) == Visibility.Collapsed ? new(20, 56, 0, 0) : new(20, 84, 0, 0);
 }
