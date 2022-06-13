bccdaa1e4275b6be007bb1dd714db8616feb8f0d
SunnieShine <HowdySunnie@outlook.com>
2022-06-13 18:58:22 +0800

    Extract `DateOnlyExtension` markup extension to simplify the templated conversion.

---
+++ src/Sudoku.UI/Data/MarkupExtensions/DateOnlyMarkupExtension.cs
@@ -0,0 +1,37 @@
+﻿namespace Sudoku.UI.Data.MarkupExtensions;
+
+/// <summary>
+/// Defines a date-only markup extension.
+/// </summary>
+[MarkupExtensionReturnType(ReturnType = typeof(DateOnly?))]
+public sealed class DateOnlyExtension : MarkupExtension
+{
+	/// <summary>
+	/// Initializes a <see cref="DateOnlyExtension"/> instance.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public DateOnlyExtension() : base()
+	{
+	}
+
+
+	/// <summary>
+	/// Indicates the raw date text.
+	/// </summary>
+	public string? RawDateText { get; set; }
+
+	/// <summary>
+	/// Indicates the format.
+	/// </summary>
+	public string? Format { get; set; }
+
+
+	/// <inheritdoc/>
+	protected override object? ProvideValue()
+		=> (RawDateText, Format) switch
+		{
+			(not null, null) when DateOnly.TryParse(RawDateText, out var result) => result,
+			(not null, not null) when DateOnly.TryParseExact(RawDateText, Format, out var result) => result,
+			_ => null,
+		};
+}
