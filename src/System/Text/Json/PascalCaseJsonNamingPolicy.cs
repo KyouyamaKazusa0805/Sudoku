11f0d29d56751d700ea2edafa85907608c53e80e
SunnieShine <HowdySunnie@outlook.com>
2022-07-02 22:10:50 +0800

    Add type `PascalCaseJsonNamingPolicy`.

---
+++ src/System/Text/Json/PascalCaseJsonNamingPolicy.cs
@@ -0,0 +1,40 @@
+﻿namespace System.Text.Json;
+
+/// <summary>
+/// Defines the pascal casing JSON naming policy.
+/// </summary>
+/// <remarks>
+/// This type cannot be initialized; instead, you can use the property <see cref="PascalCase"/> to get the instance.
+/// </remarks>
+/// <seealso cref="PascalCase"/>
+public sealed class PascalCaseJsonNamingPolicy : JsonNamingPolicy
+{
+	/// <summary>
+	/// Initializes a <see cref="PascalCaseJsonNamingPolicy"/> instance.
+	/// </summary>
+	private PascalCaseJsonNamingPolicy()
+	{
+	}
+
+
+	/// <summary>
+	/// Gets the naming policy for pascal-casing.
+	/// </summary>
+	/// <returns>The naming policy for pascal-casing.</returns>
+	public static JsonNamingPolicy PascalCase
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => new PascalCaseJsonNamingPolicy();
+	}
+
+
+	/// <inheritdoc/>
+	public override string ConvertName(string name)
+		=> name switch
+		{
+			[] => string.Empty,
+			[var firstChar and >= 'a' and <= 'z', .. var slice] => $"{(char)(firstChar - ' ')}{slice}",
+			[>= 'A' and <= 'Z', ..] => name,
+			[var firstChar, .. var slice] => $"{firstChar}{ConvertName(slice)}"
+		};
+}
