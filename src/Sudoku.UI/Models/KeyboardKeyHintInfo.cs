16b9f46244510bc6d641211261e4747f389c4d63
SunnieShine <HowdySunnie@outlook.com>
2022-07-05 16:44:49 +0800

    Use one-element instead.

--- src/Sudoku.UI/Models/KeyboardKeyHintInfo.cs
+++ src/Sudoku.UI/Models/KeyboardKeyHintInfo.cs
@@ -1,10 +1,9 @@
 ﻿namespace Sudoku.UI.Models;
 
 /// <summary>
-/// Provides with a keyboard key information that is used for construction for a <see cref="KeyboardKeyHint"/> instance.
+/// Provides with a keyboard key information that is used for construction for an instance.
 /// </summary>
-/// <param name="KeyNameResourceKey">The resource key that describes the key hint name.</param>
-/// <param name="KeyboardPairs">The keyboard pairs. Please see the type <see cref="KeyboardPair"/>.</param>
-/// <seealso cref="KeyboardKeyHint"/>
-/// <seealso cref="KeyboardPair"/>
-internal readonly record struct KeyboardKeyHintInfo(string KeyNameResourceKey, IList<KeyboardPair> KeyboardPairs);
+/// <param name="KeyName">The key hint name.</param>
+/// <param name="KeyboardPair">The keyboard pairs. Please see the type <see cref="Models.KeyboardPair"/>.</param>
+/// <seealso cref="Models.KeyboardPair"/>
+internal readonly record struct KeyboardKeyHintInfo(string KeyName, KeyboardPair KeyboardPair);
--- src/Sudoku.UI/Views/Pages/KeyboardPage.xaml.cs
+++ src/Sudoku.UI/Views/Pages/KeyboardPage.xaml.cs
@@ -15,86 +15,51 @@ public sealed partial class KeyboardPage : Page
 	/// </summary>
 	private readonly IList<KeyboardKeyHintInfo> _data = new List<KeyboardKeyHintInfo>
 	{
-		new(R["KeyboardPage_ControlO"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.O) }),
-		new(R["KeyboardPage_ControlS"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.S) }),
-		new(R["KeyboardPage_ControlC"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.C) }),
-		new(R["KeyboardPage_ControlV"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.V) }),
-		new(R["KeyboardPage_ControlTab"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.Tab) }),
-		new(R["KeyboardPage_ControlShiftTab"]!, new List<KeyboardPair>(1) { new(Modifier.Control | Modifier.Shift, Key.Tab) }),
-		new(R["KeyboardPage_ControlZ"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.Z) }),
-		new(R["KeyboardPage_ControlY"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.Y) }),
-		new(R["KeyboardPage_ControlH"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.H) }),
-		new(R["KeyboardPage_Number0"]!, new List<KeyboardPair>(1) { new(Modifier.Control, Key.Number0) }),
-		new(R["KeyboardPage_OtherNumbers"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control, new[]
-			{
-				Key.Number1,
-				Key.Number2,
-				Key.Number3,
-				Key.Number4,
-				Key.Number5,
-				Key.Number6,
-				Key.Number7,
-				Key.Number8,
-				Key.Number9
-			})
-		}),
-		new(R["KeyboardPage_ShiftOtherNumbers"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control | Modifier.Shift, new[]
-			{
-				Key.Number1,
-				Key.Number2,
-				Key.Number3,
-				Key.Number4,
-				Key.Number5,
-				Key.Number6,
-				Key.Number7,
-				Key.Number8,
-				Key.Number9
-			})
-		}),
-		new(R["KeyboardPage_ControlNumbers"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control, new[]
-			{
-				Key.Number1,
-				Key.Number2,
-				Key.Number3,
-				Key.Number4,
-				Key.Number5,
-				Key.Number6
-			})
-		}),
-		new(R["KeyboardPage_ControlShiftNumbers"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control | Modifier.Shift, new[] { Key.Number1, Key.Number2, Key.Number3, Key.Number4 })
-		}),
-		new(R["KeyboardPage_ControlBack"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control, new[] { Key.Number0, Key.Back })
-		}),
-		new(R["KeyboardPage_ControlShiftBack"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control | Modifier.Shift, new[] { Key.Number0, Key.Back })
-		}),
-		new(R["KeyboardPage_ControlLeft"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control, new[] { Key.Left, Key.Right })
-		}),
-		new(R["KeyboardPage_ControlUp"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control, new[] { Key.Up, Key.Down })
-		}),
-		new(R["KeyboardPage_ControlShiftLeft"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control | Modifier.Shift, new[] { Key.Left, Key.Right })
-		}),
-		new(R["KeyboardPage_ControlShiftUp"]!, new List<KeyboardPair>(1)
-		{
-			new(Modifier.Control | Modifier.Shift, new[] { Key.Up, Key.Down })
-		})
+		new(R["KeyboardPage_ControlO"]!, new(Modifier.Control, Key.O)),
+		new(R["KeyboardPage_ControlS"]!, new(Modifier.Control, Key.S)),
+		new(R["KeyboardPage_ControlC"]!, new(Modifier.Control, Key.C)),
+		new(R["KeyboardPage_ControlV"]!, new(Modifier.Control, Key.V)),
+		new(R["KeyboardPage_ControlTab"]!, new(Modifier.Control, Key.Tab)),
+		new(R["KeyboardPage_ControlShiftTab"]!, new(Modifier.Control | Modifier.Shift, Key.Tab)),
+		new(R["KeyboardPage_ControlZ"]!, new(Modifier.Control, Key.Z)),
+		new(R["KeyboardPage_ControlY"]!, new(Modifier.Control, Key.Y)),
+		new(R["KeyboardPage_ControlH"]!, new(Modifier.Control, Key.H)),
+		new(R["KeyboardPage_Number0"]!, new(Modifier.Control, Key.Number0)),
+		new(
+			R["KeyboardPage_OtherNumbers"]!,
+			new(
+				Modifier.Control,
+				new[]
+				{
+					Key.Number1, Key.Number2, Key.Number3,
+					Key.Number4, Key.Number5, Key.Number6,
+					Key.Number7, Key.Number8, Key.Number9
+				}
+			)
+		),
+		new(
+			R["KeyboardPage_ShiftOtherNumbers"]!,
+			new(
+				Modifier.Control | Modifier.Shift,
+				new[]
+				{
+					Key.Number1, Key.Number2, Key.Number3,
+					Key.Number4, Key.Number5, Key.Number6,
+					Key.Number7, Key.Number8, Key.Number9
+				}
+			)
+		),
+		new(
+			R["KeyboardPage_ControlNumbers"]!,
+			new(Modifier.Control, new[] { Key.Number1, Key.Number2, Key.Number3, Key.Number4, Key.Number5, Key.Number6 })
+		),
+		new(R["KeyboardPage_ControlShiftNumbers"]!, new(Modifier.Control | Modifier.Shift, new[] { Key.Number1, Key.Number2, Key.Number3, Key.Number4 })),
+		new(R["KeyboardPage_ControlBack"]!, new(Modifier.Control, new[] { Key.Number0, Key.Back })),
+		new(R["KeyboardPage_ControlShiftBack"]!, new(Modifier.Control | Modifier.Shift, new[] { Key.Number0, Key.Back })),
+		new(R["KeyboardPage_ControlLeft"]!, new(Modifier.Control, new[] { Key.Left, Key.Right })),
+		new(R["KeyboardPage_ControlUp"]!, new(Modifier.Control, new[] { Key.Up, Key.Down })),
+		new(R["KeyboardPage_ControlShiftLeft"]!, new(Modifier.Control | Modifier.Shift, new[] { Key.Left, Key.Right })),
+		new(R["KeyboardPage_ControlShiftUp"]!, new(Modifier.Control | Modifier.Shift, new[] { Key.Up, Key.Down }))
 	};
 
 
