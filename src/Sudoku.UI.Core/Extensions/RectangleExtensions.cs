005b81f33ce38a3975df34cd45ec248e28f4ceb8
SunnieShine <HowdySunnie@outlook.com>
2022-07-19 10:31:42 +0800

    Move to a new file.

---
+++ src/Sudoku.UI.Core/Extensions/RectangleExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Microsoft.UI.Xaml.Shapes;
+
+/// <summary>
+/// Provides with extension methods on <see cref="Rectangle"/>.
+/// </summary>
+/// <seealso cref="Rectangle"/>
+public static class RectangleExtensions
+{
+	/// <summary>
+	/// Sets the properties <see cref="Rectangle.RadiusX"/> and <see cref="Rectangle.RadiusY"/>
+	/// with the specified values.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Rectangle WithRadius(this Rectangle @this, double radius) => @this.WithRadius(radius, radius);
+
+	/// <summary>
+	/// Sets the properties <see cref="Rectangle.RadiusX"/> and <see cref="Rectangle.RadiusY"/>
+	/// with the specified values.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Rectangle WithRadius(this Rectangle @this, double radiusX, double radiusY)
+	{
+		@this.RadiusX = radiusX;
+		@this.RadiusY = radiusY;
+		return @this;
+	}
+}
--- src/Sudoku.UI.Core/Extensions/ShapeExtensions.cs
+++ src/Sudoku.UI.Core/Extensions/ShapeExtensions.cs
@@ -96,29 +96,3 @@ public static class ShapeExtensions
 		return @this;
 	}
 }
-
-/// <summary>
-/// Provides with extension methods on <see cref="Rectangle"/>.
-/// </summary>
-/// <seealso cref="Rectangle"/>
-public static class RectangleExtensions
-{
-	/// <summary>
-	/// Sets the properties <see cref="Rectangle.RadiusX"/> and <see cref="Rectangle.RadiusY"/>
-	/// with the specified values.
-	/// </summary>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static Rectangle WithRadius(this Rectangle @this, double radius) => @this.WithRadius(radius, radius);
-
-	/// <summary>
-	/// Sets the properties <see cref="Rectangle.RadiusX"/> and <see cref="Rectangle.RadiusY"/>
-	/// with the specified values.
-	/// </summary>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static Rectangle WithRadius(this Rectangle @this, double radiusX, double radiusY)
-	{
-		@this.RadiusX = radiusX;
-		@this.RadiusY = radiusY;
-		return @this;
-	}
-}
