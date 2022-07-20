1cb80b8d00796e3dd962c4010fd28ce9ce39b5cf
SunnieShine <HowdySunnie@outlook.com>
2022-07-20 13:53:20 +0800

    Add type `AdjacentNodeRelation`.

---
+++ src/Sudoku.Core/Concepts/Solving/TechniqueStructures/AdjacentNodesRelation.cs
@@ -0,0 +1,22 @@
+﻿namespace Sudoku.Concepts.Solving.TechniqueStructures;
+
+/// <summary>
+/// Represents a relation between two adjacent nodes.
+/// </summary>
+public enum AdjacentNodesRelation
+{
+	/// <summary>
+	/// Indicates the relation is normal.
+	/// </summary>
+	Normal,
+
+	/// <summary>
+	/// Indicates the relation is almost locked set.
+	/// </summary>
+	AlmostLockedSet,
+
+	/// <summary>
+	/// Indicates the relation is almost hidden set.
+	/// </summary>
+	AlmostHiddenSet
+}
