9d53eacb88b896f57729420de3038dceb9a1ca8f
SunnieShine <HowdySunnie@outlook.com>
2022-08-07 17:18:51 +0800

    Add Win11-styled setting expander control.

---
+++ src/Sudoku.UI/DataConversion/ValueConverters/NullToBooleanConverter.cs
@@ -0,0 +1,55 @@
+﻿namespace Sudoku.UI.DataConversion.ValueConverters;
+
+/// <summary>
+/// Converts a null value to a <see cref="bool"/> value.
+/// </summary>
+public sealed class NullToBooleanConverter : IValueConverter
+{
+	/// <summary>
+	/// Indicates whether the value conversion is reverted.
+	/// </summary>
+	public bool IsInverted { get; set; }
+
+	/// <summary>
+	/// Indicates whether the converter will handle for whitespaces.
+	/// </summary>
+	public bool EnforceNonWhiteSpaceString { get; set; }
+
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public object Convert(object? value, Type targetType, object parameter, string language)
+	{
+		if (value?.GetType() == typeof(string))
+		{
+			if (IsInverted)
+			{
+				if (EnforceNonWhiteSpaceString)
+				{
+					return !string.IsNullOrWhiteSpace((string)value);
+				}
+
+				return !string.IsNullOrEmpty((string)value);
+			}
+
+			if (EnforceNonWhiteSpaceString)
+			{
+				return !string.IsNullOrWhiteSpace((string)value);
+			}
+
+			return string.IsNullOrEmpty((string)value);
+		}
+
+		if (IsInverted)
+		{
+			return value is not null;
+		}
+
+		return value is null;
+	}
+
+	/// <inheritdoc/>
+	[DoesNotReturn]
+	public object ConvertBack(object? value, Type? targetType, object? parameter, string? language)
+		=> throw new NotImplementedException();
+}
---
+++ src/Sudoku.UI/Views/Controls/Win11SettingHeaderControl.xaml.cs
@@ -0,0 +1,117 @@
+﻿namespace Sudoku.UI.Views.Controls;
+
+/// <summary>
+/// Defines a control that represents a setting header that uses Win11 setting style.
+/// </summary>
+[ContentProperty(Name = nameof(SettingActionableElement))]
+public sealed partial class Win11SettingHeaderControl : UserControl
+{
+	/// <summary>
+	/// Defines a dependency property that binds with the property <see cref="Title"/>.
+	/// </summary>
+	/// <seealso cref="Title"/>
+	public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+		nameof(Title),
+		typeof(string),
+		typeof(Win11SettingHeaderControl),
+		new(string.Empty, (d, e) => AutomationProperties.SetName(d, (string)e.NewValue))
+	);
+
+	/// <summary>
+	/// Defines a dependency property that binds with the property <see cref="Description"/>.
+	/// </summary>
+	/// <seealso cref="Description"/>
+	public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+		nameof(Description),
+		typeof(string),
+		typeof(Win11SettingHeaderControl),
+		new(string.Empty, (d, e) => AutomationProperties.SetHelpText(d, (string)e.NewValue))
+	);
+
+	/// <summary>
+	/// Defines a dependency property that binds with the property <see cref="Icon"/>.
+	/// </summary>
+	/// <seealso cref="Icon"/>
+	public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+		nameof(Icon),
+		typeof(IconElement),
+		typeof(Win11SettingHeaderControl),
+		new(null)
+	);
+
+
+	/// <summary>
+	/// Initializes a <see cref="Win11SettingHeaderControl"/> instance.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public Win11SettingHeaderControl()
+	{
+		InitializeComponent();
+
+		VisualStateManager.GoToState(this, "NormalState", false);
+	}
+
+
+	/// <summary>
+	/// Indicates the title of the control.
+	/// </summary>
+	public string Title
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => (string)GetValue(TitleProperty);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => SetValue(TitleProperty, value);
+	}
+
+	/// <summary>
+	/// Indicates the description of the control.
+	/// </summary>
+	public string Description
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => (string)GetValue(DescriptionProperty);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => SetValue(DescriptionProperty, value);
+	}
+
+	/// <summary>
+	/// Indicates the icon.
+	/// </summary>
+	public IconElement Icon
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => (IconElement)GetValue(IconProperty);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		set => SetValue(IconProperty, value);
+	}
+
+#pragma warning disable CS1591
+	public FrameworkElement? SettingActionableElement { get; set; }
+#pragma warning restore CS1591
+
+
+	/// <summary>
+	/// Triggers when the size is changed of the main panel.
+	/// </summary>
+	/// <param name="sender">The object that triggers this event.</param>
+	/// <param name="e">The event arguments provided.</param>
+	private void MainPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+	{
+		if (e.NewSize.Width == e.PreviousSize.Width || _cActionableElement is null)
+		{
+			return;
+		}
+
+		if (_cActionableElement.ActualWidth > e.NewSize.Width / 3)
+		{
+			VisualStateManager.GoToState(this, "CompactState", false);
+		}
+		else
+		{
+			VisualStateManager.GoToState(this, "NormalState", false);
+		}
+	}
+}
