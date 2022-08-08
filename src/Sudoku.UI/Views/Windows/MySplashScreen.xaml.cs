90e18f58dc97249b0d69ca51294279f92ae8f6ff
SunnieShine <HowdySunnie@outlook.com>
2022-08-08 13:55:31 +0800

    Add icon support for splash screen.

--- src/Sudoku.UI/Views/Windows/MySplashScreen.xaml.cs
+++ src/Sudoku.UI/Views/Windows/MySplashScreen.xaml.cs
@@ -10,9 +10,26 @@ public sealed partial class MySplashScreen : SplashScreenBase
 	/// </summary>
 	/// <param name="typeOfWindow">The type of the window.</param>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public MySplashScreen(Type typeOfWindow) : base(typeOfWindow) => InitializeComponent();
+	public MySplashScreen(Type typeOfWindow) : base(typeOfWindow)
+	{
+		InitializeComponent();
+		SetProgramNameToTitle();
+		SetIconFromAssetFile();
+	}
 
 
 	/// <inheritdoc/>
 	protected override async Task OnLoading() => await Task.Delay(3000);
+
+	/// <summary>
+	/// Try to set the title.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void SetProgramNameToTitle() => Title = R["ProgramName"];
+
+	/// <summary>
+	/// Sets the icon from assets file.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void SetIconFromAssetFile() => this.GetAppWindow().SetIcon(@"Assets\Logo.ico");
 }
