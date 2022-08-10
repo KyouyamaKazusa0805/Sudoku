070321519031ccb916be376df6bfdb363b55f7fb
SunnieShine <HowdySunnie@outlook.com>
2022-08-10 20:15:11 +0800

    Add indenting.

--- src/Sudoku.Solving.Manual/Searchers/IPatternOverlayStepSearcher.cs
+++ src/Sudoku.Solving.Manual/Searchers/IPatternOverlayStepSearcher.cs
@@ -58,29 +58,61 @@ public interface IPatternOverlayStepSearcher : ILastResortStepSearcher
 	/// <returns>The templates.</returns>
 	protected static sealed IEnumerable<Cells> GetTemplates()
 	{
-#pragma warning disable IDE0011, IDE0055
 		for (int i1 = 0; i1 < 9; i1++)
-		for (int i2 = 0; i2 < 9; i2++)
-		if (i2 / 3 != i1 / 3)
-		for (int i3 = 0; i3 < 9; i3++)
-		if (i3 / 3 != i1 / 3 && i3 / 3 != i2 / 3)
-		for (int i4 = 0; i4 < 9; i4++)
-		if (i4 != i1 && i4 != i2 && i4 != i3)
-		for (int i5 = 0; i5 < 9; i5++)
-		if (i5 != i1 && i5 != i2 && i5 != i3 && i5 / 3 != i4 / 3)
-		for (int i6 = 0; i6 < 9; i6++)
-		if (i6 != i1 && i6 != i2 && i6 != i3 && i6 / 3 != i4 / 3 && i6 / 3 != i5 / 3)
-		for (int i7 = 0; i7 < 9; i7++)
-		if (i7 != i1 && i7 != i2 && i7 != i3 && i7 != i4 && i7 != i5 && i7 != i6)
-		for (int i8 = 0; i8 < 9; i8++)
-		if (i8 != i1 && i8 != i2 && i8 != i3 && i8 != i4 && i8 != i5 && i8 != i6 && i8 / 3 != i7 / 3)
-		for (int i9 = 0; i9 < 9; i9++)
-		if (i9 != i1 && i9 != i2 && i9 != i3 && i9 != i4 && i9 != i5 && i9 != i6 && i9 / 3 != i7 / 3 && i9 / 3 != i8 / 3)
-		yield return Cells.CreateByBits(
-			1 << i1 | 1 << (i2 + 9) | 1 << (i3 + 18),
-			1 << i4 | 1 << (i5 + 9) | 1 << (i6 + 18),
-			1 << i7 | 1 << (i8 + 9) | 1 << (i9 + 18)
-		);
-#pragma warning restore IDE0011, IDE0055
+		{
+			for (int i2 = 0; i2 < 9; i2++)
+			{
+				if (i2 / 3 != i1 / 3)
+				{
+					for (int i3 = 0; i3 < 9; i3++)
+					{
+						if (i3 / 3 != i1 / 3 && i3 / 3 != i2 / 3)
+						{
+							for (int i4 = 0; i4 < 9; i4++)
+							{
+								if (i4 != i1 && i4 != i2 && i4 != i3)
+								{
+									for (int i5 = 0; i5 < 9; i5++)
+									{
+										if (i5 != i1 && i5 != i2 && i5 != i3 && i5 / 3 != i4 / 3)
+										{
+											for (int i6 = 0; i6 < 9; i6++)
+											{
+												if (i6 != i1 && i6 != i2 && i6 != i3 && i6 / 3 != i4 / 3 && i6 / 3 != i5 / 3)
+												{
+													for (int i7 = 0; i7 < 9; i7++)
+													{
+														if (i7 != i1 && i7 != i2 && i7 != i3 && i7 != i4 && i7 != i5 && i7 != i6)
+														{
+															for (int i8 = 0; i8 < 9; i8++)
+															{
+																if (i8 != i1 && i8 != i2 && i8 != i3 && i8 != i4 && i8 != i5 && i8 != i6 && i8 / 3 != i7 / 3)
+																{
+																	for (int i9 = 0; i9 < 9; i9++)
+																	{
+																		if (i9 != i1 && i9 != i2 && i9 != i3 && i9 != i4 && i9 != i5 && i9 != i6 && i9 / 3 != i7 / 3 && i9 / 3 != i8 / 3)
+																		{
+																			yield return Cells.CreateByBits(
+																				1 << i1 | 1 << (i2 + 9) | 1 << (i3 + 18),
+																				1 << i4 | 1 << (i5 + 9) | 1 << (i6 + 18),
+																				1 << i7 | 1 << (i8 + 9) | 1 << (i9 + 18)
+																			);
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
