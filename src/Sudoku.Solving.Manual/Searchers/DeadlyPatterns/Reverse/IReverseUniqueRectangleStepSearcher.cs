7437bcc22e6fed39c3d9a0fc4d8ee4853a5cdaae
SunnieShine <HowdySunnie@outlook.com>
2022-08-17 21:26:34 +0800

    Update doc comment.

--- src/Sudoku.Solving.Manual/Searchers/DeadlyPatterns/Reverse/IReverseUniqueRectangleStepSearcher.cs
+++ src/Sudoku.Solving.Manual/Searchers/DeadlyPatterns/Reverse/IReverseUniqueRectangleStepSearcher.cs
@@ -12,6 +12,12 @@
 /// -->
 /// </list>
 /// </summary>
+/// <!--
+/// Test examples (May or may not be used):
+/// 
+/// 1) Split Reverse UR pairs
+/// ......9...812...6.6.2.3.7.8...4...8....5.6....9...7...3.5.8.1.4.4...123...9......
+/// -->
 public interface IReverseUniqueRectangleStepSearcher : IReverseBivalueUniversalGraveStepSearcher
 {
 }
