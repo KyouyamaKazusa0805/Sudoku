f1e2b27dda2cd10fba3c6a5e26a646edc2037236
SunnieShine <HowdySunnie@outlook.com>
2022-08-22 21:29:11 +0800

    Merge generated files to one.

--- src/Sudoku.Diagnostics.CodeGen/Generators/StepSearcherOptionsGenerator.cs
+++ src/Sudoku.Diagnostics.CodeGen/Generators/StepSearcherOptionsGenerator.cs
@@ -20,202 +20,212 @@ public sealed class StepSearcherOptionsGenerator : IIncrementalGenerator
 
 	/// <inheritdoc/>
 	public void Initialize(IncrementalGeneratorInitializationContext context)
-		=> context.RegisterSourceOutput(context.CompilationProvider, CreateSourceGeneration);
+		=> context.RegisterSourceOutput(
+			context.CompilationProvider,
+			static (spc, compilation) =>
+			{
+				if (compilation is not { Assembly: { Name: Projects.ManualSolving } assemblySymbol })
+				{
+					return;
+				}
 
-	private void CreateSourceGeneration(SourceProductionContext spc, Compilation compilation)
-	{
-		if (compilation is not { Assembly: { Name: Projects.ManualSolving } assemblySymbol })
-		{
-			return;
-		}
+				// Checks whether the assembly has marked any attributes.
+				if (assemblySymbol.GetAttributes() is not { IsDefaultOrEmpty: false } attributesData)
+				{
+					return;
+				}
 
-		// Checks whether the assembly has marked any attributes.
-		if (assemblySymbol.GetAttributes() is not { IsDefaultOrEmpty: false } attributesData)
-		{
-			return;
-		}
+				var enabledAreaTypeSymbol = compilation.GetTypeByMetadataName("Sudoku.Runtime.AnalysisServices.SearcherEnabledArea")!;
+				var disabledReasonTypeSymbol = compilation.GetTypeByMetadataName("Sudoku.Runtime.AnalysisServices.SearcherDisabledReason")!;
 
-		var enabledAreaTypeSymbol = compilation.GetTypeByMetadataName("Sudoku.Runtime.AnalysisServices.SearcherEnabledArea")!;
-		var disabledReasonTypeSymbol = compilation.GetTypeByMetadataName("Sudoku.Runtime.AnalysisServices.SearcherDisabledReason")!;
+				var enabledAreasFields = new Dictionary<byte, string>();
+				var disabledReasonFields = new Dictionary<short, string>();
+				foreach (var fieldSymbol in enabledAreaTypeSymbol.GetMembers().OfType<IFieldSymbol>())
+				{
+					enabledAreasFields.Add((byte)fieldSymbol.ConstantValue!, fieldSymbol.Name);
+				}
+				foreach (var fieldSymbol in disabledReasonTypeSymbol.GetMembers().OfType<IFieldSymbol>())
+				{
+					disabledReasonFields.Add((short)fieldSymbol.ConstantValue!, fieldSymbol.Name);
+				}
 
-		var enabledAreasFields = new Dictionary<byte, string>();
-		var disabledReasonFields = new Dictionary<short, string>();
-		foreach (var fieldSymbol in enabledAreaTypeSymbol.GetMembers().OfType<IFieldSymbol>())
-		{
-			enabledAreasFields.Add((byte)fieldSymbol.ConstantValue!, fieldSymbol.Name);
-		}
-		foreach (var fieldSymbol in disabledReasonTypeSymbol.GetMembers().OfType<IFieldSymbol>())
-		{
-			disabledReasonFields.Add((short)fieldSymbol.ConstantValue!, fieldSymbol.Name);
-		}
-
-		// Gather the valid attributes data.
-		var foundAttributesData = new List<FoundAttributeData>();
-		const string comma = ", ";
-		const string attributeTypeName = $"{Projects.ManualSolving}.SearcherConfigurationAttribute<>";
-		int priorityValue = 0;
-		foreach (var attributeData in attributesData)
-		{
-			// Check validity.
+				// Gather the valid attributes data.
+				var foundAttributesData = new List<FoundAttributeData>();
+				const string comma = ", ";
+				const string attributeTypeName = $"{Projects.ManualSolving}.SearcherConfigurationAttribute<>";
+				int priorityValue = 0;
+				foreach (var attributeData in attributesData)
+				{
 #pragma warning disable IDE0055
-			if (attributeData is not
-				{
-					AttributeClass:
-					{
-						IsGenericType: true,
-						TypeArguments:
-						[
-							INamedTypeSymbol
+					// Check validity.
+					if (attributeData is not
+						{
+							AttributeClass:
 							{
-								IsRecord: false,
-								ContainingNamespace: var containingNamespace,
-								Name: var stepSearcherName
-							} stepSearcherTypeSymbol
-						]
-					} attributeClassSymbol,
-					ConstructorArguments: [{ Type.TypeKind: Kind.Enum, Value: byte dl }],
-					NamedArguments: var namedArguments
-				})
+								IsGenericType: true,
+								TypeArguments:
+								[
+									INamedTypeSymbol
+									{
+										IsRecord: false,
+										ContainingNamespace: var containingNamespace,
+										Name: var stepSearcherName
+									} stepSearcherTypeSymbol
+								]
+							} attributeClassSymbol,
+							ConstructorArguments: [{ Type.TypeKind: Kind.Enum, Value: byte dl }],
+							NamedArguments: var namedArguments
+						})
 #pragma warning restore IDE0055
-			{
-				continue;
-			}
-
-			// Checks whether the type is valid.
-			var unboundAttributeTypeSymbol = attributeClassSymbol.ConstructUnboundGenericType();
-			if (unboundAttributeTypeSymbol.ToDisplayString(TypeFormats.FullName) != attributeTypeName)
-			{
-				continue;
-			}
-
-			// Adds the necessary info into the collection.
-			foundAttributesData.Add(
-				new(
-					containingNamespace,
-					priorityValue++,
-					dl,
-					stepSearcherName,
-					namedArguments
-				)
-			);
-		}
-
-		// Iterate on each valid attribute data, and checks the inner value to be used
-		// by the source generator to output.
-		foreach (var (@namespace, priority, level, name, namedArguments) in foundAttributesData)
-		{
-			// Checks whether the attribute has configured any extra options.
-			byte? enabledArea = null;
-			short? disabledReason = null;
-			if (namedArguments is not [])
-			{
-				foreach (var kvp in namedArguments)
-				{
-					switch (kvp)
 					{
-						case (EnabledAreaPropertyName, { Value: byte ea }):
+						continue;
+					}
+
+					// Checks whether the type is valid.
+					var unboundAttributeTypeSymbol = attributeClassSymbol.ConstructUnboundGenericType();
+					if (unboundAttributeTypeSymbol.ToDisplayString(TypeFormats.FullName) != attributeTypeName)
+					{
+						continue;
+					}
+
+					// Adds the necessary info into the collection.
+					foundAttributesData.Add(
+						new(
+							containingNamespace,
+							priorityValue++,
+							dl,
+							stepSearcherName,
+							namedArguments
+						)
+					);
+				}
+
+				// Iterate on each valid attribute data, and checks the inner value to be used
+				// by the source generator to output.
+				var generatedCodeSnippets = new List<string>();
+				var namespaceUsed = foundAttributesData[0].Namespace;
+				foreach (var (_, priority, level, name, namedArguments) in foundAttributesData)
+				{
+					// Checks whether the attribute has configured any extra options.
+					byte? enabledArea = null;
+					short? disabledReason = null;
+					if (namedArguments is not [])
+					{
+						foreach (var kvp in namedArguments)
 						{
-							enabledArea = ea;
-							break;
-						}
-						case (DisabledReasonPropertyName, { Value: short dr }):
-						{
-							disabledReason = dr;
-							break;
+							switch (kvp)
+							{
+								case (EnabledAreaPropertyName, { Value: byte ea }):
+								{
+									enabledArea = ea;
+									break;
+								}
+								case (DisabledReasonPropertyName, { Value: short dr }):
+								{
+									disabledReason = dr;
+									break;
+								}
+							}
 						}
 					}
+
+					// Gather the extra options on step searcher.
+					StringBuilder? sb = null;
+					if (enabledArea is not null || disabledReason is not null)
+					{
+						sb = new StringBuilder().Append(comma);
+						if (enabledArea is { } ea)
+						{
+							string targetStr = createExpression(ea, $"Searcher{EnabledAreaPropertyName}", enabledAreasFields, disabledReasonFields);
+							sb.Append($"{EnabledAreaPropertyName}: {targetStr}{comma}");
+						}
+						if (disabledReason is { } dr)
+						{
+							string targetStr = createExpression(dr, $"Searcher{DisabledReasonPropertyName}", enabledAreasFields, disabledReasonFields);
+							sb.Append($"{DisabledReasonPropertyName}: {targetStr}{comma}");
+						}
+
+						sb.Remove(sb.Length - comma.Length, comma.Length);
+					}
+
+					// Output the generated code.
+					generatedCodeSnippets.Add(
+						$$"""
+						partial class {{name}}
+						{
+							/// <inheritdoc/>
+							[global::{{typeof(GeneratedCodeAttribute).FullName}}("{{typeof(StepSearcherOptionsGenerator).FullName}}", "{{VersionValue}}")]
+							[global::{{typeof(CompilerGeneratedAttribute).FullName}}]
+							public global::Sudoku.Runtime.AnalysisServices.SearcherInitializationOptions Options { get; set; } =
+								new({{priority}}, global::Sudoku.Runtime.AnalysisServices.SearcherDisplayingLevel.{{(char)(level + 'A' - 1)}}{{sb}});
+						}
+						"""
+					);
+				}
+
+				spc.AddSource(
+					$"StepSearcherConfig.g.{Shortcuts.StepSearcherOptions}.cs",
+					$$"""
+					// <auto-generated/>
+					
+					namespace {{namespaceUsed.ToDisplayString(TypeFormats.FullName)}};
+					
+					{{string.Join("\r\n\r\n", generatedCodeSnippets)}}
+					"""
+				);
+
+
+				static unsafe string createExpression<TUnmanaged>(
+					TUnmanaged field,
+					string typeName,
+					IDictionary<byte, string> enabledAreasFields,
+					IDictionary<short, string> disabledReasonFields)
+					where TUnmanaged : unmanaged
+				{
+					long l = sizeof(TUnmanaged) switch
+					{
+						1 or 2 or 4 => Unsafe.As<TUnmanaged, int>(ref field),
+						8 => Unsafe.As<TUnmanaged, long>(ref field)
+					};
+
+					// Special case: If the value is zero, just get the default field in the enumeration field
+					// or just get the expression '(T)0' as the emitted code.
+					if (l == 0)
+					{
+						return $"(global::Sudoku.Runtime.AnalysisServices.{typeName})0";
+					}
+
+					var targetList = new List<string>();
+					for (var (temp, i) = (l, 0); temp != 0; temp >>= 1, i++)
+					{
+						if ((temp & 1) == 0)
+						{
+							continue;
+						}
+
+						switch (typeName)
+						{
+							case $"Searcher{EnabledAreaPropertyName}"
+							when enabledAreasFields[(byte)(1 << i)] is var fieldValue:
+							{
+								targetList.Add($"global::Sudoku.Runtime.AnalysisServices.Searcher{EnabledAreaPropertyName}.{fieldValue}");
+
+								break;
+							}
+							case $"Searcher{DisabledReasonPropertyName}"
+							when disabledReasonFields[(short)(1 << i)] is var fieldValue:
+							{
+								targetList.Add($"global::Sudoku.Runtime.AnalysisServices.Searcher{DisabledReasonPropertyName}.{fieldValue}");
+
+								break;
+							}
+						}
+					}
+
+					return string.Join(" | ", targetList);
 				}
 			}
-
-			// Gather the extra options on step searcher.
-			StringBuilder? sb = null;
-			if (enabledArea is not null || disabledReason is not null)
-			{
-				sb = new StringBuilder().Append(comma);
-				if (enabledArea is { } ea)
-				{
-					string targetStr = CreateExpression(ea, $"Searcher{EnabledAreaPropertyName}", enabledAreasFields, disabledReasonFields);
-					sb.Append($"{EnabledAreaPropertyName}: {targetStr}{comma}");
-				}
-				if (disabledReason is { } dr)
-				{
-					string targetStr = CreateExpression(dr, $"Searcher{DisabledReasonPropertyName}", enabledAreasFields, disabledReasonFields);
-					sb.Append($"{DisabledReasonPropertyName}: {targetStr}{comma}");
-				}
-
-				sb.Remove(sb.Length - comma.Length, comma.Length);
-			}
-
-			// Output the generated code.
-			spc.AddSource(
-				$"{name}.g.{Shortcuts.StepSearcherOptions}.cs",
-				$$"""
-				// <auto-generated/>
-				
-				namespace {{@namespace.ToDisplayString(TypeFormats.FullName)}};
-				
-				partial class {{name}}
-				{
-					/// <inheritdoc/>
-					[global::{{typeof(GeneratedCodeAttribute).FullName}}("{{typeof(StepSearcherOptionsGenerator).FullName}}", "{{VersionValue}}")]
-					[global::{{typeof(CompilerGeneratedAttribute).FullName}}]
-					public global::Sudoku.Runtime.AnalysisServices.SearcherInitializationOptions Options { get; set; } =
-						new({{priority}}, global::Sudoku.Runtime.AnalysisServices.SearcherDisplayingLevel.{{(char)(level + 'A' - 1)}}{{sb}});
-				}
-				"""
-			);
-		}
-	}
-
-	private unsafe string CreateExpression<TUnmanaged>(
-		TUnmanaged field,
-		string typeName,
-		IDictionary<byte, string> enabledAreasFields,
-		IDictionary<short, string> disabledReasonFields)
-		where TUnmanaged : unmanaged
-	{
-		long l = sizeof(TUnmanaged) switch
-		{
-			1 or 2 or 4 => Unsafe.As<TUnmanaged, int>(ref field),
-			8 => Unsafe.As<TUnmanaged, long>(ref field)
-		};
-
-		// Special case: If the value is zero, just get the default field in the enumeration field
-		// or just get the expression '(T)0' as the emitted code.
-		if (l == 0)
-		{
-			return $"(global::Sudoku.Runtime.AnalysisServices.{typeName})0";
-		}
-
-		var targetList = new List<string>();
-		for (var (temp, i) = (l, 0); temp != 0; temp >>= 1, i++)
-		{
-			if ((temp & 1) == 0)
-			{
-				continue;
-			}
-
-			switch (typeName)
-			{
-				case $"Searcher{EnabledAreaPropertyName}"
-				when enabledAreasFields[(byte)(1 << i)] is var fieldValue:
-				{
-					targetList.Add($"global::Sudoku.Runtime.AnalysisServices.Searcher{EnabledAreaPropertyName}.{fieldValue}");
-
-					break;
-				}
-				case $"Searcher{DisabledReasonPropertyName}"
-				when disabledReasonFields[(short)(1 << i)] is var fieldValue:
-				{
-					targetList.Add($"global::Sudoku.Runtime.AnalysisServices.Searcher{DisabledReasonPropertyName}.{fieldValue}");
-
-					break;
-				}
-			}
-		}
-
-		return string.Join(" | ", targetList);
-	}
+		);
 }
 
 /// <summary>
