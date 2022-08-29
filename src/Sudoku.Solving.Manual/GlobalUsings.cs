bd933adf390b8235cb533e622251eee98c4f6370
SunnieShine <HowdySunnie@outlook.com>
2022-08-29 14:18:36 +0800

    Remove unused using directive.

--- src/Sudoku.Solving.Manual/GlobalUsings.cs
+++ src/Sudoku.Solving.Manual/GlobalUsings.cs
@@ -3,7 +3,6 @@ global using System.Algorithm;
 global using System.Buffers;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
-global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Numerics;
