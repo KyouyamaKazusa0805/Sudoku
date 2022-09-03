01885150eb62a3f533da8b525e210a7107ad29b4
SunnieShine <HowdySunnie@outlook.com>
2022-09-03 22:51:17 +0800

    Extract method.

--- src/Sudoku.UI/Program.cs
+++ src/Sudoku.UI/Program.cs
@@ -14,21 +14,31 @@ public static partial class Program
 	/// <summary>
 	/// Indicates the main entry point of the program.
 	/// </summary>
-	/// <param name="args">The command line arguments. The default value is an empty array.</param>
+	/// <param name="args">
+	/// <para>The command line arguments. The default value is an empty array.</para>
+	/// <para><i>
+	/// This method does not use command-line arguments, but it will be reserved in order to be used in the future.
+	/// </i></para>
+	/// </param>
 	[STAThread]
-	private static void Main([SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")] string[] args)
+	[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+	private static void Main(string[] args)
 	{
 		CheckProcessRequirements();
 
 		ComWrappersSupport.InitializeComWrappers();
-		Application.Start(
-			static delegate
-			{
-				var context = new DispatcherQueueSynchronizationContext(MsDispatcherQueue.GetForCurrentThread());
-				SynchronizationContext.SetSynchronizationContext(context);
-				_ = new App();
-			}
-		);
+		Application.Start(ApplicationStartingCallback);
+	}
+
+	/// <summary>
+	/// The callback method that is called by main method. <i>Please do not call this method in other places.</i>
+	/// </summary>
+	/// <param name="p">The callback method arguments.</param>
+	private static void ApplicationStartingCallback(ApplicationInitializationCallbackParams p)
+	{
+		var context = new DispatcherQueueSynchronizationContext(MsDispatcherQueue.GetForCurrentThread());
+		SynchronizationContext.SetSynchronizationContext(context);
+		_ = new App();
 	}
 
 	[LibraryImport("Microsoft.ui.xaml", EntryPoint = "XamlCheckProcessRequirements")]
