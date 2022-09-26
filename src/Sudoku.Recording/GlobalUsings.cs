568e3f68831044e033ff649946a5898957fd2ff5
SunnieShine <HowdySunnie@outlook.com>
2022-09-26 16:45:26 +0800

    Extract project `Sudoku.Recording`.

---
+++ src/Sudoku.Recording/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Collections.Immutable;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Numerics;
+global using System.Runtime.CompilerServices;
+global using Sudoku.Concepts;
