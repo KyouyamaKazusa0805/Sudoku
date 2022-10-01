4b66d249e502be2083ba79e0de8ae6c426eaf8b6
SunnieShine <HowdySunnie@outlook.com>
2022-10-01 14:34:39 +0800

    Inline lambda.

--- src/Sudoku.Diagnostics.CodeGen/Generators/EnumSwitchExpressionGenerator.cs
+++ src/Sudoku.Diagnostics.CodeGen/Generators/EnumSwitchExpressionGenerator.cs
@@ -26,160 +26,155 @@ public sealed class EnumSwitchExpressionGenerator : IIncrementalGenerator
 	public void Initialize(IncrementalGeneratorInitializationContext context)
 		=> context.RegisterSourceOutput(
 			context.SyntaxProvider
-				.CreateSyntaxProvider(NodePredicate, Transform)
+				.CreateSyntaxProvider(
+					static (node, _) => node is EnumDeclarationSyntax,
+					static (INamedTypeSymbol, DataTuple[])? (gsc, ct) =>
+					{
+						if (gsc is not
+							{
+								Node: EnumDeclarationSyntax enumDeclarationSyntaxNode,
+								SemanticModel: { Compilation: var compilation } semanticModel
+							})
+						{
+							return null;
+						}
+
+						var rawTypeSymbol = semanticModel.GetDeclaredSymbol(enumDeclarationSyntaxNode, ct);
+						if (rawTypeSymbol is not INamedTypeSymbol typeSymbol)
+						{
+							return null;
+						}
+
+						var switchExprRoot = compilation.GetTypeByMetadataName(SwitchExprRootFullName);
+						var typeAttributesData = (
+							from attributeData in typeSymbol.GetAttributes()
+							where SymbolEqualityComparer.Default.Equals(attributeData.AttributeClass, switchExprRoot)
+							select attributeData
+						).Distinct(new AttributeDataComparer()).ToArray();
+						if (typeAttributesData.Length == 0)
+						{
+							return null;
+						}
+
+						var result = new List<DataTuple>();
+						var switchExprArm = compilation.GetTypeByMetadataName(SwitchExprArmFullName);
+						var fieldAndItsCorrespondingAttributeData = new List<(IFieldSymbol, AttributeData)>();
+						foreach (var attributeData in typeAttributesData)
+						{
+							var key = (string)attributeData.ConstructorArguments[0].Value!;
+							foreach (var field in typeSymbol.GetMembers().OfType<IFieldSymbol>())
+							{
+								var fieldAttributeData = (
+									from fad in field.GetAttributes()
+									where SymbolEqualityComparer.Default.Equals(fad.AttributeClass, switchExprArm)
+									let construtorArgs = fad.ConstructorArguments
+									where construtorArgs.Length >= 1
+									let firstConstructorArg = (string)construtorArgs[0].Value!
+									where firstConstructorArg == key
+									select fad
+								).FirstOrDefault();
+
+								fieldAndItsCorrespondingAttributeData.Add((field, fieldAttributeData));
+							}
+
+							result.Add(new(typeSymbol, key, attributeData, fieldAndItsCorrespondingAttributeData.ToArray()));
+							fieldAndItsCorrespondingAttributeData.Clear();
+						}
+
+						return (typeSymbol, result.ToArray());
+					}
+				)
 				.Where(static pair => pair is not null)
 				.Collect(),
-			GenerateSource
-		);
-
-	private (INamedTypeSymbol, DataTuple[])? Transform(GeneratorSyntaxContext gsc, CancellationToken ct)
-	{
-		if (gsc is not
+			(spc, collection) =>
 			{
-				Node: EnumDeclarationSyntax enumDeclarationSyntaxNode,
-				SemanticModel: { Compilation: var compilation } semanticModel
-			})
-		{
-			return null;
-		}
-
-		var rawTypeSymbol = semanticModel.GetDeclaredSymbol(enumDeclarationSyntaxNode, ct);
-		if (rawTypeSymbol is not INamedTypeSymbol typeSymbol)
-		{
-			return null;
-		}
-
-		var switchExprRoot = compilation.GetTypeByMetadataName(SwitchExprRootFullName);
-		var typeAttributesData = (
-			from attributeData in typeSymbol.GetAttributes()
-			where SymbolEqualityComparer.Default.Equals(attributeData.AttributeClass, switchExprRoot)
-			select attributeData
-		).Distinct(new AttributeDataComparer()).ToArray();
-		if (typeAttributesData.Length == 0)
-		{
-			return null;
-		}
-
-		var result = new List<DataTuple>();
-		var switchExprArm = compilation.GetTypeByMetadataName(SwitchExprArmFullName);
-		var fieldAndItsCorrespondingAttributeData = new List<(IFieldSymbol, AttributeData)>();
-		foreach (var attributeData in typeAttributesData)
-		{
-			var key = (string)attributeData.ConstructorArguments[0].Value!;
-			foreach (var field in typeSymbol.GetMembers().OfType<IFieldSymbol>())
-			{
-				var fieldAttributeData = (
-					from fad in field.GetAttributes()
-					where SymbolEqualityComparer.Default.Equals(fad.AttributeClass, switchExprArm)
-					let construtorArgs = fad.ConstructorArguments
-					where construtorArgs.Length >= 1
-					let firstConstructorArg = (string)construtorArgs[0].Value!
-					where firstConstructorArg == key
-					select fad
-				).FirstOrDefault();
-
-				fieldAndItsCorrespondingAttributeData.Add((field, fieldAttributeData));
-			}
-
-			result.Add(new(typeSymbol, key, attributeData, fieldAndItsCorrespondingAttributeData.ToArray()));
-			fieldAndItsCorrespondingAttributeData.Clear();
-		}
-
-		return (typeSymbol, result.ToArray());
-	}
-
-	/// <inheritdoc/>
-	public void GenerateSource(SourceProductionContext spc, ImmutableArray<(INamedTypeSymbol, DataTuple[])?> collection)
-	{
-		// Iterates on each tuple array.
-		// An array of tuples is a group, storing a set of tuples whose key and type are same.
-		foreach (var pair in collection)
-		{
-			if (pair is not var (type, tuples))
-			{
-				continue;
-			}
-
-			var fullName = type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-
-			var emittedMethods = new List<string>();
-
-			// Iterates on each tuple element in the array.
-			foreach (var (_, key, typeAttributeData, listOfFieldsAndTheirOwnAttributesData) in tuples)
-			{
-				var innerParts = new List<string>();
-
-				foreach (var (fieldSymbol, attributeData) in listOfFieldsAndTheirOwnAttributesData)
+				// Iterates on each tuple array.
+				// An array of tuples is a group, storing a set of tuples whose key and type are same.
+				foreach (var pair in collection)
 				{
-					var value = (string)attributeData.ConstructorArguments[1].Value!;
-					innerParts.Add($"""{fullName}.{fieldSymbol.Name} => "{value}",""");
-				}
+					if (pair is not var (type, tuples))
+					{
+						continue;
+					}
 
-				int notDefinedBehavior = typeAttributeData.GetNamedArgument<byte>("DefaultBehavior", 2);
-				var notFoundBehaviorStr = notDefinedBehavior switch
-				{
-					// ReturnIntegerValue
-					0 => "@this.ToString()",
+					var fullName = type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
 
-					// ReturnNull
-					1 => "null",
+					var emittedMethods = new List<string>();
 
-					// ThrowForNotDefined
-					_ => "throw new global::System.ArgumentOutOfRangeException(nameof(@this))"
-				};
+					// Iterates on each tuple element in the array.
+					foreach (var (_, key, typeAttributeData, listOfFieldsAndTheirOwnAttributesData) in tuples)
+					{
+						var innerParts = new List<string>();
+						foreach (var (fieldSymbol, attributeData) in listOfFieldsAndTheirOwnAttributesData)
+						{
+							var value = (string)attributeData.ConstructorArguments[1].Value!;
+							innerParts.Add($"""{fullName}.{fieldSymbol.Name} => "{value}",""");
+						}
 
-				var methodDescription = typeAttributeData.GetNamedArgument<string>("MethodDescription")
-					?? $"Method <c>{key}</c>";
-				var thisParamDescription = typeAttributeData.GetNamedArgument<string>("ThisParameterDescription")
-					?? "The current instance.";
-				var returnValueDescription = typeAttributeData.GetNamedArgument<string>("ReturnValueDescription")
-					?? "The result value.";
-				var returnType = notDefinedBehavior == 1 ? "string?" : "string";
+						int notDefinedBehavior = typeAttributeData.GetNamedArgument<byte>("DefaultBehavior", 2);
+						var notFoundBehaviorStr = notDefinedBehavior switch
+						{
+							// ReturnIntegerValue
+							0 => "@this.ToString()",
 
-				emittedMethods.Add(
-					$$"""
-					/// <summary>
-						/// {{methodDescription}}
+							// ReturnNull
+							1 => "null",
+
+							// ThrowForNotDefined
+							_ => "throw new global::System.ArgumentOutOfRangeException(nameof(@this))"
+						};
+
+						var methodDescription = typeAttributeData.GetNamedArgument<string>("MethodDescription")
+							?? $"Method <c>{key}</c>";
+						var thisParamDescription = typeAttributeData.GetNamedArgument<string>("ThisParameterDescription")
+							?? "The current instance.";
+						var returnValueDescription = typeAttributeData.GetNamedArgument<string>("ReturnValueDescription")
+							?? "The result value.";
+						var returnType = notDefinedBehavior == 1 ? "string?" : "string";
+
+						emittedMethods.Add(
+							$$"""
+							/// <summary>
+								/// {{methodDescription}}
+								/// </summary>
+								/// <param name="this">{{thisParamDescription}}</param>
+								/// <returns>{{returnValueDescription}}</returns>
+								[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+								[global::System.CodeDom.Compiler.GeneratedCodeAttribute("{{GetType().FullName}}", "{{VersionValue}}")]
+								[global::System.Runtime.CompilerServices.MethodImplAttribute(global::System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+								public static {{returnType}} {{key}}(this {{fullName}} @this)
+									=> @this switch
+									{
+										{{string.Join("\r\n\t\t\t", innerParts)}}
+										_ => {{notFoundBehaviorStr}}
+									};
+							"""
+						);
+					}
+
+					spc.AddSource(
+						$"{type.ToFileName()}.g.{Shortcuts.EnumSwitchExpression}.cs",
+						$$"""
+						// <auto-generated/>
+						
+						#nullable enable
+						
+						namespace {{SymbolOutputInfo.FromSymbol(type).NamespaceName}};
+						
+						/// <summary>
+						/// Provides with extension methods for switching on the current type.
 						/// </summary>
-						/// <param name="this">{{thisParamDescription}}</param>
-						/// <returns>{{returnValueDescription}}</returns>
 						[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]
 						[global::System.CodeDom.Compiler.GeneratedCodeAttribute("{{GetType().FullName}}", "{{VersionValue}}")]
-						[global::System.Runtime.CompilerServices.MethodImplAttribute(global::System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-						public static {{returnType}} {{key}}(this {{fullName}} @this)
-							=> @this switch
-							{
-								{{string.Join("\r\n\t\t\t", innerParts)}}
-								_ => {{notFoundBehaviorStr}}
-							};
-					"""
-				);
-			}
-
-			spc.AddSource(
-				$"{type.ToFileName()}.g.{Shortcuts.EnumSwitchExpression}.cs",
-				$$"""
-				// <auto-generated/>
-				
-				#nullable enable
-				
-				namespace {{SymbolOutputInfo.FromSymbol(type).NamespaceName}};
-				
-				/// <summary>
-				/// Provides with extension methods for switching on the current type.
-				/// </summary>
-				[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]
-				[global::System.CodeDom.Compiler.GeneratedCodeAttribute("{{GetType().FullName}}", "{{VersionValue}}")]
-				public static class {{type.Name}}_EnumSwitchExpressionExtensions
-				{
-					{{string.Join("\r\n\r\n\t", emittedMethods)}}
+						public static class {{type.Name}}_EnumSwitchExpressionExtensions
+						{
+							{{string.Join("\r\n\r\n\t", emittedMethods)}}
+						}
+						"""
+					);
 				}
-				"""
-			);
-		}
-	}
-
-	private static bool NodePredicate(SyntaxNode node, CancellationToken _) => node is EnumDeclarationSyntax;
+			}
+		);
 }
 
 /// <summary>
