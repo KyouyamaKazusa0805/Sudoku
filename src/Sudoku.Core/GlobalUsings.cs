e51e84d88220d0c14f7dda59495b2d7055012235
SunnieShine <HowdySunnie@outlook.com>
2022-10-05 10:53:21 +0800

    Remove unused using.

--- src/Sudoku.Core/GlobalUsings.cs
+++ src/Sudoku.Core/GlobalUsings.cs
@@ -13,7 +13,6 @@ global using System.Linq;
 global using System.Numerics;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
-global using System.Runtime.InteropServices;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
