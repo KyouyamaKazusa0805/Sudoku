bdf301887a9a1ca6122cba5fc69b4162df8c248a
SunnieShine <HowdySunnie@outlook.com>
2022-10-15 14:47:22 +0800

    Update doc comment.

--- src/System/Diagnostics/CodeAnalysis/DisallowFunctionPointerInvocationAttribute.cs
+++ src/System/Diagnostics/CodeAnalysis/DisallowFunctionPointerInvocationAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace System.Diagnostics.CodeAnalysis;
 
 /// <summary>
-/// Indicates a field of function pointer type that cannot be invokable. 
+/// Indicates a field of function pointer type that cannot be invokable outside the scope of the type declaring that field.
 /// </summary>
 [AttributeUsage(AttributeTargets.Field, Inherited = false)]
 public sealed class DisallowFunctionPointerInvocationAttribute : Attribute
