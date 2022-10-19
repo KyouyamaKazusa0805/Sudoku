e1698fe57d5cabc701496b018279e6ee7cfa54e1
SunnieShine <HowdySunnie@outlook.com>
2022-10-19 18:51:58 +0800

    Update code.

--- src/Sudoku.Gdip/Gdip/IPointCalculator.cs
+++ src/Sudoku.Gdip/Gdip/IPointCalculator.cs
@@ -232,128 +232,147 @@ file sealed class PointCalculator : IPointCalculator
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetCell(scoped in PointF point)
-		=> point with { X = point.X - Offset, Y = point.Y - Offset } switch
+	{
+		var (x, y) = point with { X = point.X - Offset, Y = point.Y - Offset };
+		if (x < 0 || x > GridSize.Width || y < 0 || y > GridSize.Height)
 		{
-			var (x, y) => (x >= 0 && x <= GridSize.Width && y >= 0 && y <= GridSize.Height) switch
-			{
-				true => CellSize switch
-				{
-					var (cw, ch) => (int)(y / ch) * 9 + (int)(x / cw) switch
-					{
-						var result and >= 0 and < 81 => result,
-						_ => -1
-					}
-				},
-				_ => -1
-			}
-		};
+			return -1;
+		}
+
+		var (cw, ch) = CellSize;
+		var result = (int)(y / ch) * 9 + (int)(x / cw);
+		if (result is not >= 0 or not < 81)
+		{
+			return -1;
+		}
+
+		return result;
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetCandidate(scoped in PointF point)
-		=> (point, CandidateSize) switch
-		{
-			var ((x, y), (cw, ch)) => ((int)((y - Offset) / ch), (int)((x - Offset) / cw)) switch
-			{
-				var (a, b) => GetCell(point) * 9 + a % 3 * 3 + b % 3
-			}
-		};
+	{
+		var ((x, y), (cw, ch)) = (point, CandidateSize);
+		var (a, b) = ((int)((y - Offset) / ch), (int)((x - Offset) / cw));
+		return GetCell(point) * 9 + a % 3 * 3 + b % 3;
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public PointF GetMouseCenter(scoped in Candidates map)
-		=> map switch
+	{
+		switch (map)
 		{
-			[var min, .., var max] => (min / 9, min % 9, max / 9, max % 9) switch
+			case [var min, .., var max]:
 			{
-				var (cMin, dMin, cMax, dMax) => (GetMousePointInCenter(cMin, dMin), GetMousePointInCenter(cMax, dMax)) switch
-				{
-					var ((x1, y1), (x2, y2)) => new((x1 + x2) / 2, (y1 + y2) / 2)
-				}
-			},
-			[var s] => GetMousePointInCenter(s / 9, s % 9),
-			_ => throw new ArgumentException("Cannot get at least 1 candidate in the map.")
+				var (cMin, dMin, cMax, dMax) = (min / 9, min % 9, max / 9, max % 9);
+				var ((x1, y1), (x2, y2)) = (GetMousePointInCenter(cMin, dMin), GetMousePointInCenter(cMax, dMax));
+				return new((x1 + x2) / 2, (y1 + y2) / 2);
+			}
+			case [var s]:
+			{
+				return GetMousePointInCenter(s / 9, s % 9);
+			}
+			default:
+			{
+				throw new ArgumentException("Cannot get at least 1 candidate in the map.");
+			}
 		};
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public PointF GetMouseCenter(scoped in LockedTarget lockedTarget)
-		=> lockedTarget switch
+	{
+		switch (lockedTarget)
 		{
-			{ Cells: var cells, Digit: var digit } => cells switch
+			case { Cells: [var a, .., var b], Digit: var digit }:
 			{
-				[var a, .., var b] => (a * 9 + digit, b * 9 + digit) switch
-				{
-					var (min, max) => (GetMousePointInCenter(min / 9, min % 9), GetMousePointInCenter(max / 9, max % 9)) switch
-					{
-						var ((x1, y1), (x2, y2)) => new((x1 + x2) / 2, (y1 + y2) / 2)
-					}
-				},
-				[var s] => (s * 9 + digit) switch { var c => GetMousePointInCenter(c / 9, c % 9) },
-				_ => throw new ArgumentException("Cannot get at least 1 cell in the map.")
+				var (min, max) = (a * 9 + digit, b * 9 + digit);
+				var ((x1, y1), (x2, y2)) = (GetMousePointInCenter(min / 9, min % 9), GetMousePointInCenter(max / 9, max % 9));
+				return new((x1 + x2) / 2, (y1 + y2) / 2);
 			}
-		};
+			case { Cells: [var s], Digit: var digit }:
+			{
+				var c = s * 9 + digit;
+				return GetMousePointInCenter(c / 9, c % 9);
+			}
+			default:
+			{
+				throw new ArgumentException("Cannot get at least 1 cell in the map.");
+			}
+		}
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public RectangleF GetMouseRectangle(scoped in Candidates map)
-		=> (map, CandidateSize) switch
+	{
+		switch (map, CandidateSize)
 		{
-			([var min, .., var max], var (cw, ch)) => (min / 9, min % 9, max / 9, max % 9) switch
+			case ([var min, .., var max], var (cw, ch)):
 			{
-				var (cMin, dMin, cMax, dMax) => (GetMousePointInCenter(cMin, dMin), GetMousePointInCenter(cMax, dMax)) switch
-				{
-					var (a, b) => (a with { X = a.X - cw / 2, Y = a.Y - ch / 2 }, b with { X = b.X + cw / 2, Y = b.Y + ch / 2 }) switch
-					{
-						var (p, q) => RectangleMarshal.CreateInstance(p, q)
-					}
-				}
-			},
-			_ => throw new ArgumentException("Cannot get at least 1 candidate in the map.")
+				var (cMin, dMin, cMax, dMax) = (min / 9, min % 9, max / 9, max % 9);
+				var (a, b) = (GetMousePointInCenter(cMin, dMin), GetMousePointInCenter(cMax, dMax));
+				var (p, q) = (a with { X = a.X - cw / 2, Y = a.Y - ch / 2 }, b with { X = b.X + cw / 2, Y = b.Y + ch / 2 });
+				return RectangleMarshal.CreateInstance(p, q);
+			}
+			default:
+			{
+				throw new ArgumentException("Cannot get at least 1 candidate in the map.");
+			}
 		};
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public RectangleF GetMouseRectangleViaCell(int cell)
-		=> (this, GetMousePointInCenter(cell)) switch
-		{
-			({ CellSize: var (cw, ch) }, var (x, y)) => new(x - cw / 2, y - ch / 2, cw, ch)
-		};
+	{
+		var ((cw, ch), (x, y)) = (CellSize, GetMousePointInCenter(cell));
+		return new(x - cw / 2, y - ch / 2, cw, ch);
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public RectangleF GetMouseRectangle(int cell, int digit)
-		=> (this, GetMousePointInCenter(cell, digit)) switch
-		{
-			({ CandidateSize: var (cw, ch) }, var (x, y)) => new(x - cw / 2, y - ch / 2, cw, ch)
-		};
+	{
+		var ((cw, ch), (x, y)) = (CandidateSize, GetMousePointInCenter(cell, digit));
+		return new(x - cw / 2, y - ch / 2, cw, ch);
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public RectangleF GetMouseRectangleViaHouse(int house)
-		=> GetAnchorsViaHouse(house) switch { var (l, r) => RectangleMarshal.CreateInstance(l, r) };
+	{
+		var (l, r) = GetAnchorsViaHouse(house);
+		return RectangleMarshal.CreateInstance(l, r);
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public (PointF LeftUp, PointF RightDown) GetAnchorsViaHouse(int house)
 		=> house switch
 		{
-			>= 0 and < 9 => (house % 3, house / 3) switch { var (v1, v2) => (GridPoints[v1 * 9, v2 * 9], GridPoints[v1 * 9 + 9, v2 * 9 + 9]) },
-			>= 9 and < 18 => (house - 9) switch { var v => (GridPoints[0, v * 3], GridPoints[27, v * 3 + 3]) },
-			>= 18 and < 27 => (house - 18) switch { var v => (GridPoints[v * 3, 0], GridPoints[v * 3 + 3, 27]) },
+			>= 0 and < 9 when (house % 3, house / 3) is var (v1, v2) => (GridPoints[v1 * 9, v2 * 9], GridPoints[v1 * 9 + 9, v2 * 9 + 9]),
+			>= 9 and < 18 when house - 9 is var v => (GridPoints[0, v * 3], GridPoints[27, v * 3 + 3]),
+			>= 18 and < 27 when house - 18 is var v => (GridPoints[v * 3, 0], GridPoints[v * 3 + 3, 27]),
 			_ => throw new ArgumentOutOfRangeException(nameof(house))
 		};
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public PointF GetMousePointInCenter(int cell)
-		=> (this, GridPoints[cell % 9 * 3, cell / 9 * 3]) switch { ({ CellSize: var (cw, ch) }, var (x, y)) => new(x + cw / 2, y + ch / 2) };
+	{
+		var ((cw, ch), (x, y)) = (CellSize, GridPoints[cell % 9 * 3, cell / 9 * 3]);
+		return new(x + cw / 2, y + ch / 2);
+	}
 
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public PointF GetMousePointInCenter(int cell, int digit)
-		=> (this, GridPoints[cell % 9 * 3 + digit % 3, cell / 9 * 3 + digit / 3]) switch
-		{
-			({ CandidateSize: var (cw, ch) }, var (x, y)) => new(x + cw / 2, y + ch / 2)
-		};
+	{
+		var ((cw, ch), (x, y)) = (CandidateSize, GridPoints[cell % 9 * 3 + digit % 3, cell / 9 * 3 + digit / 3]);
+		return new(x + cw / 2, y + ch / 2);
+	}
 }
