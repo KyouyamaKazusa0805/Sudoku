2211302880105a9936ee21fc4bdefe587813f762
SunnieShine <HowdySunnie@outlook.com>
2022-10-20 21:04:53 +0800

    Fix typo.

--- src/Sudoku.Test/Apis/DrawingApi/SudokuPainterTest.cs
+++ src/Sudoku.Test/Apis/DrawingApi/SudokuPainterTest.cs
@@ -5,7 +5,7 @@ public sealed class SudokuPainterTest
 {
 	[TestMethod]
 	[SupportedOSPlatform("windows")]
-	public void TestCase_Wmf_AlignmentRight()
+	public void TestCase_Wmf_AlignRight()
 	{
 		var sudokuPainter = ISudokuPainter.Create(1498, 20)
 			.WithGridCode("6...8..4..+4.....862..4.67+5.+1+5397+486+2.7...+3+51+4+462+5183+9+7..61.2+4+7572+4+8...+3..1..4+7.+28:314 916 921 325 925 932 933 371 985 991")
