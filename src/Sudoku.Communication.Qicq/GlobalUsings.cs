6f016337eb405c09df267ee5dc3a796fe8d60eae
SunnieShine <HowdySunnie@outlook.com>
2022-10-27 13:47:14 +0800

    Add resource dictionary.

--- src/Sudoku.Communication.Qicq/GlobalUsings.cs
+++ src/Sudoku.Communication.Qicq/GlobalUsings.cs
@@ -1,6 +1,10 @@
 ﻿global using System;
 global using System.Reactive.Linq;
+global using System.Resources;
 global using System.Threading;
+global using Mirai.Net.Data.Events.Concretes.Group;
 global using Mirai.Net.Data.Messages.Receivers;
 global using Mirai.Net.Sessions;
+global using Mirai.Net.Sessions.Http.Managers;
 global using Mirai.Net.Utils.Scaffolds;
+global using Sudoku.Communication.Qicq.Strings;
--- src/Sudoku.Communication.Qicq/Program.cs
+++ src/Sudoku.Communication.Qicq/Program.cs
@@ -1,17 +1,46 @@
-﻿using var bot = new MiraiBot { Address = "localhost:8080", QQ = "979329690", VerifyKey = "helloworld" };
+﻿using static LocalResourceFetcher;
+
+// Creates and initializes a bot.
+using var bot = new MiraiBot { Address = GetString("HostPort"), QQ = GetString("BotQQ"), VerifyKey = GetString("VerifyKey") };
 await bot.LaunchAsync();
 
+// Registers some necessary events.
 bot.MessageReceived.OfType<GroupMessageReceiver>().Subscribe(onGroupMessageReceiving);
+bot.EventReceived.OfType<MemberJoinedEvent>().Subscribe(onMemberJoined);
 
-// Blocks the main thread, in order to avoid main thread returning too fast.
+// Blocks the main thread, in order to prevent the main thread exits too fast.
 var signal = new ManualResetEvent(false);
 signal.WaitOne();
 
 
+static async void onMemberJoined(MemberJoinedEvent e)
+{
+	if (e.Member.Group is { Id: var id } group && id == GetString("SudokuGroupQQ"))
+	{
+		await group.SendGroupMessageAsync(GetString("SampleMemberJoinedMessage"));
+	}
+}
+
 static async void onGroupMessageReceiving(GroupMessageReceiver e)
 {
-	if (e.Sender.Id == "747507738")
+	// Test code.
+	if (e.Sender.Id == GetString("AdminQQ"))
 	{
-		await e.SendMessageAsync("Hello, world!");
+		await e.SendMessageAsync(GetString("SampleReplyMessage"));
 	}
+}
+
+
+/// <summary>
+/// Defines a local common resource fetcher.
+/// </summary>
+file static class LocalResourceFetcher
+{
+	/// <summary>
+	/// Fetches the resource via the key. This method simply calls <see cref="ResourceManager.GetString(string)"/>.
+	/// </summary>
+	/// <param name="key">The resource key.</param>
+	/// <returns>The string value.</returns>
+	/// <seealso cref="ResourceManager"/>
+	public static string? GetString(string key) => Resources.ResourceManager.GetString(key);
 }
