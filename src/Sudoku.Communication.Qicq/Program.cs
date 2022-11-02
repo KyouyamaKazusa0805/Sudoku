2ef0f28a95f7d91a2ad00d9be9a053c3d2eaa7b7
SunnieShine <HowdySunnie@outlook.com>
2022-11-02 14:08:04 +0800

    Update code.

--- src/Sudoku.Communication.Qicq/Program.cs
+++ src/Sudoku.Communication.Qicq/Program.cs
@@ -10,48 +10,10 @@ try
 	await bot.LaunchAsync();
 
 	// Registers some necessary events.
-	bot.SubscribeGroupMessageReceived(async e =>
-	{
-		if (e is { Sender.Permission: var permission, MessageChain: (_, { } messageTrimmed, _) })
-		{
-			foreach (var type in currentAssembly.GetDerivedTypes<Command>())
-			{
-				if (type.GetConstructor(Array.Empty<Type>()) is not null
-					&& type.GetCustomAttribute<CommandAttribute>() is { AllowedPermissions: var allowPermissions }
-					&& Array.IndexOf(allowPermissions, permission) != -1
-					&& await ((Command)Activator.CreateInstance(type)!).ExecuteAsync(messageTrimmed, e))
-				{
-					return;
-				}
-			}
-		}
-	});
-	bot.SubscribeMemberJoined(static async e =>
-	{
-		if (e.Member.Group is { Id: var groupId } group && groupId == R["SudokuGroupQQ"])
-		{
-			await group.SendGroupMessageAsync(R["SampleMemberJoinedMessage"]);
-		}
-	});
-	bot.SubscribeNewMemberRequested(async e =>
-	{
-		if (e is { GroupId: var groupId, Message: var message } && groupId == R["SudokuGroupQQ"]
-			&& await bot.CanHandleInvitationOrJoinRequestAsync(groupId))
-		{
-			await (
-				message.Trim().IsMatch("""((\u54d4\u54e9)\2|[Bb]\s{0,3}\u7ad9|[Bb]i(li)bi\3)""")
-					? e.ApproveAsync()
-					: e.RejectAsync(R["_MessageFormat_RejectJoiningGroup"]!)
-			);
-		}
-	});
-	bot.SubscribeNewInvitationRequested(async e =>
-	{
-		if (e is { GroupId: var groupId } && groupId == R["SudokuGroupQQ"] && await bot.CanHandleInvitationOrJoinRequestAsync(groupId))
-		{
-			await e.ApproveAsync();
-		}
-	});
+	bot.SubscribeGroupMessageReceived(onGroupMessageReceived);
+	bot.SubscribeMemberJoined(onMemberJoined);
+	bot.SubscribeNewMemberRequested(onNewMemberRequested);
+	bot.SubscribeNewInvitationRequested(onNewInvitationRequested);
 
 	// Blocks the main thread, in order to prevent the main thread exits too fast.
 	Terminal.WriteLine(R["BootingSuccessMessage"]!, ConsoleColor.DarkGreen);
@@ -66,3 +28,50 @@ catch (InvalidResponseException)
 }
 
 Terminal.Pause();
+
+
+async void onGroupMessageReceived(GroupMessageReceiver e)
+{
+	if (e is { Sender.Permission: var permission, MessageChain: (_, { } messageTrimmed, _) })
+	{
+		foreach (var type in currentAssembly.GetDerivedTypes<Command>())
+		{
+			if (type.GetConstructor(Array.Empty<Type>()) is not null
+				&& type.GetCustomAttribute<CommandAttribute>() is { AllowedPermissions: var allowPermissions }
+				&& Array.IndexOf(allowPermissions, permission) != -1
+				&& await ((Command)Activator.CreateInstance(type)!).ExecuteAsync(messageTrimmed, e))
+			{
+				return;
+			}
+		}
+	}
+}
+
+static async void onMemberJoined(MemberJoinedEvent e)
+{
+	if (e.Member.Group is { Id: var groupId } group && groupId == R["SudokuGroupQQ"])
+	{
+		await group.SendGroupMessageAsync(R["SampleMemberJoinedMessage"]);
+	}
+}
+
+async void onNewMemberRequested(NewMemberRequestedEvent e)
+{
+	if (e is { GroupId: var groupId, Message: var message } && groupId == R["SudokuGroupQQ"]
+		&& await bot.CanHandleInvitationOrJoinRequestAsync(groupId))
+	{
+		await (
+			message.Trim().IsMatch("""((\u54d4\u54e9)\2|[Bb]\s{0,3}\u7ad9|[Bb]i(li)bi\3)""")
+				? e.ApproveAsync()
+				: e.RejectAsync(R["_MessageFormat_RejectJoiningGroup"]!)
+		);
+	}
+}
+
+async void onNewInvitationRequested(NewInvitationRequestedEvent e)
+{
+	if (e is { GroupId: var groupId } && groupId == R["SudokuGroupQQ"] && await bot.CanHandleInvitationOrJoinRequestAsync(groupId))
+	{
+		await e.ApproveAsync();
+	}
+}
