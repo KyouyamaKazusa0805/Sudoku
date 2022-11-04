0d4094e5c7539561ff4eea3f8326ad7ab27932be
SunnieShine <HowdySunnie@outlook.com>
2022-11-04 20:58:52 +0800

    Remove unused namespace.

--- src/Sudoku.Compatibility/GlobalUsings.cs
+++ src/Sudoku.Compatibility/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿global using System;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Numerics;
-global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Text;
 global using Sudoku.Concepts;
