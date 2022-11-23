da33a0de268b3b98e0ac57826ee8edbf85e829a6
SunnieShine <HowdySunnie@outlook.com>
2022-11-23 17:26:48 +0800

    Remove deprecated project referencing.

--- src/Global.CodeGen/GlobalAttributes.cs
+++ src/Global.CodeGen/GlobalAttributes.cs
@@ -1,4 +1,3 @@
-﻿[assembly: InternalsVisibleTo("Sudoku.Diagnostics.CodeAnalysis.CodeGen")]
-[assembly: InternalsVisibleTo("Sudoku.Diagnostics.CodeGen")]
+﻿[assembly: InternalsVisibleTo("Sudoku.Diagnostics.CodeGen")]
 [assembly: InternalsVisibleTo("Sudoku.Solving.Logical.CodeGen")]
 [assembly: InternalsVisibleTo("SystemExtensions.CodeGen")]
