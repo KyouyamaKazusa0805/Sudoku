1f8cce491abb68ac4d8f0492ac063e9f401d2fa2
SunnieShine <HowdySunnie@outlook.com>
2022-11-25 14:49:40 +0800

    Remove unused `using` directives.

--- src/Sudoku.Gdip/GlobalUsings.cs
+++ src/Sudoku.Gdip/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System;
 global using System.Collections.Generic;
-global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Diagnostics.CodeGen;
 global using System.Drawing;
