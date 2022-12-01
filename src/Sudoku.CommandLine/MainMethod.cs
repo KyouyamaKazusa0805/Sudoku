441376479ca206fb4a26fbb0fbac850a6823169f
SunnieShine <HowdySunnie@outlook.com>
2022-12-01 16:11:21 +0800

    Update code.

--- src/Sudoku.CommandLine/MainMethod.cs
+++ src/Sudoku.CommandLine/MainMethod.cs
@@ -1,27 +1,4 @@
-﻿#pragma warning disable CA1416
-
-using Sudoku.Presentation.Nodes;
-
-var sudokuPainter = ISudokuPainter.Create(1000, 20)
-	.WithPreferenceSettings(static pref => pref.FigurePadding = 20F)
-	.WithRenderingCandidates(false)
-	.WithGrid(Grid.Empty)
-	.WithNodes(
-		new FigureViewNode[]
-		{
-			new SquareViewNode((96, 0, 0, 0), 0),
-			new TriangleViewNode((96, 0, 0, 0), 10),
-			new CircleViewNode((96, 0, 0, 0), 20),
-			new DiamondViewNode((96, 0, 0, 0), 30)
-		}
-	);
-
-var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-sudokuPainter.SaveTo($"""{desktop}\temp.png""");
-
-return 0;
-
-// Registers the resource fetching.
+﻿// Registers the resource fetching.
 R.AddExternalResourceFetecher(typeof(Program).Assembly, static key => Resources.ResourceManager.GetString(key));
 
 // Parse and route commands.
