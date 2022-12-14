7a8fb6fda6b8b032cc81fa68dda79da27e358d11
SunnieShine <HowdySunnie@outlook.com>
2022-12-14 18:27:51 +0800

    Support displaying check-in time.

--- src/Sudoku.Platforms.QQ/Commands/CheckInCommand.cs
+++ src/Sudoku.Platforms.QQ/Commands/CheckInCommand.cs
@@ -22,38 +22,41 @@ file sealed class CheckInCommand : Command
 		}
 
 		var userData = InternalReadWrite.Read(senderId, new() { QQ = senderId });
-		if (userData.LastCheckIn == DateTime.Today)
+		switch (userData)
 		{
-			// Disallow user checking in multiple times in a same day.
-			await e.SendMessageAsync(R["_MessageFormat_CheckInFailedDueToMultipleInSameDay"]!);
-			return true;
-		}
+			case { LastCheckIn: { Date: var date, TimeOfDay: var time } } when date == DateTime.Today:
+			{
+				// Disallow user checking in multiple times in a same day.
+				await e.SendMessageAsync(string.Format(R["_MessageFormat_CheckInFailedDueToMultipleInSameDay"]!, $"{time:hh \u70b9 mm \u5206}"));
+				return true;
+			}
+			case { LastCheckIn: var dateTime } when (DateTime.Today - dateTime).Days == 1:
+			{
+				// Continuous.
+				userData.ComboCheckedIn++;
 
-		if ((DateTime.Today - userData.LastCheckIn).Days == 1)
-		{
-			// Continuous.
-			userData.ComboCheckedIn++;
+				var expEarned = ICommandDataProvider.GenerateValueEarned(userData.ComboCheckedIn);
+				userData.Score += expEarned;
+				userData.LastCheckIn = DateTime.Now;
 
-			var expEarned = ICommandDataProvider.GenerateValueEarned(userData.ComboCheckedIn);
-			userData.Score += expEarned;
-			userData.LastCheckIn = DateTime.Today;
+				await e.SendMessageAsync(string.Format(R["_MessageFormat_CheckInSuccessfulAndContinuous"]!, userData.ComboCheckedIn, expEarned));
+				break;
+			}
+			default:
+			{
+				// Normal case.
+				userData.ComboCheckedIn = 1;
 
-			await e.SendMessageAsync(string.Format(R["_MessageFormat_CheckInSuccessfulAndContinuous"]!, userData.ComboCheckedIn, expEarned));
-		}
-		else
-		{
-			// Normal case.
-			userData.ComboCheckedIn = 1;
+				var expEarned = ICommandDataProvider.GenerateOriginalValueEarned();
+				userData.Score += expEarned;
+				userData.LastCheckIn = DateTime.Now;
 
-			var expEarned = ICommandDataProvider.GenerateOriginalValueEarned();
-			userData.Score += expEarned;
-			userData.LastCheckIn = DateTime.Today;
-
-			await e.SendMessageAsync(string.Format(R["_MessageFormat_CheckInSuccessful"]!, expEarned));
+				await e.SendMessageAsync(string.Format(R["_MessageFormat_CheckInSuccessful"]!, expEarned));
+				break;
+			}
 		}
 
 		InternalReadWrite.Write(userData);
-
 		return true;
 	}
 }
--- src/Sudoku.Platforms.QQ/Configurations/UserDefinedConfigurations.cs
+++ src/Sudoku.Platforms.QQ/Configurations/UserDefinedConfigurations.cs
@@ -3,20 +3,11 @@
 /// <summary>
 /// Defines a type that stores a user-defined configurations.
 /// </summary>
-public sealed class UserDefinedConfigurations : ICloneable<UserDefinedConfigurations>
+public sealed class UserDefinedConfigurations
 {
 	/// <summary>
 	/// Indicates how many members will be displayed onto the ranking page. The more members to display, the longer the output text will be.
 	/// </summary>
 	[JsonPropertyName("rankingDisplayUsersCount")]
 	public int RankingDisplayUsersCount { get; set; } = 10;
-
-
-	/// <inheritdoc/>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public UserDefinedConfigurations Clone() => this.ReflectionClone();
-
-	/// <inheritdoc cref="ReflectionCopying.ReflectionCover{T}(T, T)"/>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public void CoverBy(UserDefinedConfigurations @new) => this.ReflectionCover(@new);
 }
