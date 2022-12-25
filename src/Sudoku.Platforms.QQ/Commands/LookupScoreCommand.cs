4b6e60cacd6ed0d7ebab5cf6c210844b07663fd4
SunnieShine <HowdySunnie@outlook.com>
2022-12-25 15:05:24 +0800

    Update code.

--- src/Sudoku.Platforms.QQ/Commands/LookupScoreCommand.cs
+++ src/Sudoku.Platforms.QQ/Commands/LookupScoreCommand.cs
@@ -10,25 +10,49 @@ file sealed class LookupScoreCommand : Command
 	public override string CommandName => R.Command("LookupScore")!;
 
 	/// <inheritdoc/>
-	public override CommandComparison ComparisonMode => CommandComparison.Strict;
+	public override CommandComparison ComparisonMode => CommandComparison.Prefix;
 
 
 	/// <inheritdoc/>
 	protected override async Task<bool> ExecuteCoreAsync(string args, GroupMessageReceiver e)
 	{
-		if (e is not { Sender: { Id: var senderId, Name: var senderName, MmeberProfile.NickName: var senderOriginalName } })
+		if (e is not { Sender: { Group: var group, Id: var senderId, Name: var senderName, MmeberProfile.NickName: var senderOriginalName } })
 		{
 			return false;
 		}
 
-		if (InternalReadWrite.Read(senderId) is not { Score: var score } userData)
+		var targetMessage = args switch
 		{
-			await e.SendMessageAsync(string.Format(R.MessageFormat("UserScoreNotFound")!, senderName, senderOriginalName));
-			return true;
+			[] => InternalReadWrite.Read(senderId) switch
+			{
+				{ Score: var score } userData when Scorer.GetGrade(score) is var grade
+					=> string.Format(R.MessageFormat("UserScoreIs")!, senderName, score, senderOriginalName, grade),
+				_ => string.Format(R.MessageFormat("UserScoreNotFound")!, senderName, senderOriginalName)
+			},
+			_ => await getMatchedMembers(group, args) switch
+			{
+				[] => R.MessageFormat("LookupNameOrIdInvalid")!,
+				[{ Id: var foundMemberId }] => InternalReadWrite.Read(foundMemberId) switch
+				{
+					{ Score: var score } userData when Scorer.GetGrade(score) is var grade
+						=> string.Format(R.MessageFormat("UserScoreIs")!, senderName, score, senderOriginalName, grade),
+					_ => string.Format(R.MessageFormat("UserScoreNotFound")!, senderName, senderOriginalName)
+				},
+				{ Length: > 1 } => R.MessageFormat("LookupNameOrIdAmbiguous")!,
+				_ => null
+			}
+		};
+
+		if (targetMessage is not null)
+		{
+			await e.SendMessageAsync(targetMessage);
 		}
 
-		var grade = Scorer.GetGrade(score);
-		await e.SendMessageAsync(string.Format(R.MessageFormat("UserScoreIs")!, senderName, score, senderOriginalName, grade));
 		return true;
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static async Task<Member[]> getMatchedMembers(Group group, string args)
+			=> (from m in await @group.GetGroupMembersAsync() where new[] { m.Id, m.Name }.Any(e => e == args) select m).ToArray();
 	}
 }
