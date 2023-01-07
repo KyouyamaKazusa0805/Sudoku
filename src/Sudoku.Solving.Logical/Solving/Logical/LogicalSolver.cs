f33abd3174f017bf950569b97b0e1d364e291f8a
SunnieShine <HowdySunnie@outlook.com>
2023-01-07 10:20:56 +0800

    Update code.

--- src/Sudoku.Solving.Logical/Solving/Logical/LogicalSolver.cs
+++ src/Sudoku.Solving.Logical/Solving/Logical/LogicalSolver.cs
@@ -31,21 +31,45 @@
 /// ]]></code>
 /// </remarks>
 /// <completionlist cref="CommonLogicalSolvers"/>
-public sealed partial record LogicalSolver : IComplexSolver<LogicalSolver, LogicalSolverResult>, ILogicalSolverOptions
+public sealed partial record LogicalSolver : IComplexSolver<LogicalSolver, LogicalSolverResult>
 {
-	/// <inheritdoc/>
+	/// <summary>
+	/// Indicates whether the solver will apply all found steps in a step searcher,
+	/// in order to solve a puzzle faster. If the value is <see langword="true"/>,
+	/// the third argument of <see cref="IStepSearcher.GetAll(in LogicalAnalysisContext)"/>
+	/// will be set <see langword="false"/> value, in order to find all possible steps in a step searcher,
+	/// and all steps will be applied at the same time.
+	/// </summary>
 	/// <remarks>
 	/// The default value is <see langword="false"/>.
 	/// </remarks>
+	/// <seealso cref="IStepSearcher.GetAll(in LogicalAnalysisContext)"/>
 	public bool IsFullApplying { get; set; }
 
-	/// <inheritdoc/>
+	/// <summary>
+	/// Indicates whether the solver will ignore slow step searchers being configured
+	/// <see cref="StepSearcherRunningOptions.SlowAlgorithm"/>.
+	/// </summary>
 	/// <remarks>
 	/// The default value is <see langword="false"/>.
 	/// </remarks>
+	/// <seealso cref="StepSearcherRunningOptions.SlowAlgorithm"/>
 	public bool IgnoreSlowAlgorithms { get; set; }
 
-	/// <inheritdoc/>
+	/// <summary>
+	/// <para>
+	/// Indicates the custom searcher collection you defined to solve a puzzle. By default,
+	/// the solver will use <see cref="StepSearcherPool.Collection"/> to solve a puzzle.
+	/// If you assign a new array of <see cref="IStepSearcher"/>s into this property
+	/// the step searchers will use this property instead of <see cref="StepSearcherPool.Collection"/> to solve a puzzle.
+	/// </para>
+	/// <para>
+	/// Please note that the property will keep the <see langword="null"/> value if you don't assign any values into it;
+	/// however, if you want to use the customized collection to solve a puzzle, assign a non-<see langword="null"/>
+	/// array into it.
+	/// </para>
+	/// </summary>
+	/// <seealso cref="StepSearcherPool.Collection"/>
 	[DisallowNull]
 	public StepSearcherCollection? CustomSearcherCollection { get; set; }
 
