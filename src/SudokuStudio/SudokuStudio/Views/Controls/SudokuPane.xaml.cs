fa4a80c14bc809cd58413f40fcd2da64c4a2f937
SunnieShine <HowdySunnie@outlook.com>
2023-01-13 19:03:27 +0800

    Update code.

--- src/SudokuStudio/SudokuStudio/Views/Controls/SudokuPane.xaml.cs
+++ src/SudokuStudio/SudokuStudio/Views/Controls/SudokuPane.xaml.cs
@@ -430,7 +430,7 @@ public sealed partial class SudokuPane : UserControl, INotifyPropertyChanged
 		_redoStack.Push(_puzzle);
 
 		var target = _undoStack.Pop();
-		SetPuzzle(target, isOndoOrRedo: true);
+		SetPuzzle(target, whileOndoingOrRedoing: true);
 	}
 
 	/// <summary>
@@ -447,7 +447,7 @@ public sealed partial class SudokuPane : UserControl, INotifyPropertyChanged
 		_undoStack.Push(_puzzle);
 
 		var target = _redoStack.Pop();
-		SetPuzzle(target, isOndoOrRedo: true);
+		SetPuzzle(target, whileOndoingOrRedoing: true);
 	}
 
 	/// <summary>
@@ -478,7 +478,7 @@ public sealed partial class SudokuPane : UserControl, INotifyPropertyChanged
 	/// <para>Indicates whether the stack fields <see cref="_undoStack"/> and <see cref="_redoStack"/> will be cleared.</para>
 	/// <para>The default value is <see langword="false"/>.</para>
 	/// </param>
-	/// <param name="isOndoOrRedo">Indicates whether the current operation occurred while undoing and redoing a grid step.</param>
+	/// <param name="whileOndoingOrRedoing">Indicates whether the current operation occurred while undoing and redoing a grid step.</param>
 	/// <remarks>
 	/// <para>
 	/// This method is nearly equal to <see cref="set_Puzzle(Grid)"/>, but this method can also control undoing and redoing stacks.
@@ -489,14 +489,14 @@ public sealed partial class SudokuPane : UserControl, INotifyPropertyChanged
 	/// <seealso cref="_redoStack"/>
 	/// <seealso cref="set_Puzzle(Grid)"/>
 	/// <seealso cref="Puzzle"/>
-	internal void SetPuzzle(scoped in Grid value, bool clearStack = false, bool isOndoOrRedo = false)
+	internal void SetPuzzle(scoped in Grid value, bool clearStack = false, bool whileOndoingOrRedoing = false)
 	{
 		if (_puzzle == value)
 		{
 			return;
 		}
 
-		if (!isOndoOrRedo && !clearStack)
+		if (!whileOndoingOrRedoing && !clearStack)
 		{
 			_undoStack.Push(_puzzle);
 		}
