42c9cd349f90cb916546906f9591dcc89643dd3b
SunnieShine <HowdySunnie@outlook.com>
2023-01-21 16:57:57 +0800

    Update code.

---
+++ src/SudokuStudio/SudokuStudio/Views/Pages/Analyze/Summary.xaml.cs
@@ -0,0 +1,12 @@
+namespace SudokuStudio.Views.Pages.Analyze;
+
+/// <summary>
+/// Defines a summary page.
+/// </summary>
+public sealed partial class Summary : Page
+{
+	/// <summary>
+	/// Initializes a <see cref="Summary"/> instance.
+	/// </summary>
+	public Summary() => InitializeComponent();
+}
