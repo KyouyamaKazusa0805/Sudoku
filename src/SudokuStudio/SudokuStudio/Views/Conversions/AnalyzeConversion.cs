587086bffbaade8dd872aecaf70922785145a254
SunnieShine <HowdySunnie@outlook.com>
2023-01-22 16:42:02 +0800

    Update code.

--- src/SudokuStudio/SudokuStudio/Views/Conversions/AnalyzeConversion.cs
+++ src/SudokuStudio/SudokuStudio/Views/Conversions/AnalyzeConversion.cs
@@ -1,6 +1,12 @@
 ﻿namespace SudokuStudio.Views.Conversions;
 
+/// <summary>
+/// Provides with conversion methods used by XAML designer, about analyze tab pages.
+/// </summary>
 internal static class AnalyzeConversion
 {
 	public static bool GetIsEnabled(Grid grid) => !grid.GetSolution().IsUndefined;
+
+	public static Visibility GetSummaryTableVisibility(IEnumerable itemsSource)
+		=> itemsSource is null ? Visibility.Collapsed : Visibility.Visible;
 }
