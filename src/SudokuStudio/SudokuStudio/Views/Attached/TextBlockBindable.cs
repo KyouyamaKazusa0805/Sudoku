5dc17423e6ce69ed8d2dbccd4a29e156ec421361
SunnieShine <HowdySunnie@outlook.com>
2023-01-24 17:49:06 +0800

    Add more types.

---
+++ src/SudokuStudio/SudokuStudio/Views/Attached/TextBlockBindable.cs
@@ -0,0 +1,47 @@
+﻿namespace SudokuStudio.Views.Attached;
+
+/// <summary>
+/// Defines a bind behaviors on <see cref="TextBlock"/> instances for property <see cref="TextBlock.Inlines"/>.
+/// </summary>
+/// <seealso cref="TextBlock"/>
+/// <seealso cref="TextBlock.Inlines"/>
+public static class TextBlockBindable
+{
+	/// <summary>
+	/// Defines a dependency property instance that is bindable inline collection.
+	/// </summary>
+	public static readonly DependencyProperty InlinesProperty =
+		DependencyProperty.RegisterAttached(
+			nameof(TextBlock.Inlines),
+			typeof(IEnumerable<Inline>),
+			typeof(TextBlockBindable),
+			new(
+				null,
+				static (d, e) =>
+				{
+					if (d is not TextBlock target)
+					{
+						return;
+					}
+
+					target.Inlines.Clear();
+					target.Inlines.AddRange((IEnumerable<Inline>)e.NewValue);
+				}
+			)
+		);
+
+
+	/// <summary>
+	/// Gets a <see cref="Inline"/> list.
+	/// </summary>
+	/// <param name="obj">The dependency object.</param>
+	/// <returns>A list of <see cref="Inline"/>s.</returns>
+	public static IEnumerable<Inline> GetInlines(DependencyObject obj) => (IEnumerable<Inline>)obj.GetValue(InlinesProperty);
+
+	/// <summary>
+	/// Sets a <see cref="Inline"/> list. This will trigger the event to append inlines into the target <see cref="TextBlock"/>.
+	/// </summary>
+	/// <param name="obj">The dependency object.</param>
+	/// <param name="value">The inlines to be added.</param>
+	public static void SetInlines(DependencyObject obj, IEnumerable<Inline> value) => obj.SetValue(InlinesProperty, value);
+}
--- src/SudokuStudio/SudokuStudio/Views/Pages/Analyze/SolvingPath.xaml.cs
+++ src/SudokuStudio/SudokuStudio/Views/Pages/Analyze/SolvingPath.xaml.cs
@@ -3,18 +3,30 @@ namespace SudokuStudio.Views.Pages.Analyze;
 /// <summary>
 /// Defines the solving path page.
 /// </summary>
-public sealed partial class SolvingPath : Page, IAnalyzeTabPage
+public sealed partial class SolvingPath : Page, IAnalyzeTabPage, INotifyPropertyChanged
 {
 	/// <summary>
 	/// Indicates the analysis result.
 	/// </summary>
 	private LogicalSolverResult? _analysisResult;
 
+	/// <summary>
+	/// Indicates the tooltip display kind.
+	/// </summary>
+	[NotifyBackingField]
+	private StepTooltipDisplayKind _stepTooltipDisplayKind = StepTooltipDisplayKind.TechniqueName
+		| StepTooltipDisplayKind.DifficultyRating
+		| StepTooltipDisplayKind.SimpleDescription;
+
 
 	/// <summary>
 	/// Initializes a <see cref="SolvingPath"/> instance.
 	/// </summary>
 	public SolvingPath() => InitializeComponent();
+
+
+	/// <inheritdoc/>
+	public event PropertyChangedEventHandler? PropertyChanged;
 
 
 	/// <inheritdoc/>
@@ -33,5 +45,79 @@ public sealed partial class SolvingPath : Page, IAnalyzeTabPage
 
 			SolvingPathList.ItemsSource = value?.Steps.ToList();
 		}
+	}
+
+
+	private void ListViewItem_RightTapped(object sender, RightTappedRoutedEventArgs e)
+	{
+		if (sender is not ListViewItem { Tag: IStep step } item)
+		{
+			return;
+		}
+
+		Dialog_StepInfo.Target = item;
+		TextBlockBindable.SetInlines(StepInfoLabel, createInlines(step, StepTooltipDisplayKind));
+
+		Dialog_StepInfo.IsOpen = true;
+
+
+		static IEnumerable<Inline> createInlines(IStep step, StepTooltipDisplayKind displayKind)
+		{
+			var result = new List<Inline>();
+
+			if (displayKind.Flags(StepTooltipDisplayKind.TechniqueName))
+			{
+				result.Add(new Run { Text = GetString("AnalyzePage_TechniqueName") }.SingletonSpan<Bold>());
+				result.Add(new LineBreak());
+				result.Add(new Run { Text = step.Name });
+			}
+
+			if (displayKind.Flags(StepTooltipDisplayKind.DifficultyRating))
+			{
+				f();
+
+				result.Add(new Run { Text = GetString("AnalyzePage_TechniqueDifficultyRating") }.SingletonSpan<Bold>());
+				result.Add(new LineBreak());
+				result.Add(new Run { Text = step.Difficulty.ToString("0.0") });
+			}
+
+			if (displayKind.Flags(StepTooltipDisplayKind.SimpleDescription))
+			{
+				f();
+
+				result.Add(new Run { Text = GetString("AnalyzePage_SimpleDescription") }.SingletonSpan<Bold>());
+				result.Add(new LineBreak());
+				result.Add(new Run { Text = step.ToString()! });
+			}
+
+			return result;
+
+
+			void f()
+			{
+				if (result.Count != 0)
+				{
+					result.Add(new LineBreak());
+					result.Add(new LineBreak());
+				}
+			}
+		}
+	}
+}
+
+/// <include file='../../../global-doc-comments.xml' path='g/csharp11/feature[@name="file-local"]/target[@name="class" and @when="extension"]'/>
+file static class Extensions
+{
+	/// <summary>
+	/// Creates a <see cref="Bold"/> ionstance with a singleton value of <see cref="Run"/>.
+	/// </summary>
+	/// <param name="this">The <see cref="Run"/> instance.</param>
+	/// <returns>A <see cref="Bold"/> instance.</returns>
+	public static T SingletonSpan<T>(this Run @this) where T : Span, new()
+	{
+		var result = new T();
+		result.Inlines.Add(@this);
+
+		return result;
 	}
 }
