dbadc80cca5b2d4ecc12620e84c62ca4f450c2c1
SunnieShine <HowdySunnie@outlook.com>
2023-01-28 14:38:05 +0800

    Update code.

--- src/SudokuStudio/SudokuStudio/Views/Controls/SudokuPaneCell.xaml.cs
+++ src/SudokuStudio/SudokuStudio/Views/Controls/SudokuPaneCell.xaml.cs
@@ -134,8 +134,7 @@ public sealed partial class SudokuPaneCell : UserControl, INotifyPropertyChanged
 
 	private void Flyout_Opening(object sender, object e)
 	{
-		if (BasePane is not { SelectedCell: var cell and not -1, Puzzle: var puzzle }
-			|| puzzle.GetStatus(cell) != CellStatus.Empty)
+		if (BasePane is not { SelectedCell: var cell and not -1, Puzzle: var puzzle } || puzzle.GetStatus(cell) != CellStatus.Empty)
 		{
 			MainGridContextFlyout.Hide();
 			return;
