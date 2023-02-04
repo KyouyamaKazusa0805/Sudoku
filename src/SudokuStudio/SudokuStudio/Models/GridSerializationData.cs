f865943d4ae1be63be1725e405b624d9ad81fb43
SunnieShine <HowdySunnie@outlook.com>
2023-02-04 16:04:14 +0800

    Update code.

--- src/SudokuStudio/SudokuStudio/Models/GridSerializationData.cs
+++ src/SudokuStudio/SudokuStudio/Models/GridSerializationData.cs
@@ -3,7 +3,10 @@
 /// <summary>
 /// Defines a sudoku grid serialization data.
 /// </summary>
-public sealed class GridSerializationData
+[GeneratedOverloadingOperator(GeneratedOperator.EqualityOperators)]
+public sealed partial class GridSerializationData :
+	IEquatable<GridSerializationData>,
+	IEqualityOperators<GridSerializationData, GridSerializationData, bool>
 {
 	/// <summary>
 	/// Indicates the format description.
@@ -14,4 +17,33 @@ public sealed class GridSerializationData
 	/// Indicates the grid string.
 	/// </summary>
 	public required string GridString { get; set; }
+
+	[DebuggerHidden]
+	private int HashCode => Grid.Parse(GridString).GetHashCode();
+
+
+	[GeneratedOverriddingMember(GeneratedEqualsBehavior.AsCastAndCallingOverloading)]
+	public override partial bool Equals(object? obj);
+
+	/// <inheritdoc/>
+	public bool Equals([NotNullWhen(true)] GridSerializationData? other)
+	{
+		if (other is null)
+		{
+			return false;
+		}
+
+		if (!Grid.TryParse(GridString, out var a) || !Grid.TryParse(other.GridString, out var b))
+		{
+			return false;
+		}
+
+		return a == b;
+	}
+
+	[GeneratedOverriddingMember(GeneratedGetHashCodeBehavior.CallingHashCodeCombine, nameof(HashCode))]
+	public override partial int GetHashCode();
+
+	[GeneratedOverriddingMember(GeneratedToStringBehavior.RecordLike, nameof(GridString), nameof(FormatDescription))]
+	public override partial string ToString();
 }
