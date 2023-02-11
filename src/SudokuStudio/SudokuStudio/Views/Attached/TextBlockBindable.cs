90ba1d7e6625b05e7b9c2a21a5f4ad9149b85fc4
SunnieShine <HowdySunnie@outlook.com>
2023-02-11 10:18:27 +0800

    Use source generator to generate attached properties.

--- src/SudokuStudio/SudokuStudio/Views/Attached/TextBlockBindable.cs
+++ src/SudokuStudio/SudokuStudio/Views/Attached/TextBlockBindable.cs
@@ -1,47 +1,20 @@
 ﻿namespace SudokuStudio.Views.Attached;
 
 /// <summary>
-/// Defines a bind behaviors on <see cref="TextBlock"/> instances for property <see cref="TextBlock.Inlines"/>.
+/// Defines a bind behaviors on <see cref="TextBlock"/> instances.
 /// </summary>
 /// <seealso cref="TextBlock"/>
-/// <seealso cref="TextBlock.Inlines"/>
-public static class TextBlockBindable
+[AttachedProperty<IEnumerable<Inline>>(nameof(TextBlock.Inlines), CallbackMethodName = nameof(InlinesPropertyCallback))]
+public static partial class TextBlockBindable
 {
-	/// <summary>
-	/// Defines a dependency property instance that is bindable inline collection.
-	/// </summary>
-	public static readonly DependencyProperty InlinesProperty =
-		DependencyProperty.RegisterAttached(
-			nameof(TextBlock.Inlines),
-			typeof(IEnumerable<Inline>),
-			typeof(TextBlockBindable),
-			new(
-				null,
-				static (d, e) =>
-				{
-					if (d is not TextBlock target)
-					{
-						return;
-					}
+	private static void InlinesPropertyCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	{
+		if (d is not TextBlock target)
+		{
+			return;
+		}
 
-					target.Inlines.Clear();
-					target.Inlines.AddRange((IEnumerable<Inline>)e.NewValue);
-				}
-			)
-		);
-
-
-	/// <summary>
-	/// Gets a <see cref="Inline"/> list.
-	/// </summary>
-	/// <param name="obj">The dependency object.</param>
-	/// <returns>A list of <see cref="Inline"/>s.</returns>
-	public static IEnumerable<Inline> GetInlines(DependencyObject obj) => (IEnumerable<Inline>)obj.GetValue(InlinesProperty);
-
-	/// <summary>
-	/// Sets a <see cref="Inline"/> list. This will trigger the event to append inlines into the target <see cref="TextBlock"/>.
-	/// </summary>
-	/// <param name="obj">The dependency object.</param>
-	/// <param name="value">The inlines to be added.</param>
-	public static void SetInlines(DependencyObject obj, IEnumerable<Inline> value) => obj.SetValue(InlinesProperty, value);
+		target.Inlines.Clear();
+		target.Inlines.AddRange((IEnumerable<Inline>)e.NewValue);
+	}
 }
