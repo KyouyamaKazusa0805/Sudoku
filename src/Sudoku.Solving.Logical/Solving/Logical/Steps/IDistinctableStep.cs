cda104527f80ce28671cc960b25ae6e51be4cb20
SunnieShine <HowdySunnie@outlook.com>
2023-02-28 15:15:07 +0800

    Update code.

--- src/Sudoku.Solving.Logical/Solving/Logical/Steps/IDistinctableStep.cs
+++ src/Sudoku.Solving.Logical/Solving/Logical/Steps/IDistinctableStep.cs
@@ -1,17 +1,20 @@
-﻿namespace Sudoku.Solving.Logical.Steps.Specialized;
+﻿namespace Sudoku.Solving.Logical.Steps;
 
 /// <summary>
 /// Defines a step that can be distinctable.
 /// </summary>
-/// <typeparam name="T">The type of the element to compare.</typeparam>
+/// <typeparam name="TSelf">
+/// The type of the element to compare. The type must derive from <see cref="IStep"/> (e.g. <see cref="Step"/>),
+/// and implement this interface type, with type argument be itself.
+/// </typeparam>
 /// <remarks>
 /// A <b>distinctable step</b> is a step that is with the unique information,
 /// in order that multiple steps of the same type can be recognized by the relative methods to filter and remove same-value instances.
 /// </remarks>
-internal interface IDistinctableStep<in T> : IStep where T : Step, IDistinctableStep<T>
+public interface IDistinctableStep<in TSelf> : IStep where TSelf : IDistinctableStep<TSelf>
 {
 	/// <summary>
-	/// To compare 2 instances of type <typeparamref name="T"/>, to determine whether 2 instances holds the same value.
+	/// To compare 2 instances of type <typeparamref name="TSelf"/>, to determine whether 2 instances holds the same value.
 	/// </summary>
 	/// <param name="left">Indicates the first instance to compare.</param>
 	/// <param name="right">Indicates the second instance to compare.</param>
@@ -32,11 +35,11 @@ internal interface IDistinctableStep<in T> : IStep where T : Step, IDistinctable
 	/// The method can be the same implemented as the method <see cref="object.Equals(object?)"/>,
 	/// but <see langword="record"/>s are automatically implemented useless and unmeaningful methods.
 	/// </remarks>
-	static abstract bool Equals(T left, T right);
+	static abstract bool Equals(TSelf left, TSelf right);
 
 	/// <summary>
 	/// Distinct the list, that is, remove all duplicate elements in this list,
-	/// which uses the method <see cref="Equals(T, T)"/> defined in this interface.
+	/// which uses the method <see cref="Equals(TSelf, TSelf)"/> defined in this interface.
 	/// </summary>
 	/// <param name="list">The list of steps to be processed.</param>
 	/// <returns>The list of steps.</returns>
@@ -44,14 +47,14 @@ internal interface IDistinctableStep<in T> : IStep where T : Step, IDistinctable
 	/// This method does not change the ordering of the original list.
 	/// In other words, if the original list is in order, the final list after invoking this method will be also in order.
 	/// </remarks>
-	/// <seealso cref="Equals(T, T)"/>
-	static IEnumerable<T> Distinct(IList<T> list)
+	/// <seealso cref="Equals(TSelf, TSelf)"/>
+	static IEnumerable<TSelf> Distinct(IList<TSelf> list)
 		=> list switch
 		{
-			[] => Array.Empty<T>(),
+			[] => Array.Empty<TSelf>(),
 			[var firstElement] => new[] { firstElement },
-			[var a, var b] => T.Equals(a, b) ? new[] { a } : new[] { a, b },
-			_ => new HashSet<T>(list, Comparer<T>.Instance)
+			[var a, var b] => TSelf.Equals(a, b) ? new[] { a } : new[] { a, b },
+			_ => new HashSet<TSelf>(list, Comparer<TSelf>.Instance)
 		};
 }
 
@@ -59,7 +62,7 @@ internal interface IDistinctableStep<in T> : IStep where T : Step, IDistinctable
 /// The internal comparer instance.
 /// </summary>
 /// <typeparam name="T">The type of the step.</typeparam>
-file sealed class Comparer<T> : IEqualityComparer<T> where T : Step, IDistinctableStep<T>
+file sealed class Comparer<T> : IEqualityComparer<T> where T : IDistinctableStep<T>
 {
 	/// <summary>
 	/// Indicates the singleton instance.
