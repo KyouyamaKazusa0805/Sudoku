a004bd0ca6d5d4a513463b7b584575781acdd384
SunnieShine <HowdySunnie@outlook.com>
2023-03-13 08:12:53 +0800

    Revert "Fix a little bug."
    This reverts commit 8575983f33138c456b1f6da51077f54f2f8c9a63.

--- src/Sudoku.Workflow.Bot.Oicq/Annotations/EnumFieldAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Workflow.Bot.Oicq.Annotations;
-
-/// <summary>
-/// 表示一个抽象基类型，为所有用于标记给枚举字段的特性类型提供基类型抽象。
-/// 虽然里面没有什么玩意儿，但它可能会被用于例如模式匹配之类的地方上。
-/// </summary>
-public abstract class EnumFieldAttribute : Attribute
-{
-}
