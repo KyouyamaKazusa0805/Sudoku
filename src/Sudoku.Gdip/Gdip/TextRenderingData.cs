a14280df8d654da09b8145fe2164588268e8c255
SunnieShine <HowdySunnie@outlook.com>
2023-03-15 10:28:54 +0800

    Apply C# 12 feature "Primary Constructors" for project `Sudoku.Gdip`.

--- src/Sudoku.Gdip/Gdip/TextRenderingData.cs
+++ src/Sudoku.Gdip/Gdip/TextRenderingData.cs
@@ -3,11 +3,30 @@
 /// <summary>
 /// The inner text rendering data.
 /// </summary>
-/// <param name="Font">The font used.</param>
-/// <param name="ExtraHeight">The extra height.</param>
-/// <param name="StringFormat">The string format.</param>
-internal sealed record TextRenderingData(Font Font, float ExtraHeight, StringFormat StringFormat) : IDisposable
+/// <param name="font">The font used.</param>
+/// <param name="extraHeight">The extra height.</param>
+/// <param name="stringFormat">The string format.</param>
+internal sealed partial class TextRenderingData(Font font, float extraHeight, StringFormat stringFormat) : IDisposable
 {
+	/// <summary>
+	/// Indicates the extra height.
+	/// </summary>
+	public float ExtraHeight { get; } = extraHeight;
+
+	/// <summary>
+	/// Indicates the font.
+	/// </summary>
+	public Font Font { get; } = font;
+
+	/// <summary>
+	/// Indicates the string format.
+	/// </summary>
+	public StringFormat StringFormat { get; } = stringFormat;
+
+
+	[DeconstructionMethod]
+	public partial void Deconstruct(out Font font, out float extraHeight, out StringFormat stringFormat);
+
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	public void Dispose()
