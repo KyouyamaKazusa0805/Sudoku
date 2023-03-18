b42e9d81d897877f652f1c22878f554e282c3935
SunnieShine <HowdySunnie@outlook.com>
2023-03-18 16:59:56 +0800

    Update code.

---
+++ src/Sudoku.Analytics/StepSearchers/SingleDigitPatterns/EmptyRectangleStepSearcher.cs
@@ -0,0 +1,89 @@
+﻿namespace Sudoku.Analytics.StepSearchers;
+
+/// <summary>
+/// Provides with an <b>Empty Rectangle</b> step searcher.
+/// The step searcher will include the following techniques:
+/// <list type="bullet">
+/// <item>Empty Rectangle</item>
+/// </list>
+/// </summary>
+[StepSearcher]
+public sealed partial class EmptyRectangleStepSearcher
+{
+	/// <inheritdoc/>
+	protected internal override Step? GetAll(scoped ref AnalysisContext context)
+	{
+		scoped ref readonly var grid = ref context.Grid;
+		for (var digit = 0; digit < 9; digit++)
+		{
+			for (var block = 0; block < 9; block++)
+			{
+				// Check the empty rectangle occupies more than 2 cells.
+				// and the structure forms an empty rectangle.
+				var erMap = CandidatesMap[digit] & HousesMap[block];
+				if (erMap.Count < 2 || !EmptyRectangleHelper.IsEmptyRectangle(erMap, block, out var row, out var column))
+				{
+					continue;
+				}
+
+				// Search for conjugate pair.
+				for (var i = 0; i < 12; i++)
+				{
+					var linkMap = CandidatesMap[digit] & HousesMap[EmptyRectangleLinkIds[block, i]];
+					if (linkMap.Count != 2)
+					{
+						continue;
+					}
+
+					var blockMask = linkMap.BlockMask;
+					if (IsPow2(blockMask) || i < 6 && !(linkMap & HousesMap[column]) || i >= 6 && !(linkMap & HousesMap[row]))
+					{
+						continue;
+					}
+
+					var t = (linkMap - HousesMap[i < 6 ? column : row])[0];
+					var elimHouse = i < 6 ? t % 9 + 18 : t / 9 + 9;
+					if ((CandidatesMap[digit] & HousesMap[elimHouse] & HousesMap[i < 6 ? row : column]) is not [var elimCell, ..])
+					{
+						continue;
+					}
+
+					if (!CandidatesMap[digit].Contains(elimCell))
+					{
+						continue;
+					}
+
+					// Gather all highlight candidates.
+					var candidateOffsets = new List<CandidateViewNode>();
+					using scoped var cpCells = new ValueList<int>(2);
+					foreach (var cell in HousesMap[block] & CandidatesMap[digit])
+					{
+						candidateOffsets.Add(new(DisplayColorKind.Auxiliary1, cell * 9 + digit));
+					}
+					foreach (var cell in linkMap)
+					{
+						candidateOffsets.Add(new(DisplayColorKind.Normal, cell * 9 + digit));
+						cpCells.Add(cell);
+					}
+
+					var step = new EmptyRectangleStep(
+						new[] { new Conclusion(Elimination, elimCell, digit) },
+						new[] { View.Empty | candidateOffsets | new HouseViewNode(DisplayColorKind.Normal, block) },
+						digit,
+						block,
+						new(cpCells[0], cpCells[1], digit)
+					);
+
+					if (context.OnlyFindOne)
+					{
+						return step;
+					}
+
+					context.Accumulator.Add(step);
+				}
+			}
+		}
+
+		return null;
+	}
+}
---
+++ src/Sudoku.Analytics/Steps/SingleDigitPatterns/EmptyRectangleStep.cs
@@ -0,0 +1,44 @@
+﻿namespace Sudoku.Analytics.Steps;
+
+/// <summary>
+/// Provides with a step that is an <b>Empty Rectangle</b> technique.
+/// </summary>
+public sealed class EmptyRectangleStep(Conclusion[] conclusions, View[]? views, int digit, int block, scoped in Conjugate conjugatePair) :
+	SingleDigitPatternStep(conclusions, views, digit)
+{
+	/// <inheritdoc/>
+	public override decimal BaseDifficulty => 4.6M;
+
+	/// <summary>
+	/// Indicates the block that the real empty rectangle structure lis in.
+	/// </summary>
+	public int Block { get; } = block;
+
+	/// <inheritdoc/>
+	public override DifficultyLevel DifficultyLevel => DifficultyLevel.Hard;
+
+	/// <inheritdoc/>
+	public override TechniqueGroup Group => TechniqueGroup.EmptyRectangle;
+
+	/// <inheritdoc/>
+	public override Technique Code => Technique.EmptyRectangle;
+
+	/// <summary>
+	/// Indicates the conjugate pair used.
+	/// </summary>
+	public Conjugate ConjugatePair { get; } = conjugatePair;
+
+	/// <inheritdoc/>
+	public override IReadOnlyDictionary<string, string[]?> FormatInterpolatedParts
+		=> new Dictionary<string, string[]?>
+		{
+			{ "en", new[] { DigitStr, HouseStr, ConjStr } },
+			{ "zh", new[] { DigitStr, HouseStr, ConjStr } }
+		};
+
+	private string DigitStr => (Digit + 1).ToString();
+
+	private string HouseStr => HouseFormatter.Format(1 << Block);
+
+	private string ConjStr => ConjugatePair.ToString();
+}
