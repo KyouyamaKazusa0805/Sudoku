fa710a6ef023c4939977d626f0b222dc1dd20f00
SunnieShine <HowdySunnie@outlook.com>
2023-03-24 13:36:07 +0800

    Update code.

--- src/Sudoku.Compatibility/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Diagnostics.CodeGen;
-global using System.Numerics;
-global using System.Runtime.CompilerServices;
-global using System.Text;
-global using Sudoku.Concepts;
