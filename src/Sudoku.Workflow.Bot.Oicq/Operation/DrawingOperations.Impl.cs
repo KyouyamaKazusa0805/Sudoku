843f99dc8742652829e13391be8e271c54394480
SunnieShine <HowdySunnie@outlook.com>
2023-03-30 20:50:23 +0800

    Add grid applying.

--- src/Sudoku.Workflow.Bot.Oicq/Operation/DrawingOperations.Impl.cs
+++ src/Sudoku.Workflow.Bot.Oicq/Operation/DrawingOperations.Impl.cs
@@ -1,3 +1,5 @@
+using Sudoku.Runtime.MaskServices;
+
 namespace Sudoku.Workflow.Bot.Oicq.Operation;
 
 partial class DrawingOperations
@@ -287,5 +289,70 @@ partial class DrawingOperations
 		// 注意顺序。强弱关系本身是没有方向性的，但是在绘制链的时候，是有方向性的，所以如果删除的方向反了，也是不可以正确删除掉的。
 		var node = new LinkViewNode(default, new(startDigit, startCell), new(endDigit, endCell), default);
 		await messageReceiver.SendPictureThenDeleteAsync(drawingContext.Painter.RemoveNodes(new[] { node }));
+	}
+
+	/// <summary>
+	/// 从 <paramref name="gridString"/> 之中获取盘面的文本字符串，并直接解析成合适的 <see cref="Grid"/> 盘面数据后，直接导入到绘图盘面里使用。
+	/// </summary>
+	/// <remarks>
+	/// 注意，这里除了解析之后，还得考虑候选数的情况。因为前文我们用到了一种特殊逻辑去支持用户友好的标记候选数的模式（直观模式）。这个模式下，盘面的空格
+	/// 并非为 <see cref="CellStatus.Empty"/> 状态的，而是 <see cref="CellStatus.Undefined"/> 状态的。盘面默认情况是 <see cref="CellStatus.Empty"/>，
+	/// 所以我们在解析成正确结果后，还需要将每一个空格的状态手动调整成 <see cref="CellStatus.Undefined"/>，来达到兼容。
+	/// </remarks>
+	/// <seealso cref="Grid"/>
+	/// <seealso cref="CellStatus.Empty"/>
+	/// <seealso cref="CellStatus.Undefined"/>
+	public static async Task ApplyGridAsync(GroupMessageReceiver messageReceiver, DrawingContext drawingContext, string gridString)
+	{
+		// 解析 gridString，看它是不是合适的盘面的文本字符串。
+		if (!Grid.TryParse(gridString, out var grid))
+		{
+			await messageReceiver.SendMessageAsync("请输入正确的盘面文本字符串。");
+			return;
+		}
+
+		// 手动处理盘面，将底层的比特位重新调整一下，将空格状态调整为 CellStatus.Undefined。
+		manualUpdateCellStatus(ref grid);
+
+		// 调整完成，赋值回去，并渲染出来即可。
+		drawingContext.Puzzle = grid;
+		await messageReceiver.SendPictureThenDeleteAsync(drawingContext.Painter.WithGrid(grid));
+
+
+		static void manualUpdateCellStatus(scoped ref Grid grid)
+		{
+			// 这里我们用到了一个 C# 7 的特性。C# 7 有一个特性（我不知道叫什么，因为官方只是说了 ref-local 和 ref-return 的概念，
+			// 但这个只是用到了思想，而不是概念），它允许我们在 foreach 循环里的迭代变量上声明 ref 关键字（或 ref readonly 组合关键字），
+			// 来迭代一个数据类型的成员的引用。实现逻辑是，使用自定义的迭代器类型对象（一般都叫 Enumerator）。
+			// 一般来说，迭代器的实现需要两个重要成员：
+			//
+			//     1、Current 属性（表示当前迭代到哪里了，这个属性返回的就是迭代变量的各个值）；
+			//     2、MoveNext 方法（表示每一次迭代单位下，“游标”往下移动一个单位的操作）。
+			//
+			// C# 7 则允许我们将 Current 属性设置为 ref 和 ref readonly 返回的属性，这样每一个成员的结果都将通过 Current 返回出对应的引用。
+			// 如果是 ref readonly，则引用只读（即你不能修改变量本身的指向）；如果是 ref，则你可以通过该迭代变量改掉底层指向的那个迭代数据，它自身的数据。
+			// 这里我们就用到的是这个特性。
+			// C# 11 又允许我们手动控制引用的生命周期（使用的是 scoped 关键字），scoped 关键字表示，它只能在当前代码块内可用（比如方法里声明的 scoped 变量，
+			// 就表示该变量只能在这个方法体内可以用；如果你要传入别的调用方法里，需要同时给这个方法的这个传入的参数的引用也追加 scoped 关键字）。
+			// 这里，foreach 循环的迭代变量只能在大括号里使用，所以它其实是隐式声明了 scoped 关键字的，也就是这个 foreach 的头部其实完整写法长这样：
+			//
+			//     foreach (scoped ref var mask in grid.EnumerateMasks())
+			//
+			// 但是，因为 foreach 循环的迭代变量本身就不可能超出这个循环体，所以它标记不标记 scoped 都是一回事。写了可以，但是不写也可以。
+			// 官方叫这种 scoped 情况叫“隐式 scoped”（Implicitly-Scoped）。
+			foreach (ref var mask in grid.EnumerateMasks())
+			{
+				// mask 是盘面每一个单元格的底层掩码。掩码用的是 12 个比特位表示一个单元格的信息。其中最后 9 个比特是表示这个格子里数字 1 到 9 的可能填数状态
+				// （即有或无）；而高 3 个比特是表示单元格当前状态（空格、提示数、填入数）。
+				// 如果一个格子从数据上来说合法，那么它的掩码里，高 3 个比特位上必然不是 0。但是，我们这里要故意让高 3 个比特位置为 0，
+				// 是为了兼容前面设计的这种绘图模式，所以，我们必须舍弃掉高 3 个比特位的数字（除非它不是空格）。
+				// 所以，表达式应为 “(mask >> 9 & 7) == (int)CellStatus.Empty”。
+				// 不过这里我提供了一个底层的处理方法，叫 MaskToStatus，我们直接调用它就行，它和这个表达式是等价的。
+				if (MaskOperations.MaskToStatus(mask) == CellStatus.Empty)
+				{
+					mask &= Grid.MaxCandidatesMask;
+				}
+			}
+		}
 	}
 }
--- src/Sudoku.Workflow.Bot.Oicq/RootCommands/Generalized/MentioningCommand.cs
+++ src/Sudoku.Workflow.Bot.Oicq/RootCommands/Generalized/MentioningCommand.cs
@@ -29,9 +29,17 @@ file sealed class MentioningCommand : IModule
 			return;
 		}
 
-		// 去掉头尾的空格。
-		m = m.Trim();
-
+		// 去掉头尾的空格，然后将字符串按命令行的模式进行解析。
+		// 命令行的基本规则：按空格分隔参数；如果参数需要带空格，那参数必须带双引号。
+		// 这里稍微推广一下，因为是中文环境，所以引号也得用中文的（当然也兼容英语的引号输入）。
+		// 这个正则表达式 [\""“”].+?[\""“”]|[^ ]+ 有点难理解，它其实就是专门匹配命令行参数的核心处理规则。
+		// 主要用到的思路是非贪婪匹配，这里的“.+?”就是非贪婪匹配：“.”表示匹配非空白字符，“+”表示至少得有一个字符，而“+?”配在一起表示非贪婪匹配。
+		// 如果一个序列特别长，那么按照基本的正则表达式处理，这一段都会被算成一个匹配项，这就叫贪婪匹配；加个问号防止正则匹配的时候匹配长文本（也就是越短越好）。
+		// 给你个参考链接：
+		// https://sharplab.io/#v2:EYLgtghglgdgNAFxAJwK4wD4AEBMBGAWACgsAGAAizwDoAZWARwG5izKaAVAUwA8FqASlwDmqADYRkAUR4AHZFwDOiqAHsYilkWIBjdYoTsKCJQgCCyYYvIBeYuQfkARC/uPqAHXmrhyCGGpeLnIAWghyPAoQ4HIAZiidZwALLjExVThyAHdVZDEAEydQ/OcAeQBrcgAzXPJJK2yoBCTyRVkIHSUnNwcXJy0AN0lyBWFeW3IYLizyITGeAAo+gG0PFwBdagBqAH5VjYxlgD1yda2+gEpB4cgEHRTrGxGRXmoAWQg7h4WTAwsrK7EGoKDotBZDZDkW73ciwKGfe5KagAYQgBgAPB8vgA+BYXC49dgATgW0KS1AAahAxKguFcgA===
+		// 你照着这个链接看下就知道了。
+		// 注意，匹配之后的命令行参数，如果本身用了引号，那么结果也带引号，这里需要手动把引号去除。这里就比较简单了，用的是 string.Trim(char[]) 方法。
+		var args = m.Trim().ParseAsCommandLine("""[\""“”].+?[\""“”]|[^ ]+""", new[] { '"', '“', '”' });
 		switch (context)
 		{
 			// 开始游戏指令。
@@ -44,7 +52,7 @@ file sealed class MentioningCommand : IModule
 			// 开始绘图指令。
 			case { DrawingContext: var drawingContext, ExecutingCommand: "开始绘图" }:
 			{
-				var task = m.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) switch
+				var task = args switch
 				{
 					["清空"] => DrawingOperations.ClearAsync(messageReceiver, context),
 					[var s] => DrawingOperations.SetDigitAsync(messageReceiver, drawingContext, s),
@@ -56,6 +64,7 @@ file sealed class MentioningCommand : IModule
 					["反代数" or "反袋鼠", var s] => DrawingOperations.RemoveBabaGroupNodesAsync(messageReceiver, drawingContext, s),
 					[var l and ("强" or "弱"), var s, var e] => DrawingOperations.AddLinkNodeAsync(messageReceiver, drawingContext, l, s, e),
 					["反强" or "反弱", var s, var e] => DrawingOperations.RemoveLinkNodeAsync(messageReceiver, drawingContext, s, e),
+					["盘面", var g] => DrawingOperations.ApplyGridAsync(messageReceiver, drawingContext, g),
 					_ => null
 				};
 				if (task is not null)
@@ -67,4 +76,22 @@ file sealed class MentioningCommand : IModule
 			}
 		}
 	}
+}
+
+/// <include file='../../global-doc-comments.xml' path='g/csharp11/feature[@name="file-local"]/target[@name="class" and @when="extension"]'/>
+file static class Extensions
+{
+	/// <summary>
+	/// 用来将一个字符串直接拆解成一个一个的参数序列。以空格分隔。如果带有引号，则引号是一个整体，里面可包含空格。
+	/// </summary>
+	/// <param name="s">字符串。</param>
+	/// <param name="argumentMatcher">匹配字符串参数的正则表达式。</param>
+	/// <param name="trimmedCharacters">表示最终拆解字符串的时候，需要额外去除的字符。比如引号。</param>
+	/// <returns>解析后的参数序列，按次序排列。</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string[] ParseAsCommandLine(
+		this string s,
+		[StringSyntax(StringSyntax.Regex)] string argumentMatcher,
+		char[]? trimmedCharacters
+	) => from match in new Regex(argumentMatcher, RegexOptions.Singleline).Matches(s) select match.Value.Trim(trimmedCharacters);
 }
