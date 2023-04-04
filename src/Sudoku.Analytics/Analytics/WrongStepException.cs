44d1ce050b020bd736f053d2fa725ca9e4363bb7
SunnieShine <HowdySunnie@outlook.com>
2023-04-04 17:38:11 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/WrongStepException.cs
+++ src/Sudoku.Analytics/Analytics/WrongStepException.cs
@@ -1,21 +1,14 @@
-﻿namespace Sudoku.Analytics;
+namespace Sudoku.Analytics;
 
 /// <summary>
 /// Indicates an error that throws when a solving step is wrong (may be due to wrong algorithm, bug, etc.).
 /// </summary>
-public sealed class WrongStepException : Exception
+public sealed class WrongStepException(scoped in Grid grid, Step wrongStep) : Exception
 {
-	/// <summary>
-	/// Initializes a <see cref="WrongStepException"/> with the specified invalid grid and the wrong step.
-	/// </summary>
-	/// <param name="grid">The invalid sudoku grid.</param>
-	/// <param name="wrongStep">The wrong step.</param>
-	public WrongStepException(scoped in Grid grid, Step wrongStep) => (CurrentInvalidGrid, WrongStep) = (grid, wrongStep);
-
-
 	/// <inheritdoc/>
 	public override string Message
-		=> $"""
+		=>
+		$"""
 		The step: may exist bug that causes the wrong handling.
 		Current grid: '{CurrentInvalidGrid:#}'
 		Current step: '{WrongStep}'
@@ -26,10 +19,10 @@ public sealed class WrongStepException : Exception
 	/// <see cref="Exception.Data"/>.
 	/// </summary>
 	/// <seealso cref="Exception.Data"/>
-	public Grid CurrentInvalidGrid { get; }
+	public Grid CurrentInvalidGrid { get; } = grid;
 
 	/// <summary>
 	/// Indicates the wrong step.
 	/// </summary>
-	public Step WrongStep { get; }
+	public Step WrongStep { get; } = wrongStep;
 }
