47e9e268c35c898f17aa655c6d5d23f2ee93fae6
SunnieShine <HowdySunnie@outlook.com>
2023-04-20 14:42:55 +0800

    Update code.

--- src/Sudoku.Gdip/Gdip/TextRenderingData.cs
+++ src/Sudoku.Gdip/Gdip/TextRenderingData.cs
@@ -1,29 +1,17 @@
-﻿namespace Sudoku.Gdip;
+namespace Sudoku.Gdip;
 
 /// <summary>
 /// The inner text rendering data.
 /// </summary>
-/// <param name="font">The font used.</param>
-/// <param name="extraHeight">The extra height.</param>
-/// <param name="stringFormat">The string format.</param>
-internal sealed partial class TextRenderingData(Font font, float extraHeight, StringFormat stringFormat) : IDisposable
+/// <param name="font">Indicates the font.</param>
+/// <param name="extraHeight">Indicates the extra height.</param>
+/// <param name="stringFormat">Indicates the string format.</param>
+internal sealed partial class TextRenderingData(
+	[PrimaryConstructorParameter] Font font,
+	[PrimaryConstructorParameter] float extraHeight,
+	[PrimaryConstructorParameter] StringFormat stringFormat
+) : IDisposable
 {
-	/// <summary>
-	/// Indicates the extra height.
-	/// </summary>
-	public float ExtraHeight { get; } = extraHeight;
-
-	/// <summary>
-	/// Indicates the font.
-	/// </summary>
-	public Font Font { get; } = font;
-
-	/// <summary>
-	/// Indicates the string format.
-	/// </summary>
-	public StringFormat StringFormat { get; } = stringFormat;
-
-
 	[DeconstructionMethod]
 	public partial void Deconstruct(out Font font, out float extraHeight, out StringFormat stringFormat);
 
