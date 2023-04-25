28f39d343968d6326061a733dd4877799a26c1bd
SunnieShine <HowdySunnie@outlook.com>
2023-04-25 14:09:04 +0800

    Update doc comment, and remove unnecessary `in` modifier.

--- src/System/Algorithm/Combinatorics.cs
+++ src/System/Algorithm/Combinatorics.cs
@@ -23,7 +23,7 @@ public static class Combinatorics
 		return result;
 
 
-		static void g(int last, int count, int index, scoped Span<int> tempArray, scoped Span<T> @this, in List<T[]> resultList)
+		static void g(int last, int count, int index, scoped Span<int> tempArray, scoped Span<T> @this, List<T[]> resultList)
 		{
 			for (var i = last; i >= index; i--)
 			{
@@ -64,7 +64,7 @@ public static class Combinatorics
 		return result;
 
 
-		static void g(int last, int count, int index, scoped Span<int> tempArray, scoped ReadOnlySpan<T> @this, in List<T[]> resultList)
+		static void g(int last, int count, int index, scoped Span<int> tempArray, scoped ReadOnlySpan<T> @this, List<T[]> resultList)
 		{
 			for (var i = last; i >= index; i--)
 			{
@@ -93,18 +93,20 @@ public static class Combinatorics
 	/// </summary>
 	/// <param name="this">The array of elements.</param>
 	/// <returns>
-	/// The subsets of the list. For example, if the input array is <c>{ 1, 2, 3 }</c>, the output
+	/// The subsets of the list. For example, if the input array is <c>[1, 2, 3]</c>, the output
 	/// should be as follows:
-	/// <list type="bullet">
-	/// <item><c>{ 1 }</c></item>
-	/// <item><c>{ 2 }</c></item>
-	/// <item><c>{ 3 }</c></item>
-	/// <item><c>{ 1, 2 }</c></item>
-	/// <item><c>{ 1, 3 }</c></item>
-	/// <item><c>{ 2, 3 }</c></item>
-	/// <item><c>{ 1, 2, 3 }</c></item>
-	/// </list>
-	/// 7 cases (without null set) in total.
+	/// <code>
+	/// [
+	///     [1],
+	///     [2],
+	///     [3],
+	///     [1, 2],
+	///     [1, 3],
+	///     [2, 3],
+	///     [1, 2, 3]
+	/// ]
+	/// </code>
+	/// 7 cases (without null set).
 	/// </returns>
 	public static IEnumerable<T[]> GetSubsets<T>(this IReadOnlyList<T> @this)
 	{
@@ -124,14 +126,12 @@ public static class Combinatorics
 	/// <param name="this">The array.</param>
 	/// <param name="count">The number of elements you want to take.</param>
 	/// <returns>
-	/// The subsets of the list. For example, if the input array is <c>{ 1, 2, 3 }</c> and
-	/// the argument <paramref name="count"/> is 2, the output should be as follows:
-	/// <list type="table">
-	/// <item><c>{ 1, 2 }</c>,</item>
-	/// <item><c>{ 1, 3 }</c>,</item>
-	/// <item><c>{ 2, 3 }</c></item>
-	/// </list>
-	/// 3 cases in total.
+	/// The subsets of the list.
+	/// For example, if the input array is <c>[1, 2, 3]</c> and the argument <paramref name="count"/> is 2, the result will be
+	/// <code>
+	/// [[1, 2], [1, 3], [2, 3]]
+	/// </code>
+	/// 3 cases.
 	/// </returns>
 	public static IEnumerable<T[]> GetSubsets<T>(this IReadOnlyList<T> @this, int count)
 	{
@@ -145,7 +145,7 @@ public static class Combinatorics
 		return result;
 
 
-		static void g(int last, int count, int index, scoped Span<int> tempArray, IReadOnlyList<T> @this, in List<T[]> resultList)
+		static void g(int last, int count, int index, scoped Span<int> tempArray, IReadOnlyList<T> @this, List<T[]> resultList)
 		{
 			for (var i = last; i >= index; i--)
 			{
@@ -173,26 +173,25 @@ public static class Combinatorics
 	/// </summary>
 	/// <param name="this">The jigsaw array.</param>
 	/// <returns>
-	/// All combinations that each sub-array choose one. For example, if the array is
-	/// <c>{ { 1, 2, 3 }, { 1, 3 }, { 1, 4, 7, 10 } }</c>, all combinations are:
-	/// <list type="table">
-	/// <item><c>{ 1, 1, 1 }</c>, <c>{ 1, 1, 4 }</c>, <c>{ 1, 1, 7 }</c>, <c>{ 1, 1, 10 }</c>,</item>
-	/// <item><c>{ 1, 3, 1 }</c>, <c>{ 1, 3, 4 }</c>, <c>{ 1, 3, 7 }</c>, <c>{ 1, 3, 10 }</c>,</item>
-	/// <item><c>{ 2, 1, 1 }</c>, <c>{ 2, 1, 4 }</c>, <c>{ 2, 1, 7 }</c>, <c>{ 2, 1, 10 }</c>,</item>
-	/// <item><c>{ 2, 3, 1 }</c>, <c>{ 2, 3, 4 }</c>, <c>{ 2, 3, 7 }</c>, <c>{ 2, 3, 10 }</c>,</item>
-	/// <item><c>{ 3, 1, 1 }</c>, <c>{ 3, 1, 4 }</c>, <c>{ 3, 1, 7 }</c>, <c>{ 3, 1, 10 }</c>,</item>
-	/// <item><c>{ 3, 3, 1 }</c>, <c>{ 3, 3, 4 }</c>, <c>{ 3, 3, 7 }</c>, <c>{ 3, 3, 10 }</c></item>
-	/// </list>
-	/// 24 cases in total.
+	/// All combinations that each sub-array choose one.
+	/// For example, if one array is <c>[[1, 2, 3], [1, 3], [1, 4, 7, 10]]</c>, the final combinations will be
+	/// <code>
+	/// [
+	///     [1, 1, 1], [1, 1, 4], [1, 1, 7], [1, 1, 10],
+	///     [1, 3, 1], [1, 3, 4], [1, 3, 7], [1, 3, 10],
+	///     [2, 1, 1], [2, 1, 4], [2, 1, 7], [2, 1, 10],
+	///     [2, 3, 1], [2, 3, 4], [2, 3, 7], [2, 3, 10],
+	///     [3, 1, 1], [3, 1, 4], [3, 1, 7], [3, 1, 10],
+	///     [3, 3, 1], [3, 3, 4], [3, 3, 7], [3, 3, 10]
+	/// ]
+	/// </code>
+	/// 24 cases.
 	/// </returns>
-	/// <remarks>
-	/// Please note that each return values unit (an array) contains the same number of elements
-	/// with the whole array.
-	/// </remarks>
 	[SkipLocalsInit]
-	public static unsafe IEnumerable<T[]> GetExtractedCombinations<T>(this T[][] @this)
+	public static unsafe T[][] GetExtractedCombinations<T>(this T[][] @this)
 	{
-		int length = @this.Length, resultCount = 1;
+		var length = @this.Length;
+		var resultCount = 1;
 		var tempArray = stackalloc int[length];
 		for (var i = 0; i < length; i++)
 		{
@@ -201,7 +200,8 @@ public static class Combinatorics
 		}
 
 		var result = new T[resultCount][];
-		int m = -1, n = -1;
+		var m = -1;
+		var n = -1;
 		do
 		{
 			if (m < length - 1)
