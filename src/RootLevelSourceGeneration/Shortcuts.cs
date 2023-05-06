479e0925504c6e022de5d3fb10115b415116c933
SunnieShine <HowdySunnie@outlook.com>
2023-05-06 10:07:47 +0800

    Remove unused field.

--- src/RootLevelSourceGeneration/Shortcuts.cs
+++ src/RootLevelSourceGeneration/Shortcuts.cs
@@ -11,7 +11,6 @@ internal static class Shortcuts
 	public const string GeneratedOverriddenMemberEquals = "gom.e";
 	public const string GeneratedOverriddenMemberGetHashCode = "gom.ghc";
 	public const string GeneratedOverriddenMemberToString = "gom.ts";
-	public const string PropertyBinding = "pb";
 	public const string DependencyProperty = "dp";
 	public const string AttachedProperty = "ap";
 	public const string PrimaryConstructorParameter = "pcp";
