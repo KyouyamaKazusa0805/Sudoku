72c1fed5db1bb6f2c492f362e6c690b384b41d30
SunnieShine <HowdySunnie@outlook.com>
2023-06-23 10:17:08 +0800

    Add `Where` method.

--- src/System/Linq/ArrayEnumerable.cs
+++ src/System/Linq/ArrayEnumerable.cs
@@ -28,4 +28,26 @@ public static class ArrayEnumerable
 
 		return result;
 	}
+
+	/// <summary>
+	/// Filters a sequence of values based on a predicate.
+	/// </summary>
+	/// <typeparam name="T">The type of the elements of source.</typeparam>
+	/// <param name="this">A (An) <typeparamref name="T"/>[] to filter.</param>
+	/// <param name="predicate">A function to test each element for a condition.</param>
+	/// <returns>A (An) <typeparamref name="T"/>[] that contains elements from the input sequence that satisfy the condition.</returns>
+	public static T[] Where<T>(this T[] @this, Func<T, bool> predicate)
+	{
+		var (length, finalIndex) = (@this.Length, 0);
+		var result = new T[length];
+		for (var i = 0; i < length; i++)
+		{
+			if (predicate(@this[i]))
+			{
+				result[finalIndex++] = @this[i];
+			}
+		}
+
+		return result[..finalIndex];
+	}
 }
