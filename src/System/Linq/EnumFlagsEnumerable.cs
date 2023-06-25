cbd88c485685f06541c047535fbdbb58db71badb
SunnieShine <HowdySunnie@outlook.com>
2023-06-25 09:42:24 +0800

    Update code.

--- src/System/Linq/EnumFlagsEnumerable.cs
+++ src/System/Linq/EnumFlagsEnumerable.cs
@@ -6,24 +6,31 @@ namespace System.Linq;
 public static class EnumFlagsEnumerable
 {
 	/// <summary>
-	/// Try to get all <typeparamref name="TEnum"/> elements as flags stored in argument <paramref name="this"/>,
-	/// and convert them into an array of <typeparamref name="T"/> instances via specified argument <paramref name="selector"/>,
+	/// Try to get all <typeparamref name="T"/> elements as flags stored in argument <paramref name="this"/>,
+	/// and convert them into an array of <typeparamref name="TResult"/> instances via specified argument <paramref name="selector"/>,
 	/// then return the array.
 	/// </summary>
-	/// <typeparam name="TEnum">The type of the enumeration.</typeparam>
-	/// <typeparam name="T">The type of the result elements.</typeparam>
+	/// <typeparam name="T">The type of the enumeration.</typeparam>
+	/// <typeparam name="TResult">The type of the result elements.</typeparam>
 	/// <param name="this">The enumeration type field.</param>
 	/// <param name="selector">The selector that can convert the enumeration field into the target-typed instance.</param>
-	/// <returns>An array of <typeparamref name="T"/> elements.</returns>
+	/// <returns>An array of <typeparamref name="TResult"/> elements.</returns>
 	/// <exception cref="InvalidOperationException">
 	/// Throws when the enumeration type is not marked <see cref="FlagsAttribute"/>.
 	/// </exception>
-	public static T[] Select<TEnum, T>(this TEnum @this, Func<TEnum, T> selector) where TEnum : unmanaged, Enum
+	public static TResult[] Select<T, TResult>(this T @this, Func<T, TResult> selector) where T : unmanaged, Enum
 	{
-		var flags = @this.GetAllFlags() is not (var f and not [])
-			? throw new InvalidOperationException($"The enumeration type must be marked as '{nameof(FlagsAttribute)}'.")
-			: f;
-		var result = new T[flags.Length];
+		if (!typeof(T).IsDefined(typeof(FlagsAttribute)))
+		{
+			throw new InvalidOperationException($"The enumeration type must be marked as '{nameof(FlagsAttribute)}'.");
+		}
+
+		if (@this.GetAllFlags() is not (var flags and not []))
+		{
+			return Array.Empty<TResult>();
+		}
+
+		var result = new TResult[flags.Length];
 		for (var i = 0; i < flags.Length; i++)
 		{
 			result[i] = selector(flags[i]);
