15d9cba8a18854b9347a401b97053b866318a5b1
SunnieShine <HowdySunnie@outlook.com>
2023-07-11 15:30:01 +0800

    Extract method.

--- src/Sudoku.Cli/MainMethod.cs
+++ src/Sudoku.Cli/MainMethod.cs
@@ -1,4 +1,4 @@
-var rootCommand = new RootCommand("Sample application using System.CommandLine official API.");
-rootCommand.AddCommand(ICommand<GenerateCommand>.CreateCommand());
+var rootCommand = new RootCommand("A modern program that operates with sudoku puzzles using command line interface.");
+rootCommand.AddCommand<GenerateCommand>();
 
 return rootCommand.Invoke(args);
