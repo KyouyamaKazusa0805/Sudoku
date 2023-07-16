2cede4129a18744e377b5af46c4b031f64d90ce3
SunnieShine <HowdySunnie@outlook.com>
2023-07-16 17:07:24 +0800

    Append conclusion checking on equality comparison on UR steps.

--- src/Sudoku.Analytics/Analytics/Steps/DeadlyPatterns/Rectangles/UniqueRectangleStep.cs
+++ src/Sudoku.Analytics/Analytics/Steps/DeadlyPatterns/Rectangles/UniqueRectangleStep.cs
@@ -48,5 +48,8 @@ public abstract partial class UniqueRectangleStep(
 	/// <inheritdoc/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	static bool IEquatableStep<UniqueRectangleStep>.operator ==(UniqueRectangleStep left, UniqueRectangleStep right)
-		=> (left.Code, left.AbsoluteOffset, left.Digit1, left.Digit2) == (right.Code, right.AbsoluteOffset, right.Digit1, right.Digit2);
+		=> (left.Code, left.AbsoluteOffset, left.Digit1, left.Digit2) == (right.Code, right.AbsoluteOffset, right.Digit1, right.Digit2)
+		&& CandidateMap.Empty + (from conclusion in left.Conclusions select conclusion.Candidate) is var leftConclusion
+		&& CandidateMap.Empty + (from conclusion in right.Conclusions select conclusion.Candidate) is var rightConclusion
+		&& leftConclusion == rightConclusion;
 }
