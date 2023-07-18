edbc2db87ae13c9fac13221fcdf8ca4221ccc487
SunnieShine <HowdySunnie@outlook.com>
2023-07-18 15:02:09 +0800

    Fixes #437.

--- src/Sudoku.Analytics/Analytics/StepSearchers/RankTheory/Negative/BivalueOddagonStepSearcher.cs
+++ src/Sudoku.Analytics/Analytics/StepSearchers/RankTheory/Negative/BivalueOddagonStepSearcher.cs
@@ -19,7 +19,7 @@ namespace Sudoku.Analytics.StepSearchers;
 /// </summary>
 [StepSearcher(
 	Technique.BivalueOddagonType2, Technique.BivalueOddagonType3, Technique.GroupedBivalueOddagon,
-	ConditionalCases = ConditionalCase.UnlimitedTimeComplexity)]
+	ConditionalCases = ConditionalCase.UnlimitedTimeComplexity | ConditionalCase.UnlimitedSpaceComplexity)]
 public sealed partial class BivalueOddagonStepSearcher : StepSearcher
 {
 	/// <inheritdoc/>
