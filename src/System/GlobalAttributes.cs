c5b3a46fe68e3b4c40d8a74c04cf8388af481306
SunnieShine <HowdySunnie@outlook.com>
2023-07-29 17:17:42 +0800

    Remove deprecated `'InternalVisibleTo]` usage.

--- src/System/GlobalAttributes.cs
+++ src/System/GlobalAttributes.cs
@@ -1,4 +1,3 @@
 [assembly: InternalsVisibleTo("Sudoku.Analytics")]
 [assembly: InternalsVisibleTo("Sudoku.Core")]
-[assembly: InternalsVisibleTo("Sudoku.CommandLine")]
 [assembly: InternalsVisibleTo("SudokuStudio")]
