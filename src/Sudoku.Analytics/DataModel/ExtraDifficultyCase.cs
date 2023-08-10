91bb1eeeb19d07c5e99e080cdd908eff4a9d87da
SunnieShine <HowdySunnie@outlook.com>
2023-08-10 21:17:16 +0800

    Update doc comment.

--- src/Sudoku.Analytics/DataModel/ExtraDifficultyCase.cs
+++ src/Sudoku.Analytics/DataModel/ExtraDifficultyCase.cs
@@ -3,6 +3,9 @@ namespace Sudoku.DataModel;
 /// <summary>
 /// Defines a pair of data that represents the extra difficulty rating for a technique step, limited by its name and the value.
 /// </summary>
-/// <param name="Name">The name of extra difficulty rating item.</param>
+/// <param name="Name">
+/// <completionlist cref="ExtraDifficultyCaseNames"/>
+/// The name of extra difficulty rating item.
+/// </param>
 /// <param name="Value">The rating value.</param>
 public readonly record struct ExtraDifficultyCase(string Name, decimal Value);
