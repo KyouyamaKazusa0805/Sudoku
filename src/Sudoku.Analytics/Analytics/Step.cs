34400d386f2ec0678edab29c1d55057f6055e801
SunnieShine <HowdySunnie@outlook.com>
2023-08-15 20:24:11 +0800

    Format code.

--- src/Sudoku.Analytics/Analytics/Step.cs
+++ src/Sudoku.Analytics/Analytics/Step.cs
@@ -6,8 +6,7 @@ namespace Sudoku.Analytics;
 /// <param name="conclusions"><inheritdoc cref="IRenderable.Conclusions" path="/summary"/></param>
 /// <param name="views"><inheritdoc cref="IRenderable.Views" path="/summary"/></param>
 [GetHashCode(OtherModifiers = "sealed")]
-public abstract partial class Step([DataMember] Conclusion[] conclusions, [DataMember] View[]? views) :
-	IRenderable
+public abstract partial class Step([DataMember] Conclusion[] conclusions, [DataMember] View[]? views) : IRenderable
 {
 	/// <summary>
 	/// The placeholder of the hash code.
