5829476506c9fdce8cc3c18b60681645939581fe
SunnieShine <HowdySunnie@outlook.com>
2023-08-18 19:16:36 +0800

    Remove `[UnscopedRef]` for unnecessary primary constructor parameters.

--- src/Sudoku.Core/Concepts/Grid.CandidateEnumerator.cs
+++ src/Sudoku.Core/Concepts/Grid.CandidateEnumerator.cs
@@ -6,7 +6,7 @@ partial struct Grid
 	/// Defines the default enumerator that iterates the <see cref="Grid"/> through the candidates in the current <see cref="Grid"/> instance.
 	/// </summary>
 	/// <param name="arr">The reference to an array.</param>
-	public ref struct CandidateEnumerator([UnscopedRef] ref Mask arr)
+	public ref struct CandidateEnumerator(ref Mask arr)
 	{
 		/// <summary>
 		/// The pointer to the start value.
