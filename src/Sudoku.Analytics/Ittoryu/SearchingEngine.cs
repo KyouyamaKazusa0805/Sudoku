5585381684f21ef9211832e1db313e7e0e3773f9
SunnieShine <HowdySunnie@outlook.com>
2023-09-02 10:42:03 +0800

    Inline member methods as local functions.

--- src/Sudoku.Analytics/Ittoryu/SearchingEngine.cs
+++ src/Sudoku.Analytics/Ittoryu/SearchingEngine.cs
@@ -36,9 +36,9 @@ public sealed class SearchingEngine
 			var foundNodes = new List<PathNode>();
 			for (var digit = 0; digit < 9; digit++)
 			{
-				ForFullHouse(grid, foundNodes, digit);
-				ForHiddenSingle(grid, foundNodes, digit);
-				ForNakedSingle(grid, foundNodes, digit);
+				fullHouses(grid, foundNodes, digit);
+				hiddenSingles(grid, foundNodes, digit);
+				nakedSingles(grid, foundNodes, digit);
 			}
 
 			foreach (var digit in MaskCreator.Create(from node in foundNodes select node.Digit))
@@ -86,9 +86,9 @@ public sealed class SearchingEngine
 			{
 				// If the current digit is not completed, we should continue searching for this digit.
 				var tempNodes = new List<PathNode>(16);
-				ForFullHouse(grid, tempNodes, digit);
-				ForHiddenSingle(grid, tempNodes, digit);
-				ForNakedSingle(grid, tempNodes, digit);
+				fullHouses(grid, tempNodes, digit);
+				hiddenSingles(grid, tempNodes, digit);
+				nakedSingles(grid, tempNodes, digit);
 
 				dfs(grid, digit, digitsStack, tempNodes.ToArray(), finishedDigits);
 			}
@@ -109,9 +109,9 @@ public sealed class SearchingEngine
 				var tempNodes = new List<PathNode>(16);
 				foreach (var anotherDigit in (Mask)(Grid.MaxCandidatesMask & ~finishedDigits))
 				{
-					ForFullHouse(grid, tempNodes, anotherDigit);
-					ForHiddenSingle(grid, tempNodes, anotherDigit);
-					ForNakedSingle(grid, tempNodes, anotherDigit);
+					fullHouses(grid, tempNodes, anotherDigit);
+					hiddenSingles(grid, tempNodes, anotherDigit);
+					nakedSingles(grid, tempNodes, anotherDigit);
 				}
 
 				// Iterate on found path nodes.
@@ -130,82 +130,64 @@ public sealed class SearchingEngine
 				digitsStack.Pop();
 			}
 		}
-	}
 
-	/// <summary>
-	/// Get all full houses.
-	/// </summary>
-	/// <param name="grid">The grid to be checked.</param>
-	/// <param name="foundNodes">A list of found path nodes.</param>
-	/// <param name="digit">Indicates the digit to be checked.</param>
-	private void ForFullHouse(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
-	{
-		if (Array.IndexOf(SupportedTechniques, Technique.FullHouse) == -1)
+		void fullHouses(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
 		{
-			return;
-		}
-
-		var emptyCells = grid.EmptyCells;
-		for (var house = 0; house < 27; house++)
-		{
-			if ((emptyCells & HousesMap[house]) is [var fullHouseCell])
+			if (Array.IndexOf(SupportedTechniques, Technique.FullHouse) == -1)
 			{
-				var currentDigit = TrailingZeroCount(grid[HousesMap[house] - fullHouseCell]);
-				if (currentDigit == digit)
+				return;
+			}
+
+			var emptyCells = grid.EmptyCells;
+			for (var house = 0; house < 27; house++)
+			{
+				if ((emptyCells & HousesMap[house]) is [var fullHouseCell])
 				{
-					foundNodes.Add(new(grid, house, fullHouseCell * 9 + digit));
+					var currentDigit = TrailingZeroCount(grid[HousesMap[house] - fullHouseCell]);
+					if (currentDigit == digit)
+					{
+						foundNodes.Add(new(grid, house, fullHouseCell * 9 + digit));
+					}
 				}
 			}
 		}
-	}
 
-	/// <summary>
-	/// Get all hidden singles.
-	/// </summary>
-	/// <param name="grid"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='grid']"/></param>
-	/// <param name="foundNodes"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='foundNodes']"/></param>
-	/// <param name="digit"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='digit']"/></param>
-	private void ForHiddenSingle(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
-	{
-		var candidatesMap = grid.CandidatesMap;
-		for (var house = 0; house < 27; house++)
+		void hiddenSingles(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
 		{
-			var houseCode = house.ToHouseType() switch
+			var candidatesMap = grid.CandidatesMap;
+			for (var house = 0; house < 27; house++)
 			{
-				HouseType.Block => Technique.HiddenSingleBlock,
-				HouseType.Row => Technique.HiddenSingleRow,
-				_ => Technique.HiddenSingleColumn
-			};
-			if (Array.IndexOf(SupportedTechniques, houseCode) == -1)
-			{
-				continue;
-			}
+				var houseCode = house.ToHouseType() switch
+				{
+					HouseType.Block => Technique.HiddenSingleBlock,
+					HouseType.Row => Technique.HiddenSingleRow,
+					_ => Technique.HiddenSingleColumn
+				};
+				if (Array.IndexOf(SupportedTechniques, houseCode) == -1)
+				{
+					continue;
+				}
 
-			if ((HousesMap[house] & candidatesMap[digit]) / house is var mask && IsPow2((uint)mask))
-			{
-				foundNodes.Add(new(grid, house, HouseCells[house][Log2((uint)mask)] * 9 + digit));
+				if ((HousesMap[house] & candidatesMap[digit]) / house is var mask && IsPow2((uint)mask))
+				{
+					foundNodes.Add(new(grid, house, HouseCells[house][Log2((uint)mask)] * 9 + digit));
+				}
 			}
 		}
-	}
 
-	/// <summary>
-	/// Get all naked singles.
-	/// </summary>
-	/// <param name="grid"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='grid']"/></param>
-	/// <param name="foundNodes"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='foundNodes']"/></param>
-	/// <param name="digit"><inheritdoc cref="ForFullHouse(in Grid, List{PathNode}, Digit)" path="/param[@name='digit']"/></param>
-	private void ForNakedSingle(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
-	{
-		if (Array.IndexOf(SupportedTechniques, Technique.NakedSingle) == -1)
+		void nakedSingles(scoped in Grid grid, List<PathNode> foundNodes, Digit digit)
 		{
-			return;
-		}
-
-		foreach (var cell in grid.EmptyCells)
-		{
-			if (grid.GetCandidates(cell) == 1 << digit)
+			if (Array.IndexOf(SupportedTechniques, Technique.NakedSingle) == -1)
 			{
-				foundNodes.Add(new(grid, -1, cell * 9 + digit));
+				return;
+			}
+
+			foreach (var cell in grid.EmptyCells)
+			{
+				if (grid.GetCandidates(cell) == 1 << digit)
+				{
+					foundNodes.Add(new(grid, -1, cell * 9 + digit));
+				}
 			}
 		}
 	}
