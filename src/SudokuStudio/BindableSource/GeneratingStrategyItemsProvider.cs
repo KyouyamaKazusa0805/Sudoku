2d78680766f51d64327865b68a54d5186ad2e1fa
SunnieShine <HowdySunnie@outlook.com>
2023-09-07 13:26:04 +0800

    Extract methods.

--- src/SudokuStudio/BindableSource/GeneratingStrategyItemsProvider.cs
+++ src/SudokuStudio/BindableSource/GeneratingStrategyItemsProvider.cs
@@ -5,105 +5,135 @@ namespace SudokuStudio.BindableSource;
 /// </summary>
 public sealed class GeneratingStrategyItemsProvider : IRunningStrategyItemsProvider
 {
-	private const string GeneratingStrategyPage_DifficultyLevel = nameof(GeneratingStrategyPage_DifficultyLevel);
-
-	private const string GeneratingStrategyPage_Symmetry = nameof(GeneratingStrategyPage_Symmetry);
-
-	private const string GeneratingStrategyPage_TechniquesMustIncluded = nameof(GeneratingStrategyPage_TechniquesMustIncluded);
-
-	private const string GeneratingStrategyPage_IsMinimalPuzzle = nameof(GeneratingStrategyPage_IsMinimalPuzzle);
-
-	private const string GeneratingStrategyPage_FirstAssignmentAttribute = nameof(GeneratingStrategyPage_FirstAssignmentAttribute);
-
-
 	/// <inheritdoc/>
 	public IList<RunningStrategyItem> Items
 		=> [
 			new(
-				GeneratingStrategyPage_DifficultyLevel,
+				"GeneratingStrategyPage_DifficultyLevel",
 				new(
-					GeneratingStrategyPage_DifficultyLevel,
-					static () => new ComboBox
-					{
-						SelectedIndex = 0,
-						ItemsSource = (ComboBoxItem[])[
-							new() { Content = GetString("DifficultyLevel_None"), Tag = DifficultyLevel.Unknown },
-							new() { Content = GetString("DifficultyLevel_Easy"), Tag = DifficultyLevel.Easy },
-							new() { Content = GetString("DifficultyLevel_Moderate"), Tag = DifficultyLevel.Moderate },
-							new() { Content = GetString("DifficultyLevel_Hard"), Tag = DifficultyLevel.Hard },
-							new() { Content = GetString("DifficultyLevel_Fiendish"), Tag = DifficultyLevel.Fiendish },
-							new() { Content = GetString("DifficultyLevel_Nightmare"), Tag = DifficultyLevel.Nightmare }
-						]
-					},
-					static () => DifficultyLevelConversion.GetName(((App)Application.Current).Preference.UIPreferences.GeneratorDifficultyLevel),
-					static c => ((App)Application.Current).Preference.UIPreferences.GeneratorDifficultyLevel = (DifficultyLevel)((ComboBoxItem)((ComboBox)c).SelectedItem).Tag!
+					"GeneratingStrategyPage_DifficultyLevel",
+					DiffficultyLevelControlCreator,
+					DifficultyLevelInitializedValueDisplayer,
+					DifficultyLevelValueRouter
 				)
 			),
 			new(
-				GeneratingStrategyPage_Symmetry,
+				"GeneratingStrategyPage_Symmetry",
 				new(
-					GeneratingStrategyPage_Symmetry,
-					static () => new ComboBox
-					{
-						SelectedIndex = 0,
-						ItemsSource = (ComboBoxItem[])[
-							new() { Content = GetString("SymmetricType_None"), Tag = SymmetricType.None },
-							new() { Content = GetString("SymmetricType_Central"), Tag = SymmetricType.Central },
-							new() { Content = GetString("SymmetricType_Diagonal"), Tag = SymmetricType.Diagonal },
-							new() { Content = GetString("SymmetricType_AntiDiagonal"), Tag = SymmetricType.AntiDiagonal },
-							new() { Content = GetString("SymmetricType_AntiDiagonal"), Tag = SymmetricType.AntiDiagonal },
-							new() { Content = GetString("SymmetricType_XAxis"), Tag = SymmetricType.XAxis },
-							new() { Content = GetString("SymmetricType_YAxis"), Tag = SymmetricType.YAxis },
-							new() { Content = GetString("SymmetricType_AxisBoth"), Tag = SymmetricType.AxisBoth },
-							new() { Content = GetString("SymmetricType_DiagonalBoth"), Tag = SymmetricType.DiagonalBoth },
-							new() { Content = GetString("SymmetricType_All"), Tag = SymmetricType.All }
-						]
-					},
-					static () => GetString($"SymmetricType_{((App)Application.Current).Preference.UIPreferences.GeneratorSymmetricPattern}"),
-					static c => ((App)Application.Current).Preference.UIPreferences.GeneratorSymmetricPattern = (SymmetricType)((ComboBoxItem)((ComboBox)c).SelectedItem).Tag!
+					"GeneratingStrategyPage_Symmetry",
+					SymmetricTypeControlCreator,
+					SymmetricTypeInitializedValueDisplayer,
+					SymmetricTypeValueRouter
 				)
 			),
 			new(
-				GeneratingStrategyPage_TechniquesMustIncluded,
+				"GeneratingStrategyPage_TechniquesMustIncluded",
 				new(
-					GeneratingStrategyPage_TechniquesMustIncluded,
-					static () => new TechniqueSelector { SelectedIndex = 0 },
-					static () => ((App)Application.Current).Preference.UIPreferences.SelectedTechnique switch
-					{
-						Technique.None => GetString("TechniqueSelector_NoTechniqueSelected"),
-						var n => $"{n.GetName()} ({n.GetEnglishName()})"
-					},
-					static c => ((App)Application.Current).Preference.UIPreferences.SelectedTechnique = c switch
-					{
-						TechniqueSelector { ItemsSource: var source, SelectedIndex: var index }
-							=> index switch { -1 => Technique.None, _ => source[index].Technique },
-						_
-							=> throw new InvalidOperationException("The status is invalid.")
-					}
+					"GeneratingStrategyPage_TechniquesMustIncluded",
+					TechniqueMustIncludedControlCreator,
+					TechniqueMustIncludedInitializedValueDisplayer,
+					TechniqueMustIncludedValueRouter
 				)
 			),
 			new(
-				GeneratingStrategyPage_IsMinimalPuzzle,
+				"GeneratingStrategyPage_IsMinimalPuzzle",
 				new(
-					GeneratingStrategyPage_IsMinimalPuzzle,
-					static () => new ToggleSwitch(),
-					static () => ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBeMinimal.ToString(),
-					static c => ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBeMinimal = ((ToggleSwitch)c).IsOn
+					"GeneratingStrategyPage_IsMinimalPuzzle",
+					IsMinimalControlCreator,
+					IsMinimalInitializedValueDisplayer,
+					IsMinimalValueRouter
 				)
 			),
 			new(
-				GeneratingStrategyPage_FirstAssignmentAttribute,
+				"GeneratingStrategyPage_FirstAssignmentAttribute",
 				new(
-					GeneratingStrategyPage_FirstAssignmentAttribute,
-					static () => new ToggleSwitch(),
-					static () => ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBePearl switch
-					{
-						true => GetString("GeneratingStrategyPage_PearlPuzzle"),
-						false => GetString("GeneratingStrategyPage_NormalPuzzle"),
-						//_ => GetString("GeneratingStrategyPage_DiamondPuzzle")
-					},
-					static c => ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBePearl = ((ToggleSwitch)c).IsOn
+					"GeneratingStrategyPage_FirstAssignmentAttribute",
+					FirstAssignmentAttributeControlCreator,
+					FirstAssignmentAttributeInitializedValueDisplayer,
+					FirstAssignmentAttributeValueRouter
 				)
 			)
 		];
+
+
+	private static ComboBox DiffficultyLevelControlCreator()
+		=> new()
+		{
+			SelectedIndex = 0,
+			ItemsSource = (ComboBoxItem[])[
+				new() { Content = GetString("DifficultyLevel_None"), Tag = DifficultyLevel.Unknown },
+				new() { Content = GetString("DifficultyLevel_Easy"), Tag = DifficultyLevel.Easy },
+				new() { Content = GetString("DifficultyLevel_Moderate"), Tag = DifficultyLevel.Moderate },
+				new() { Content = GetString("DifficultyLevel_Hard"), Tag = DifficultyLevel.Hard },
+				new() { Content = GetString("DifficultyLevel_Fiendish"), Tag = DifficultyLevel.Fiendish },
+				new() { Content = GetString("DifficultyLevel_Nightmare"), Tag = DifficultyLevel.Nightmare }
+			]
+		};
+
+	private static ComboBox SymmetricTypeControlCreator()
+		=> new()
+		{
+			SelectedIndex = 0,
+			ItemsSource = (ComboBoxItem[])[
+				new() { Content = GetString("SymmetricType_None"), Tag = SymmetricType.None },
+				new() { Content = GetString("SymmetricType_Central"), Tag = SymmetricType.Central },
+				new() { Content = GetString("SymmetricType_Diagonal"), Tag = SymmetricType.Diagonal },
+				new() { Content = GetString("SymmetricType_AntiDiagonal"), Tag = SymmetricType.AntiDiagonal },
+				new() { Content = GetString("SymmetricType_AntiDiagonal"), Tag = SymmetricType.AntiDiagonal },
+				new() { Content = GetString("SymmetricType_XAxis"), Tag = SymmetricType.XAxis },
+				new() { Content = GetString("SymmetricType_YAxis"), Tag = SymmetricType.YAxis },
+				new() { Content = GetString("SymmetricType_AxisBoth"), Tag = SymmetricType.AxisBoth },
+				new() { Content = GetString("SymmetricType_DiagonalBoth"), Tag = SymmetricType.DiagonalBoth },
+				new() { Content = GetString("SymmetricType_All"), Tag = SymmetricType.All }
+			]
+		};
+
+	private static TechniqueSelector TechniqueMustIncludedControlCreator() => new() { SelectedIndex = 0 };
+
+	private static ToggleSwitch IsMinimalControlCreator() => new();
+
+	private static ToggleSwitch FirstAssignmentAttributeControlCreator() => new();
+
+	private static string DifficultyLevelInitializedValueDisplayer()
+		=> DifficultyLevelConversion.GetName(((App)Application.Current).Preference.UIPreferences.GeneratorDifficultyLevel);
+
+	private static string SymmetricTypeInitializedValueDisplayer()
+		=> GetString($"SymmetricType_{((App)Application.Current).Preference.UIPreferences.GeneratorSymmetricPattern}");
+
+	private static string TechniqueMustIncludedInitializedValueDisplayer()
+		=> ((App)Application.Current).Preference.UIPreferences.SelectedTechnique switch
+		{
+			Technique.None => GetString("TechniqueSelector_NoTechniqueSelected"),
+			var n => $"{n.GetName()} ({n.GetEnglishName()})"
+		};
+
+	private static string IsMinimalInitializedValueDisplayer()
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBeMinimal.ToString();
+
+	private static string FirstAssignmentAttributeInitializedValueDisplayer()
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBePearl switch
+		{
+			true => GetString("GeneratingStrategyPage_PearlPuzzle"),
+			false => GetString("GeneratingStrategyPage_NormalPuzzle"),
+			//_ => GetString("GeneratingStrategyPage_DiamondPuzzle")
+		};
+
+	private static void DifficultyLevelValueRouter(FrameworkElement c)
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratorDifficultyLevel = (DifficultyLevel)((ComboBoxItem)((ComboBox)c).SelectedItem).Tag!;
+
+	private static void SymmetricTypeValueRouter(FrameworkElement c)
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratorSymmetricPattern = (SymmetricType)((ComboBoxItem)((ComboBox)c).SelectedItem).Tag!;
+
+	private static void TechniqueMustIncludedValueRouter(FrameworkElement c)
+		=> ((App)Application.Current).Preference.UIPreferences.SelectedTechnique = c switch
+		{
+			TechniqueSelector { ItemsSource: var source, SelectedIndex: var index } => index switch { -1 => Technique.None, _ => source[index].Technique },
+			_ => throw new InvalidOperationException("The status is invalid.")
+		};
+
+	private static void IsMinimalValueRouter(FrameworkElement c)
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBeMinimal = ((ToggleSwitch)c).IsOn;
+
+	private static void FirstAssignmentAttributeValueRouter(FrameworkElement c)
+		=> ((App)Application.Current).Preference.UIPreferences.GeneratedPuzzleShouldBePearl = ((ToggleSwitch)c).IsOn;
 }
