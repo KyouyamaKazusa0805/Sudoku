655a1277b26cb26a0ad327646feb58fef31409db
SunnieShine <HowdySunnie@outlook.com>
2023-09-26 19:57:45 +0800

    Add controls.

---
+++ src/SudokuStudio/Views/Controls/DefaultableSettingsCard.xaml.cs
@@ -0,0 +1,31 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using SudokuStudio.ComponentModel;
+using WinUICommunity;
+
+namespace SudokuStudio.Views.Controls;
+
+/// <summary>
+/// Represents a <see cref="SettingsCard"/> control that can revert the value to default.
+/// </summary>
+/// <seealso cref="SettingsCard"/>
+[DependencyProperty<object>("Header", IsNullable = true)]
+[DependencyProperty<object>("Description", IsNullable = true)]
+[DependencyProperty<ContentAlignment>("ContentAlignment")]
+[DependencyProperty<IconElement>("ActionIcon", IsNullable = true)]
+[DependencyProperty<IconElement>("HeaderIcon", IsNullable = true)]
+public sealed partial class DefaultableSettingsCard : UserControl
+{
+	/// <summary>
+	/// Initializes a <see cref="DefaultableSettingsCard"/> instance.
+	/// </summary>
+	public DefaultableSettingsCard() => InitializeComponent();
+
+
+	/// <inheritdoc cref="ButtonBase.Click"/>
+	public event RoutedEventHandler? UndoButtonClick;
+
+
+	private void Button_Click(object sender, RoutedEventArgs e) => UndoButtonClick?.Invoke(sender, e);
+}
---
+++ src/SudokuStudio/Views/Controls/DefaultableSettingsExpander.xaml.cs
@@ -0,0 +1,33 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using SudokuStudio.ComponentModel;
+using WinUICommunity;
+
+namespace SudokuStudio.Views.Controls;
+
+/// <summary>
+/// Represents a <see cref="SettingsExpander"/> control that can revert the value to default.
+/// </summary>
+/// <seealso cref="SettingsExpander"/>
+[DependencyProperty<object>("Header", IsNullable = true)]
+[DependencyProperty<object>("Description", IsNullable = true)]
+[DependencyProperty<object>("ItemsSource", IsNullable = true)]
+[DependencyProperty<UIElement>("ItemsHeader", IsNullable = true)]
+[DependencyProperty<UIElement>("ItemsFooter", IsNullable = true)]
+[DependencyProperty<IconElement>("HeaderIcon", IsNullable = true)]
+[DependencyProperty<IList<object>>("Items", IsNullable = true)]
+public sealed partial class DefaultableSettingsExpander : UserControl
+{
+	/// <summary>
+	/// Initializes a <see cref="DefaultableSettingsExpander"/> instance.
+	/// </summary>
+	public DefaultableSettingsExpander() => InitializeComponent();
+
+
+	/// <inheritdoc cref="ButtonBase.Click"/>
+	public event RoutedEventHandler? UndoButtonClick;
+
+
+	private void Button_Click(object sender, RoutedEventArgs e) => UndoButtonClick?.Invoke(sender, e);
+}
