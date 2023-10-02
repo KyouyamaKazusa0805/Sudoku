c93c67be30448d71222a1d135816a6c02af5afd4
SunnieShine <HowdySunnie@outlook.com>
2023-10-02 17:09:32 +0800

    Rename page: `TechniqueDebuggerPage` -> `TechniqueGalleryPage`.

---
+++ src/SudokuStudio/Views/Pages/TechniqueGalleryPage.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SudokuStudio.Views.Pages;
+
+/// <summary>
+/// A page that displays all techniques.
+/// </summary>
+public sealed partial class TechniqueGalleryPage : Page
+{
+	/// <summary>
+	/// Initializes a <see cref="TechniqueGalleryPage"/> instance.
+	/// </summary>
+	public TechniqueGalleryPage() => InitializeComponent();
+}
--- src/SudokuStudio/Views/Windows/MainWindow.xaml.cs
+++ src/SudokuStudio/Views/Windows/MainWindow.xaml.cs
@@ -97,7 +97,7 @@ public sealed partial class MainWindow : Window
 			(container => container == AboutPageItem, typeof(AboutPage)),
 			(container => container == SingleCountingPageItem, typeof(SingleCountingPracticingPage)),
 			(container => container == HotkeyCheatTablePage, typeof(HotkeyCheatTablePage)),
-			(container => container == TechniqueDebuggerPage, typeof(TechniqueDebuggerPage))
+			(container => container == TechniqueDebuggerPage, typeof(TechniqueGalleryPage))
 		];
 
 	/// <summary>
