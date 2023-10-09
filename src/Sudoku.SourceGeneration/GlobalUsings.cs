9b5cc777cfb755cd94b516f07d636e3cc47f74db
SunnieShine <HowdySunnie@outlook.com>
2023-10-09 14:13:43 +0800

    Use auto-generated labels, sync with .NET generated files'.

--- src/Sudoku.SourceGeneration/GlobalUsings.cs
+++ src/Sudoku.SourceGeneration/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using System.Text;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
+global using RootLevelSourceGeneration;
 global using static Microsoft.CodeAnalysis.NullableAnnotation;
 global using static Microsoft.CodeAnalysis.SpecialType;
 global using static RootLevelSourceGeneration.SolutionVersion;
