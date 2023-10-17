6c4702454078c45396642afe1a5090b14c718447
SunnieShine <HowdySunnie@outlook.com>
2023-10-17 16:17:16 +0800

    Extract LINQ methods.

--- src/Sudoku.Core/Concepts/Grid.cs
+++ src/Sudoku.Core/Concepts/Grid.cs
@@ -989,42 +989,6 @@ public unsafe partial struct Grid :
 	}
 
 	/// <summary>
-	/// Filters the candidates that satisfies the specified condition.
-	/// </summary>
-	/// <param name="predicate">The condition to filter candidates.</param>
-	/// <returns>All candidates satisfied the specified condition.</returns>
-	public readonly ReadOnlySpan<Candidate> Where(Func<Candidate, bool> predicate)
-	{
-		var (result, i) = (new Candidate[CandidatesCount], 0);
-		foreach (var candidate in EnumerateCandidates())
-		{
-			if (predicate(candidate))
-			{
-				result[i++] = candidate;
-			}
-		}
-		return result.AsSpan()[..i];
-	}
-
-	/// <summary>
-	/// Projects each element of a sequence into a new form.
-	/// </summary>
-	/// <typeparam name="TResult">The type of the value returned by <paramref name="selector"/>.</typeparam>
-	/// <param name="selector">A transform function to apply to each element.</param>
-	/// <returns>
-	/// An array of <typeparamref name="TResult"/> elements converted.
-	/// </returns>
-	public readonly ReadOnlySpan<TResult> Select<TResult>(Func<Candidate, TResult> selector)
-	{
-		var (result, i) = (new TResult[CandidatesCount], 0);
-		foreach (var candidate in EnumerateCandidates())
-		{
-			result[i++] = selector(candidate);
-		}
-		return result.AsSpan()[..i];
-	}
-
-	/// <summary>
 	/// Reset the sudoku grid, to set all modifiable values to empty ones.
 	/// </summary>
 	public void Reset()
---
+++ src/Sudoku.Core/Linq/GridEnumerable.cs
@@ -0,0 +1,48 @@
+using Sudoku.Concepts;
+
+namespace Sudoku.Linq;
+
+/// <summary>
+/// Provides with a list of LINQ methods operating with <see cref="Grid"/> instances.
+/// </summary>
+/// <seealso cref="Grid"/>
+public static class GridEnumerable
+{
+	/// <summary>
+	/// Filters the candidates that satisfies the specified condition.
+	/// </summary>
+	/// <param name="this">The instance to be iterated.</param>
+	/// <param name="predicate">The condition to filter candidates.</param>
+	/// <returns>All candidates satisfied the specified condition.</returns>
+	public static ReadOnlySpan<Candidate> Where(this scoped ref readonly Grid @this, Func<Candidate, bool> predicate)
+	{
+		var (result, i) = (new Candidate[@this.CandidatesCount], 0);
+		foreach (var candidate in @this.EnumerateCandidates())
+		{
+			if (predicate(candidate))
+			{
+				result[i++] = candidate;
+			}
+		}
+		return result.AsSpan()[..i];
+	}
+
+	/// <summary>
+	/// Projects each element of a sequence into a new form.
+	/// </summary>
+	/// <typeparam name="TResult">The type of the value returned by <paramref name="selector"/>.</typeparam>
+	/// <param name="this">The instance to be iterated.</param>
+	/// <param name="selector">A transform function to apply to each element.</param>
+	/// <returns>
+	/// An array of <typeparamref name="TResult"/> elements converted.
+	/// </returns>
+	public static ReadOnlySpan<TResult> Select<TResult>(this scoped ref readonly Grid @this, Func<Candidate, TResult> selector)
+	{
+		var (result, i) = (new TResult[@this.CandidatesCount], 0);
+		foreach (var candidate in @this.EnumerateCandidates())
+		{
+			result[i++] = selector(candidate);
+		}
+		return result.AsSpan()[..i];
+	}
+}
