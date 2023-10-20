597424bd01e4056a508ab573b33a6578c2c9cef2
SunnieShine <HowdySunnie@outlook.com>
2023-10-20 10:41:06 +0800

    Update code.

--- src/Sudoku.Core/Linq/BitStatusMapGroupEnumerable.cs
+++ src/Sudoku.Core/Linq/BitStatusMapGroupEnumerable.cs
@@ -9,10 +9,27 @@ namespace Sudoku.Linq;
 public static class BitStatusMapGroupEnumerable
 {
 	/// <inheritdoc cref="Enumerable.Select{TSource, TResult}(IEnumerable{TSource}, Func{TSource, TResult})"/>
-	public static CellMap Select<TKey>(this scoped ReadOnlySpan<BitStatusMapGroup<CellMap, Cell, TKey>> @this, Func<BitStatusMapGroup<CellMap, Cell, TKey>, Cell> selector)
-		where TKey : notnull
+	public static CellMap Select<TKey>(
+		this scoped ReadOnlySpan<BitStatusMapGroup<CellMap, Cell, TKey>> @this,
+		Func<BitStatusMapGroup<CellMap, Cell, TKey>, Cell> selector
+	) where TKey : notnull
 	{
 		var result = CellMap.Empty;
+		foreach (var group in @this)
+		{
+			result.Add(selector(group));
+		}
+
+		return result;
+	}
+
+	/// <inheritdoc cref="Enumerable.Select{TSource, TResult}(IEnumerable{TSource}, Func{TSource, TResult})"/>
+	public static CandidateMap Select<TKey>(
+		this scoped ReadOnlySpan<BitStatusMapGroup<CandidateMap, Candidate, TKey>> @this,
+		Func<BitStatusMapGroup<CandidateMap, Candidate, TKey>, Candidate> selector
+	) where TKey : notnull
+	{
+		var result = CandidateMap.Empty;
 		foreach (var group in @this)
 		{
 			result.Add(selector(group));
