db3e09a4eb7578b6997fac0c0de9e6821aa41eff
SunnieShine <HowdySunnie@outlook.com>
2023-10-21 10:42:44 +0800

    Update doc comment.

--- src/Sudoku.Core/Linq/BitStatusMapGroupEnumerable.cs
+++ src/Sudoku.Core/Linq/BitStatusMapGroupEnumerable.cs
@@ -8,7 +8,15 @@ namespace Sudoku.Linq;
 /// <seealso cref="BitStatusMapGroup{TMap, TElement, TKey}"/>
 public static class BitStatusMapGroupEnumerable
 {
-	/// <inheritdoc cref="Enumerable.Select{TSource, TResult}(IEnumerable{TSource}, Func{TSource, TResult})"/>
+	/// <summary>
+	/// Projects a list of <see cref="BitStatusMapGroup{TMap, TElement, TKey}"/> of types <see cref="CellMap"/>, <see cref="Cell"/>
+	/// and <typeparamref name="TKey"/>, into a <see cref="Cell"/> value; collect converted results and merge
+	/// into a <see cref="CellMap"/> instance.
+	/// </summary>
+	/// <typeparam name="TKey">The type of the grouping.</typeparam>
+	/// <param name="this">The list to be checked.</param>
+	/// <param name="selector">The transform method to apply to each element.</param>
+	/// <returns>The result.</returns>
 	public static CellMap Select<TKey>(
 		this scoped ReadOnlySpan<BitStatusMapGroup<CellMap, Cell, TKey>> @this,
 		Func<BitStatusMapGroup<CellMap, Cell, TKey>, Cell> selector
@@ -23,7 +31,15 @@ public static class BitStatusMapGroupEnumerable
 		return result;
 	}
 
-	/// <inheritdoc cref="Enumerable.Select{TSource, TResult}(IEnumerable{TSource}, Func{TSource, TResult})"/>
+	/// <summary>
+	/// Projects a list of <see cref="BitStatusMapGroup{TMap, TElement, TKey}"/> of types <see cref="CandidateMap"/>, <see cref="Candidate"/>
+	/// and <typeparamref name="TKey"/>, into a <see cref="Candidate"/> value; collect converted results and merge
+	/// into a <see cref="CandidateMap"/> instance.
+	/// </summary>
+	/// <typeparam name="TKey">The type of the grouping.</typeparam>
+	/// <param name="this">The list to be checked.</param>
+	/// <param name="selector">The transform method to apply to each element.</param>
+	/// <returns>The result.</returns>
 	public static CandidateMap Select<TKey>(
 		this scoped ReadOnlySpan<BitStatusMapGroup<CandidateMap, Candidate, TKey>> @this,
 		Func<BitStatusMapGroup<CandidateMap, Candidate, TKey>, Candidate> selector
