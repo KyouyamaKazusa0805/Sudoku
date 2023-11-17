a10d118153ae6a7a2e06681f761399f7263afcb9
SunnieShine <HowdySunnie@outlook.com>
2023-11-17 16:47:39 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/StepCollectorFactory.cs
+++ src/Sudoku.Analytics/Analytics/StepCollectorFactory.cs
@@ -55,6 +55,29 @@ public static class StepCollectorFactory
 	}
 
 	/// <summary>
+	/// Try to set property with the specified value for the <typeparamref name="TStepSearcher"/> type.
+	/// If the target <see cref="StepSearcher"/> collection does not contain the step searcher instance
+	/// of type <typeparamref name="TStepSearcher"/>, the assignment will be skipped, never throwing exceptions.
+	/// </summary>
+	/// <typeparam name="TStepSearcher">The type of the <see cref="StepSearcher"/>.</typeparam>
+	/// <param name="this">The current <see cref="Analyzer"/> instance.</param>
+	/// <param name="propertySetter">The method to set the target property with new value.</param>
+	/// <seealso cref="StepSearcher"/>
+	public static StepCollector WithStepSearcherSetters<TStepSearcher>(this StepCollector @this, Action<TStepSearcher> propertySetter)
+		where TStepSearcher : StepSearcher
+	{
+		foreach (var stepSearcher in @this.ResultStepSearchers)
+		{
+			if (stepSearcher is TStepSearcher target)
+			{
+				propertySetter(target);
+			}
+		}
+
+		return @this;
+	}
+
+	/// <summary>
 	/// Sets the property <see cref="StepCollector.DifficultyLevelMode"/> with the target value.
 	/// </summary>
 	/// <param name="this">The collector instance.</param>
