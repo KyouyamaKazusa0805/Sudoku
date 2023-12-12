711d26c44f3b2016f8a6d69f6681249561473799
SunnieShine <HowdySunnie@outlook.com>
2023-12-12 11:56:48 +0800

    Add `CrossViewNode`.

---
+++ src/Sudoku.Core/Rendering/Nodes/CrossViewNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sudoku.Rendering.Nodes;
+
+/// <summary>
+/// Defines a cross view node.
+/// </summary>
+/// <param name="identifier"><inheritdoc cref="IconViewNode(ColorIdentifier, Cell)" path="/param[@name='identifier']"/></param>
+/// <param name="cell"><inheritdoc cref="IconViewNode(ColorIdentifier, Cell)" path="/param[@name='cell']"/></param>
+public sealed class CrossViewNode(ColorIdentifier identifier, Cell cell) : IconViewNode(identifier, cell)
+{
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public override CrossViewNode Clone() => new(Identifier, Cell);
+}
