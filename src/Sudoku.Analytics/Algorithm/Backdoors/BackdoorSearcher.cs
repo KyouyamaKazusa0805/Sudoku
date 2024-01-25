76854231aa402e015c4842ecbc0c4688d42fd068
SunnieShine <HowdySunnie@outlook.com>
2024-01-25 15:18:52 +0800

    Update code.

--- src/Sudoku.Analytics/Algorithm/Backdoors/BackdoorSearcher.cs
+++ src/Sudoku.Analytics/Algorithm/Backdoors/BackdoorSearcher.cs
@@ -17,52 +17,50 @@ public static class BackdoorSearcher
 	/// <param name="grid">The grid to be checked.</param>
 	/// <returns>A list of backdoors.</returns>
 	/// <exception cref="ArgumentException">Throws when the grid is not unique, or the puzzle is too easy.</exception>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static ReadOnlySpan<Conclusion> GetBackdoors(scoped ref readonly Grid grid)
 	{
-		switch (grid, SstsChecker.Analyze(in grid))
+		return (grid, SstsChecker.Analyze(in grid)) switch
 		{
-			case ({ IsValid: false } or { IsSolved: true }, _):
+			({ IsValid: false } or { IsSolved: true }, _) => throw new ArgumentException("The value is invalid or solved.", nameof(grid)),
+			({ SolutionGrid: var solution }, { IsSolved: true })
+				=>
+				from candidate in grid
+				let digit = solution.GetDigit(candidate / 9)
+				where digit != -1
+				select new Conclusion(digit == candidate % 9 ? Assignment : Elimination, candidate),
+			_ => g(in grid)
+		};
+
+
+		static ReadOnlySpan<Conclusion> g(scoped ref readonly Grid grid)
+		{
+			var (assignment, elimination, solution) = (new List<Conclusion>(81), new List<Conclusion>(729), grid.SolutionGrid);
+			foreach (var cell in grid.EmptyCells)
 			{
-				throw new ArgumentException("The value is invalid or solved.", nameof(grid));
-			}
-			case (_, { IsSolved: true }):
-			{
-				var solution = grid.SolutionGrid;
-				return
-					from candidate in grid
-					let digit = solution.GetDigit(candidate / 9)
-					where digit != -1
-					select new Conclusion(digit == candidate % 9 ? Assignment : Elimination, candidate);
-			}
-			default:
-			{
-				var (assignment, elimination, solution) = (new List<Conclusion>(81), new List<Conclusion>(729), grid.SolutionGrid);
-				foreach (var cell in grid.EmptyCells)
+				// Case 1: Assignments.
+				var case1Playground = grid;
+				case1Playground.SetDigit(cell, solution.GetDigit(cell));
+
+				if (SstsChecker.Analyze(in case1Playground).IsSolved)
 				{
-					// Case 1: Assignments.
-					var case1Playground = grid;
-					case1Playground.SetDigit(cell, solution.GetDigit(cell));
+					assignment.Add(new(Assignment, cell, solution.GetDigit(cell)));
 
-					if (SstsChecker.Analyze(in case1Playground).IsSolved)
+					// Case 2: Eliminations.
+					foreach (var digit in (Mask)(grid.GetCandidates(cell) & (Mask)~(1 << solution.GetDigit(cell))))
 					{
-						assignment.Add(new(Assignment, cell, solution.GetDigit(cell)));
+						var case2Playground = grid;
+						case2Playground.SetExistence(cell, digit, false);
 
-						// Case 2: Eliminations.
-						foreach (var digit in (Mask)(grid.GetCandidates(cell) & (Mask)~(1 << solution.GetDigit(cell))))
+						if (SstsChecker.Analyze(in case2Playground).IsSolved)
 						{
-							var case2Playground = grid;
-							case2Playground.SetExistence(cell, digit, false);
-
-							if (SstsChecker.Analyze(in case2Playground).IsSolved)
-							{
-								elimination.Add(new(Elimination, cell, digit));
-							}
+							elimination.Add(new(Elimination, cell, digit));
 						}
 					}
 				}
-
-				return (Conclusion[])[.. assignment, .. elimination];
 			}
+
+			return (Conclusion[])[.. assignment, .. elimination];
 		}
 	}
 }
