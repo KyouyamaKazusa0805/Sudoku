ffbd7182cbf4001ecf23b41b3e551d496fcfad40
SunnieShine <HowdySunnie@outlook.com>
2024-01-25 15:10:33 +0800

    Remove project `Sudoku.Recognition.Behaviors`. This project will be reconsidered.

--- src/Sudoku.Recognition.Behaviors/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Runtime.CompilerServices;
-global using Sudoku.Analytics;
-global using Sudoku.Concepts;
-global using Sudoku.Linq;
-global using static System.Numerics.BitOperations;
-global using static Sudoku.Analytics.ConclusionType;
