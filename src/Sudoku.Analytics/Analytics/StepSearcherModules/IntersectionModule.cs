fbe314529e1281ed1b35cd576b4d9c2c7da878c9
SunnieShine <HowdySunnie@outlook.com>
2024-02-05 20:43:43 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/StepSearcherModules/IntersectionModule.cs
+++ src/Sudoku.Analytics/Analytics/StepSearcherModules/IntersectionModule.cs
@@ -6,28 +6,6 @@ namespace Sudoku.Analytics.StepSearcherModules;
 internal static class IntersectionModule
 {
 	/// <summary>
-	/// Indicates whether the pattern is a locked candidates.
-	/// </summary>
-	/// <param name="grid">Indicates the grid used.</param>
-	/// <param name="a">Indicates the 6 cells in the line, but not intersects with block.</param>
-	/// <param name="b">Indicates the 6 cells in the block, but not intersects with line.</param>
-	/// <param name="c">Indicates the intersection 3 cells.</param>
-	/// <param name="emptyCells">The empty cells in the pattern.</param>
-	/// <param name="digitsMask">Indicates the mask of digits that are locked candidates in the pattern.</param>
-	/// <returns>A <see cref="bool"/> result.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	internal static bool IsLockedCandidates(
-		scoped ref readonly Grid grid,
-		scoped ref readonly CellMap a,
-		scoped ref readonly CellMap b,
-		scoped ref readonly CellMap c,
-		scoped ref readonly CellMap emptyCells,
-		out Mask digitsMask
-	)
-		=> (digitsMask = 0, emptyCells & c, grid[in a], grid[in b], grid[in c]) is (_, not [], var maskA, var maskB, var maskC)
-		&& (digitsMask = (Mask)(maskC & (maskA ^ maskB))) != 0;
-
-	/// <summary>
 	/// Try to create a list of <see cref="CellViewNode"/>s indicating the crosshatching base cells.
 	/// </summary>
 	/// <param name="grid">The grid.</param>
@@ -61,4 +39,26 @@ internal static class IntersectionModule
 
 		return result.AsReadOnlySpan();
 	}
+
+	/// <summary>
+	/// Indicates whether the pattern is a locked candidates.
+	/// </summary>
+	/// <param name="grid">Indicates the grid used.</param>
+	/// <param name="a">Indicates the 6 cells in the line, but not intersects with block.</param>
+	/// <param name="b">Indicates the 6 cells in the block, but not intersects with line.</param>
+	/// <param name="c">Indicates the intersection 3 cells.</param>
+	/// <param name="emptyCells">The empty cells in the pattern.</param>
+	/// <param name="digitsMask">Indicates the mask of digits that are locked candidates in the pattern.</param>
+	/// <returns>A <see cref="bool"/> result.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static bool IsLockedCandidates(
+		scoped ref readonly Grid grid,
+		scoped ref readonly CellMap a,
+		scoped ref readonly CellMap b,
+		scoped ref readonly CellMap c,
+		scoped ref readonly CellMap emptyCells,
+		out Mask digitsMask
+	)
+		=> (digitsMask = 0, emptyCells & c, grid[in a], grid[in b], grid[in c]) is (_, not [], var maskA, var maskB, var maskC)
+		&& (digitsMask = (Mask)(maskC & (maskA ^ maskB))) != 0;
 }
