80ac456b58096d83c8edf41507d6c112cb2bc468
SunnieShine <HowdySunnie@outlook.com>
2024-02-12 14:23:36 +0800

    Update code.

--- src/Sudoku.Core/Runtime/LibraryServices/LibraryInitializedException.cs
+++ src/Sudoku.Core/Runtime/LibraryServices/LibraryInitializedException.cs
@@ -6,7 +6,12 @@ namespace Sudoku.Runtime.LibraryServices;
 /// </summary>
 /// <param name="directory"><inheritdoc cref="Library(string, string)" path="/param[@name='directory']"/></param>
 /// <param name="fileId"><inheritdoc cref="Library(string, string)" path="/param[@name='fileId']"/></param>
+/// <remarks><i>
+/// This type is only used by Windows platform because the relied type <see cref="Library"/>
+/// is marked <see cref="SupportedOSPlatformAttribute"/>, limited in Windows.
+/// </i></remarks>
 /// <seealso cref="Library.Initialize"/>
+[SupportedOSPlatform("windows")]
 public sealed partial class LibraryInitializedException(
 	[PrimaryConstructorParameter] string directory,
 	[PrimaryConstructorParameter] string fileId
