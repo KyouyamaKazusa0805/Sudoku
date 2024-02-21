9195c08f2069f8e6703fe228c33e6bdd14decf50
SunnieShine <HowdySunnie@outlook.com>
2024-02-21 14:08:00 +0800

    Move files.

--- src/Sudoku.Strategying/Strategying/Validation/ValidationResult.cs
+++ src/Sudoku.Strategying/Strategying/Validation/ValidationResult.cs
@@ -4,12 +4,29 @@ namespace Sudoku.Strategying.Validation;
 /// Represents a result data that describes why the validation failed.
 /// </summary>
 /// <param name="Success">Indicates whether the validation is passed and no error produced.</param>
-/// <param name="FailedPropertyNames">Indicates the failed property names to be set.</param>
+/// <param name="FailedPropertyName">Indicates the failed property name to be set.</param>
 /// <param name="Reason">Indicates the failed reason.</param>
 /// <param name="Severity">Indicates the severity of the failure.</param>
 public abstract record ValidationResult(
-	[property: MemberNotNullWhen(false, "FailedPropertyNames")] bool Success,
-	string? FailedPropertyNames,
+	[property: MemberNotNullWhen(false, "FailedPropertyName")] bool Success,
+	string? FailedPropertyName,
 	ValidationReason Reason,
 	ValidationSeverity Severity
-);
+)
+{
+	/// <summary>
+	/// Represents a <see cref="ValidationResult"/> that describes for a successful message.
+	/// </summary>
+	public static ValidationResult Successful => new SuccessValidationResult();
+
+
+	/// <summary>
+	/// Represents a <see cref="ValidationResult"/> that describes for a failed message.
+	/// </summary>
+	/// <param name="failedPropertyName">Indicates the name of the failed property.</param>
+	/// <param name="reason">Indicates the reason why raises the failure.</param>
+	/// <param name="severity">Indicates the severity of the failure.</param>
+	/// <returns>The <see cref="ValidationResult"/> instance.</returns>
+	public static ValidationResult Failed(string failedPropertyName, ValidationReason reason, ValidationSeverity severity)
+		=> new FailedValidationResult(failedPropertyName, reason, severity);
+}
