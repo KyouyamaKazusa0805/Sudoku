f72f20d329123d238a27bc33685629e7b88dfab1
SunnieShine <HowdySunnie@outlook.com>
2024-02-24 15:32:25 +0800

    Update code.

--- src/Sudoku.Core/ModuleInitializer.cs
+++ src/Sudoku.Core/ModuleInitializer.cs
@@ -1,14 +1,10 @@
 namespace Sudoku;
 
 /// <include file="../../global-doc-comments.xml" path="//g/csharp9/feature[@name='module-initializer']/target[@name='type']"/>
+[EditorBrowsable(EditorBrowsableState.Never)]
 internal static class ModuleInitializer
 {
 	/// <include file="../../global-doc-comments.xml" path="//g/csharp9/feature[@name='module-initializer']/target[@name='method']"/>
-	[DebuggerStepThrough]
-	[EditorBrowsable(EditorBrowsableState.Never)]
 	[ModuleInitializer]
-	[MethodImpl(MethodImplOptions.NoInlining)]
-	[Obsolete("This method cannot be called by ourselves.", true)]
-	[RequiresUnreferencedCode("Module initializer cannot be invoked by user ourselves.")]
 	public static void Initialize() => ResourceDictionary.RegisterResourceManager<CoreResources>();
 }
