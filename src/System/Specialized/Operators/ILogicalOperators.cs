97e1349e08a525e34a2e67ff4bd6dd65be767494
SunnieShine <HowdySunnie@outlook.com>
2024-03-03 15:15:39 +0800

    Update code.

--- src/System/Specialized/Operators/ILogicalOperators.cs
+++ src/System/Specialized/Operators/ILogicalOperators.cs
@@ -1,44 +1,11 @@
 namespace System;
 
 /// <summary>
-/// Represents for an abstraction unit that supports for boolean logical operators
-/// <see langword="operator"/> <![CDATA[&&]]> and <see langword="operator"/> <![CDATA[||]]>.
+/// Defines a mechanism for computing the logical relation between two instances of type <typeparamref name="TSelf"/>.
 /// </summary>
 /// <typeparam name="TSelf">The implementation.</typeparam>
 public interface ILogicalOperators<TSelf> where TSelf : ILogicalOperators<TSelf>?
 {
-	/// <summary>
-	/// Negates the current instance, and makes the result to be negated one.
-	/// </summary>
-	/// <param name="value">The value.</param>
-	/// <returns>A <see cref="bool"/> result.</returns>
-	public static abstract bool operator !(TSelf value);
-
-	/// <summary>
-	/// Equivalent to <c><![CDATA[false(left) ? left : false(left & right)]]></c>.
-	/// </summary>
-	/// <param name="left">The left-side value.</param>
-	/// <param name="right">The right-side value.</param>
-	/// <returns>A <typeparamref name="TSelf"/> result indicating the calculation result.</returns>
-	public static abstract TSelf operator &(TSelf left, TSelf right);
-
-	/// <summary>
-	/// Equivalent to <c><![CDATA[true(left) ? left : false(left | right)]]></c>.
-	/// </summary>
-	/// <param name="left">The left-side value.</param>
-	/// <param name="right">The right-side value.</param>
-	/// <returns>A <typeparamref name="TSelf"/> result indicating the calculation result.</returns>
-	public static abstract TSelf operator |(TSelf left, TSelf right);
-
-	/// <summary>
-	/// Equivalent to <c><![CDATA[left & ~right | ~left & right]]></c>.
-	/// <b>Not <c><![CDATA[left && ~right || ~left && right]]></c>.</b>
-	/// </summary>
-	/// <param name="left">The left-side value.</param>
-	/// <param name="right">The right-side value.</param>
-	/// <returns>A <typeparamref name="TSelf"/> result indicating the calculation result.</returns>
-	public static abstract TSelf operator ^(TSelf left, TSelf right);
-
 	/// <summary>
 	/// Determine whether the specified object is determined <see langword="true"/>.
 	/// </summary>
@@ -51,6 +18,36 @@ public interface ILogicalOperators<TSelf> where TSelf : ILogicalOperators<TSelf>
 	/// </summary>
 	/// <param name="value">The value to be determined.</param>
 	/// <returns>A <see cref="bool"/> result.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static virtual bool operator false(TSelf value) => !(value ? true : false);
+
+	/// <summary>
+	/// Negates the current instance, and makes the result to be negated one.
+	/// </summary>
+	/// <param name="value">The value.</param>
+	/// <returns>A <see cref="bool"/> result.</returns>
+	public static abstract bool operator !(TSelf value);
+
+	/// <summary>
+	/// Equivalent to <c><![CDATA[false(left) ? left : false(left & right)]]></c>.
+	/// </summary>
+	/// <param name="left">The left-side value.</param>
+	/// <param name="right">The right-side value.</param>
+	/// <returns>A instance of type <typeparamref name="TSelf"/>, as the result value.</returns>
+	public static abstract TSelf operator &(TSelf left, TSelf right);
+
+	/// <summary>
+	/// Equivalent to <c><![CDATA[true(left) ? left : false(left | right)]]></c>.
+	/// </summary>
+	/// <param name="left">The left-side value.</param>
+	/// <param name="right">The right-side value.</param>
+	/// <returns><inheritdoc cref="op_BitwiseAnd(TSelf, TSelf)" path="/returns"/></returns>
+	public static abstract TSelf operator |(TSelf left, TSelf right);
+
+	/// <summary>
+	/// Equivalent to <c><![CDATA[left & ~right | ~left & right]]></c>.
+	/// </summary>
+	/// <param name="left">The left-side value.</param>
+	/// <param name="right">The right-side value.</param>
+	/// <returns><inheritdoc cref="op_BitwiseAnd(TSelf, TSelf)" path="/returns"/></returns>
+	public static abstract TSelf operator ^(TSelf left, TSelf right);
 }
