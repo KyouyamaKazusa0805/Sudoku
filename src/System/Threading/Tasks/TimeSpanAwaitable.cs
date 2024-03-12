a89146f3f97b2a29c076653366b9d122f930d423
SunnieShine <HowdySunnie@outlook.com>
2024-03-12 20:56:22 +0800

    Move file.

--- src/System/Threading/Tasks/TimeSpanAwaitable.cs
+++ src/System/Threading/Tasks/TimeSpanAwaitable.cs
@@ -1,6 +1,21 @@
 namespace System.Threading.Tasks;
 
 /// <summary>
+/// Provides with extension methods on <see cref="TimeSpan"/>.
+/// </summary>
+/// <seealso cref="TimeSpan"/>
+public static class TimeSpanAwaitable
+{
+	/// <summary>
+	/// Creates a <see cref="TimeSpanAwaiter"/> instance used for <see langword="await"/> expressions.
+	/// </summary>
+	/// <param name="this">The time span.</param>
+	/// <returns>A <see cref="TimeSpanAwaiter"/> instance.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static TimeSpanAwaiter GetAwaiter(this TimeSpan @this) => new(Task.Delay(@this).GetAwaiter());
+}
+
+/// <summary>
 /// Represents an awaiter for <see cref="TimeSpan"/> instance.
 /// </summary>
 /// <param name="awaiter">The base awaiter instance.</param>
