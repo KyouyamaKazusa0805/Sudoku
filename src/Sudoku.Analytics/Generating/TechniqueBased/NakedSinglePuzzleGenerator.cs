fcd09fe67343044908b7f753b85f5143037e3831
SunnieShine <HowdySunnie@outlook.com>
2024-03-28 16:26:38 +0800

    Implement for naked single technique-based generator.

--- src/Sudoku.Analytics/Generating/TechniqueBased/NakedSinglePuzzleGenerator.cs
+++ src/Sudoku.Analytics/Generating/TechniqueBased/NakedSinglePuzzleGenerator.cs
@@ -11,7 +11,35 @@ public sealed class NakedSinglePuzzleGenerator : SinglePuzzleGenerator
 
 	/// <inheritdoc/>
 	public override JustOneCellPuzzle GenerateJustOneCell()
-		=> new JustOneCellPuzzleFailed(GeneratingFailedReason.NotSupported);
+	{
+		// Randomly select a cell as target cell.
+		var targetCell = Rng.Next(0, 81);
+		var peerCells = PeersMap[targetCell].ToArray()[..];
+		ShuffleSequence(peerCells);
+		ShuffleSequence(DigitSeed);
+
+		// Update values and create a grid.
+		var puzzle = Grid.Empty;
+		var i = 0;
+		var digitsMask = (Mask)511;
+		foreach (var cell in peerCells[..8])
+		{
+			var digit = DigitSeed[i++];
+			puzzle.SetDigit(cell, digit);
+			puzzle.SetState(cell, CellState.Given);
+
+			digitsMask &= (Mask)~(1 << digit);
+		}
+
+		var targetDigit = Log2((uint)digitsMask);
+		var blockCellsCount = (HousesMap[targetCell.ToHouseIndex(HouseType.Block)] - puzzle.EmptyCells).Count;
+		return new JustOneCellPuzzleSuccessful(
+			in puzzle,
+			targetCell,
+			targetDigit,
+			new NakedSingleStep([], [], new(), targetCell, targetDigit, SingleTechniqueSubtype.NakedSingle0 + blockCellsCount)
+		);
+	}
 
 	/// <inheritdoc/>
 	public override FullPuzzle GenerateUnique(CancellationToken cancellationToken = default)
