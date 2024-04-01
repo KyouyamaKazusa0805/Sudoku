32202cbfec9ba6e42386a69e8e915684572bc9c4
SunnieShine <HowdySunnie@outlook.com>
2024-04-01 17:05:59 +0800

    Update code.

--- src/Sudoku.Analytics/Concepts/ObjectModel/SolvingPathElement.cs
+++ src/Sudoku.Analytics/Concepts/ObjectModel/SolvingPathElement.cs
@@ -3,12 +3,7 @@ namespace Sudoku.Concepts.ObjectModel;
 /// <summary>
 /// Represents an element that is the unit data structure of a <see cref="SolvingPath"/>.
 /// </summary>
-/// <param name="steppingGrid">Indicates the stepping grid.</param>
-/// <param name="step">Indicates the current step.</param>
+/// <param name="SteppingGrid">Indicates the stepping grid.</param>
+/// <param name="Step">Indicates the current step.</param>
 /// <seealso cref="SolvingPath"/>
-public readonly partial struct SolvingPathElement([PrimaryConstructorParameter] scoped ref readonly Grid steppingGrid, [PrimaryConstructorParameter] Step step)
-{
-	/// <include file="../../global-doc-comments.xml" path="g/csharp7/feature[@name='deconstruction-method']/target[@name='method']"/>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public void Deconstruct(out Grid steppingGrid, out Step step) => (steppingGrid, step) = (SteppingGrid, Step);
-}
+public readonly record struct SolvingPathElement(scoped ref readonly Grid SteppingGrid, Step Step);
