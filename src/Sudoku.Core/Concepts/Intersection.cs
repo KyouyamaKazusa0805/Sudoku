276259879450492b400c776570d4b3036f58af85
Qitian Zhang <HowdySunnie@outlook.com>
2024-04-16 19:18:49 +0800

    Update format.

--- src/Sudoku.Core/Concepts/Intersection.cs
+++ src/Sudoku.Core/Concepts/Intersection.cs
@@ -77,6 +77,11 @@ public readonly record struct Intersection(scoped ref readonly IntersectionBase 
 			}
 		}
 
-		IntersectionMaps = dic.ToFrozenDictionary(ValueComparison.Create<IntersectionBase>(static (x, y) => x == y, static v => v.Line << 5 | v.Block));
+		IntersectionMaps = dic.ToFrozenDictionary(
+			ValueComparison.Create<IntersectionBase>(
+				static (x, y) => x == y,
+				static v => v.Line << 5 | v.Block
+			)
+		);
 	}
 }
