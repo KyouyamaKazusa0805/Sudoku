c3d4dbe90307c71c66825462f453df254b8ff7d7
SunnieShine <HowdySunnie@outlook.com>
2024-04-27 15:47:18 +0800

    Move files.

--- src/RootLevelSourceGeneration/GlobalUsings.cs
+++ src/RootLevelSourceGeneration/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.ComponentModel;
 global using System.Diagnostics;
 global using System.Linq;
+global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Text;
 global using System.Xml;
--- src/SudokuStudio.SourceGeneration/GlobalUsings.cs
+++ src/SudokuStudio.SourceGeneration/GlobalUsings.cs
@@ -2,7 +2,6 @@ global using System;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Linq;
-global using System.Reflection;
 global using System.Threading;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
