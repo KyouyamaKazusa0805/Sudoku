f36f9966ad1d7d3fd3610dd2ee6cf26c463c28ff
Qitian Zhang <HowdySunnie@outlook.com>
2024-05-21 20:42:48 +0800

    Update code.

---
+++ src/System/Extensions/ValueMatchExtensions.cs
@@ -0,0 +1,18 @@
+namespace System.Text.RegularExpressions;
+
+/// <summary>
+/// Provides with extension methdos on <see cref="ValueMatch"/>.
+/// </summary>
+/// <seealso cref="ValueMatch"/>
+public static class ValueMatchExtensions
+{
+	/// <summary>
+	/// Try to get the target match string at the specified position the current instance specified.
+	/// </summary>
+	/// <param name="this">Indicates the instance.</param>
+	/// <param name="originalString">The original string.</param>
+	/// <returns>The target string.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ReadOnlySpan<char> MatchString(this ValueMatch @this, string originalString)
+		=> originalString.AsSpan().Slice(@this.Index, @this.Length);
+}
---
+++ src/System/ValueMatchEnumerator.cs
@@ -0,0 +1,37 @@
+namespace System;
+
+/// <summary>
+/// Represents an enumerator that is used for iteration on matches.
+/// </summary>
+/// <param name="originalString">Indicates the original string value.</param>
+/// <param name="backingEnumerator">Indicates the backing enumerator.</param>
+[StructLayout(LayoutKind.Auto)]
+public ref partial struct ValueMatchEnumerator(
+	string originalString,
+	[PrimaryConstructorParameter(MemberKinds.Field, IsImplicitlyReadOnly = false)] Regex.ValueMatchEnumerator backingEnumerator
+)
+{
+	/// <inheritdoc cref="IEnumerator.Current"/>
+	public readonly ReadOnlySpan<char> Current
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => originalString.AsSpan().Slice(_backingEnumerator.Current.Index, _backingEnumerator.Current.Length);
+	}
+
+
+	/// <inheritdoc cref="IAnyAllMethod{TSelf, TSource}.Any()"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public readonly bool Any()
+	{
+		var copied = this;
+		return copied.MoveNext();
+	}
+
+	/// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public readonly ValueMatchEnumerator GetEnumerator() => this;
+
+	/// <inheritdoc cref="IEnumerator.MoveNext"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool MoveNext() => _backingEnumerator.MoveNext();
+}
