ba45c1481f2675c9ee89a0c6267d7469bb10d596
Qitian Zhang <HowdySunnie@outlook.com>
2024-05-28 17:42:05 +0800

    Extract project `Sudoku.Generating.JustOneCell`.

---
+++ src/Sudoku.Generating.JustOneCell/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.Diagnostics.CodeAnalysis;
+global using System.Runtime.CompilerServices;
+global using System.SourceGeneration;
+global using Sudoku.Analytics;
+global using Sudoku.Analytics.Configuration;
+global using Sudoku.Concepts;
+global using Sudoku.Drawing;
