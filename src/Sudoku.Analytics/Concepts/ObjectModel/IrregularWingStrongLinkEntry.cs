db3589a539c0fc45155d74ee7f860e8e7beb3820
Qitian Zhang <HowdySunnie@outlook.com>
2024-06-07 20:01:28 +0800

    Remove deprecated files.

--- src/Sudoku.Analytics/Concepts/ObjectModel/IrregularWingStrongLinkEntry.cs
+++
@@ -1,7 +0,0 @@
-namespace Sudoku.Concepts.ObjectModel;
-
-/// <summary>
-/// Represents an aliased type for <see cref="Dictionary{TKey, TValue}"/> on irregular wing checking.
-/// </summary>
-/// <param name="capacity">Indicates the capacity to be initialized.</param>
-internal sealed class IrregularWingStrongLinkEntry(int capacity) : Dictionary<(House House, Digit Digit), List<StrongLinkInfo>>(capacity);
