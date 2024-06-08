ee94e3563db1024cfe47012166d2d3543013baad
SunnieShine <HowdySunnie@outlook.com>
2024-06-08 15:32:35 +0800

    Remove deprecated files.

--- src/Sudoku.Generating.JustOneCell/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Diagnostics.CodeAnalysis;
-global using System.Runtime.CompilerServices;
-global using System.SourceGeneration;
-global using Sudoku.Analytics;
-global using Sudoku.Analytics.Configuration;
-global using Sudoku.Concepts;
-global using Sudoku.Drawing;
