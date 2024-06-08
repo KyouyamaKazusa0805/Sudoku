32ffc14c027baea7eb2530f3ab91ce134e175359
SunnieShine <HowdySunnie@outlook.com>
2024-06-08 15:53:33 +0800

    Merge files.

--- src/Sudoku.Measuring/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Resources;
-global using System.Runtime.CompilerServices;
-global using System.SourceGeneration;
-global using Sudoku.Concepts;
-global using static Sudoku.SolutionFields;
--- src/Sudoku.Text.Tokenization/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Resources;
-global using System.Runtime.CompilerServices;
-global using System.Text;
-global using static Sudoku.SolutionFields;
