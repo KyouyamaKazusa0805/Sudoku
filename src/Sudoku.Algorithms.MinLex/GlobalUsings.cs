298a3df93a133d9651f5bcfb0a398e2b439653c7
SunnieShine <HowdySunnie@outlook.com>
2024-06-09 22:18:03 +0800

    Move files.

--- src/Sudoku.Algorithms.MinLex/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Resources;
-global using System.Runtime.CompilerServices;
-global using System.SourceGeneration;
-global using System.Text;
-global using Sudoku.Algorithms.MinLex;
-global using Sudoku.Concepts;
-global using Sudoku.Solving;
--- src/Sudoku.Algorithms.Transformation/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System;
-global using System.ComponentModel;
-global using System.Runtime.CompilerServices;
-global using Sudoku.Algorithms.Transformation;
-global using Sudoku.Concepts;
-global using Sudoku.Randomization;
-global using static Sudoku.SolutionFields;
