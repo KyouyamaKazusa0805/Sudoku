27baba80d6ecd84c219cce36e08e4c55d98a6108
Qitian Zhang <HowdySunnie@outlook.com>
2024-06-19 10:33:07 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/Chaining/ChainingRule.cs
+++ src/Sudoku.Analytics/Analytics/Chaining/ChainingRule.cs
@@ -77,26 +77,6 @@ public abstract partial class ChainingRule
 		=> nodes = [];
 
 	/// <summary>
-	/// Collects for extra view nodes for the pattern on multiple forcing chains.
-	/// This method will be useful in advanced chaining rules such as ALS, and AUR extra maps checking.
-	/// </summary>
-	/// <param name="grid">The grid as candidate references.</param>
-	/// <param name="pattern">The pattern to collect view nodes.</param>
-	/// <param name="views">The <see cref="View"/> instances to be updated.</param>
-	/// <seealso cref="View"/>
-	protected internal virtual void CollectExtraViewNodes(ref readonly Grid grid, MultipleForcingChains pattern, View[] views)
-	{
-		var viewIndex = 1;
-		foreach (var branch in pattern.Values)
-		{
-			CollectExtraViewNodes(in grid, branch, views[viewIndex], out var nodes);
-			views[0].AddRange(nodes);
-
-			viewIndex++;
-		}
-	}
-
-	/// <summary>
 	/// Try to find extra eliminations that can only be created inside a Grouped Continuous Nice Loop.
 	/// This method will be useful in advanced chaining rules such as ALS, AHS and AUR eliminations checking.
 	/// </summary>
@@ -107,4 +87,22 @@ public abstract partial class ChainingRule
 	/// This method should not be overridden if no eliminations exists in the loop pattern.
 	/// </remarks>
 	protected internal virtual ConclusionSet CollectLoopConclusions(Loop loop, ref readonly Grid grid) => [];
+
+	/// <summary>
+	/// Collects for extra view nodes for the pattern on multiple forcing chains.
+	/// This method will be useful in advanced chaining rules such as ALS, and AUR extra maps checking.
+	/// </summary>
+	/// <param name="grid">The grid as candidate references.</param>
+	/// <param name="pattern">The pattern to collect view nodes.</param>
+	/// <param name="views">The <see cref="View"/> instances to be updated.</param>
+	/// <seealso cref="View"/>
+	internal void CollectExtraViewNodes(ref readonly Grid grid, MultipleForcingChains pattern, View[] views)
+	{
+		var viewIndex = 1;
+		foreach (var branch in pattern.Values)
+		{
+			CollectExtraViewNodes(in grid, branch, views[viewIndex++], out var nodes);
+			views[0].AddRange(nodes);
+		}
+	}
 }
