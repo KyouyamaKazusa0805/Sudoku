ff7bf8ba96de5e7b80967925b8fab19ba4683865
Qitian Zhang <HowdySunnie@outlook.com>
2024-08-06 11:24:03 +0800

    Overload `PrintMembers`.

--- src/Sudoku.Analytics/Concepts/UniqueLoops/UniqueLoop.cs
+++ src/Sudoku.Analytics/Concepts/UniqueLoops/UniqueLoop.cs
@@ -13,4 +13,24 @@ public readonly record struct UniqueLoop(ref readonly CellMap Loop, Cell[] Path,
 
 	/// <inheritdoc/>
 	public bool Equals(UniqueLoop other) => Loop == other.Loop;
+
+	[SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+	private bool PrintMembers(StringBuilder stringBuilder)
+	{
+		stringBuilder.Append($"{nameof(Loop)} = {Loop.ToString()}");
+		stringBuilder.Append(", ");
+		stringBuilder.Append($"{nameof(Path)} = [");
+		for (var i = 0; i < Path.Length; i++)
+		{
+			var cell = Path[i];
+			stringBuilder.Append(cell);
+			if (i != Path.Length - 1)
+			{
+				stringBuilder.Append(", ");
+			}
+		}
+		stringBuilder.Append("], ");
+		stringBuilder.Append($"{nameof(DigitsMask)} = {DigitsMask} (0b{Convert.ToString(DigitsMask, 2).PadLeft(9, '0')})");
+		return true;
+	}
 }
