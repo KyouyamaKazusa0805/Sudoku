fba50aab2c3592e20dc408e60d6434148bb71938
Qitian Zhang <HowdySunnie@outlook.com>
2024-08-08 16:44:13 +0800

    Swap doc comment.

--- src/Sudoku.Analytics/Inferring/DeadlyPatternInferredResult.cs
+++ src/Sudoku.Analytics/Inferring/DeadlyPatternInferredResult.cs
@@ -3,15 +3,18 @@ namespace Sudoku.Inferring;
 /// <summary>
 /// Indicates the result value after <see cref="DeadlyPatternInferrer.TryInfer(ref readonly Grid, out DeadlyPatternInferredResult)"/> called.
 /// </summary>
-/// <param name="grid">Indicates the pattern to be checked.</param>
+/// <param name="grid"><inheritdoc cref="Grid" path="/summary"/></param>
 /// <param name="isDeadlyPattern">Indicates the pattern is a real deadly pattern.</param>
 /// <seealso cref="DeadlyPatternInferrer.TryInfer(ref readonly Grid, out DeadlyPatternInferredResult)"/>
 [StructLayout(LayoutKind.Auto)]
 [TypeImpl(TypeImplFlag.AllObjectMethods)]
-public readonly ref partial struct DeadlyPatternInferredResult(ref readonly Grid grid, [PrimaryConstructorParameter] bool isDeadlyPattern)
+public readonly ref partial struct DeadlyPatternInferredResult(
+	ref readonly Grid grid,
+	[PrimaryConstructorParameter] bool isDeadlyPattern
+)
 {
 	/// <summary>
-	/// <inheritdoc cref="DeadlyPatternInferredResult(ref readonly Grid, bool)" path="/param[@name='grid']"/>
+	/// Indicates the pattern to be checked.
 	/// </summary>
 	public Grid Grid { get; } = grid;
 
