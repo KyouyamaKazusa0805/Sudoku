306c6c958b725a5ded37f174cd187bd44ab78e5b
Qitian Zhang <HowdySunnie@outlook.com>
2024-08-22 14:06:41 +0800

    Update code.

--- src/Sudoku.Core/Drawing/Drawing2D/IPointCalculator.cs
+++ src/Sudoku.Core/Drawing/Drawing2D/IPointCalculator.cs
@@ -1,8 +1,5 @@
 namespace Sudoku.Drawing.Drawing2D;
 
-using LocalPoint = (float X, float Y);
-using LocalSize = (float Width, float Height);
-
 /// <summary>
 /// Represents a base type of pointer calculator.
 /// </summary>
@@ -26,27 +23,27 @@ public interface IPointCalculator
 	/// <summary>
 	/// Indicates the control size.
 	/// </summary>
-	public LocalSize ControlSize { get; }
+	public (float Width, float Height) ControlSize { get; }
 
 	/// <summary>
 	/// Indicates the grid size.
 	/// </summary>
-	public LocalSize GridSize { get; }
+	public (float Width, float Height) GridSize { get; }
 
 	/// <summary>
 	/// Indicates the cell size.
 	/// </summary>
-	public LocalSize CellSize { get; }
+	public (float Width, float Height) CellSize { get; }
 
 	/// <summary>
 	/// Indicates the candidate size.
 	/// </summary>
-	public LocalSize CandidateSize { get; }
+	public (float Width, float Height) CandidateSize { get; }
 
 	/// <summary>
 	/// Indicates the absolutely points in grid cross-lines.
 	/// This property will be assigned later (and not <see langword="null"/>).
 	/// </summary>
 	/// <remarks>Note that the size of this 2D array is always 28 by 28.</remarks>
-	public LocalPoint[,] GridPoints { get; }
+	public (float X, float Y)[,] GridPoints { get; }
 }
