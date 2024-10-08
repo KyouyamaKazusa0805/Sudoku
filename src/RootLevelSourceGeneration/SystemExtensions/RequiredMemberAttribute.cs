dc5ebe5a9a0c5d4276b5bfb9d646243ac11874a4
Qitian Zhang <HowdySunnie@outlook.com>
2024-10-08 17:49:01 +0800

    Use PolySharp package instead.

--- src/RootLevelSourceGeneration/SystemExtensions/RequiredMemberAttribute.cs
+++
@@ -1,6 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#pragma warning disable CS1591
-[AttributeUsage(AttributeTargets.All)]
-public sealed class RequiredMemberAttribute : Attribute;
