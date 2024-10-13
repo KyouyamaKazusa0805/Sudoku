60e1ef57c49557e8f7c0f51772bae30ed7c6274b
SunnieShine <HowdySunnie@outlook.com>
2024-10-13 21:34:43 +0800

    Update code.

--- src/System/Collections/Generic/ListExtensions.cs
+++ src/System/Collections/Generic/ListExtensions.cs
@@ -73,14 +73,6 @@ public static class ListExtensions
 		}
 	}
 
-	/// <summary>
-	/// Removes the last element stored in the current <see cref="List{T}"/>.
-	/// </summary>
-	/// <typeparam name="T">The type of each element.</typeparam>
-	/// <param name="this">The list to be modified.</param>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static void Remove<T>(this List<T> @this) => @this.RemoveAt(@this.Count - 1);
-
 	/// <inheritdoc cref="List{T}.RemoveAt(int)"/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void RemoveAt<T>(this List<T> @this, Index index) => @this.RemoveAt(index.GetOffset(@this.Count));
