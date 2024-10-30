4e463a918e7e8524bc3f1c01ba857230593080ad
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-10-30 18:00:43 +0800

    Update doc comment.

--- src/Sudoku.Analytics/Analytics/Construction/Chaining/ChainingDriver.cs
+++ src/Sudoku.Analytics/Analytics/Construction/Chaining/ChainingDriver.cs
@@ -3,9 +3,6 @@ namespace Sudoku.Analytics.Construction.Chaining;
 /// <summary>
 /// Provides a driver that can generate normal chains and forcing chains.
 /// </summary>
-/// <remarks>
-/// This file is a hub. Please check separated files to get chaining algorithm.
-/// </remarks>
 internal static partial class ChainingDriver
 {
 	/// <summary>
