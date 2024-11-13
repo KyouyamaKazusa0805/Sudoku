f06f0f526b79ae7f4eb0c23b6009cdd434477bde
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-11-13 15:25:16 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/Construction/Chaining/Rules/UniqueRectangleSameDigitChainingRule.cs
+++ src/Sudoku.Analytics/Analytics/Construction/Chaining/Rules/UniqueRectangleSameDigitChainingRule.cs
@@ -88,38 +88,4 @@ public sealed class UniqueRectangleSameDigitChainingRule : UniqueRectangleChaini
 			}
 		}
 	}
-
-	/// <inheritdoc/>
-	public override void GetLoopConclusions(ref ChainingRuleLoopConclusionContext context)
-	{
-		// VARIABLE_DECLARATION_BEGIN
-		_ = 42;
-		// VARIABLE_DECLARATION_END
-
-		// This example will contain extra eliminations:
-		//   4.........2..5...6..8..23...9..8.7....59.+7.6.7+8.3.4..2.7..4.6....3..1..91......8.:548 549 167 168 695
-		//
-		// Extra eliminations should be r7c8(25):
-		//   .-----------------------.-----------------------.------------------------.
-		//   | 4      156(3)  679-1  | 1678   1679(3)  689-3 | 12589  12579    1578   |
-		//   | 9(3)   2       79-1   | 1478   5        389   | 1489   1479     6      |
-		//   | 569    156     8      | 1467   1679     2     | 3      14579    1457   |
-		//   |-----------------------+-----------------------+------------------------|
-		//   | (236)  9       (1246) | 1256   8        56    | 7      (134)    (134)  |
-		//   | (23)   34-1    5      | 9      12       7     | 148    6        1348   |
-		//   | 7      8       (16)   | 3      16       4     | 59     59       2      |
-		//   |-----------------------+-----------------------+------------------------|
-		//   | 2589   7       29     | 258    4        3589  | 6      -25(13)  5(13)  |
-		//   | 2568   456     3      | 25678  267      1     | 245    2457     9      |
-		//   | 1      456     2469   | 2567   279(3)   569-3 | 245    8        457(3) |
-		//   '-----------------------'-----------------------'------------------------'
-		//
-		// However I don't have any unified rules to calculate such eliminations
-		// because AUR eliminations should be treated as "forced" ones, meaning we should check them by supposing it with "true"
-		// to make a contradiction.
-		// This will be implemented in the future.
-		//
-		// Binds with issue #680: https://github.com/KyouyamaKazusa0805/Sudoku/issues/680
-		base.GetLoopConclusions(ref context);
-	}
 }
