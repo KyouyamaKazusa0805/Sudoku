2ce759ae92df7e86fce05919f9663fe09d383f7c
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-02-05 14:34:42 +0800

    Add `out` parameter indicating top-left cell index.

--- src/Sudoku.Core/Metric/EmptyArea.cs
+++ src/Sudoku.Core/Metric/EmptyArea.cs
@@ -9,66 +9,94 @@ public static class EmptyArea
 	/// Try to get the maximum empty area exists in the specified grid.
 	/// </summary>
 	/// <param name="this">The grid to be checked.</param>
+	/// <param name="topLeftCell">
+	/// <inheritdoc cref="GetMaxEmptySquareArea(ref readonly CellMap, out Cell)" path="/param[@name='topLeftCell']"/>
+	/// </param>
 	/// <returns>A <see cref="Cell"/> value indicating the result.</returns>
 	/// <remarks>
-	/// <inheritdoc cref="GetMaxEmptyArea(ref readonly CellMap)" path="/remarks"/>
+	/// <inheritdoc cref="GetMaxEmptyArea(ref readonly CellMap, out Cell)" path="/remarks"/>
 	/// </remarks>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static Cell GetMaxEmptyArea(this ref readonly Grid @this) => @this.EmptyCells.GetMaxEmptyArea();
+	public static Cell GetMaxEmptyArea(this ref readonly Grid @this, out Cell topLeftCell)
+		=> @this.EmptyCells.GetMaxEmptyArea(out topLeftCell);
 
 	/// <summary>
 	/// Try to get the maximum empty square area exists in the specified grid.
 	/// </summary>
 	/// <param name="this">The grid to be checked.</param>
+	/// <param name="topLeftCell">
+	/// <inheritdoc cref="GetMaxEmptySquareArea(ref readonly CellMap, out Cell)" path="/param[@name='topLeftCell']"/>
+	/// </param>
 	/// <returns>A <see cref="Cell"/> value indicating the result.</returns>
 	/// <remarks>
-	/// <inheritdoc cref="GetMaxEmptySquareArea(ref readonly CellMap)" path="/remarks"/>
+	/// <inheritdoc cref="GetMaxEmptySquareArea(ref readonly CellMap, out Cell)" path="/remarks"/>
 	/// </remarks>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static Cell GetMaxEmptySquareArea(this ref readonly Grid @this) => @this.EmptyCells.GetMaxEmptySquareArea();
+	public static Cell GetMaxEmptySquareArea(this ref readonly Grid @this, out Cell topLeftCell)
+		=> @this.EmptyCells.GetMaxEmptySquareArea(out topLeftCell);
 
 	/// <summary>
 	/// Try to get the maximum empty area exists in the specified cells.
 	/// </summary>
 	/// <param name="this">The cells to be checked.</param>
+	/// <param name="topLeftCell">
+	/// <inheritdoc cref="GetMaxEmptySquareArea(ref readonly CellMap, out Cell)" path="/param[@name='topLeftCell']"/>
+	/// </param>
 	/// <returns>A <see cref="Cell"/> value indicating the result.</returns>
 	/// <remarks>
 	/// This algorithm is from the puzzle called
 	/// <see href="https://leetcode.com/problems/maximal-rectangle/"><i>Maximal Rectangle</i></see>.
 	/// </remarks>
-	public static Cell GetMaxEmptyArea(this ref readonly CellMap @this)
+	public static Cell GetMaxEmptyArea(this ref readonly CellMap @this, out Cell topLeftCell)
 	{
 		var dp = (stackalloc Cell[9]);
 		dp.Clear();
 
-		var max = 0;
+		(topLeftCell, var max) = (-1, 0);
 		for (var i = 0; i < 9; i++)
 		{
 			for (var j = 0; j < 9; j++)
 			{
 				dp[j] = @this.Contains(i * 9 + j) ? dp[j] + 1 : 0;
 			}
-			max = Math.Max(max, getMaxRow(dp));
+
+			int currentMax;
+			int currentTopLeft;
+			(currentMax, currentTopLeft) = getMaxRow(dp, i);
+
+			if (currentMax > max)
+			{
+				max = currentMax;
+				topLeftCell = currentTopLeft;
+			}
 		}
 		return max;
 
 
-		static Cell getMaxRow(ReadOnlySpan<Cell> height)
+		static (int max, int topLeft) getMaxRow(ReadOnlySpan<int> height, int row)
 		{
-			var stack = new Stack<Cell>();
+			var stack = new Stack<int>();
 			var max = 0;
+			var topLeft = -1;
+
 			for (var i = 0; i <= 9; i++)
 			{
 				var h = i == 9 ? 0 : height[i];
 				while (stack.Count != 0 && height[stack.Peek()] >= h)
 				{
 					var maxHeight = height[stack.Pop()];
-					var area = stack.Count == 0 ? i * maxHeight : maxHeight * (i - 1 - stack.Peek());
-					max = Math.Max(max, area);
+					var width = stack.Count == 0 ? i : i - 1 - stack.Peek();
+					var area = maxHeight * width;
+
+					if (area > max)
+					{
+						max = area;
+						topLeft = (row - maxHeight + 1) * 9 + (stack.Count == 0 ? 0 : stack.Peek() + 1);
+					}
 				}
 				stack.Push(i);
 			}
-			return max;
+			return (max, topLeft);
 		}
 	}
 
@@ -76,28 +104,64 @@ public static class EmptyArea
 	/// Try to get the maximum empty square area exists in the specified cells.
 	/// </summary>
 	/// <param name="this">The cells to be checked.</param>
+	/// <param name="topLeftCell">Indicates the cell at the top left index of the square.</param>
 	/// <returns>A <see cref="Cell"/> value indicating the result.</returns>
 	/// <remarks>
 	/// This algorithm is from the puzzle called
 	/// <see href="https://leetcode.com/problems/maximal-square/"><i>Maximal Square</i></see>.
 	/// </remarks>
-	public static Cell GetMaxEmptySquareArea(this ref readonly CellMap @this)
+	public static Cell GetMaxEmptySquareArea(this ref readonly CellMap @this, out Cell topLeftCell)
 	{
-		var maxSide = 0;
+		(topLeftCell, var maxSide) = (-1, 0);
 		var dp = (stackalloc Cell[81]);
+
 		for (var i = 0; i < 9; i++)
 		{
 			for (var j = 0; j < 9; j++)
 			{
-				if (@this.Contains(i * 9 + j))
+				var index = i * 9 + j;
+				if (@this.Contains(index))
 				{
-					dp[i * 9 + j] = (i, j) is not (not 0, not 0)
-						? 1
-						: MathExtensions.Min(dp[(i - 1) * 9 + j], dp[i * 9 + j - 1], dp[(i - 1) * 9 + j - 1]) + 1;
-					maxSide = Math.Max(maxSide, dp[i * 9 + j]);
+					if (i == 0 || j == 0)
+					{
+						dp[index] = 1;
+					}
+					else
+					{
+						var up = dp[(i - 1) * 9 + j];
+						var left = dp[i * 9 + (j - 1)];
+						var upLeft = dp[(i - 1) * 9 + (j - 1)];
+						dp[index] = MathExtensions.Min(up, left, upLeft) + 1;
+					}
+				}
+				else
+				{
+					dp[index] = 0;
+				}
+
+				var currentSide = dp[index];
+				if (currentSide > maxSide)
+				{
+					maxSide = currentSide;
+					var topRow = i - maxSide + 1;
+					var leftCol = j - maxSide + 1;
+					topLeftCell = topRow * 9 + leftCol;
+				}
+				else if (currentSide == maxSide && currentSide > 0)
+				{
+					var currentTopRow = i - currentSide + 1;
+					var currentLeftCol = j - currentSide + 1;
+					var currentTopIndex = currentTopRow * 9 + currentLeftCol;
+
+					// To choose the minimal index of the top-left cell.
+					if (currentTopRow < topLeftCell / 9 || currentTopRow == topLeftCell / 9 && currentLeftCol < topLeftCell % 9)
+					{
+						topLeftCell = currentTopIndex;
+					}
 				}
 			}
 		}
+
 		return maxSide * maxSide;
 	}
 }
