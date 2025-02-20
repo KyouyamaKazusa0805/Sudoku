6c43748cf9dcdaae266ed90cc4d07d6130bf3bab
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-02-20 14:09:38 +0800

    Implement async version.

--- src/Sudoku.Core/Solving/SolutionEnumerableSolverExtensions.cs
+++ src/Sudoku.Core/Solving/SolutionEnumerableSolverExtensions.cs
@@ -55,4 +55,58 @@ public static class SolutionEnumerableSolverExtensions
 		void this_SolutionFound(ISolutionEnumerableSolver _, SolverSolutionFoundEventArgs e)
 			=> buffer.Add(e.Solution, cancellationToken);
 	}
+
+	/// <summary>
+	/// Try to enumerate all possible solutions of the specified grid, by using the current solver, in asynchronous way.
+	/// </summary>
+	/// <param name="this">The solver.</param>
+	/// <param name="grid">The grid.</param>
+	/// <param name="cancellationToken">The cancellation token that can cancel the current operation.</param>
+	/// <returns>A sequence of <see cref="Grid"/> values indicating the raw solution text to the puzzle.</returns>
+	public static async IAsyncEnumerable<Grid> EnumerateSolutionsAsync(
+		this ISolutionEnumerableSolver @this,
+		Grid grid,
+		[EnumeratorCancellation] CancellationToken cancellationToken = default
+	)
+	{
+		using var buffer = new BlockingCollection<Grid>();
+		try
+		{
+			// Temporarily add handler.
+			@this.SolutionFound += this_SolutionFound;
+
+			// Perform adding operation.
+			// Here we must omit 'await' keyword here because here we should make code concurrently executed on purpose,
+			// i.e. making the following code (consuming enumerable method) become available.
+			_ = Task.Run(
+				() =>
+				{
+					try
+					{
+						@this.EnumerateSolutionsCore(grid, cancellationToken);
+					}
+					finally
+					{
+						buffer.CompleteAdding();
+					}
+				},
+				cancellationToken
+			);
+
+			// Consume the solutions concurrently.
+			await foreach (var item in buffer.GetConsumingEnumerableAsync(cancellationToken))
+			{
+				yield return item;
+			}
+		}
+		finally
+		{
+			// Remove handler.
+			@this.SolutionFound -= this_SolutionFound;
+		}
+
+
+		void this_SolutionFound(ISolutionEnumerableSolver _, SolverSolutionFoundEventArgs e)
+			=> buffer.Add(e.Solution, cancellationToken);
+	}
 }
