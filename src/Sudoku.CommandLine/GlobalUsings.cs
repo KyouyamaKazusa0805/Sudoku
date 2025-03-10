cb5622c23fa8088adc71ee8bfaa3090cc26fd3f6
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-03-10 16:45:32 +0800

    Update code.

--- src/Sudoku.CommandLine/GlobalUsings.cs
+++ src/Sudoku.CommandLine/GlobalUsings.cs
@@ -1,3 +1,7 @@
 global using System;
+global using System.Collections;
+global using System.Collections.Generic;
 global using System.Linq;
+global using System.Linq.Enumerators;
+global using System.Runtime.CompilerServices;
 global using System.Text.RegularExpressions;
