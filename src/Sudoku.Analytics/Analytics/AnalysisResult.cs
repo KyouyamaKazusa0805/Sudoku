d4a522a7bbfdaa46f05ae3a6453ddf0d12d50837
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-03-11 15:58:49 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/AnalysisResult.cs
+++ src/Sudoku.Analytics/Analytics/AnalysisResult.cs
@@ -904,16 +904,16 @@ public sealed partial record AnalysisResult([property: EquatableMember] in Grid 
 	/// <seealso cref="Console.Out"/>
 	public static string ConsoleEnhancer(string str, Step step)
 	{
-		var @default = VariantColor.Default;
+		var @default = (-1, -1, -1);
 		var c = step.DifficultyLevel switch
 		{
-			DifficultyLevel.Moderate => new(0, 255, 0),
-			DifficultyLevel.Hard => new(255, 255, 0),
-			DifficultyLevel.Fiendish => new(255, 150, 80),
-			DifficultyLevel.Nightmare => new(255, 100, 100),
+			DifficultyLevel.Moderate => (0, 255, 0),
+			DifficultyLevel.Hard => (255, 255, 0),
+			DifficultyLevel.Fiendish => (255, 150, 80),
+			DifficultyLevel.Nightmare => (255, 100, 100),
 			_ => @default
 		};
-		return c == @default ? str : $"{c.ToConsoleColorString(true)}{str}{@default.ToConsoleColorString(true)}";
+		return c == @default ? str : $"\e[38;2;{c.Item1};{c.Item2};{c.Item3}m{str}\e[0m";
 	}
 
 	/// <summary>
