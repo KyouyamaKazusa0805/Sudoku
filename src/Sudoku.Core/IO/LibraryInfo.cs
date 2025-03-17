f223ebc0cb1f764cd4fe96a9d151ce9de5a942a3
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-03-17 16:47:39 +0800

    Update code.

--- src/Sudoku.Core/IO/LibraryInfo.cs
+++ src/Sudoku.Core/IO/LibraryInfo.cs
@@ -6,22 +6,43 @@ namespace Sudoku.IO;
 public sealed class LibraryInfo
 {
 	/// <summary>
+	/// Initializes a <see cref="LibraryInfo"/> instance.
+	/// </summary>
+	/// <param name="name">The name.</param>
+	/// <param name="description">The description.</param>
+	/// <param name="author">The author.</param>
+	/// <param name="tags">The tags.</param>
+	[SetsRequiredMembers]
+	[JsonConstructor]
+	public LibraryInfo(string name, string description, string author, string[] tags)
+		=> (Name, Description, Author, Tags) = (name, description, author, tags[..]);
+
+	/// <summary>
+	/// Initializes a <see cref="LibraryInfo"/> instance.
+	/// </summary>
+	[SetsRequiredMembers]
+	internal LibraryInfo() : this(string.Empty, string.Empty, string.Empty, [])
+	{
+	}
+
+
+	/// <summary>
 	/// Indicates the name of the library.
 	/// </summary>
-	public string Name { get; set; } = string.Empty;
+	public required string Name { get; set; }
 
 	/// <summary>
 	/// Indicates the description to the library.
 	/// </summary>
-	public string Description { get; set; } = string.Empty;
+	public required string Description { get; set; }
 
 	/// <summary>
 	/// Indicates the author of the library.
 	/// </summary>
-	public string Author { get; set; } = string.Empty;
+	public required string Author { get; set; }
 
 	/// <summary>
 	/// Indicates the tags of the library.
 	/// </summary>
-	public string[] Tags { get; set; } = [];
+	public required string[] Tags { get; set; }
 }
