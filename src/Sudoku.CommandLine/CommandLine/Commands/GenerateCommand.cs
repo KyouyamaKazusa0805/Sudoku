653c455c0b1702efe6e16d3ac8d0dd87b62d2282
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-03-25 18:19:02 +0800

    Update code.

--- src/Sudoku.CommandLine/CommandLine/Commands/GenerateCommand.cs
+++ src/Sudoku.CommandLine/CommandLine/Commands/GenerateCommand.cs
@@ -3,15 +3,11 @@ namespace Sudoku.CommandLine.Commands;
 /// <summary>
 /// Represents a generate command.
 /// </summary>
-public sealed class GenerateCommand : Command, INonLeafCommand
+public sealed class GenerateCommand : Command
 {
 	/// <summary>
 	/// Initializes a <see cref="GenerateCommand"/> instance.
 	/// </summary>
-	public GenerateCommand() : base("generate", "Generate a puzzle using the specified way") => this.AddRange(CommandsCore);
-
-
-	/// <inheritdoc/>
-	public ReadOnlySpan<Command> CommandsCore
-		=> (Command[])[new GenerateDefaultCommand(), new GeneratePatternCommand(), new GenerateHardCommand()];
+	public GenerateCommand() : base("generate", "Generate a puzzle using the specified way")
+		=> this.AddRange(new GenerateDefaultCommand(), new GeneratePatternCommand(), new GenerateHardCommand());
 }
