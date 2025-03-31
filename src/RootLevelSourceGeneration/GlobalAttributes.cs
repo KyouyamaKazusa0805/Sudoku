017b18aae10cd894561892b7c70c563028c16d1d
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-03-31 11:13:17 +0800

    Remove deprecated files.

--- src/RootLevelSourceGeneration/GlobalAttributes.cs
+++
@@ -1,5 +0,0 @@
-[assembly: InternalsVisibleTo("Sudoku.SourceGeneration")]
-[assembly: InternalsVisibleTo("Sudoku.Analytics.SourceGeneration")]
-[assembly: InternalsVisibleTo("SystemExtensions.SourceGeneration")]
-[assembly: InternalsVisibleTo("SudokuStudio.SourceGeneration")]
-[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "<Pending>")]
--- src/System.SourceGeneration/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using System.Collections.Generic;
-global using System.Linq;
-global using Microsoft.CodeAnalysis;
-global using Sudoku.SourceGeneration.Handlers;
-global using static SolutionVersion;
