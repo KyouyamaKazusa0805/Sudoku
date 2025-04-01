ca62b5d5aac1dc5d567d9bbab9ac0353e8509edc
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-04-01 20:12:16 +0800

    Update code.

--- src/Sudoku.Core/Concepts/Coordinates/K9Parser.cs
+++ src/Sudoku.Core/Concepts/Coordinates/K9Parser.cs
@@ -7,16 +7,111 @@ namespace Sudoku.Concepts.Coordinates;
 public sealed partial record K9Parser : CoordinateParser
 {
 	/// <inheritdoc/>
-	public override Func<string, CellMap> CellParser => OnCellParsing;
+	public override Func<string, CellMap> CellParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitCellGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = CellMap.Empty;
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var indexOfTheFirstDigit = Array.FindIndex(s.ToCharArray(), char.IsDigit);
+				var rows = s[..indexOfTheFirstDigit];
+				var columns = s[(indexOfTheFirstDigit + 1)..];
+				foreach (var row in rows)
+				{
+					var finalRow = row is 'I' or 'i' or 'J' or 'j' or 'K' or 'k' ? 'I' : char.ToUpper(row);
+					foreach (var column in columns)
+					{
+						result.Add((finalRow - 'A') * 9 + column - '1');
+					}
+				}
+			}
+			return result;
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, CandidateMap> CandidateParser => OnCandidateParsing;
+	public override Func<string, CandidateMap> CandidateParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitCandidateGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = CandidateMap.Empty;
+			foreach (var match in matches.Cast<Match>())
+			{
+				if (match.Captures is not [{ Value: var rows }, { Value: var columns }, { Value: var digits }])
+				{
+					continue;
+				}
+
+				foreach (var row in rows)
+				{
+					var finalRow = row is 'I' or 'i' or 'J' or 'j' or 'K' or 'k' ? 'I' : char.ToUpper(row);
+					foreach (var column in columns)
+					{
+						foreach (var digit in digits)
+						{
+							result.Add(((finalRow - 'A') * 9 + column - '1') * 9 + digit - '1');
+						}
+					}
+				}
+			}
+			return result;
+		};
 
 	/// <inheritdoc/>
 	public override Func<string, HouseMask> HouseParser => throw new NotSupportedException();
 
 	/// <inheritdoc/>
-	public override Func<string, ConclusionSet> ConclusionParser => OnConclusionParsing;
+	public override Func<string, ConclusionSet> ConclusionParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitConclusionGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new ConclusionSet();
+			foreach (var match in matches.Cast<Match>())
+			{
+				if (match.Captures is not [{ Value: var cells }, _, { Value: var equalityOperator }, { Value: var digits }])
+				{
+					continue;
+				}
+
+				var conclusionType = equalityOperator is "=" or "==" ? Assignment : Elimination;
+				foreach (var cell in cells)
+				{
+					foreach (var digit in digits)
+					{
+						result.Add(new(conclusionType, cell, digit - '1'));
+					}
+				}
+			}
+			return result;
+		};
 
 	/// <inheritdoc/>
 	public override Func<string, Mask> DigitParser => new RxCyParser().DigitParser;
@@ -25,7 +120,29 @@ public sealed partial record K9Parser : CoordinateParser
 	public override Func<string, ReadOnlySpan<Chute>> ChuteParser => throw new NotSupportedException();
 
 	/// <inheritdoc/>
-	public override Func<string, ReadOnlySpan<Conjugate>> ConjugateParser => OnConjugateParsing;
+	public override Func<string, ReadOnlySpan<Conjugate>> ConjugateParser
+		=> str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitConjugateGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new List<Conjugate>();
+			foreach (var match in matches.Cast<Match>())
+			{
+				if (match.Captures is [{ Value: var cell1 }, { Value: var cell2 }, { Value: [var digitChar] }])
+				{
+					result.Add(new(CellParser(cell1)[0], CellParser(cell2)[0], digitChar - '1'));
+				}
+			}
+			return result.AsSpan();
+		};
 
 	/// <inheritdoc/>
 	public override Func<string, ReadOnlySpan<Miniline>> IntersectionParser => throw new NotSupportedException();
@@ -47,126 +164,4 @@ public sealed partial record K9Parser : CoordinateParser
 	/// <inheritdoc/>
 	[return: NotNullIfNotNull(nameof(formatType))]
 	public override object? GetFormat(Type? formatType) => formatType == typeof(CoordinateParser) ? this : null;
-
-
-	private static CellMap OnCellParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitCellGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = CellMap.Empty;
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var indexOfTheFirstDigit = Array.FindIndex(s.ToCharArray(), char.IsDigit);
-			var rows = s[..indexOfTheFirstDigit];
-			var columns = s[(indexOfTheFirstDigit + 1)..];
-			foreach (var row in rows)
-			{
-				var finalRow = row is 'I' or 'i' or 'J' or 'j' or 'K' or 'k' ? 'I' : char.ToUpper(row);
-				foreach (var column in columns)
-				{
-					result.Add((finalRow - 'A') * 9 + column - '1');
-				}
-			}
-		}
-		return result;
-	}
-
-	private static CandidateMap OnCandidateParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitCandidateGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = CandidateMap.Empty;
-		foreach (var match in matches.Cast<Match>())
-		{
-			if (match.Captures is not [{ Value: var rows }, { Value: var columns }, { Value: var digits }])
-			{
-				continue;
-			}
-
-			foreach (var row in rows)
-			{
-				var finalRow = row is 'I' or 'i' or 'J' or 'j' or 'K' or 'k' ? 'I' : char.ToUpper(row);
-				foreach (var column in columns)
-				{
-					foreach (var digit in digits)
-					{
-						result.Add(((finalRow - 'A') * 9 + column - '1') * 9 + digit - '1');
-					}
-				}
-			}
-		}
-		return result;
-	}
-
-	private ConclusionSet OnConclusionParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitConclusionGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new ConclusionSet();
-		foreach (var match in matches.Cast<Match>())
-		{
-			if (match.Captures is not [{ Value: var cells }, _, { Value: var equalityOperator }, { Value: var digits }])
-			{
-				continue;
-			}
-
-			var conclusionType = equalityOperator is "=" or "==" ? Assignment : Elimination;
-			foreach (var cell in cells)
-			{
-				foreach (var digit in digits)
-				{
-					result.Add(new(conclusionType, cell, digit - '1'));
-				}
-			}
-		}
-		return result;
-	}
-
-	private ReadOnlySpan<Conjugate> OnConjugateParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitConjugateGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new List<Conjugate>();
-		foreach (var match in matches.Cast<Match>())
-		{
-			if (match.Captures is [{ Value: var cell1 }, { Value: var cell2 }, { Value: [var digitChar] }])
-			{
-				result.Add(new(CellParser(cell1)[0], CellParser(cell2)[0], digitChar - '1'));
-			}
-		}
-		return result.AsSpan();
-	}
 }
--- src/Sudoku.Core/Concepts/Coordinates/RxCyParser.cs
+++ src/Sudoku.Core/Concepts/Coordinates/RxCyParser.cs
@@ -7,28 +7,263 @@ namespace Sudoku.Concepts.Coordinates;
 public sealed partial record RxCyParser : CoordinateParser
 {
 	/// <inheritdoc/>
-	public override Func<string, CellMap> CellParser => OnCellParsing;
+	public override Func<string, CellMap> CellParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitCellGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = CellMap.Empty;
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
+				var rowDigits = s[1..indexOfColumnLabel];
+				var columnDigits = s[(indexOfColumnLabel + 1)..];
+				foreach (var row in rowDigits)
+				{
+					foreach (var column in columnDigits)
+					{
+						result.Add((row - '1') * 9 + column - '1');
+					}
+				}
+			}
+
+			return result;
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, CandidateMap> CandidateParser => OnCandidateParsing;
+	public override Func<string, CandidateMap> CandidateParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitCandidateGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = CandidateMap.Empty;
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				if (s.Contains('('))
+				{
+					var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
+					var indexOfOpenBraceLabel = s.IndexOf('(');
+					var rowDigits = s[1..indexOfColumnLabel];
+					var columnDigits = s[(indexOfColumnLabel + 1)..indexOfOpenBraceLabel];
+					var digitDigits = s[(indexOfOpenBraceLabel + 1)..^1];
+					foreach (var row in rowDigits)
+					{
+						foreach (var column in columnDigits)
+						{
+							foreach (var digit in digitDigits)
+							{
+								result.Add(((row - '1') * 9 + column - '1') * 9 + digit - '1');
+							}
+						}
+					}
+				}
+				else
+				{
+					var indexOfRowLabel = s.IndexOfAny(['R', 'r']);
+					var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
+					var digitDigits = s[..indexOfRowLabel];
+					var rowDigits = s[(indexOfRowLabel + 1)..indexOfColumnLabel];
+					var columnDigits = s[(indexOfColumnLabel + 1)..];
+					foreach (var row in rowDigits)
+					{
+						foreach (var column in columnDigits)
+						{
+							foreach (var digit in digitDigits)
+							{
+								result.Add(((row - '1') * 9 + column - '1') * 9 + digit - '1');
+							}
+						}
+					}
+				}
+			}
+
+			return result;
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, HouseMask> HouseParser => OnHouseParsing;
+	public override Func<string, HouseMask> HouseParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return 0;
+			}
+
+			if (UnitHousePattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return 0;
+			}
+
+			var result = 0;
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var label = s[0];
+				foreach (var house in from digit in s[1..] select digit - '1')
+				{
+					result |= 1 << label switch { 'R' or 'r' => 9, 'C' or 'c' => 18, _ => 0 } + house;
+				}
+			}
+
+			return result;
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, ConclusionSet> ConclusionParser => OnConclusionParsing;
+	public override Func<string, ConclusionSet> ConclusionParser
+		=> str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitConclusionGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new ConclusionSet();
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var indexOfEqualityOperatorCharacters = s.Split(["==", "<>", "=", "!="], 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+				var cells = CellParser(indexOfEqualityOperatorCharacters[0]);
+				var digits = MaskOperations.Create(from character in indexOfEqualityOperatorCharacters[1] select character - '1');
+				var conclusionType = ConclusionOperatorsPattern.Match(s) is { Success: true, Value: "==" or "=" } ? Assignment : Elimination;
+				foreach (var cell in cells)
+				{
+					foreach (var digit in digits)
+					{
+						result.Add(new(conclusionType, cell, digit));
+					}
+				}
+			}
+
+			return result;
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, Mask> DigitParser => OnDigitParsing;
+	public override Func<string, Mask> DigitParser
+		=> static str => DigitPattern.Matches(str) is { Count: <= 9 } matches
+			? MaskOperations.Create(from match in matches select match.Value[0] - '1')
+			: throw new InvalidOperationException(SR.ExceptionMessage("ErrorInfo_DuplicatedValuesMayExistOrInvalid"));
 
 	/// <inheritdoc/>
-	public override Func<string, ReadOnlySpan<Chute>> ChuteParser => OnChuteParsing;
+	public override Func<string, ReadOnlySpan<Chute>> ChuteParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitMegaLineGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new List<Chute>(6);
+			foreach (var match in matches.Cast<Match>())
+			{
+				switch (match.Value)
+				{
+					case [_, 'R' or 'r', var r and >= '1' and <= '3']:
+					{
+						result.AddRef(Chutes[r - '1']);
+						break;
+					}
+					case [_, 'C' or 'c', var c and >= '1' and <= '3']:
+					{
+						result.AddRef(Chutes[c + 3 - '1']);
+						break;
+					}
+				}
+			}
+			return result.AsSpan();
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, ReadOnlySpan<Conjugate>> ConjugateParser => OnConjugateParsing;
+	public override Func<string, ReadOnlySpan<Conjugate>> ConjugateParser
+		=> str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitConjugateGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new List<Conjugate>();
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var split = s.Split("==", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+				var leftCell = CellParser(split[0])[0];
+				var rightCellAndDigit = CandidateParser(split[1])[0];
+				var rightCell = rightCellAndDigit / 9;
+				var digit = rightCellAndDigit % 9;
+				result.Add(new(leftCell, rightCell, digit));
+			}
+			return result.AsSpan();
+		};
 
 	/// <inheritdoc/>
-	public override Func<string, ReadOnlySpan<Miniline>> IntersectionParser => OnIntersectionParsing;
+	public override Func<string, ReadOnlySpan<Miniline>> IntersectionParser
+		=> static str =>
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return [];
+			}
+
+			if (UnitIntersectionGroupPattern.Matches(str) is not { Count: not 0 } matches)
+			{
+				return [];
+			}
+
+			var result = new List<Miniline>();
+			foreach (var match in matches.Cast<Match>())
+			{
+				var s = match.Value;
+				var indexOfBlockLabel = s.IndexOfAny(['B', 'b']);
+				var lineLabel = s[0];
+				var lines = s[1..indexOfBlockLabel];
+				var blocks = s[(indexOfBlockLabel + 1)..];
+				foreach (var line in lines)
+				{
+					foreach (var block in blocks)
+					{
+						var @base = new MinilineBase(
+							(byte)(lineLabel is 'R' or 'r' ? line + 9 - '1' : line + 18 - '1'),
+							(byte)(block - '1')
+						);
+						result.Add(new(@base, Miniline.Map[@base]));
+					}
+				}
+			}
+			return result.AsSpan();
+		};
 
 
 	[GeneratedRegex("""r[1-9]+c[1-9]+""", RegexOptions.Compiled | RegexOptions.IgnoreCase)]
@@ -62,249 +297,4 @@ public sealed partial record RxCyParser : CoordinateParser
 	/// <inheritdoc/>
 	[return: NotNullIfNotNull(nameof(formatType))]
 	public override object? GetFormat(Type? formatType) => formatType == typeof(CoordinateParser) ? this : null;
-
-
-	private static CellMap OnCellParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitCellGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = CellMap.Empty;
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
-			var rowDigits = s[1..indexOfColumnLabel];
-			var columnDigits = s[(indexOfColumnLabel + 1)..];
-			foreach (var row in rowDigits)
-			{
-				foreach (var column in columnDigits)
-				{
-					result.Add((row - '1') * 9 + column - '1');
-				}
-			}
-		}
-
-		return result;
-	}
-
-	private static CandidateMap OnCandidateParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitCandidateGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = CandidateMap.Empty;
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			if (s.Contains('('))
-			{
-				var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
-				var indexOfOpenBraceLabel = s.IndexOf('(');
-				var rowDigits = s[1..indexOfColumnLabel];
-				var columnDigits = s[(indexOfColumnLabel + 1)..indexOfOpenBraceLabel];
-				var digitDigits = s[(indexOfOpenBraceLabel + 1)..^1];
-				foreach (var row in rowDigits)
-				{
-					foreach (var column in columnDigits)
-					{
-						foreach (var digit in digitDigits)
-						{
-							result.Add(((row - '1') * 9 + column - '1') * 9 + digit - '1');
-						}
-					}
-				}
-			}
-			else
-			{
-				var indexOfRowLabel = s.IndexOfAny(['R', 'r']);
-				var indexOfColumnLabel = s.IndexOfAny(['C', 'c']);
-				var digitDigits = s[..indexOfRowLabel];
-				var rowDigits = s[(indexOfRowLabel + 1)..indexOfColumnLabel];
-				var columnDigits = s[(indexOfColumnLabel + 1)..];
-				foreach (var row in rowDigits)
-				{
-					foreach (var column in columnDigits)
-					{
-						foreach (var digit in digitDigits)
-						{
-							result.Add(((row - '1') * 9 + column - '1') * 9 + digit - '1');
-						}
-					}
-				}
-			}
-		}
-
-		return result;
-	}
-
-	private static HouseMask OnHouseParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return 0;
-		}
-
-		if (UnitHousePattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return 0;
-		}
-
-		var result = 0;
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var label = s[0];
-			foreach (var house in from digit in s[1..] select digit - '1')
-			{
-				result |= 1 << label switch { 'R' or 'r' => 9, 'C' or 'c' => 18, _ => 0 } + house;
-			}
-		}
-
-		return result;
-	}
-
-	private ConclusionSet OnConclusionParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitConclusionGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new ConclusionSet();
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var indexOfEqualityOperatorCharacters = s.Split(["==", "<>", "=", "!="], 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-			var cells = CellParser(indexOfEqualityOperatorCharacters[0]);
-			var digits = MaskOperations.Create(from character in indexOfEqualityOperatorCharacters[1] select character - '1');
-			var conclusionType = ConclusionOperatorsPattern.Match(s) is { Success: true, Value: "==" or "=" } ? Assignment : Elimination;
-			foreach (var cell in cells)
-			{
-				foreach (var digit in digits)
-				{
-					result.Add(new(conclusionType, cell, digit));
-				}
-			}
-		}
-
-		return result;
-	}
-
-	private static Mask OnDigitParsing(string str)
-		=> DigitPattern.Matches(str) is { Count: <= 9 } matches
-			? MaskOperations.Create(from match in matches select match.Value[0] - '1')
-			: throw new InvalidOperationException(SR.ExceptionMessage("ErrorInfo_DuplicatedValuesMayExistOrInvalid"));
-
-	private static ReadOnlySpan<Chute> OnChuteParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitMegaLineGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new List<Chute>(6);
-		foreach (var match in matches.Cast<Match>())
-		{
-			switch (match.Value)
-			{
-				case [_, 'R' or 'r', var r and >= '1' and <= '3']:
-				{
-					result.AddRef(Chutes[r - '1']);
-					break;
-				}
-				case [_, 'C' or 'c', var c and >= '1' and <= '3']:
-				{
-					result.AddRef(Chutes[c + 3 - '1']);
-					break;
-				}
-			}
-		}
-		return result.AsSpan();
-	}
-
-	private ReadOnlySpan<Conjugate> OnConjugateParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitConjugateGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new List<Conjugate>();
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var split = s.Split("==", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-			var leftCell = CellParser(split[0])[0];
-			var rightCellAndDigit = CandidateParser(split[1])[0];
-			var rightCell = rightCellAndDigit / 9;
-			var digit = rightCellAndDigit % 9;
-			result.Add(new(leftCell, rightCell, digit));
-		}
-		return result.AsSpan();
-	}
-
-	private static ReadOnlySpan<Miniline> OnIntersectionParsing(string str)
-	{
-		if (string.IsNullOrWhiteSpace(str))
-		{
-			return [];
-		}
-
-		if (UnitIntersectionGroupPattern.Matches(str) is not { Count: not 0 } matches)
-		{
-			return [];
-		}
-
-		var result = new List<Miniline>();
-		foreach (var match in matches.Cast<Match>())
-		{
-			var s = match.Value;
-			var indexOfBlockLabel = s.IndexOfAny(['B', 'b']);
-			var lineLabel = s[0];
-			var lines = s[1..indexOfBlockLabel];
-			var blocks = s[(indexOfBlockLabel + 1)..];
-			foreach (var line in lines)
-			{
-				foreach (var block in blocks)
-				{
-					var @base = new MinilineBase(
-						(byte)(lineLabel is 'R' or 'r' ? line + 9 - '1' : line + 18 - '1'),
-						(byte)(block - '1')
-					);
-					result.Add(new(@base, Miniline.Map[@base]));
-				}
-			}
-		}
-		return result.AsSpan();
-	}
 }
